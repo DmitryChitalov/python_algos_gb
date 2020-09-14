75c391c2aa083a150138284f821c48b6569a2be9
IliaPronin <71208904+IliaPronin@users.noreply.github.com>
2020-09-14 17:01:02 +0300

    task 5 done

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,67 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+class StackClass:
+    def __init__(self):
+        self.elems = []
+        self.elems2 = []
+        self.max_size = 5
+
+    def is_empty(self):
+        return self.elems == [], self.elems2 == []
+
+    def push_in(self, el):
+        self.elems.append(el) if self.stack1_size() < self.max_size else self.elems2.append(el)
+
+    def pop_out(self):
+        return self.elems2.pop() if len(self.elems2) != 0 else self.elems.pop()
+
+    def get_val(self):
+        return self.elems[len(self.elems) - 1] if self.elems2 is True else self.elems2[len(self.elems2) - 1]
+
+    def stack1_size(self):
+        return len(self.elems)
+
+    def stack2_size(self):
+        return len(self.elems2)
+
+if __name__ == '__main__':
+    SC_OBJ = StackClass()
+
+    print(SC_OBJ.is_empty())  # -> стек пустой
+
+    # наполняем стек
+    SC_OBJ.push_in(10)
+    SC_OBJ.push_in('code')
+    SC_OBJ.push_in(False)
+    SC_OBJ.push_in(5.5)
+    SC_OBJ.push_in(101)
+    SC_OBJ.push_in(11)
+    SC_OBJ.push_in(112)
+
+
+    # получаем значение первого элемента с вершины стека, но не удаляем сам элемент из стека
+    print(SC_OBJ.get_val())  # -> 112
+
+    # узнаем размер 1го стека
+    print(SC_OBJ.stack1_size())  # -> 5
+
+    # узнаем размер 2го стека
+    print(SC_OBJ.stack2_size())  # -> 2
+
+    print(SC_OBJ.is_empty())  # -> стек уже непустой false
+
+    # кладем еще один элемент в стек
+    SC_OBJ.push_in(4.4)
+
+    # получаем значение первого элемента с вершины стека, но не удаляем сам элемент из стека
+    print(SC_OBJ.get_val())  # -> 4.4
+
+
+    # снова убираем элемент с вершины стека и возвращаем его значение
+    print(SC_OBJ.pop_out())  # -> 4.4
+
+    # проверяем 
+    print(SC_OBJ.get_val())
+
