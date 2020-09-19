8f8447f58001ebc1de09fc5370da4d520bb540d8
IliaPronin <71208904+IliaPronin@users.noreply.github.com>
2020-09-19 10:14:52 +0300

    Update task_6.py

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -9,3 +9,23 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+from random import randint
+
+
+def game(n=randint(1, 100), try_counter=1):
+    if try_counter > 10:
+        return print(f' You`re loser! correct number is {n}')
+    print(f'your try is {try_counter}')
+    user_n = int(input('number: '))
+    if user_n == n:
+        return print('You`re winner')
+    elif user_n > n:
+        print('incorrect, ur number is bigger')
+    elif user_n < n:
+        print('incorrect, ur number is smaller')
+    try_counter += 1
+    return game(n, try_counter)
+
+
+game()
