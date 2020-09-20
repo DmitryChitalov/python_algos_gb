d5423361a8a2f8682fc848ad2809bba0ed1bb425
Nlegion <nparanoid@gmail.com>
2020-09-20 19:46:16 +0300

    Байков_СВ_ДЗ_3
    Байков_СВ_ДЗ_3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -10,3 +10,44 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+
+import random
+import time
+
+
+def check_list(n):
+    start_val = time.time()
+    list = []
+    for item in range(n):
+        list.append(random.random())
+    end_val = time.time()
+    return list, end_val - start_val
+
+
+def check_list_gen(n):
+    start_val = time.time()
+    list = [random.random() for item in range(n)]
+    end_val = time.time()
+    return list, end_val - start_val
+
+
+def check_voc(n):
+    voc = {}
+    start_voc = time.time()
+    for item in range(n):
+        voc[item] = {random.random(): random.random()}
+    end_voc = time.time()
+    return list, end_voc - start_voc
+
+
+def check_voc_gen(n):
+    start_voc = time.time()
+    voc = {random.random(): random.random() for item in range(n)}
+    end_voc = time.time()
+    return list, end_voc - start_voc
+
+
+print(f'Операция Список заняла {check_list(1000000)[1]} сек')
+print(f'Операция Список-генератор заняла {check_list_gen(1000000)[1]} сек')
+print(f'Операция Cловарь заняла {check_voc(1000000)[1]} сек')
+print(f'Операция Cловарь-генератор заняла {check_voc_gen(1000000)[1]} сек')
--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,14 @@
 р
 а
 """
+import hashlib
+
+s_1 = input('Введите строку: ')
+
+subs_lst = set()
+
+for item in range(len(s_1)):
+    for j in range(item, len(s_1)):
+        subs_lst.add(hashlib.sha1(s_1[item:j + 1].encode('utf-8')).hexdigest())
+
+print('Количество неповторяющихся подстрок - ', len(list(subs_lst)))
