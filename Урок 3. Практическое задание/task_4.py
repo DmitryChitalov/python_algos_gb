3272fd968ebe121c42ed036357d32c03d3ab1da2
Anton_Kiryushin <papas2290@mail.ru>
2020-09-21 23:12:05 +0200

    DZ_3

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,23 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+import hashlib
+
+cache = {}
+
+
+def get_page(url, user):
+    salt = user.encode()
+    hash_url = hashlib.sha256(url.encode() + salt).hexdigest()
+    if cache.get(url):
+        return cache
+    else:
+        cache[url] = hash_url
+        return cache
+
+
+print(get_page('www.yandex.ru', 'Ivan'))
+print(get_page('www.yandex.ru', 'Ivan'))
+print(get_page('www.qwe.ru', 'Asdf'))
+print()
+print(cache)
