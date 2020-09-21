aec5af2b065cda647bb2528de7fb8181cee88065
levlab79 <lev.lsb56@gmail.com>
2020-09-21 20:10:21 +0400

    did the 3rd hw

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -15,3 +15,28 @@
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
+
+import hashlib
+import uuid
+
+
+def get_hash(password, salt=False):
+    if not salt:
+        # Генерируем соль.
+        salt = uuid.uuid4().hex
+    # Получаем хеш.
+    return hashlib.sha256(salt.encode() + password.encode() + salt.encode()).hexdigest() + salt
+
+
+if __name__ == "__main__":
+    password = input('[?] Введите пароль: ')
+    # Получаем хеш.
+    hash_with_salt = get_hash(password=password)
+    print(f'[=] Созданный хеш: {hash_with_salt}')
+    password_again = input('[?] Введите пароль еще раз для проверки: ')
+
+    only_salt = hash_with_salt[-32:]
+    if hash_with_salt == get_hash(password=password_again, salt=only_salt):
+        print('[v] Пароли совпадают.')
+    else:
+        print('[x] Неверный пароль!')
