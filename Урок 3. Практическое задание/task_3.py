e341940634b795462ab4858e1a2d11659bbe0ff2
IliaPronin <71208904+IliaPronin@users.noreply.github.com>
2020-09-21 19:57:40 +0300

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,19 @@
 р
 а
 """
+
+
+def count_str():
+    my_str = 'papa'
+    s_list = []
+    s_dict = {}
+    for i in range(len(my_str)):
+        for j in range(len(my_str) - 1 if i == 0 else len(my_str), i, -1):
+            s_list.append(hash(my_str[i:j]))
+            s_dict[my_str[i:j]] = hash(my_str[i:j])
+    print(s_dict)
+    return f' {len(set(s_list))} substrings in string -  {my_str}'
+
+
+print(count_str())
+
