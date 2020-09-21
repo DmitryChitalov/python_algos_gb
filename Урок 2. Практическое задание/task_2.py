7400b82753eb2b7d80fde6bdb72976b62e349b9e
Dmitry Rybko <dmr.main@outlook.com>
2020-09-21 10:36:55 +0300

    Homework 02

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -16,3 +16,24 @@
 Введите число: 123
 Количество четных и нечетных цифр в числе равно: (1, 2)
 """
+
+def even_uneven_count(number, even_count=0, uneven_count=0):
+
+    if len(number) == 0:
+        print (f'количество четных цфир:  {even_count}')
+        print (f'количество нечетных цифр:{uneven_count}')
+        return
+
+    num = int(number[-1])
+
+    if num%2 == 0:
+        even_count +=1
+    else:
+        uneven_count +=1
+
+    trimmed_str = number[:-1]
+    even_uneven_count(trimmed_str, even_count,uneven_count)
+
+user_input = str(4326981240)
+print(f'введено число {user_input}')
+even_uneven_count(user_input)
--- Урок 2. Практическое задание/task_7.py
+++ Урок 2. Практическое задание/task_7.py
@@ -6,3 +6,23 @@
  Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+def check_equation(elements_quantity, initial_qty, sum_of_elements=0, next_element = 1):
+    elements_qty = elements_quantity
+    initial_elements_qty = initial_qty
+
+    if elements_qty == 0:
+        print (f'результат прямого пересчета суммы элементов:{sum_of_elements}')
+        print (f'результат вычисления по формуле n(n+1)/2   :{initial_elements_qty*(initial_elements_qty+1)/2}')
+        return
+
+    sum_of_elements = sum_of_elements + next_element
+    next_element += 1
+
+    elements_qty -=1
+    check_equation(elements_qty, initial_elements_qty, sum_of_elements,next_element)
+
+
+input_number_of_elements = 9
+check_equation(input_number_of_elements, input_number_of_elements)
