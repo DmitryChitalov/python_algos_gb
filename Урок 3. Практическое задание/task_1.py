35e6bc76c744699fa1646a7100acec0ea968c1f4
Dmitry Rybko <dmr.main@outlook.com>
2020-09-22 16:12:32 +0300

    Homework 03

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -10,3 +10,54 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+
+import functools
+import time
+
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Функция {func.__name__!r} выполняется за {run_time:.4f} секунд")
+        return value
+    return wrapper_timer
+
+
+@timer
+def populate_list(num_elements):
+    test_list = [element for element in range(num_elements)]
+    return test_list
+
+
+@timer
+def populate_dict(num_elements):
+    test_dict = {k: v for k, v in enumerate(range(num_elements), start=0)}
+    return test_dict
+
+
+@timer
+def search_list(input_list, element):
+    print(f'индекс элемента {element}: {test_list.index(element)}')
+
+
+@timer
+def search_dict(input_dict, key):
+    print(f'значение элемента с ключом {key}: {input_dict.get(key)}')
+
+
+test_list = populate_list(100000)
+test_dict = populate_dict(100000)
+#  словарь заполняется медленнее, чем список, за счет использования хеш-таблиц, формирование которых занимает время.
+
+search_list(test_list, 90000)
+search_dict(test_dict, 90000)
+'''
+Поиск по индексу в словаре происходит существенно быстрее за счет механизма хеш-таблиц.
+Хеш-таблицы позволяют снизить сложность операции поиска по ключу в словаре до O(1), 
+в то время как в списке сложность поиска по индексу - O(N).
+'''
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,32 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+
+import hashlib
+
+
+salt = b'just salt'
+hashed_urls = [b'www.yandex.ru', b'www.rbc.ru', b'www.google.com', b'www.mail.ru']
+stored_hashes = []
+
+def generate_hashes(url, salt):
+    hash_record = hashlib.sha256(url + salt).hexdigest()
+    return hash_record
+
+# generating stored hashes
+for i in range(len(hashed_urls)):
+    stored_hashes.append(generate_hashes(hashed_urls[i],salt))
+
+print (f'сохраненные хеши: {stored_hashes}')
+
+input_url = input('введите URL:').encode()
+input_url_hash = generate_hashes(input_url, salt)
+print (f'hash введенного URL:{input_url_hash}')
+
+if input_url_hash in stored_hashes:
+    print ('страница присутствует в кэше')
+else:
+    print ('страница отсутствует в кэше')
+    stored_hashes.append(input_url_hash)
+    print('страница добавлена в кэш')
+    print(f'обновленный список хешей:{stored_hashes}')
