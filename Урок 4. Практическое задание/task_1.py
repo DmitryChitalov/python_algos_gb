7814c710d1309dce0be1c9abe954b87e1a8623a8
Nlegion <nparanoid@gmail.com>
2020-09-24 15:34:31 +0300

    Байков_СВ_ДЗ_4
    Байков_СВ_ДЗ_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,12 +12,53 @@
 Добавьте аналитику: что вы сделали и почему
 """
 
-from timeit import timeit
-
+from timeit import Timer
 
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
+    print(new_arr)
     return new_arr
+
+def func_1_1(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    # print(new_arr)
+    return new_arr
+
+def func_2(nums):
+    new_arr = [item for item in range(len(nums)) if nums[item] % 2 == 0]
+    return new_arr
+
+def func_2_2(nums):
+    new_arr = [item for item in range(0,len(nums),2)]
+    return new_arr
+
+def func_3(nums):
+    new_arr=list(range(0, len(nums), 2))
+    return new_arr
+
+
+t1 = Timer("func_1([0,1,2,3,4,5,6,7,8,9])", "from __main__ import func_1")
+print("Func_1 ", t1.timeit(number=10000), "milliseconds")
+t1_1 = Timer("func_1_1([0,1,2,3,4,5,6,7,8,9])", "from __main__ import func_1_1")
+print("Func_1_1 ", t1_1.timeit(number=10000), "milliseconds")
+t2 = Timer("func_2([0,1,2,3,4,5,6,7,8,9])", "from __main__ import func_2")
+print("Func_2 ", t2.timeit(number=10000), "milliseconds")
+t2_2 = Timer("func_2_2([0,1,2,3,4,5,6,7,8,9])", "from __main__ import func_2_2")
+print("Func_2_2 ", t2_2.timeit(number=10000), "milliseconds")
+t3 = Timer("func_3([0,1,2,3,4,5,6,7,8,9])", "from __main__ import func_3")
+print("Func_3 ", t3.timeit(number=10000), "milliseconds")
+
+"""
+Задание 1.
+Добавьте аналитику:
+1. выкнинул лишний вывод массива (т.к. это операция сцитывания)
+2. попробовал генератор, но видимо его тормозит условие
+3. сделал генератор без условия (он быстрее)
+4. сделал вывод функцией
+"""
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -10,6 +10,9 @@
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
 
+import cProfile
+from timeit import Timer
+
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
@@ -34,3 +37,24 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+cProfile.run('revers(1234567890)')
+cProfile.run('revers_2(1234567890)')
+cProfile.run('revers_3(1234567890)')
+
+rev_1 = Timer('revers(1234567890)', 'from __main__ import revers')
+print("revers 1", rev_1.timeit(number=10000), "milliseconds")
+rev_2 = Timer('revers(1234567890)', 'from __main__ import revers')
+print("revers 2", rev_2.timeit(number=10000), "milliseconds")
+rev_3 = Timer('revers(1234567890)', 'from __main__ import revers')
+print("revers 3", rev_3.timeit(number=10000), "milliseconds")
+
+"""
+Задание 3.
+сравнение 
+revers 1 медленнее всего, т.к. содержит рекурсию и ветвление
+
+revers 2 быстрее revers 1 за счет исключения ветвления 
+
+revers 3 быстрее всех, т.к. использует встроенную функцию 
+"""
