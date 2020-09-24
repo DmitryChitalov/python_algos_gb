17a33ac897f88ac3330d35d8e67d2a1c35ea3179
levlab79 <lev.lsb56@gmail.com>
2020-09-25 02:55:19 +0400

    I did the 4th homework

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,22 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = []
+    for el in nums:
+        if not el % 2:
+            new_arr.append(el)
+    return new_arr
+
+
+result_1 = timeit("func_1(range(10))", "from __main__ import func_1", number=100000)
+result_2 = timeit("func_2(range(10))", "from __main__ import func_2", number=100000)
+print(f'Результат выполнения func_1: {result_1}')
+print(f'Результат выполнения func_2: {result_2}')
+
+"""
+После замены цикла с получением каждого элемента по индексу на цикл for in (более быстрый)
+функция начала работать быстрее в 2 раза.
+"""
