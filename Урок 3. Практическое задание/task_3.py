8cd89dcd6eeb68e326c87c6024276f61c754324e
Dmytro Tkachenko <dmitryytk@hhagency.com.ua>
2020-09-25 15:05:02 +0300

    Add files via upload

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,14 @@
 р
 а
 """
+import hashlib
+
+new_list = input('Введите слово')
+lists = []
+for i in range(0, len(new_list) + 1):
+    for i_2 in range(i + 1, len(new_list) + 1):
+        lists.append(hashlib.sha1(new_list[i:i_2].encode('utf-8')).hexdigest())
+
+lists.remove(hashlib.sha1(new_list.encode('utf-8')).hexdigest())
+
+print(f'Колличество различных подстрок в строке {new_list} равно {len(set(lists))}')
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,22 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+
+from uuid import uuid1
+import hashlib
+
+salt = uuid1().hex
+pages_lists = []
+
+
+def pages_check(page):
+    new_page = hashlib.sha256(salt.encode() + page.encode()).hexdigest()
+    if new_page in pages_lists:
+        print("Такой адрес уже существует")
+    else:
+        pages_lists.append(new_page)
+        print('Новый адрес введен')
+
+pages_check('https://www.zaycev.fm/rock')
+pages_check('https://www.zaycev.fm/rock')
+pages_check('https://www.zaycev.fm/rock')
