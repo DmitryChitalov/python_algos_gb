37fd8bf9aa3cb36d3d622146d668ee6d8da04f04
Zack8629 <Zack0094@gmail.com>
2020-09-25 23:15:37 +0300

    task 6 completed

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -15,3 +15,61 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+
+class QueueClass:
+    def __init__(self):
+        self.elements = []
+
+    def is_empty(self):
+        return self.elements == []
+
+    def to_queue(self, item):
+        self.elements.insert(0, item)
+
+    def from_queue(self):
+        return self.elements.pop()
+
+    def size(self):
+        return len(self.elements)
+
+
+class TaskBoard:
+    def __init__(self):
+        self.cur_queue = QueueClass()
+        self.revision_queue = QueueClass()
+        self.log = []
+
+    def resolve_task(self):
+        task = self.cur_queue.from_queue()
+        self.log.append(task)
+
+    def to_revision_task(self):
+        task = self.cur_queue.from_queue()
+        self.revision_queue.to_queue(task)
+
+    def to_current_queue(self, item):
+        self.cur_queue.to_queue(item)
+
+    def from_revision(self):
+        task = self.revision_queue.from_queue()
+        self.cur_queue.to_queue(task)
+
+    def current_task(self):
+        return self.cur_queue.elements[len(self.cur_queue.elements) - 1]
+
+    def current_revision(self):
+        return self.revision_queue.elements[len(self.revision_queue.elements) - 1]
+
+
+task_board = TaskBoard()
+task_board.to_current_queue('Task1')
+task_board.to_current_queue('Task2')
+task_board.to_current_queue('Task3')
+print(task_board.cur_queue.elements)
+print(task_board.current_task())
+task_board.to_revision_task()
+task_board.from_revision()
+print(task_board.cur_queue.elements)
+print(task_board.current_task())
+print(task_board.log)
