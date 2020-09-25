1c2cec742f440a43a8b1592cf01c8f0e9f101104
AM <anatolminor91@inbox.ru>
2020-09-25 09:15:51 +0300

    DZ4 all -5

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -11,8 +11,7 @@
 
 Добавьте аналитику: что вы сделали и почему
 """
-
-from timeit import timeit
+import timeit
 
 
 def func_1(nums):
@@ -21,3 +20,23 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in nums if i % 2 == 0]
+    return new_arr
+
+
+if __name__ == '__main__':
+    n = [i for i in range(99)]
+    # print(func_1(n))
+    # print(func_2(n))
+    
+    print(timeit.timeit("func_1(n)", setup="from __main__ import func_1, n", number=10000))
+    print(timeit.timeit("func_2(n)", setup="from __main__ import func_2, n", number=10000))
+
+"""
+Я решил что эту задачу будет эффективней сделать через генератор
+при расчете времени работы скрипта, подтвердилось что решение через генератор примерно в 2 раза быстрей
+Так же возможно лучше читается код
+"""
