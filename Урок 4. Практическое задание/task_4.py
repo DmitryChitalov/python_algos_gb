ab7796e47ad20e961f4358465e78e19dfe61c9e7
Serega <nsergeym@gmail.com>
2020-09-25 09:27:24 +0300

    Homework4 without 5 task

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -39,3 +39,51 @@ def func_2():
 
 print(func_1())
 print(func_2())
+from timeit import timeit
+
+array = [1, 3, 1, 3, 4, 5, 1]
+
+
+def func_1():
+    m = 0
+    num = 0
+    for i in array:
+        count = array.count(i)
+        if count > m:
+            m = count
+            num = i
+    return f'Чаще всего встречается число {num}, ' \
+           f'оно появилось в массиве {m} раз(а)'
+
+
+def func_2():
+    new_array = []
+    for el in array:
+        count2 = array.count(el)
+        new_array.append(count2)
+
+    max_2 = max(new_array)
+    elem = array[new_array.index(max_2)]
+    return f'Чаще всего встречается число {elem}, ' \
+           f'оно появилось в массиве {max_2} раз(а)'
+def func_3():
+    z = [array.count(i) for i in array]
+    m = max(z)
+    max_cnt = array[z.index(max(z))]
+    #elem_3 = array[max_3.index(max(max_3))]
+    return f'Чаще всего встречается число {max_cnt}, ' \
+            f'оно появилось в массиве {m} раз(а)'
+
+print(func_1())
+print(func_2())
+print(func_3())
+
+print(timeit("func_1", setup="from __main__ import func_1"))
+print(timeit("func_2", setup="from __main__ import func_2"))
+print(timeit("func_3", setup="from __main__ import func_3"))
+
+#Изменил func_2 сократив количество строк кода с помощью генератора.
+#0.032551
+#0.030750899999999998
+#0.026764399999999994
+
