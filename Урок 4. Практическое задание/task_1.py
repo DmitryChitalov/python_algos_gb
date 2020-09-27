e22889e87563f9e6c95aa2b23d4fd1d8d16586f2
sergeysyashin <syashin@yashinpartners.ru>
2020-09-27 10:44:55 +0300

    Lesson_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,37 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+# Делаем первые замеры
+
+
+print(timeit("""
+
+def func_1(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+"""))
+
+# цикл, if да еще и аппенд - это ту мач,
+# поэтому используем list comprehension (генераторы, но на самом деле так нельзя переводить же)
+# код становится питонистичнее, читабельнее, короче. Ну и, как показывают замеры - немного быстрее.
+# но честно говоря, я рассчитываел в более серьезную разницу в скорости.
+# максимально у меня получилось 0,082 против 0,069
+
+
+def func_2(nums):
+    new_arr = [num for num in range(len(nums)) if nums[num] % 2 == 0]
+    return new_arr
+
+
+print(timeit("""
+
+def func_2(nums):
+    new_arr = [num for num in range(len(nums)) if nums[num] % 2 == 0]
+    return new_arr
+
+"""))
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -9,6 +9,20 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+from timeit import timeit
+from random import randint
+import cProfile
+
+# решение через рекурсию - это заведомо долгое.
+# Причем: при небольших значениях рекурсия не так долго работает,
+# но когда числа большие - то мы начинаем заполнять стек огромным количеством вызовов и время начинает расти:
+# чем больше больше числа, тем дороже нам обходится рекурсия.
+
+# Решение через срезы медленное, так как срезы - это не in place --> на создание каждого объекта уходит время.
+
+# А самое быстрое решение - через старый добрый цикл
+
+# причем исследования через timeit это подтверджают, а cProfile показал всё по нулям - типа всё ок и улучшать нечего.
 
 
 def revers(enter_num, revers_num=0):
@@ -21,6 +35,34 @@ def revers(enter_num, revers_num=0):
         revers(enter_num, revers_num)
 
 
+num_100 = randint(10000, 1000000)
+num_1000 = randint(1000000, 10000000)
+num_10000 = randint(100000000, 10000000000000)
+
+print('Функция revers: ')
+
+print(
+    timeit(
+        "revers(num_100)",
+        setup='from __main__ import revers, num_100',
+        number=100000))
+print(
+    timeit(
+        "revers(num_1000)",
+        setup='from __main__ import revers, num_1000',
+        number=100000))
+print(
+    timeit(
+        "revers(num_10000)",
+        setup='from __main__ import revers, num_10000',
+        number=100000))
+
+print()
+cProfile.run('revers(num_10000)')
+
+# Самое быстрое решение - через старый добрый цикл
+
+
 def revers_2(enter_num, revers_num=0):
     while enter_num != 0:
         num = enter_num % 10
@@ -29,8 +71,51 @@ def revers_2(enter_num, revers_num=0):
     return revers_num
 
 
+print('Функция revers_2')
+print(
+    timeit(
+        "revers_2(num_100)",
+        setup='from __main__ import revers_2, num_100',
+        number=100000))
+print(
+    timeit(
+        "revers_2(num_1000)",
+        setup='from __main__ import revers_2, num_1000',
+        number=100000))
+print(
+    timeit(
+        "revers_2(num_10000)",
+        setup='from __main__ import revers_2, num_10000',
+        number=100000))
+
+print()
+cProfile.run('revers_2(num_10000)')
+
+# Решение через срезы медленное так как срезы - это не in place --> на создание каждого объекта уходит время
+
+
 def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
 
+
+print('Функция revers_3')
+print(
+    timeit(
+        "revers_3(num_100)",
+        setup='from __main__ import revers_3, num_100',
+        number=100000))
+print(
+    timeit(
+        "revers_3(num_1000)",
+        setup='from __main__ import revers_3, num_1000',
+        number=100000))
+print(
+    timeit(
+        "revers_3(num_10000)",
+        setup='from __main__ import revers_3, num_10000',
+        number=100000))
+
+print()
+cProfile.run('revers_3(num_10000)')
