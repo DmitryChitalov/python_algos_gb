7337c751fc5fcb532d764fc97a00efe91a9f39b8
max <oksyuta.maxim@gmail.com>
2020-09-28 19:57:19 +0300

    Do homework

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,48 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+from statistics import mean
+import collections
+
+
+def input_data():
+    data = collections.OrderedDict([])
+    while True:
+        try:
+            count = int(input('Введите количество предприятий для расчета прибыли: '))
+        except ValueError:
+            print('Введите число!!!')
+        else:
+            break
+    data['count'] = count
+    for i in range(count):
+        company = collections.namedtuple('company', 'company_name profit')
+        result = company(
+            company_name=input('Введите название предприятия: '),
+            profit=input('Через пробел введите прибыль данного предприятия за каждый квартал(Всего 4 квартала): ')
+        )
+        data[i] = result
+    return data
+
+
+data = input_data()
+all_profit = []
+for i in range(data['count']):
+    profit = mean(list(map(int, data[i].profit.split())))
+    all_profit.append(profit)
+    company = collections.namedtuple('company', 'company_name profit')
+    data[i] = company(
+        company_name=data[i].company_name,
+        profit=profit
+    )
+all_profit = mean(all_profit)
+print(f'Средняя годовая прибыль всех предприятий: {all_profit}')
+more = []
+less = []
+for i in range(data['count']):
+    if data[i].profit > all_profit:
+        more.append(data[i].company_name)
+    elif data[i].profit < all_profit:
+        less.append(data[i].company_name)
+print(f'''Предприятия, с прибылью выше среднего значения: {str(more)[1:-1].replace("'", '')}''')
+print(f'''Предприятия, с прибылью ниже среднего значения: {str(less)[1:-1].replace("'", '')}''')
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,26 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+
+from timeit import timeit
+from collections import deque
+
+
+def list_operations():
+    my_list = []
+    my_list.append(2)
+    my_list.insert(0, 1)
+    my_list.pop()
+    my_list.pop(0)
+
+
+def deque_operations():
+    my_deque = deque([])
+    my_deque.append(2)
+    my_deque.appendleft(1)
+    my_deque.pop()
+    my_deque.popleft()
+
+
+print(f"list  {timeit('list_operations()', setup='from __main__ import list_operations', number=1000000)}")
+print(f"deque {timeit('deque_operations()', setup='from __main__ import deque_operations', number=1000000)}")
