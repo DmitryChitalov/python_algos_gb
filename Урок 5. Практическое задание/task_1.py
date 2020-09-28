cd5ff04b0ce8eced0164c7c52ed7c249194c9eff
egor <ickzn@ya.ru>
2020-09-28 14:25:55 +0300

    add dz

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,30 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+
+from collections import namedtuple
+namedtuple_obj = namedtuple('Org', ['id', 'name', 'profit_1', 'profit_2', 'profit_3', 'profit_4'])
+org_1 = namedtuple_obj(
+    id=1,
+    name='ООО',
+    profit_1 =723,
+    profit_2 =721,
+    profit_3 =856,
+    profit_4 =654,
+)
+org_2 = namedtuple_obj(
+    id=2,
+    name='ОАО',
+    profit_1 =789,
+    profit_2 =587,
+    profit_3 =463,
+    profit_4 =963,
+
+)
+profit_year_org1 = sum(org_1[-4:])
+profit_year_org2 = sum(org_2[-4:])
+print(f'Годовая прибыль организации {org_1.name} = {profit_year_org1}')
+print(f'Годовая прибыль организации {org_2.name} = {profit_year_org2}')
+print(f'Средняя годовая прибыль по всем организвциям = {(profit_year_org1+profit_year_org2) / 2}')
+print(f'Годовая прибыль организации {org_1.name if profit_year_org1 > profit_year_org2 else org_2.name} больше средней')
+print(f'Годовая прибыль организации {org_1.name if profit_year_org1 < profit_year_org2 else org_2.name} меньше средней')
