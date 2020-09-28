1073ce50f6fd8c110577bfa418c90037a078d9f0
Pr0venzano <Pr0venzano@mail.ru>
2020-09-28 03:54:53 +0300

    task_6

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -9,3 +9,38 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+from random import randint
+
+
+def guess_number(hidden_number, user_number=-1, move=10):
+    try:
+        user_number = int(input('\nВведите целое число:'))
+    except ValueError:
+        print('Введено некорректное значение')
+        return guess_number(hidden_number, user_number, move)
+
+    if user_number == hidden_number:
+        return print('Вы отгадали')
+
+    else:
+        if move > 1:
+            move -= 1
+            if user_number > hidden_number:
+                print(f'Ваше число больше загаданного, осталось попыток: {move}')
+
+            elif user_number < hidden_number:
+                print(f'Ваше число меньше загаданного, осталось попыток: {move}')
+
+            else:
+                print(f'ОШИБКА!: Непредвиденная ситуация')
+
+            return guess_number(hidden_number, user_number, move)
+
+        else:
+            print(f'Вы не отгадали, загаданное число: {hidden_number}')
+
+
+rand_number = randint(0, 100)
+print('Отгадайте число от 0 до 100')
+guess_number(rand_number)
