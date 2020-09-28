77876d07e177114c4fe098879a8a57a1dc143694
Zack8629 <Zack0094@gmail.com>
2020-09-29 00:29:16 +0300

    task 4 completed

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -9,3 +9,16 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+def sum_series_numbers(amount_elem, cnt=0, num=1.0, total=0.0):
+    if cnt == amount_elem:
+        print(f'Количество элементов = {amount_elem} их сумма = {total}')
+    elif cnt < amount_elem:
+        return sum_series_numbers(amount_elem, cnt + 1, num / 2 * -1, total + num)
+    else:
+        print('Что-то пошло не так')
+
+
+user_in = int(input('Введите количество элементов: '))
+sum_series_numbers(user_in)
