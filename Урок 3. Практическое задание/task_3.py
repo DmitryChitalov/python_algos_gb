b52d4959c6b8198c56a1f0a253181805aae1db64
Pr0venzano <Pr0venzano@mail.ru>
2020-09-29 00:16:17 +0300

    task_3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,22 @@
 р
 а
 """
+from hashlib import sha256
+
+
+def hash_set(string):
+    result = set()
+    for i in range(len(string), -1, -1):
+
+        for k in range(len(string)):
+
+            if not string[k:i] in (string, ''):
+                print(string[k:i])
+                hash_elem = sha256(string[k:i].encode()).hexdigest()
+                result.add(hash_elem)
+    print(f'{result}')
+    print(f'Количество уникальных подстрок в строке "{string}" = {len(result)}\n')
+
+
+hash_set('papa')
+hash_set('utf')
