416d3ef6d2417e89be7be6e8fe206b9b2a7cfaed
Dmitry Rybko <dmr.main@outlook.com>
2020-09-29 19:34:06 +0300

    Homework 05

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,83 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+
+import timeit
+import collections
+
+print("исходный list и deque")
+list_test = ['A', 'B', 'C', 'D']
+print(list_test)
+deque_test = collections.deque(list_test)
+print(deque_test)
+print()
+
+new_list = ['E', 'F', 'J', 'H']
+
+print('тестируем extend')
+
+def extend_list(input_list_1, input_list_2):
+    list_test = input_list_1
+    new_list = input_list_2
+    list_test.extend(new_list)
+
+print(f'тест extend_list: '
+      f'{timeit.timeit("extend_list(list_test, new_list)", setup="from __main__ import extend_list, list_test, new_list", number=1000)}')
+
+def extend_deque(input_deque, input_list_2):
+    deque_test = input_deque
+    new_list = input_list_2
+    deque_test.extend(new_list)
+
+print(f'тест extend_deque: '
+      f'{timeit.timeit("extend_deque(deque_test, new_list)", setup="from __main__ import extend_deque, deque_test, new_list", number=1000)}')
+
+print('тестируем pop')
+
+def list_pop(list):
+    list_test = list
+    list_test.pop()
+
+print(f'тест list_pop: '
+      f'{timeit.timeit("list_pop(list_test)", setup="from __main__ import list_pop, list_test", number=1000)}')
+
+def deque_pop(input_deque):
+    deque_test = input_deque
+    deque_test.pop()
+
+print(f'тест deque_pop: '
+      f'{timeit.timeit("deque_pop(deque_test)", setup="from __main__ import deque_pop, deque_test", number=1000)}')
+
+
+print('тестируем insert')
+
+def list_insert(input_list, position=0):
+    list_test = input_list
+    list_test.insert(position, new_list)
+
+print(f'тест list_insert: '
+      f'{timeit.timeit("list_insert(new_list)", setup="from __main__ import list_insert, new_list", number=1000)}')
+
+def deque_insert(input_deque, position=0):
+    deque_test = input_deque
+    deque_test.insert(position, new_list)
+
+print(f'тест deque_insert: '
+      f'{timeit.timeit("deque_insert(new_list)", setup="from __main__ import deque_insert, new_list", number=1000)}')
+
+
+'''
+тестируем extend
+тест extend_list: 0.0005660000000000001
+тест extend_deque: 0.0006034000000000001
+тестируем pop
+тест list_pop: 0.0007871999999999998
+тест deque_pop: 0.0003725
+тестируем insert
+тест list_insert: 0.0011376
+тест deque_insert: 0.0018411
+
+Исходя из тестов, deque совсем не показал прироста скорости 
+выполнения вышеприведенных операций по сравнению с list.
+
+'''
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,84 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+
+import collections
+import timeit
+
+
+print('исходные словари')
+test_dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6}
+print(test_dict)
+test_ordered_dict = collections.OrderedDict(test_dict)
+print(test_ordered_dict)
+print()
+
+print('тестируем нахождение значения по ключу')
+
+def find_dict_value(key='C'):
+    return test_dict[key]
+
+print(f'dict: '
+      f'{timeit.timeit("find_dict_value()", setup="from __main__ import find_dict_value", number=1000)}')
+
+def find_ordered_dict_value(key='C'):
+    return test_ordered_dict[key]
+
+print(f'ordered_dict: '
+      f'{timeit.timeit("find_ordered_dict_value()", setup="from __main__ import find_ordered_dict_value", number=1000)}')
+
+'''
+тестируем нахождение значения по ключу
+dict: 0.00010530000000000001
+ordered_dict: 9.81e-05
+нахождение значения по ключу в большинстве случаев происходит немного быстрее в ordered dictionary
+'''
+
+print()
+print('тестируем popitem')
+
+def popitem_dict():
+    test_dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6}
+    test_dict.popitem()
+
+print(f'dict: '
+      f'{timeit.timeit("popitem_dict()", setup="from __main__ import popitem_dict", number=1000)}')
+
+def popitem_ordered_dict():
+    test_ordered_dict = OrderedDict([('A', 1), ('B', 2), ('C', 3), ('D', 4), ('E', 5), ('F', 6)])
+    test_ordered_dict.popitem()
+
+print(f'ordered_dict: '
+      f'{timeit.timeit("popitem_dict()", setup="from __main__ import popitem_dict", number=1000)}')
+
+'''
+тестируем popitem
+dict: 0.000492
+ordered_dict: 0.0003277
+popitem работает примерно одинаково
+'''
+
+print()
+print('тестируем update')
+
+def update_dict():
+    test_dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6}
+    test_dict.update({'A': 7, 'B': 8, 'C': 9})
+
+print(f'update_dict: '
+      f'{timeit.timeit("update_dict()", setup="from __main__ import update_dict", number=1000)}')
+
+def update_ordered_dict():
+    test_ordered_dict = collections.OrderedDict([('A', 1), ('B', 2), ('C', 3), ('D', 4), ('E', 5), ('F', 6)])
+    test_ordered_dict.update(collections.OrderedDict([('A', 7), ('B', 8), ('C', 9)]))
+
+print(f'update_ordered_dict: '
+      f'{timeit.timeit("update_ordered_dict()", setup="from __main__ import update_ordered_dict", number=1000)}')
+
+
+'''
+тестируем update
+update_dict: 0.0005615999999999998
+update_ordered_dict: 0.0038485
+update работает примерно одинаково
+'''
