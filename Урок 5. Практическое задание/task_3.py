e6dddfa5efbe99c45cc1c56de0c4288343116826
bLaM4e <konnerspz@mail.ru>
2020-09-29 15:52:19 +0300

    DZ done

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,89 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+from collections import deque
+from random import randint
+from timeit import timeit
+
+
+def list_append(lst):
+    for i in range(100):
+        lst.append(99)
+    return lst
+
+
+def list_insert_left(lst):
+    for i in range(100):
+        lst.insert(0, 99)
+    return lst
+
+
+def list_insert_random(lst):
+    for i in range(100):
+        rand = randint(0, 100)
+        lst.insert(rand, 99)
+    return lst
+
+
+def deque_append(deq):
+    for i in range(100):
+        deq.append(99)
+    return deq
+
+
+def deque_append_left(deq):
+    for i in range(100):
+        deq.appendleft(99)
+    return deq
+
+
+def deque_insert_random(deq):
+    for i in range(100):
+        rand = randint(0, 100)
+        deq.insert(rand, 99)
+    return deq
+
+
+def get_random_list(lst):
+    for i in range(100):
+        rand = randint(0, 100)
+        n = lst[rand]
+
+
+def get_random_deque(deq):
+    for i in range(100):
+        rand = randint(0, 100)
+        n = deq[rand]
+
+
+my_list = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+           11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+           11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+           11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+           11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+           11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+my_deque = deque(my_list)
+
+print(f"Добавление в конец списка: "
+      f"{timeit('list_append(my_list)', 'from __main__ import list_append, my_list', number=1000)}")
+print(f"Добавление в конец дека: "
+      f"{timeit('deque_append(my_deque)', 'from __main__ import deque_append, my_deque', number=1000)}")
+print(f"Добавление в начало списка: "
+      f"{timeit('list_insert_left(my_list)', 'from __main__ import list_insert_left, my_list', number=1000)}")
+print(f"Добавление в начало дека: "
+      f"{timeit('deque_append_left(my_deque)', 'from __main__ import deque_append_left, my_deque', number=1000)}")
+print(f"Добавление в случайное место списка: "
+      f"{timeit('list_insert_random(my_list)', 'from __main__ import list_insert_random, my_list', number=1000)}")
+print(f"Добавление в случайное место дека: "
+      f"{timeit('deque_insert_random(my_deque)', 'from __main__ import deque_insert_random, my_deque', number=1000)}")
+print(f"Получение случайного элемента из списка: "
+      f"{timeit('get_random_list(my_list)', 'from __main__ import get_random_list, my_list', number=1000)}")
+print(f"Получение случайного элемента из дека: "
+      f"{timeit('get_random_deque(my_deque)', 'from __main__ import get_random_deque, my_deque', number=1000)}")
+
+"""
+Выаод:
+    Время добавления элементов в конец списка и дека происходит примерно одинаково.
+    Добавление элементов в начало или в случайное место дека происходит гораздно быстрее, чем в начало списка.
+    Время получения элементов из списка и из дека примерно одинаково.
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,51 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+from collections import OrderedDict
+from timeit import timeit
+from random import choice, randint
+
+my_dict = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'i': 8,
+    'h': 9,
+    'j': 0
+}
+my_OrderDict = OrderedDict(my_dict)
+
+print(timeit("""
+sum = 0 
+for key, val in my_dict.items():
+    sum += val
+""", "from __main__ import my_dict"))
+
+print(timeit("""
+sum = 0 
+for key, val in my_OrderDict.items():
+    sum += val
+""", "from __main__ import my_OrderDict"))
+
+print(timeit("""
+for i in range(3):
+    n_key = choice('klmnopqrstuvwxyz1234567890')
+    my_dict[n_key] = randint(0, 100)
+""", "from __main__ import my_dict, choice, randint"))
+
+print(timeit("""
+for i in range(3):
+    n_key = choice('klmnopqrstuvwxyz1234567890')
+    my_OrderDict[n_key] = randint(0, 100)
+""", "from __main__ import my_OrderDict, choice, randint"))
+
+"""
+Вывод:
+    Все происходит примерно одинаково.
+"""
+# print(my_dict)
+# print(my_OrderDict)
