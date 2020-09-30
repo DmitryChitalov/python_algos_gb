226ec6810f719ac6ad4c5859bc2032859c058477
Vasiliy <vash050@rambler.ru>
2020-09-30 13:36:50 +0300

    lesson_5_done

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,28 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+from collections import defaultdict
+
+
+def get_from_user():
+    hex_dict = defaultdict(list)
+    hex_dict['first_num_hex'] = list(input("Введите первое число: "))
+    hex_dict['second_num_hex'] = list(input("Введите второе число: "))
+    return hex_dict
+
+
+def sum_hex_func(h_dict_in):
+    sum_hex = hex(int(''.join(h_dict_in['first_num_hex']), 16) + \
+                  int(''.join(h_dict_in['second_num_hex']), 16))
+    return list(sum_hex[2:])
+
+
+def multiplication(h_dict_in):
+    mult_hex = hex(int(''.join(h_dict_in['first_num_hex']), 16) * \
+                   int(''.join(h_dict_in['second_num_hex']), 16))
+    return list(mult_hex[2:])
+
+
+num_h = get_from_user()
+print(sum_hex_func(num_h))
+print(multiplication(num_h))
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,126 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+"""
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+   
+   100000    0.080    0.000    2.894    0.000 task_3.py:17(list_create)
+   100000    0.566    0.000    3.361    0.000 task_3.py:21(deque_create)
+   
+   100000    0.039    0.000    0.054    0.000 task_3.py:25(list_append)
+   100000    0.032    0.000    0.045    0.000 task_3.py:30(deque_append)
+   
+   100000    0.031    0.000    0.075    0.000 task_3.py:35(list_insert_into_beginning)
+   100000    0.030    0.000    0.043    0.000 task_3.py:40(deque_insert_into_beginning)
+   
+   100000    0.032    0.000    0.046    0.000 task_3.py:45(list_pop)
+   100000    0.032    0.000    0.046    0.000 task_3.py:49(deque_pop)
+   
+   100000    0.030    0.000    0.054    0.000 task_3.py:53(list_pop_from_beginning)
+   100000    0.030    0.000    0.043    0.000 task_3.py:57(deque_pop_from_beginning)
+   
+   100000    0.030    0.000    0.058    0.000 task_3.py:61(list_insert_into_middle)
+   100000    0.031    0.000    0.100    0.000 task_3.py:66(deque_insert_into_middle)
+   
+   100000    0.018    0.000    0.018    0.000 task_3.py:71(list_get)
+   100000    0.019    0.000    0.019    0.000 task_3.py:75(deque_get)
+   
+операция                -   кто быстрее
+создание                -   список 
+добавление в конец      -   очередь
+добавление в начало     -   очередь
+удаление из конца       -   одинаково
+удаление из начала      -   очередь
+добавление в средину    -   список
+получение из средины    -   список
+
+Вывод:
+При операциях добавление и удаление в начале и конце используем очередь.
+При операциях с "i" элементами импользуем список
+"""
+import cProfile
+from collections import deque
+
+
+def list_create():
+    return [x for x in range(1000)]
+
+
+def deque_create():
+    return deque([x for x in range(1000)])
+
+
+def list_append(my_list_in):
+    my_list_in.append('value')
+    return my_list_in
+
+
+def deque_append(my_deque_in):
+    my_deque_in.append('value')
+    return my_deque_in
+
+
+def list_insert_into_beginning(my_list_in):
+    my_list_in.insert(0, '500')
+    return my_list_in
+
+
+def deque_insert_into_beginning(my_deque_in):
+    my_deque_in.appendleft('500')
+    return my_deque_in
+
+
+def list_pop(my_list_in):
+    return my_list_in.pop()
+
+
+def deque_pop(my_deque_in):
+    return my_deque_in.pop()
+
+
+def list_pop_from_beginning(my_list_in):
+    return my_list_in.pop(0)
+
+
+def deque_pop_from_beginning(my_deque_in):
+    return my_deque_in.popleft()
+
+
+def list_insert_into_middle(my_list_in):
+    my_list_in.insert(500, '500')
+    return my_list_in
+
+
+def deque_insert_into_middle(my_deque_in):
+    my_deque_in.insert(500, '500')
+    return my_deque_in
+
+
+def list_get(my_list_in):
+    return my_list_in[500]
+
+
+def deque_get(my_deque_in):
+    return my_deque_in[500]
+
+
+def main():
+    for _ in range(100000):
+        x = list_create()
+        y = deque_create()
+        list_append(x)
+        deque_append(y)
+        list_pop(x)
+        deque_pop(y)
+        list_insert_into_beginning(x)
+        deque_insert_into_beginning(y)
+        list_pop_from_beginning(x)
+        deque_pop_from_beginning(y)
+        list_insert_into_middle(x)
+        deque_insert_into_middle(y)
+        list_get(x)
+        deque_get(y)
+
+
+if __name__ == '__main__':
+    cProfile.run('main()')
