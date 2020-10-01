e34e958bb0f107e6ddbc40027c18024dccb5eb97
Dmytro Tkachenko <dmitryytk@hhagency.com.ua>
2020-10-01 18:09:04 +0300

    Add files via upload

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,30 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+
+# Вывод. Дэк действительно быстрее при аналогичных операциях.
+
+import random
+import collections
+import timeit
+
+
+def list_add():
+    my_list = [random.randint(1, 100) for i in range(10)]
+    my_list.append([random.randint(1, 100) for i in range(10)])
+    my_list.insert(0, [random.randint(1, 100) for i in range(10)])
+    return my_list
+
+
+def deque_add():
+    my_deque = collections.deque([random.randint(1, 100) for i in range(10)])
+    my_deque.append([random.randint(1, 100) for i in range(10)])
+    my_deque.appendleft([random.randint(1, 100) for i in range(10)])
+    return my_deque
+
+
+print(list_add())
+print(deque_add())
+
+print(timeit.timeit('list_add', setup='from __main__ import list_add', number=10000))
+print(timeit.timeit('deque_add', setup='from __main__ import deque_add', number=10000))
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,37 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+# Вывод: defaultdict быстрее обычного словаря
+
+import random
+import collections
+import timeit
+
+
+
+def dict_add():
+    my_dict = {}
+    for i in range(10):
+        my_dict[f'{i}'] = [random.randint(1, 100) for i in range(10)]
+    my_dict.popitem()
+    my_dict.values()
+    my_dict.keys()
+
+    return my_dict
+
+
+def default_dict_add():
+    my_def_dict = collections.defaultdict(int)
+    for i in range(10):
+        my_def_dict[f'{i}'] = [random.randint(1, 100) for i in range(10)]
+    my_def_dict.popitem()
+    my_def_dict.values()
+    my_def_dict.keys()
+    return my_def_dict
+
+
+print(dict_add())
+print(default_dict_add())
+
+print(timeit.timeit('dict_add', setup='from __main__ import dict_add', number=10000))
+print(timeit.timeit('default_dict_add', setup='from __main__ import default_dict_add', number=10000))
