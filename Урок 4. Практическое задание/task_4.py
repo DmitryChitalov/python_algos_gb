0dd2aa9c61a1baa2b1eb16d98f91a3738037b9aa
Zack8629 <Zack0094@gmail.com>
2020-10-01 20:20:07 +0300

    task 4 completed

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -9,8 +9,9 @@
 Попытайтесь написать третью версию, которая будет самой быстрой.
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
 """
+from timeit import timeit
 
-array = [1, 3, 1, 3, 4, 5, 1]
+array = [1, 3, 1, 3, 4, 5, 1, 1, 3, 4, 5, 1, 6, 1, 1, 1, 2, 1, 3, 1, 4, 1, 5, 1, 6, 1, 7, 1, 8, 1]
 
 
 def func_1():
@@ -30,12 +31,33 @@ def func_2():
     for el in array:
         count2 = array.count(el)
         new_array.append(count2)
-
     max_2 = max(new_array)
     elem = array[new_array.index(max_2)]
     return f'Чаще всего встречается число {elem}, ' \
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+def func_3():
+    numb = max(array, key=array.count)
+    return f'Чаще всего встречается число {numb}, ' \
+           f'оно появилось в массиве {array.count(numb)} раз(а)'
+
+
+print(f'func_1 = {func_1()}')
+print(f'func_2 = {func_2()}')
+print(f'func_3 = {func_3()}')
+
+print(f'*' * 75)
+
+print(f'func_1 = '
+      f'{timeit("func_1()", setup="from __main__ import func_1")}')
+
+print(f'func_2 = '
+      f'{timeit("func_2()", setup="from __main__ import func_2")}')
+
+print(f'func_3 = '
+      f'{timeit("func_3()", setup="from __main__ import func_3")}')
+
+'''
+func_2 Самая медленная, func_3 чуть быстрее чем func_1
+'''
