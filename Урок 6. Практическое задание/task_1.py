b24b87daced8f6c43725809979b9b77b0f0139a2
Shmurygin Roman <roman.shmurygin@utair.ru>
2020-10-02 01:01:25 +0500

    Алгоритмы Python. Урок 6 ДЗ.

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -13,3 +13,71 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+from memory_profiler import profile
+from sys import getsizeof
+from pympler import asizeof
+
+
+@profile
+def func_1(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+
+@profile
+def func_2(nums):
+    return [i for id, i in enumerate(nums) if i % 2 == 0]
+
+
+@profile
+def func_3(nums):
+    return list(filter(lambda x: x % 2 == 0, nums))
+
+
+n = [i for i in range(1000000)]
+print(asizeof.asizeof(n))  # 20348728 байт
+func_1(n)
+func_2(n)
+func_3(n)
+
+"""
+Версия Python 3.8
+64-разрядная ОС
+
+Результаты:
+func_1 - 9.5 MiB
+func_2 - 1.9 MiB
+func_3 - 1.9 MiB
+
+По результатам замеров памяти, лучший показатель у функций func_2 и func_3.
+В принципе, примерно этот же результат мы получали при использовании timeit.
+
+
+Line #    Mem usage    Increment   Line Contents
+================================================
+    19     32.6 MiB     32.6 MiB   @profile
+    20                             def func_1(nums):
+    21     32.6 MiB      0.0 MiB       new_arr = []
+    22     42.1 MiB      0.0 MiB       for i in range(len(nums)):
+    23     42.1 MiB      0.1 MiB           if nums[i] % 2 == 0:
+    24     42.1 MiB      0.2 MiB               new_arr.append(i)
+    25     42.1 MiB      0.0 MiB       return new_arr
+
+
+Line #    Mem usage    Increment   Line Contents
+================================================
+    28     32.8 MiB     32.8 MiB   @profile
+    29                             def func_2(nums):
+    30     34.7 MiB      0.2 MiB       return [i for id, i in enumerate(nums) if i % 2 == 0]
+
+
+Line #    Mem usage    Increment   Line Contents
+================================================
+    33     32.8 MiB     32.8 MiB   @profile
+    34                             def func_3(nums):
+    35     34.7 MiB      0.2 MiB       return list(filter(lambda x: x % 2 == 0, nums))
+
+"""
--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -5,3 +5,75 @@
 
 Например, один из вариантов, использование генераторов
 """
+from memory_profiler import memory_usage
+from numpy import array
+from pympler import asizeof
+from time import process_time
+
+
+def runtime(func):
+    def wrapper(*args):
+        t1 = process_time()
+        m1 = memory_usage()
+        result = func(*args)
+        t2 = process_time()
+        m2 = memory_usage()
+        print(f'Выполнение заняло {t2 - t1} сек и {m2[0] - m1[0]} MiB.')
+        return result
+    return wrapper
+
+
+def runtime_2(func):
+    def wrapper(*args):
+        t1 = process_time()
+        m1 = memory_usage()
+        result = list(func(*args))
+        t2 = process_time()
+        m2 = memory_usage()
+        print(f'Выполнение заняло {t2 - t1} сек и {m2[0] - m1[0]} MiB.')
+        return result
+    return wrapper
+
+
+@runtime
+def func_1(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+
+@runtime
+def func_2(nums):
+    return [i for id, i in enumerate(nums) if i % 2 == 0]
+
+
+@runtime
+def func_3(nums):
+    return list(filter(lambda x: x % 2 == 0, nums))
+
+
+@runtime_2
+def func_4(nums):
+    for i in nums:
+        if i % 2 == 0:
+            yield i
+
+
+n = array([i for i in range(1000000)])
+print(asizeof.asizeof(n))  # 4000048 байт
+func_1(n)  # Выполнение заняло 0.75 сек и 9.20703125 MiB.
+func_2(n)  # Выполнение заняло 0.65625 сек и 9.41015625 MiB.
+func_3(n)  # Выполнение заняло 0.640625 сек и 9.359375 MiB.
+func_4(n)  # Выполнение заняло 0.546875 сек и 9.34765625 MiB.
+
+"""
+Для замеров создал 2 декоратора, для функции с генератором отдельный, чтобы функция возвращала список как остальные.
+В итоге доказательства не вышло, т.к. по результатам все 4 функции используют примерно одинаковое количество памяти.
+Таким образом делаю вывод, что генератор рационально использовать, пока используем его в ходе вычислений,
+если хотим сохранить результат сформированного генератором списка, то преимущество перед другими вариантами отсутствует.
+
+Использование array из библиотеки numpy, дает отличное преимущество по памяти.
+Для списка из 1 млн. элементов - 3,8 Мб, против 19,4 Мб при создании обычного списка.
+"""
