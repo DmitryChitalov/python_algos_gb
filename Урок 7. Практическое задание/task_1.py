694194c24fed5b905fe79e69cf4da47de8e8b9b2
Vasiliy <vash050@rambler.ru>
2020-10-03 15:01:40 +0300

    lesson_7_dz_done

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -12,3 +12,66 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+"""
+ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+10000    0.571    0.000    0.607    0.000 task_1.py:27(sort_bubble_smart)
+10000    0.585    0.000    0.628    0.000 task_1.py:47(sort_bubble)
+
+умная сортировка быстрее
+"""
+import random
+import cProfile
+
+
+def create_list():
+    """
+    функция создает список и заполняет его случайными значениями [-100; 100)
+    :return: list
+    """
+    return [random.randint(-100, 100) for _ in range(200)]
+
+
+def sort_bubble_smart(my_list_in: list) -> list:
+    """
+    функция получает список, сортирует его пузырькои по убывание и возвращяет.
+    на каждой итерации идет проверка на сортированнсть списка
+    :param my_list_in: list
+    :return: list
+    """
+    count = 1
+    while count < len(my_list_in):
+        flag = True
+        for i in range(len(my_list_in) - count):
+            if my_list_in[i] < my_list_in[i + 1]:
+                my_list_in[i], my_list_in[i + 1] = my_list_in[i + 1], my_list_in[i]
+                flag = False
+        if flag:
+            break
+        count += 1
+    return my_list_in
+
+
+def sort_bubble(my_list_in: list) -> list:
+    """
+    функция получает список, сортирует его пузырькои по убывание и возвращяет.
+    :param my_list_in: list
+    :return: list
+    """
+    count = 1
+    while count < len(my_list_in):
+        for i in range(len(my_list_in) - count):
+            if my_list_in[i] < my_list_in[i + 1]:
+                my_list_in[i], my_list_in[i + 1] = my_list_in[i + 1], my_list_in[i]
+        count += 1
+    return my_list_in
+
+
+def main():
+    for _ in range(1000):
+        my_list = create_list()
+        print(my_list)
+        print(sort_bubble_smart(my_list.copy()))
+        print(sort_bubble(my_list.copy()))
+
+
+cProfile.run('main()')
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,72 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+"""
+    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+9000/1000    0.009    0.000    0.025    0.000 task_2.py:49(merge_sort)
+     1000    0.000    0.000    0.001    0.000 task_2.py:58(sort_st)
+     
+Я сравнил сортировку слиянием с функцией sorted(), встроенная функция быстрее много кратно.
+"""
+import cProfile
+
+
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+
+    # Длина списков часто используется, поэтому создадим переменные для удобства
+    left_list_length, right_list_length = len(left_list), len(right_list)
+
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+            # Сравниваем первые элементы в начале каждого списка
+            # Если первый элемент левого подсписка меньше, добавляем его
+            # в отсортированный массив
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            # Если первый элемент правого подсписка меньше, добавляем его
+            # в отсортированный массив
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+
+        # Если достигнут конец левого списка, элементы правого списка
+        # добавляем в конец результирующего списка
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        # Если достигнут конец правого списка, элементы левого списка
+        # добавляем в отсортированный массив
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+
+    return sorted_list
+
+
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+    mid = len(nums) // 2
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+    return merge(left_list, right_list)
+
+
+def sort_st(list_num):
+    return sorted(list_num)
+
+
+def main():
+    for _ in range(1000):
+        random_list_of_nums = [46.11436617832828, 41.62921998361278, 18.45859540989644,
+                               12.128870723745806, 8.025098788570562]
+        print(random_list_of_nums)
+        print(merge_sort(random_list_of_nums.copy()))
+        print(sort_st(random_list_of_nums.copy()))
+
+
+# main()
+cProfile.run('main()')
