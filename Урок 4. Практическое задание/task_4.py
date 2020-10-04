cffe7e6f890c56cda608a6d6c6db7ea47a034109
Pr0venzano <Pr0venzano@mail.ru>
2020-10-04 03:44:24 +0300

    task_4

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -10,6 +10,8 @@
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
 """
 
+from timeit import timeit
+
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -37,5 +39,27 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+def func_3():
+    """ Версия медленнее func_1, но быстрее func_2, это максимум что у меня получился из 7 придуманных вариантов """
+    result = [0]
+    set_array = set(array)
+    for i in set_array:
+        if array.count(i) > result[0]:
+            result = [array.count(i), i]
+    return f'Чаще всего встречается число {result[1]}, ' \
+           f'оно появилось в массиве {result[0]} раз(а)'
+
+
+def func_4():
+    """ В текущей вариации данное решениеи на 3ем месте по скорости,
+    но оно становися самым быстрым при увеличении кол-ва элементов в массиве """
+    num = max(set(array), key=array.count)
+    cnt = array.count(num)
+    return f'Чаще всего встречается число {num}, ' \
+           f'оно появилось в массиве {cnt} раз(а)'
+
+
+print(func_1(), timeit('func_1()', setup='from __main__ import func_1', number=1_000))
+print(func_2(), timeit('func_2()', setup='from __main__ import func_2', number=1_000))
+print(func_3(), timeit('func_3()', setup='from __main__ import func_3', number=1_000))
+print(func_4(), timeit('func_4()', setup='from __main__ import func_4', number=1_000))
