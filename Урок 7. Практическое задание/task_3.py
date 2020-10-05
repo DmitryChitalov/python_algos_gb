10b151ae23e71e14c1354b34038dc31df0c93d6a
max <oksyuta.maxim@gmail.com>
2020-10-05 18:46:44 +0300

    Do homework

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,30 @@
 arr[m]
 from statistics import median
 """
+from random import randint
+from statistics import median
+
+
+def find_median(lst_obj):
+    for i in range(len(lst_obj)):
+        more = 0
+        less = 0
+        equal = 0
+        new_lst = lst_obj.copy()
+        new_lst.pop(i)
+        for item in new_lst:
+            if item < lst_obj[i]:
+                less += 1
+            elif item > lst_obj[i]:
+                more += 1
+            else:
+                equal += 1
+        if abs(more - less) <= equal:
+            return lst_obj[i]
+        # 1 2 2  x 4  5 4 7 6 5 6
+m = randint(3, 10)
+lst = [randint(0, 100) for i in range(2*m + 1)]
+print(lst)
+result = find_median(lst)
+print(result)
+print('Значение верно' if median(lst) == result else 'Ошибка')
