0ba0a9680a9dcd6d91383d1a5d39d56cbc7f8fd1
alex <schegoleff@gmail.com>
2020-10-05 20:09:01 +0300

    HW_1

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -18,3 +18,57 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+
+class StackClass:
+    def __init__(self, max_size):
+        self.elem = [[]]
+        self.max_size = max_size
+
+    def __str__(self):
+        return str(self.elem)
+
+    def is_empty(self):
+        return self.elem == [[]]
+
+    def push_in(self, el):
+        if len(self.elem[len(self.elem) - 1]) < self.max_size:
+            self.elem[len(self.elem) - 1].append(el)
+        else:
+            self.elem.append([])
+            self.elem[len(self.elem) - 1].append(el)
+
+    def pop_out(self):
+        result = self.elem[len(self.elem) - 1].pop()
+        if len(self.elem[len(self.elem) - 1]) == 0:
+            self.elem.pop()
+        return result
+
+    def get_val(self):
+        return self.elem[len(self.elem) - 1][len(self.elem[len(self.elem) - 1]) - 1]
+
+    def stack_size(self):
+        elem_sum = 0
+        for stack in self.elem:
+            elem_sum += len(stack)
+        return elem_sum
+
+    def stack_count(self):
+        return len(self.elem)
+
+
+s = StackClass(4)
+s.push_in(2)
+s.push_in(8)
+s.push_in('Семен')
+s.push_in(5.6)
+s.push_in(8.3)
+s.push_in('False')
+s.push_in(3)
+s.push_in('True')
+print(s)
+print(s.stack_size())
+print(s.get_val())
+print(s.pop_out())
+print(s.stack_count())
+print(s)
