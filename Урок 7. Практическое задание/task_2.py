7c5747e589fd899fe17189bf8f8fb440e16db717
Roman <barabinr@list.ru>
2020-10-06 14:19:47 +0300

    Lesson_7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,55 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+########################################################################################################################
+
+import random
+
+user_number = int(input('Введите целое число: '))
+
+
+def merge_sort(lst_obj):
+    if len(lst_obj) > 1:
+        center = len(lst_obj) // 2
+        left = lst_obj[:center]
+        right = lst_obj[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst_obj[k] = left[i]
+                i += 1
+            else:
+                lst_obj[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            lst_obj[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            lst_obj[k] = right[j]
+            j += 1
+            k += 1
+        return lst_obj
+
+
+some_list = [random.uniform(0, 50) for _ in range(user_number)]
+print(some_list)
+print(merge_sort(some_list))
+
+
+"""
+Введите целое число: 5
+Исходный:        [39.09714907040644, 36.30144962375475, 6.66805414830648, 27.127396988062213, 35.40931219494867]
+Отсортированный: [6.66805414830648, 27.127396988062213, 35.40931219494867, 36.30144962375475, 39.09714907040644]
+
+"""
+
+
