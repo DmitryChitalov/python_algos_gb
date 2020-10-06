edef059fbcf5c02598ec3972f02f8ed7184a2856
Alex T <ns2.AlexT@gmail.com>
2020-10-06 15:36:48 +0700

    Tarakanov_Alex_Lesson_7

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,32 @@
 arr[m]
 from statistics import median
 """
+"""
+Выполнил поиск медианы без сортировки. Сделал проверку работы функции, встроенными средствами библиотеки
+statistics.
+"""
+import random
+from statistics import median
+
+
+def find_m(i):
+    left = []
+    right = []
+    for idx, val in enumerate(array):
+        if val <= array[i] and idx != i:
+            left.append(val)
+        elif val >= array[i] and idx != i:
+            right.append(val)
+    if len(left) == len(right):
+        return True
+
+
+m_val = 10
+array = [random.randint(-100, 100) for _ in range(2 * m_val + 1)]
+print(f'Random array = {array}')
+
+for i_check, val in enumerate(array):
+    if find_m(i_check):
+        print(f'Without sort array: median = {array[i_check]}')
+
+print(f'Check by statistic: median = {median(array)}')
