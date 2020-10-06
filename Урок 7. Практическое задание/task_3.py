a01abb2c8dbbf1fba97c83a805772ae1bcd83e16
bLaM4e <konnerspz@mail.ru>
2020-10-06 14:49:38 +0300

    task_3_done

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,31 @@
 arr[m]
 from statistics import median
 """
+from statistics import median
+from random import randint
+
+
+def median_func(lst):
+    mdn_idx = (len(lst) - 1) // 2
+    mdn = lst[mdn_idx]
+    left, right = lst[:mdn_idx], lst[mdn_idx + 1:]
+    max_left, min_right = max(left), min(right)
+    go = True
+    while go:
+        if max_left > mdn:
+            i = left.index(max_left)
+            left[i], mdn = mdn, left[i]
+            max_left = max(left)
+        elif min_right < mdn:
+            j = right.index(min_right)
+            right[j], mdn = mdn, right[j]
+            min_right = min(right)
+        else:
+            go = False
+    return mdn
+
+
+list_origin = [randint(0, 100) for _ in range(11)]
+
+print(median_func(list_origin))
+print(median(list_origin))
