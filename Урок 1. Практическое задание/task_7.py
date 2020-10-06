17255c0169ab0e89f4e0ff18ba984da3c6257520
oleg <oleg_kosarev@mail.ru>
2020-10-06 06:41:51 +0300

    task 7

--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -10,3 +10,56 @@
 Вам нужно доработать программу так, чтобы она могла выполнить проверку на палиндром
 и в таких строках (включающих пробелы)
 """
+
+
+class DequeClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_to_front(self, elem):
+        self.elems.append(elem)
+
+    def add_to_rear(self, elem):
+        self.elems.insert(0, elem)
+
+    def remove_from_front(self):
+        rff = self.elems.pop()
+        while rff == ' ':
+        	rff = self.elems.pop()
+        return rff
+
+    def remove_from_rear(self):
+        rfr = self.elems.pop(0)
+        while rfr == ' ':
+        	rfr = self.elems.pop(0)
+        return rfr
+
+    def size(self):
+        return len(self.elems)
+
+
+
+def pal_checker(string):
+    dc_obj = DequeClass()
+
+    for el in string:
+        dc_obj.add_to_rear(el)
+
+    still_equal = True
+
+    while dc_obj.size() > 1 and still_equal:
+        first = dc_obj.remove_from_front()
+        last = dc_obj.remove_from_rear()
+        if first != last:
+            still_equal = False
+
+    return still_equal
+
+print(pal_checker(" молоко делили ледоко  лом   "))
+
+
+
+
