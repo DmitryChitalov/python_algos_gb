c756fe2777d57b3688e6ed292e40b0f934f9aae2
IliaPronin <71208904+IliaPronin@users.noreply.github.com>
2020-10-06 08:39:37 +0300

    hw_7_task_1 done

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -12,3 +12,25 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+
+import random
+from timeit import timeit
+
+
+def bubble_sort_reverse_1(my_list):
+    for i in range(len(my_list) - 1):
+        for j in range(len(my_list) - 1 - i):
+            if my_list[j] < my_list[j + 1]:
+                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+    return my_list
+
+
+sorted_list = [12, 11, 10, 9, 8, 5, 3, 2, 1, 0]
+not_sorted_list = [random.randint(-100, 100) for _ in range(10)]
+print(bubble_sort_reverse_1(sorted_list))
+
+print(timeit("bubble_sort_reverse_1(sorted_list)",
+             setup="from __main__ import bubble_sort_reverse_1, sorted_list", number=50))
+
+
+#  не смог придумать как оптимизировать, чтобы после одного прохода, если не меняется, то вернуть исходный список(
