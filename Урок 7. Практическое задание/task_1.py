370a7710a8592566e1d579df8c4fd06bf01f6561
Dmytro Tkachenko <dmitryytk@hhagency.com.ua>
2020-10-06 18:34:43 +0300

    Add files via upload

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -12,3 +12,48 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+
+# Если заканчивать функцию, в случаи отсутствия сортировки, алгоритм работает быстрее
+
+import random
+import timeit
+
+
+def bubble_sort(list_1):
+    n = 1
+    while n <= len(list_1):
+        for i in range(len(list_1) - 1):
+            if list_1[i] < list_1[i + 1]:
+                list_1[i], list_1[i + 1] = list_1[+1], list_1[i]
+        n += 1
+    return list_1
+
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+
+print(orig_list)
+print(bubble_sort(orig_list))
+# замеры 10
+print(timeit.timeit("bubble_sort(orig_list)", \
+                    setup="from __main__ import bubble_sort, orig_list", number=1000))
+
+
+def bubble_sort_2(list_1):
+    n = 1
+    while n <= len(list_1):
+        sort = 0
+        for i in range(len(list_1) - 1):
+            if list_1[i] < list_1[i + 1]:
+                list_1[i], list_1[i + 1] = list_1[+1], list_1[i]
+                sort += 1
+        if sort == 0:
+            break
+
+        n += 1
+    return list_1
+
+
+print(bubble_sort_2(orig_list))
+# замеры 10
+print(timeit.timeit("bubble_sort_2(orig_list)", \
+                    setup="from __main__ import bubble_sort_2, orig_list", number=1000))
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,32 @@
 arr[m]
 from statistics import median
 """
+import random
+
+
+def medium(nums):
+    L = []
+    R = []
+    med = 0
+    c = 0
+    while len(R) != 4 or len(R) != 5:
+        for i in range(len(nums) - 1):
+            med = nums[i]
+            if nums[i] < nums[c]:
+                L.append(nums[i])
+            else:
+                R.append(nums[i])
+        if len(R) == 4 or len(L) == 5:
+            break
+        else:
+            L = []
+            R = []
+            c += 1
+            continue
+
+    return f'Левый стек {L}, медиана - {med}, правый стек - {R}'
+
+
+orig_list = [random.randint(0, 100) for i in range(9)]
+print(medium(orig_list))
+
