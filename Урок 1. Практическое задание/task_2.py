a4a79c26970bc39793624b980588d1058ef37244
Mary <ulanovamary@yandex.ru>
2020-10-06 16:51:40 +0300

    3 tasks only
    All the rest I'll pass later and will push to this branch

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -7,6 +7,7 @@
 В основе алгоритма должно быть сравнение каждого числа со всеми другими элементами списка.
 Сложность такого алгоритма: O(n^2) - квадратичная.
 
+
 Второй, в виде функции, должен обеспечивать поиск минимального значения для списка.
 Сложность такого алгоритма: O(n) - линейная.
 
@@ -17,3 +18,26 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 """
+
+from random import randint
+
+#lst = [randint(1,100) for i in range(10)];
+#print(lst);
+#1
+def search_min():
+    lst = [randint(1, 100) for i in range(10)];
+    print(lst)
+    min_num = lst[0]
+    for i in range(1, len(lst)):
+        if lst[i] < min_num:
+            min_num = lst[i]
+    return min_num;
+
+print(search_min());
+
+#2
+def search_mini():
+    lst = [randint(1, 100) for i in range(10)];
+    print(lst)
+    return min(lst);
+print(search_mini());
