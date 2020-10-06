f8cb1db04afcef39152aa85f6b18daf7ab97f0eb
xBarbarian <alexadr.jukov@gmail.com>
2020-10-06 16:48:29 +0400

    Task 2 Done.

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -17,3 +17,27 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 """
+
+
+# 1 O(n^2) - квадратичная
+def min_lst_1(lst):
+    for i in range(len(lst)):
+        lst[i]
+        for j in range(len(lst)):
+            if lst[j] < lst[i]:
+                break
+    return lst[j]
+
+
+# O(n) - линейная
+def min_lst_2(lst):
+    a = min(lst)
+    return a
+
+
+range_list = ([i for i in range(10, 100)])
+# for i in range(len(range_list)):
+#     print(range_list[i])
+
+print(min_lst_1(range_list))
+print(min_lst_2(range_list))
