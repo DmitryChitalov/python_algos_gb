c4892cd716a62faf89c6fa5c1b6691c37034ec0b
Wesselloff <wesselloff@gmail.com>
2020-10-07 11:32:24 +0300

    Промежуточное сохранение, чтобы не потерять.

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -1,3 +1,4 @@
+#! /bin/python3
 """
 Задание 2.
 
@@ -17,3 +18,34 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 """
+
+import random
+
+
+def min1(lst):
+    """Квадратичная сложность"""
+    min_e = None
+    for i in lst:
+        for j in lst:
+            if i < j:
+                min_e = i
+            else:
+                min_e = j
+        if i < 
+    return min_e
+
+
+def min2(lst):
+    """Линейная сложность"""
+    min_e = lst[0]
+    for i in lst[1:]:
+        if i < min_e:
+            min_e = i
+    return min_e
+
+
+test_list = [random.randint(-100, 100) for i in range(0, 10)]
+print(test_list)
+print(min1(test_list))
+print(min2(test_list))
+
