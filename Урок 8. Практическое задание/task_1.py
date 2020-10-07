0bb7c1c461fb8cec26622c69a79a0611e66e22b4
max <oksyuta.maxim@gmail.com>
2020-10-07 19:45:08 +0300

    Do homework

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,44 @@
 ВНИМАНИЕ: примеры заданий будут размещены в последний день сдачи.
 Но постарайтесь обойтись без них.
 """
+from collections import Counter, deque
+
+table = dict()
+
+
+def haffman(string):
+    c = Counter(string)
+    elems = deque(sorted(c.items(), key=lambda item: item[1]))
+    while len(elems) > 1:
+        new_count = elems[0][1] + elems[1][1]
+        new_element = {
+            0: elems.popleft()[0],
+            1: elems.popleft()[0]
+        }
+        i = 0
+        try:
+            while new_count > elems[i][1]:
+                i += 1
+        except IndexError:
+            elems.append((new_element, new_count))
+        else:
+            elems.insert(i, (new_element, new_count))
+    create_haffman_table(elems[0][0])
+    result = ''
+    for i in string:
+        result += f'{table[i]} '
+    return result
+
+
+def create_haffman_table(t, path=''):
+    if not isinstance(t, dict):
+        table[t] = path
+    else:
+        create_haffman_table(t[0], path=f'{path}0')
+        create_haffman_table(t[1], path=f'{path}1')
+
+
+print(haffman("beep boop beer!"))
+"""
+Код писал сам основываясь на пример
+"""
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,6 +10,7 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных.
 """
 
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -21,8 +22,11 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
+        if not isinstance(new_node, int):
+            raise ValueError("The value must be a number")
+        if self.root < new_node:
+            raise ValueError("The left child cannot be greater than the root value")
+        if self.left_child is None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.left_child = BinaryTree(new_node)
@@ -31,13 +35,19 @@ class BinaryTree:
             # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
             # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
+            if new_node > self.left_child.root:
+                tree_obj.right_child = self.left_child
+            else:
+                tree_obj.left_child = self.left_child
             self.left_child = tree_obj
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
+        if not isinstance(new_node, int):
+            raise ValueError("The value must be a number")
+        if self.root > new_node:
+            raise ValueError("The right child cannot be less than the root value")
+        if self.right_child is None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.right_child = BinaryTree(new_node)
@@ -46,7 +56,10 @@ class BinaryTree:
             # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
             # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
+            if new_node > self.right_child.root:
+                tree_obj.right_child = self.right_child
+            else:
+                tree_obj.left_child = self.right_child
             self.right_child = tree_obj
 
     # метод доступа к правому потомку
@@ -59,11 +72,29 @@ class BinaryTree:
 
     # метод установки корня
     def set_root_val(self, obj):
+        if not isinstance(obj, int):
+            raise ValueError("The value must be a number")
+        if self.right_child and obj > self.right_child.root:
+            raise ValueError("The right child cannot be less than the root value")
+        if self.left_child and obj < self.left_child.root:
+            raise ValueError("The left child cannot be greater than the root value")
         self.root = obj
 
     # метод доступа к корню
     def get_root_val(self):
         return self.root
+
+    def get_left_child_val(self):
+        if self.left_child:
+            return self.left_child.root
+        else:
+            return None
+
+    def get_right_child_val(self):
+        if self.right_child:
+            return self.right_child.root
+        else:
+            return None
 
 
 r = BinaryTree(8)
@@ -71,9 +102,11 @@ print(r.get_root_val())
 print(r.get_left_child())
 r.insert_left(4)
 print(r.get_left_child())
-print(r.get_left_child().get_root_val())
+print(r.get_left_child_val())
 r.insert_right(12)
 print(r.get_right_child())
-print(r.get_right_child().get_root_val())
+print(r.get_right_child_val())
 r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+print(r.get_right_child_val())
+"""Сделана валидация значений узлов в соответствии с требованиями для бинарного дерева, добавлены методы получения 
+значений потомков"""
