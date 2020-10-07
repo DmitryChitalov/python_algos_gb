12ae44c6a6a89774dffc8d388dc1a963af611008
Dmitrii Blinov <diamondistka@gmail.com>
2020-10-07 19:25:31 +0800

    solved all tasks

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -28,3 +28,63 @@
 Вы вместо трехзначного числа ввели строку (((. Исправьтесь
 Введите операцию (+, -, *, / или 0 для выхода):
 """
+
+
+import operator
+
+
+def calculator():
+
+    operation = None
+    operand = None
+
+    def operation_input():
+        nonlocal operation
+        sign = input('Введите операцию (+, -, *, / или 0 для выхода): ')
+        if sign in '+-*/0':
+            operation = sign
+            return
+        else:
+            print('Ошибка ввода!')
+            operation_input()
+
+    def operand_input(side: str):
+        nonlocal operand
+        oper = input(f'Введите {side} число: ')
+        if operation == operator.truediv and side == 'второе' and oper == '0':
+            print('Деление на 0 недопустимо! Введите другой делитель!')
+            operand_input(side)
+        elif oper.isdigit():
+            operand = oper
+            return
+        else:
+            print('Вы вместо числа ввели строку (((. Исправьтесь')
+            operand_input(side)
+
+    operation_input()
+
+    if operation == '+':
+        operation = operator.add
+    elif operation == '-':
+        operation = operator.sub
+    elif operation == '*':
+        operation = operator.mul
+    elif operation == '/':
+        operation = operator.truediv
+    elif operation == '0':
+        print('Завершение работы!')
+        return
+
+    operand_input('первое')
+    left_operand = int(operand)
+
+    operand_input('второе')
+    right_operand = int(operand)
+
+    print(f'Ваш результат {operation(left_operand, right_operand)}')
+
+    calculator()
+
+
+if __name__ == '__main__':
+    calculator()
