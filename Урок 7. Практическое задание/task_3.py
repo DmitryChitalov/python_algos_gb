4f488534f63aba6ce377d3344b774d20d5ce4736
MIhey <yatkinma@gmail.com>
2020-10-08 20:48:12 +0700

    update mediana

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,11 +13,34 @@
 arr[m]
 from statistics import median
 """
-from random import randint
+from random import randint, choice
 from statistics import median
 
 nums = int(input('Введите m, для массива 2m + 1: '))
 orig_list = [randint(-100, 100) for _ in range(2 * nums + 1)]
-print(f'Медиана списка {orig_list} равна {median(orig_list)}')
+print(f'(проверка через median) Медиана списка {orig_list} равна {median(orig_list)}')
+print("*" * 30)
 
-# Выполнено без сортировки, но с подсказкой (если я кончно правильно понял задание)
+def mediana_lst(lst):
+    if len(lst) <= 1:
+        return lst
+    else:
+        i = choice(lst)
+        L = []
+        M = []
+        R = []
+        for el in lst:
+            if el < i:
+                L.append(el)
+            elif el > i:
+                R.append(el)
+            else:
+                M.append(el)
+        return mediana_lst(L) + M + mediana_lst(R)
+
+sorted_list = mediana_lst(orig_list)
+med = sorted_list[nums]
+
+print(f'Исходный список: {orig_list}')
+print(f'Отсортированный список: {sorted_list}')
+print(f'Медиана: {med}')
