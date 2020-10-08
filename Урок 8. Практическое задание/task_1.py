f7710988bf39c0da7adf9dfd98a09c2fb73a2fc0
levlab79 <lev.lsb56@gmail.com>
2020-10-09 02:43:04 +0400

    I did the 8th homework

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,63 @@
 ВНИМАНИЕ: примеры заданий будут размещены в последний день сдачи.
 Но постарайтесь обойтись без них.
 """
+
+import collections as cl
+
+
+class HardTree:
+    def __init__(self, my_string):
+        self.my_string = my_string
+        self.codes = {}
+
+    def __str__(self):
+        return ' '.join([self.codes[symb] for symb in self.my_string])
+
+    @staticmethod
+    def my_sort(my_string):
+        new_dict = {}
+        for symb in my_string:
+            new_dict[symb] = new_dict.get(symb, 0) + 1
+        return cl.deque(sorted(new_dict.items(), key=lambda el: el[1]))
+
+    def hf_tree(self):
+        sorted_els = HardTree.my_sort(self.my_string)
+        # Проверяем количество уникальных символов.
+        if len(sorted_els) > 1:
+            while len(sorted_els) - 1:
+                new_length = sorted_els[0][1] + sorted_els[1][1]
+                new_el = {0: sorted_els.popleft(
+                )[0], 1: sorted_els.popleft()[0]}
+                # Ищем место для ставки объединенного элемента
+                for idx, tmp_c in enumerate(sorted_els):
+                    if not tmp_c[1] < new_length:
+                        sorted_els.insert(idx, (new_el, new_length))
+                        break
+                else:
+                    sorted_els.append((new_el, new_length))
+        else:
+            new_length = sorted_els[0][1]
+            comb = {0: sorted_els.popleft()[0], 1: None}
+            sorted_els.append((comb, new_length))
+        return sorted_els[0][0]
+
+    def haffman_code(self, tree, path=''):
+        if not isinstance(tree, dict):
+            self.codes[tree] = path
+        else:
+            self.haffman_code(tree[0], f'{path}0')
+            self.haffman_code(tree[1], f'{path}1')
+
+
+# Создаем новое дерево.
+new_tree = HardTree("beep boop beer!")
+# Получаем таблицу.
+codes = new_tree.haffman_code(new_tree.hf_tree())
+# Выводим информацию.
+print(new_tree)
+
+"""
+Для меня эта тема оказалось достаточно тяжелой, поэтому пытался разобраться, опираясь на пример
+с урока. Старался реализовать дерево с помощью ООП, попутно максимально используя его возможности.
+Сделал перегрузку метода __str__(), чтобы для вывода на экран достаточно было вызвать функцию print().
+"""
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,6 +10,7 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных.
 """
 
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -21,33 +22,43 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
+        # Проверяем значение нового левого узла.
+        if self.root > new_node:
+            # если у узла нет левого потомка
+            if self.left_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.left_child = BinaryTree(new_node)
+            # если у узла есть левый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+            # Не позволяем вставить значение больше значения текущего узла.
+            raise ValueError
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
+        # Проверяем значение нового правого узла.
+        if self.root < new_node:
+            # если у узла нет правого потомка
+            if self.right_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.right_child = BinaryTree(new_node)
+            # если у узла есть правый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+            # Не позволяем вставить значение меньше значения текущего узла.
+            raise ValueError
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -77,3 +88,17 @@ print(r.get_right_child())
 print(r.get_right_child().get_root_val())
 r.get_right_child().set_root_val(16)
 print(r.get_right_child().get_root_val())
+
+r2 = BinaryTree(16)
+try:
+    # Выдаст ошибку.
+    r.insert_left(20)
+except ValueError:
+    print('Ошибка при создании потомка: ValueError')
+except Exception:
+    print('Ошибка при создании потомка: другая ошибка')
+
+"""
+Добавил валидизацию, а именно проверку вставляемых пользователем значений при добавлении
+левого и правого потомков.
+"""
