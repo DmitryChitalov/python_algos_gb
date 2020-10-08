7c36f3841c02bcbd9d6f6768552d4e361a4e7712
Alexey <alexeyhenke@yandex.ru>
2020-10-08 23:34:20 +0300

    задача №3

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -15,3 +15,37 @@
 Введите число, которое требуется перевернуть: 123
 Перевернутое число: 321
 """
+next_inter = True
+
+
+def revers_str(p_str, p_idx):
+    if p_str == '-':
+        vsrt = input("Введите число: ")
+        print(vsrt)
+        g_res['revers'] = ''
+    else:
+        vsrt = p_str
+
+    if len(vsrt) == 0 or vsrt == None or vsrt == '':
+        print("Строка не определена. Пока!")
+        return
+    else:
+        if len(vsrt) <= p_idx:
+            print("Обратка: " + str(g_res['revers']))
+            revers_str('-', 0)
+        else:
+            g_res['revers'] = vsrt[p_idx] + g_res['revers']
+            p_idx = p_idx + 1
+            revers_str(vsrt, p_idx)
+
+
+while next_inter:
+    g_res = {'revers': ''}
+    revers_str('-', 0)
+    while True:
+        next_add = input("Хотите продолжить (Да / Нет): ")
+        if next_add.lower() in ('да', 'нет'):
+            next_inter = next_add.lower() == 'да'
+            break
+        else:
+            print("Ошибка ввода: введите ответ еще раз")
