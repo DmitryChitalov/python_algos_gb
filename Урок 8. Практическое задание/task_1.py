41f8d9a7a92d77c5b7e1059c11733ae0d07f3217
Shmurygin Roman <roman.shmurygin@utair.ru>
2020-10-09 19:36:00 +0500

    Алгоритмы Python. Урок 8 ДЗ.

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,55 @@
 ВНИМАНИЕ: примеры заданий будут размещены в последний день сдачи.
 Но постарайтесь обойтись без них.
 """
+from collections import Counter, deque
+
+
+def haffman_tree(s):
+    cnt = Counter(s)
+    elements = deque(sorted(cnt.items(), key=lambda item: item[1]))
+    if len(elements) != 1:
+        while len(elements) > 1:
+            weight = elements[0][1] + elements[1][1]
+            elem = {0: elements.popleft()[0], 1: elements.popleft()[0]}
+            for i, j in enumerate(elements):
+                if weight > j[1]:
+                    continue
+                else:
+                    elements.insert(i, (elem, weight))
+                    break
+            else:
+                elements.append((elem, weight))
+    else:
+        weight = elements[0][1]
+        elem = {0: elements.popleft()[0], 1: None}
+        elements.append((elem, weight))
+
+    return elements[0][0]
+
+
+codes = dict()
+
+
+def haffman_code(tree, path=''):
+    if not isinstance(tree, dict):
+        codes[tree] = path
+    else:
+        haffman_code(tree[0], path=f'{path}0')
+        haffman_code(tree[1], path=f'{path}1')
+
+
+s = "beep boop beer!"
+
+print(haffman_tree(s))
+haffman_code(haffman_tree(s))
+print(codes)
+
+for i in s:
+    print(codes[i], end=' ')
+print()
+
+"""
+Решил не изобретать велосипед и рассмотреть пример из урока.
+Не нашел вариантов как данный код ещё оптимизировать или использовать другие коллекции.
+В итоге получилось копипаст с изменением имен переменных.
+"""
