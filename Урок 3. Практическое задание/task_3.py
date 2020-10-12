6c58e2be5eed7f8ab8fcf66cc8d486b780937fe2
mrwindmark <agentstorm@ya.ru>
2020-10-13 00:20:29 +0300

    Срезы это не эффективно с точки зрения ресурсов, но ограничений не было.

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,20 @@
 р
 а
 """
+import hashlib
+
+def hash_creator(line: str):
+    return hashlib.sha1(line.encode(encoding='UTF-8'))
+
+def sub_line_searcher(main_line):
+    subline_set = []
+    step = 1
+    while(step < len(main_line)):
+        for itr in range(0, len(main_line)):
+            subline_set.append(hash_creator(main_line[itr:itr + step]).hexdigest())
+        step += 1
+    return subline_set
+
+line = str(input("Enter line: "))
+sublines = sub_line_searcher(line)
+print(set(sublines))
