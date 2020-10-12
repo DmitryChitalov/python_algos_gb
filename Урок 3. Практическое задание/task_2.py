3e20f16bfe6263cfac90e95892a143e3152b78ee
Alexey <alexeyhenke@yandex.ru>
2020-10-12 23:37:11 +0300

    задача №2

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -15,3 +15,66 @@
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
+from uuid import uuid4
+import hashlib
+
+next_inter = True
+users_list = []
+user_fname = ['Имя']
+user_passw = ['Пароль']
+salt = uuid4().hex
+
+
+def createUsersList(user_fname: list, user_passw: list) -> list:
+    user_row = {user_fname[0]: user_fname[1]
+        , user_passw[0]: user_passw[1]}
+
+    users_list.append(user_row)
+    return users_list
+
+
+def encode_passwd(passwd):
+    hash_passwd = hashlib.sha256(salt.encode() + passwd.encode()).hexdigest()
+    return hash_passwd
+
+
+while next_inter:
+    while True:
+        user_value = input("Ведите 'Имя' пользователя: ")
+        if user_value != "":
+            user_fname.append(user_value)
+            break
+        else:
+            print("Ошибка ввода: поле 'Имя' пустое.")
+    while True:
+        user_value = input("Ведите 'Пароль' пользователя: ")
+        if user_value != "":
+            hash_pass = encode_passwd(user_value)
+            user_passw.append(hash_pass)
+            user_value = input("Ведите 'Пароль' еще раз: ")
+            if user_value != "":
+                hash_pass = ""
+                hash_pass = encode_passwd(user_value)
+                if hash_pass == user_passw[1]:
+                    print(f"Пароли совпадают {hash_pass} :: {user_passw[1]}")
+                    break
+                else:
+                    print(f"Пароли не совпадают {hash_pass} :: {user_passw[1]}")
+                    break
+            else:
+                print("Ошибка ввода: поле 'Пароль' пустое.")
+        else:
+            print("Ошибка ввода: поле 'Пароль' пустое.")
+
+    users_list = createUsersList(user_fname, user_passw)
+
+    while True:
+        next_add = input("Хотите добавить дополнительного пользователя? (Да / Нет): ")
+        if next_add.lower() in ('да', 'нет', 'yes', 'no', 'y', 'n'):
+            next_inter = next_add.lower() in ('да', 'yes', 'y')
+            break
+        else:
+            print("Ошибка ввода: введите ответ еще раз")
+
+print("- " * 50)
+print(users_list)
