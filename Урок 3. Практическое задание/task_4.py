aac8a805368d25ba11a2c59ad00fea67cfaa3341
Ilya <by_Ilusion@mail.ru>
2020-10-12 13:17:49 +0700

    Task 4 is completed

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -21,6 +21,11 @@ class ContentCaching:
         self.__salt = uuid.uuid4().hex
 
     def __check_cache(self, url_hash):
+        """Метод проверки наличия данных в кэше и их годность
+
+        :param url_hash: захешированный URL
+        :return: Возвращает контент если есть, либо None
+        """
         cache = self.__cache.get(url_hash)
         if cache is not None and cache['timeout'] > time.time():
             print(f"\n **** Content found in cache (time left: {cache['timeout'] - int(time.time())}) **** \n")
@@ -29,6 +34,11 @@ class ContentCaching:
             return None
 
     def __set_cache(self, url, content):
+        """Метод записи контента страницы в кэш
+
+        :param url: URL адрес страницы
+        :param content: Контент страницы (HTML-код)
+        """
         url_hash = self.__url_hash(url)
         self.__cache[url_hash] = {
             'timeout': int(time.time()) + self.__cache_life,
@@ -38,6 +48,10 @@ class ContentCaching:
         print('\n **** Content added to cache **** \n')
 
     def status_cache(self):
+        """Метод вывода статуса кэша
+
+        :return: Выводит в терминал состояние кэша
+        """
         print(f"\n{'- ' * 30}\nСтатус кэша:\n * Всего в кэше: {len(self.__cache)}")
         for i, cache in enumerate(self.__cache):
             row = f"\t{i}: "
@@ -49,9 +63,19 @@ class ContentCaching:
         print('- ' * 30, end="\n\n")
 
     def __url_hash(self, url):
+        """Метод хеширует URL-адрес
+
+        :param url: URL адрес страницы
+        :return: Возвращает захешированный URL страницы
+        """
         return hashlib.md5(self.__salt.encode() + url.encode()).hexdigest()
     
     def get_content(self, url):
+        """Метод получения контента страницы
+
+        :param url: URL адрес страницы
+        :return: Возвращает контент страницы (HTML-код)
+        """
         if url.find('http://') != 0 and url.find('https://') != 0:
             url = 'http://' + url
             print(url)
