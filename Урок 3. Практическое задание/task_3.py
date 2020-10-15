2c5b446638321a9995a8f1662d615a848de9944c
Egor <egorsilin@gmail.com>
2020-10-15 23:40:45 +0500

    added solution for task_3 from HW_3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,4 +14,20 @@
 р
 а
 """
-# TEST
+from hashlib import sha256
+
+
+def str_func(inp_str, substring_set=None):
+    if substring_set is None:
+        substring_set = set()
+    for i in range(len(inp_str)):
+        for j in range(len(inp_str) - 1 if i == 0 else len(inp_str), i, -1):
+            sub_str = inp_str[i:j]
+            sub_str_hash = sha256(sub_str.encode()).hexdigest()
+            substring_set.add(sub_str_hash)
+            print(f'Строка - "{sub_str}", '
+                  f'хеш - "{sub_str_hash}"')
+    return len(substring_set)
+
+
+print(f'Количество подстрок в строке: {str_func(inp_str=input("Введите строку: "))}')
