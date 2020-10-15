a2e37fdb35a6cbc9f23b0b1e64ffaa04d3537c81
mrwindmark <agentstorm@ya.ru>
2020-10-15 23:58:19 +0300

    Использовал list comprehension для ускорения работы функции. Поправил используемый синтаксис для timeit

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -25,17 +25,17 @@ def func_1(nums):
 my_num = [i for i in range(0, 100)]
 
 print(func_1(my_num))
-print(timeit('func_1(my_num)', setup="from __main__ import func_1; from __main__ import my_num ", number=10000))
+print(timeit('func_1(my_num)', setup="from __main__ import func_1, my_num ", number=10000))
 # 6.240210713000124
-print(repeat('func_1(my_num)', setup="from __main__ import func_1; from __main__ import my_num ", repeat=5, number=10000))
+print(repeat('func_1(my_num)', setup="from __main__ import func_1, my_num ", repeat=5, number=10000))
 # [6.305441824999434, 6.273107855999115, 6.3074074249998375, 6.246076170002198, 6.229232941001101]
 def func_2(nums):
     return [i for i in range(len(nums)) if nums[i] % 2 == 0]
 
 print(func_2(my_num))
-print(timeit('func_2(my_num)', setup="from __main__ import func_2; from __main__ import my_num ", number=10000))
+print(timeit('func_2(my_num)', setup="from __main__ import func_2, my_num ", number=10000))
 # 5.495959821000724
-print(repeat('func_2(my_num)', setup="from __main__ import func_2; from __main__ import my_num ", repeat=5, number=10000))
+print(repeat('func_2(my_num)', setup="from __main__ import func_2, my_num ", repeat=5, number=10000))
 # [5.507961403000081, 5.511396752001019, 5.505242994000582, 5.507853768001951, 5.658421536001697]
 """
 Генераторы и генераторные выражения выполняются быстрее нежели сопоставимые функции на циклах.
