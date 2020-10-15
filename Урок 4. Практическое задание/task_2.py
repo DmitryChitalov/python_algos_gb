5d194b777056c46df17672396f79ea93b4d8a035
mrwindmark <agentstorm@ya.ru>
2020-10-16 00:26:37 +0300

    Привёл пример работы со срезами и функцией reversed для получения отражённого числа. В коде была ошибка в 20-й строке. По условию "if number == 0:" к строке добавлялся лишний ноль

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -17,30 +17,51 @@ from random import randint
 
 def recursive_reverse(number):
     if number == 0:
-        return str(number % 10)
+        return ''               # здесь был косяк, добавляющий лишний символ "0" в результат
     return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+
+def mine_reverse(number):
+    return str(number)[::-1]
+
+# даже такой неудачный метод, работающий с reversed оказывается в 2 раза быстрее рекурсии
+def mine_reverse_v2(number):
+    result = list(reversed(str(number)))
+    num_rev = ''
+    for i in result:
+        num_rev += f"{i}"
+    return num_rev
+
+
+def start_timeit(func_name):
+    print(f'Не оптимизированная функция {func_name}')
+    print(
+        timeit(
+            f"{func_name}(num_100)",
+            setup=f'from __main__ import {func_name}, num_100',
+            number=10000))
+    print(
+        timeit(
+            f"{func_name}(num_1000)",
+            setup=f'from __main__ import {func_name}, num_1000',
+            number=10000))
+    print(
+        timeit(
+            f"{func_name}(num_10000)",
+            setup=f'from __main__ import {func_name}, num_10000',
+            number=10000))
 
 
 num_100 = randint(10000, 1000000)
 num_1000 = randint(1000000, 10000000)
 num_10000 = randint(100000000, 10000000000000)
 
-print('Не оптимизированная функция recursive_reverse')
-print(
-    timeit(
-        "recursive_reverse(num_100)",
-        setup='from __main__ import recursive_reverse, num_100',
-        number=10000))
-print(
-    timeit(
-        "recursive_reverse(num_1000)",
-        setup='from __main__ import recursive_reverse, num_1000',
-        number=10000))
-print(
-    timeit(
-        "recursive_reverse(num_10000)",
-        setup='from __main__ import recursive_reverse, num_10000',
-        number=10000))
+# print(num_100)
+# print(recursive_reverse(num_100))
+# print(mine_reverse(num_100))
+# print(mine_reverse_v2(num_100))
+
+start_timeit('recursive_reverse')
 
 
 def memoize(f):
@@ -79,3 +100,12 @@ print(
         'recursive_reverse_mem(num_10000)',
         setup='from __main__ import recursive_reverse_mem, num_10000',
         number=10000))
+
+start_timeit('mine_reverse')
+start_timeit('mine_reverse_v2')
+"""
+Мемоизация безусловно нужна, т.к. она позволяет сократить количество повторных вычислений
+Что даёт значительное сокращение затрачиваемого времени.
+Однако отказ от рекурсии мог бы дать прирост производительности без введения мемоизации
+Грубые методы типа reversed или среза, позволят сразу работать со строкой и производить разворот без вычислений
+"""
