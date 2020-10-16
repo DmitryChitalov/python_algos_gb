d97ab6bdd1d4d0df5103453002d5e0bd75e176aa
Dennis Veselov <wesselloff@gmail.com>
2020-10-16 07:59:05 +0300

    Задания третьего урока выполнены

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,19 @@
 р
 а
 """
+from hashlib import sha1
+
+S = input('Введите строку из строчных латинских букв: ')
+N = len(S)
+hash_set = set()
+
+for i in range(N):
+    for j in range(N, i, -1):
+        substr = S[i:j]
+        if substr == S:
+            continue
+        print(substr)
+        hash_set.add(sha1(substr.encode()).hexdigest())
+print(hash_set)
+print(f'В строке "{S}" {len(hash_set)} различных подстрок')
+
