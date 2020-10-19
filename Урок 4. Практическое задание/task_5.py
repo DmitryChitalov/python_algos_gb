cfb0e764fda89e6f99a9f66a0a191b842c1c09d8
Egor <egorsilin@gmail.com>
2020-10-20 00:14:43 +0500

    added solution for task_5 from HW_4

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -13,11 +13,15 @@
 Опишите результаты, сделайте выводы, где и какой алгоритм эффективнее
 Подумайте и по возможности определите сложность каждого алгоритма
 """
-# TEST
+
+import math
+from timeit import timeit
 
 
 def simple(i):
-    """Без использования «Решета Эратосфена»"""
+    """Без использования «Решета Эратосфена»
+    O(n**2)
+    """
     count = 1
     n = 2
     while count <= i:
@@ -36,5 +40,34 @@ def simple(i):
     return n
 
 
+def eratosthenes(inp_num):
+    """O(n log(log n))"""
+    a = []
+    number_count = 0
+    num = 2
+    while number_count <= inp_num:
+        number_count = num / math.log(num)
+        num += 1
+    for x in range(num + 1):
+        a.append(x)
+
+    a[1] = 0
+    x = 2
+    while x <= num:
+        if a[x] != 0:
+            y = x + x
+            while y <= num:
+                a[y] = 0
+                y = y + x
+        x += 1
+    a = [value for value in a if value != 0]
+    return a[inp_num - 1]
+
+
 i = int(input('Введите порядковый номер искомого простого числа: '))
 print(simple(i))
+print(f"simple: {timeit('simple(i)', setup='from __main__ import simple, i', number=100)}")
+print(eratosthenes(i))
+print(f"eratosthenes: {timeit('eratosthenes(i)', setup='from __main__ import eratosthenes, i', number=100)}")
+
+"""Чем выше номер искомого числа тем эффективнее себя проявляет метод «Решета Эратосфена»"""
