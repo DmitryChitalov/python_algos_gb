c5e909a9029b678f7e130ad69aa12a4dd8c83e54
tryVellum <amigodrigo@gmail.com>
2020-10-19 21:16:10 +0300

    Lesson 5: task 4 done

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,43 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+from collections import OrderedDict
+from timeit import timeit
+
+
+def test_dict():
+    my_dict = {}
+    for i in range(1000):
+        my_dict[i] = i*i
+    # print(my_dict)
+    for i in range(1000):
+        my_dict.pop(i)
+    return True
+
+
+def test_order():
+    my_dict = OrderedDict()
+    for i in range(1000):
+        my_dict[i] = i*i
+    # print(my_dict)
+    for i in range(1000):
+        my_dict.popitem()
+    return True
+
+
+print(
+    timeit(
+        "test_dict()",
+        setup='from __main__ import test_dict',
+        number=10000))  # 2.761655318
+
+print(
+    timeit(
+        "test_order()",
+        setup='from __main__ import test_order',
+        number=10000)) # 3.791548501
+'''Прочитав методичку и дополнительную информацию в интернете, главная фишка OrderedDict
+уже не так актуальна, а из полезных функций стоит отметить move_to_end(key, last=True). Так что не особо
+вижу применение этой коллекции в работе.'''
+test_order()
+test_dict()
