7bd9aa6a53e0e9cd0b76a4bc91f4aa9d137743ca
Egor <egorsilin@gmail.com>
2020-10-19 22:23:21 +0500

    added solution for task_1 from HW_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -11,7 +11,6 @@
 
 Добавьте аналитику: что вы сделали и почему
 """
-# TEST
 from timeit import timeit
 
 
@@ -21,3 +20,18 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+nums_example = list(range(100))
+print(timeit('func_1(nums_example)', 'from __main__ import func_1, nums_example'))
+print(timeit('func_2(nums_example)', 'from __main__ import func_2, nums_example'))
+"""
+func_2 использует генераторное выражение, 
+вместо используемого в func_1 перебора в цикле,
+что оказывается более быстродейственным, при большом количестве элементов.
+"""
