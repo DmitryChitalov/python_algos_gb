02e62b5c355d1b19ecf63cb102afe28b9e98cdea
alex <schegoleff@gmail.com>
2020-10-20 00:30:22 +0300

    HW_5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,58 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+
+from random import randint
+from timeit import timeit
+from collections import deque
+
+lst = [randint(0, 100) for i in range(10)]
+dq = deque(lst)
+print(lst)
+print(dq)
+
+
+def append_lst():
+    return lst.append(10)
+
+
+def pop_lst():
+    return lst.pop(5)
+
+
+def append_dq():
+    return dq.append(6)
+
+
+def appendleft_dq():
+    return dq.appendleft(4)
+
+
+def pop_dq():
+    return dq.pop()
+
+
+def popleft_dq():
+    return dq.popleft()
+
+
+print(f"append для списка: "
+      f"{timeit('append_lst()', setup='from __main__ import append_lst', number=100000)}")
+print(f"append для дека: "
+      f"{timeit('append_dq()', setup='from __main__ import append_dq', number=100000)}")
+print(f"appendleft для дека: "
+      f"{timeit('appendleft_dq()', setup='from __main__ import appendleft_dq', number=100000)}")
+print(f"pop для списка: "
+      f"{timeit('pop_lst()', setup='from __main__ import pop_lst', number=100000)}")
+print(f"pop для дека: "
+      f"{timeit('pop_dq()', setup='from __main__ import pop_dq', number=100000)}")
+print(f"popleft для дека: "
+      f"{timeit('popleft_dq()', setup='from __main__ import popleft_dq', number=100000)}")
+
+'''
+append и appendleft для дека работает быстрее чем append для списка.
+pop и popleft для дека по скорости значительно превосходит pop по индексу для списка. 
+Следовательно, правило из документации верно. 
+Если вам нужно дописать или удалить элемент из начала или конца - то использовать дек будет значительно быстрее. 
+Если нужно обращение к определенному элементу списка - то необходимо использовать список.
+'''
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,54 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+
+from timeit import timeit
+from collections import OrderedDict
+
+dct = {i: i for i in range(10)}
+ord_dct = OrderedDict(dct)
+print(dct)
+print(ord_dct)
+
+
+def dct_items():
+    return dct.items()
+
+
+def ord_dct_items():
+    return ord_dct.items()
+
+
+def dct_keys():
+    return dct.keys()
+
+
+def ord_dct_keys():
+    return ord_dct.keys()
+
+
+def dct_get():
+    return dct.get(5)
+
+
+def ord_dct_get():
+    return ord_dct.get(8)
+
+
+print(f"items для словаря: "
+      f"{timeit('dct_items()', setup='from __main__ import dct_items', number=1000000)}")
+print(f"items для OrderedDict: "
+      f"{timeit('ord_dct_items()', setup='from __main__ import ord_dct_items', number=1000000)}")
+print(f"keys для словаря: "
+      f"{timeit('dct_keys()', setup='from __main__ import dct_keys', number=1000000)}")
+print(f"keys для OrderedDict: "
+      f"{timeit('ord_dct_keys()', setup='from __main__ import ord_dct_keys', number=1000000)}")
+print(f"get для словаря: "
+      f"{timeit('dct_get()', setup='from __main__ import dct_get', number=1000000)}")
+print(f"get для OrderedDict: "
+      f"{timeit('ord_dct_get()', setup='from __main__ import ord_dct_get', number=1000000)}")
+
+'''
+OrderedDict при выводе выглядит как dict.items().
+операции с OrderedDict занимают немного меньше времени чем операции со словарем
+'''
