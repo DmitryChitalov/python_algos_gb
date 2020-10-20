b4e911e18c69a902fe9a2706c9a71a8bd30364c8
leonardar <leonardar88@gmail.com>
2020-10-20 13:40:26 +0300

    task 1

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,44 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+
+# Первая реализация через Counter
+import collections
+
+counter = collections.Counter()
+
+n = int(input("Введите количество предприятий для расчета прибыли: "))
+for i in range(n):
+    cname = input("Введите название предприятия: ")
+    point = list(
+        map(int, input("Через пробел введите прибыль данного предприятия за каждый квартал(всего 4):  ").split()))
+    for s in point:
+        counter[cname] += s
+
+avg_common = sum(counter.values()) / n
+
+lst1 = [k for k, v in counter.items() if v > avg_common]
+lst2 = [k for k, v in counter.items() if v < avg_common]
+
+print(f'Предприятия, с прибылью выше среднего значения: {", ".join(lst1)}',
+      f'Предприятия, с прибылью ниже среднего значения: {", ".join(lst2)}')
+
+# вторая реализация через defaultdict
+
+defdict = collections.defaultdict(int)
+
+n = int(input("Введите количество предприятий для расчета прибыли: "))
+for i in range(n):
+    cname = input("Введите название предприятия: ")
+    point = list(
+        map(int, input("Через пробел введите прибыль данного предприятия за каждый квартал(всего 4):  ").split()))
+    for s in point:
+        defdict[cname] += s
+
+avg_common = sum(defdict.values()) / n
+
+lst1 = [k for k, v in defdict.items() if v > avg_common]
+lst2 = [k for k, v in defdict.items() if v < avg_common]
+
+print(f'Предприятия, с прибылью выше среднего значения: {", ".join(lst1)}',
+      f'Предприятия, с прибылью ниже среднего значения: {", ".join(lst2)}')
