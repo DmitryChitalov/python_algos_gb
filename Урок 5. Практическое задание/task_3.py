a4d049528f9180bcb3743006a96cbaa8a085c8a4
Alexey <alexeyhenke@yandex.ru>
2020-10-23 01:27:20 +0300

    задача №3 tmp2

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,13 +11,29 @@ deque – это обобщение стеков и очередей.
 соответствует дейстивтельности.
 """
 import string
+import time
 from collections import deque
 
 
+def my_timer(f):
+    def tmp(*args, **kwargs):
+        start_time = time.time()
+        result = f(*args, **kwargs)
+        delta_time = time.time() - start_time
+        print('Время выполнения функции {}'.format(delta_time))
+        return result
+
+    return tmp
+
+
+@my_timer
 def create_list():
     return [c * 3 for c in range(100000)]
 
 
+@my_timer
 def deque_rotate():
+    global new_deque
     for i in range(100000):
         new_deque = deque(string.ascii_uppercase)
+    return new_deque
