bfe447fc440eb21c61ad3ff9bdd1238c326ac081
sergeiKostenko <sdh1985@mail.ru>
2020-10-23 19:40:18 +0300

    finished seven tasks

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -24,11 +24,13 @@ class StackPlates:
     """
     Реализует структуру, предусматривающую увеличение количества стеков в зависимости от количества элементов.
     Размер стека (стопки тарелок) принят равным трем. При заполнении текущего стека в структуру автоматически
-    добавляется новый стек.
+    добавляется новый стек. Если текущий стек (за исключением первого), опустел, то он удаляется из структуры).
     """
+
     def __init__(self):
         self.elems = [[]]
         self.current = 0  # константа определяет в каком стеке находимся в данный момент
+        self.taken_out = None  # извлекаемая из стопки тарелка
 
     def is_empty(self):
         return self.elems == [[]]
@@ -43,7 +45,11 @@ class StackPlates:
             self.elems[self.current].append(el)
 
     def pop_out(self):
-        return self.elems[self.current].pop()
+        """Удаляет тарелку из текущей стопки. Если это не 1-я стопка и кол-во тарелок в ней = 0, удаляет стек под нее"""
+        self.taken_out = self.elems[self.current].pop()
+        if self.elems[self.current] == [] and self.current != 0:
+            self.elems.pop()
+        return self.taken_out
 
     def get_val(self):
         return self.elems[self.current][len(self.elems) - 1]
@@ -65,25 +71,40 @@ if __name__ == '__main__':
     plates_one.push_in('third plate')
 
     # Удаляем тарелку из стопки
-    plates_one.pop_out()
+    plates_one.pop_out()  # удаляется треться тарелка
 
     print(plates_one.elems)
+    print('_' * 100)
     # Добавляем тарелки. Начиная с пятой (так как третья была удалена), тарелки добавляются уже во вторую стопку
     plates_one.push_in('fourth plate')
     plates_one.push_in('fifth plate')
     plates_one.push_in('sixth plate')
 
     print(plates_one.elems)
-    print(plates_one.get_val())
+    print(plates_one.get_val())  # шестая тарелка лежит сверху второй стопки
 
     # Количество стопок на данный момент
     print(plates_one.stack_size())
+    print('_' * 100)
 
     plates_one.push_in('seventh plate')
+    plates_one.push_in('eighth plate')
     plates_one.push_in('ninth plate')
+    plates_one.push_in('tenth plate')
 
     # Количество тарелок в текущей стопке
     print(plates_one.stack_el_size())
 
     # Количество стопок на данный момент
     print(plates_one.stack_size())
+
+    print(plates_one.elems)
+    print('_' * 100)
+    # Проверяем, удаляется ли текущая стопка (за исключением первой), при удалении из нее всех тарелок
+    plates_one.pop_out()
+    plates_one.pop_out()
+    plates_one.pop_out()
+    print(plates_one.elems)
+
+    # Последняя тарелка, вынутая на данный момент из текущей стопкм
+    print(plates_one.taken_out)
--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -1,5 +1,5 @@
 """
-Задание 5.
+Задание 7.
 Задание на закрепление навыков работы с деком
 
 В рассмотренном на уроке листинге есть один недостаток
@@ -10,3 +10,51 @@
 Вам нужно доработать программу так, чтобы она могла выполнить проверку на палиндром
 и в таких строках (включающих пробелы)
 """
+
+
+class DequeClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_to_front(self, elem):
+        self.elems.append(elem)
+
+    def add_to_rear(self, elem):
+        self.elems.insert(0, elem)
+
+    def remove_from_front(self):
+        return self.elems.pop()
+
+    def remove_from_rear(self):
+        return self.elems.pop(0)
+
+    def size(self):
+        return len(self.elems)
+
+
+def pal_checker(string):
+    dc_obj = DequeClass()
+    new_string = string.replace(' ', '')  # первый вариант
+    # new_string = ''.join([letter for letter in string if letter != ' '])   # второй вариант
+    # new_string = ''.join(string.split())   # третий вариант
+    for el in new_string:
+        dc_obj.add_to_rear(el)
+
+    still_equal = True
+
+    while dc_obj.size() > 1 and still_equal:
+        first = dc_obj.remove_from_front()
+        last = dc_obj.remove_from_rear()
+        if first != last:
+            still_equal = False
+
+    return still_equal
+
+
+print(pal_checker("топот"))
+print(pal_checker("молоко делили ледоколом"))
+print(pal_checker("леша на полке клопа нашел"))
+print(pal_checker("тест тест"))
