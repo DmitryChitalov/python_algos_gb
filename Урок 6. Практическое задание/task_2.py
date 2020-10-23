d50ab63fd9ee89b5bfff6ac38faa2bf9599a940e
Ilusion <by_Ilusion@mail.ru>
2020-10-23 12:27:58 +0700

    task_2 finished

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -79,11 +79,11 @@ some_str_list = [
 
 # Добавим еще текста в список
 max_i = len(some_str_list) - 1
-for _ in range(10000):
+for _ in range(100000):
     some_str_list.append(some_str_list[randint(0, max_i)])
 
 # Сгенерим еще список из случайных чисел
-some_int_list = [randint(0, 1000) for _ in range(100000)]
+some_int_list = [randint(0, 1000) for _ in range(1000000)]
 
 
 ########################################
@@ -127,10 +127,10 @@ def func_2():
 
 
 # Выполним профилирование списка в чистом виде
-# func_1()
+func_1()
 
 # Выполним профилирование списка с компрессией
-# func_2()
+func_2()
 
 """
 Вывод:
@@ -140,43 +140,106 @@ def func_2():
 
 ########################################
 """
-Тестирование модуля numpy
+Профилировка памяти с использованием:
+    - списков list
+    - массивов numpy.array
+    - кортежей tuple
+
+Python 3.8.5
+ОС: Linux Mint 20 x64
 """
 
 
 @profile
-def gen_list(c):
-    lst = [randint(0, 100) for _ in range(c)]
-    print(len(lst))
-    return lst
-
+def gen_list():
+    lst1 = deepcopy(some_int_list)
+    print(len(lst1))
+    lst2 = deepcopy(some_str_list)
+    print(len(lst2))
+    del lst1
+    del lst2
+    return
 
 
 @profile
 def gen_array():
-    # arr = []
-    # for _ in range(c):
-    #     arr = numpy.append(arr, randint(0, 100))
-    arr = numpy.array(lst1)
-    print(len(arr))
-    return arr
-
+    arr1 = numpy.array(some_int_list)
+    print(len(arr1))
+    arr2 = numpy.array(some_str_list)
+    print(len(arr2))
+    del arr1
+    del arr2
+    return
 
 
 @profile
 def gen_tuple():
-    tpl = tuple(lst1)
-    print(len(tpl))
-    return tpl
+    tpl1 = tuple(some_int_list)
+    print(len(tpl1))
+    tpl2 = tuple(some_str_list)
+    print(len(tpl2))
+    del tpl1
+    del tpl2
+    return
+
 
 
 # Генерация списка
-lst1 = gen_list(1000000)
+gen_list()
+"""
+Line #    Mem usage    Increment  Occurences   Line Contents
+============================================================
+   155     64.1 MiB     64.1 MiB           1   @profile
+   156                                         def gen_list():
+   157     72.0 MiB      7.8 MiB           1       lst1 = deepcopy(some_int_list)
+   158     72.0 MiB      0.0 MiB           1       print(len(lst1))
+   159     72.6 MiB      0.6 MiB           1       lst2 = deepcopy(some_str_list)
+   160     72.6 MiB      0.0 MiB           1       print(len(lst2))
+   161     65.1 MiB     -7.5 MiB           1       del lst1
+   162     64.3 MiB     -0.8 MiB           1       del lst2
+   163     64.3 MiB      0.0 MiB           1       return
+"""
+
 # Генерация массива numpy
 gen_array()
+"""
+Line #    Mem usage    Increment  Occurences   Line Contents
+============================================================
+   166     64.3 MiB     64.3 MiB           1   @profile
+   167                                         def gen_array():
+   168     71.6 MiB      7.2 MiB           1       arr1 = numpy.array(some_int_list)
+   169     71.6 MiB      0.0 MiB           1       print(len(arr1))
+   170    110.5 MiB     38.9 MiB           1       arr2 = numpy.array(some_str_list)
+   171    110.5 MiB      0.0 MiB           1       print(len(arr2))
+   172    110.5 MiB      0.0 MiB           1       del arr1
+   173     71.7 MiB    -38.8 MiB           1       del arr2
+   174     71.7 MiB      0.0 MiB           1       return
+"""
+
 # Генерация кортежа
 gen_tuple()
+"""
+Line #    Mem usage    Increment  Occurences   Line Contents
+============================================================
+   177     64.0 MiB     64.0 MiB           1   @profile
+   178                                         def gen_tuple():
+   179     71.7 MiB      7.7 MiB           1       tpl1 = tuple(some_int_list)
+   180     71.7 MiB      0.0 MiB           1       print(len(tpl1))
+   181     72.5 MiB      0.8 MiB           1       tpl2 = tuple(some_str_list)
+   182     72.5 MiB      0.0 MiB           1       print(len(tpl2))
+   183     64.9 MiB     -7.5 MiB           1       del tpl1
+   184     64.2 MiB     -0.8 MiB           1       del tpl2
+   185     64.2 MiB      0.0 MiB           1       return
+"""
 
 """
-На данном примере у меня не удалось увидеть особую разницу в использовании памяти.
+Вывод:
+Объем анимаемой памяти зависит от типа данных.
+Если целые числа, то numpy.array - лучше, но этот модуль очень плохо хранит строки.
+Кортежи занимают примерно тот же объем что и списки, числа чуть лучше, строки чуть хуже.
+"""
+
+"""
+Была еще мысль хранить временные переменные в базах данных, таких как Redis, но реализовывать не стал.
+Больше никаких вариантов найти не смог.
 """
