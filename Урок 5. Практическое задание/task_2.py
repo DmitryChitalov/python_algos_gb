ab150f51eff2869a1d65ac6766ffa08e29a5dd2e
Egor <egorsilin@gmail.com>
2020-10-23 10:27:07 +0500

    added solution for 2 task from 05_HW

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,4 +11,13 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
-# TEST
+from collections import deque
+
+first_number = deque(input('Ввод первого числа: '))
+second_number = deque(input('Ввод второго числа: '))
+first_number = int(''.join(first_number), 16)
+second_number = int(''.join(second_number), 16)
+sum_of_numbers = hex(first_number + second_number).upper()
+multiplication_of_numbers = hex(first_number * second_number).upper()
+print(f'Сумма чисел из примера: {deque(sum_of_numbers[2:])}')
+print(f'Произведение чисел из примера:{deque(multiplication_of_numbers[2:])}')
