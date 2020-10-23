3add7c21c9ffd505d8955615954432aeb5176cb1
Egor <egorsilin@gmail.com>
2020-10-23 11:37:30 +0500

    added solution for 4 task from 05_HW

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,4 +4,33 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
-# TEST
+from timeit import timeit
+from collections import OrderedDict
+
+dict_example = {k: k for k in range(10)}
+ordered_dict_example = {k: k for k in range(10)}
+ordered_dict_example = OrderedDict(ordered_dict_example)
+
+
+def items_d():
+    return dict_example.items()
+
+
+def items_od():
+    return ordered_dict_example.items()
+
+
+def get_d():
+    return dict_example.get(1)
+
+
+def get_od():
+    return ordered_dict_example.get(1)
+
+
+print(f"Получение пары ключ-значение из словаря: {timeit('items_d()', 'from __main__ import items_d')}")
+print(f"Получение ключа из словаря: {timeit('get_d()', 'from __main__ import get_d')}")
+print(f"Получение пары ключ-значение из упорядоченного словаря: {timeit('items_od()', 'from __main__ import items_od')}")
+print(f"Получение ключа из упорядоченного словаря: {timeit('get_od()', 'from __main__ import get_od')}")
+'''Вывод:
+Оба вида словарей работают с одинаковой скоростью, однако в OrderDict элементы упорядочены'''
