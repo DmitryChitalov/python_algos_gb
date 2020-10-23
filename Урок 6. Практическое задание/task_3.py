354a13f46b7c0f9a2f8597f4747e52861d7ec277
serzh251 <serzh251@mail.ru>
2020-10-23 11:31:27 +0300

    lesson6 done

---
+++ Урок 6. Практическое задание/task_3.py
@@ -0,0 +1,25 @@
+"""
+Задание 3 *.
+Сделать профилировку для скриптов с рекурсией и сделать,
+можно так профилировать и есть ли 'подводные камни'
+"""
+from memory_profiler import profile
+
+
+@profile
+def revers(enter_num, revers_num=0):
+    if enter_num == 0:
+        return
+    else:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+        revers(enter_num, revers_num)
+
+
+revers(123456)
+
+"""
+Профилировать с рекурсией не получится, потому что профилировка будет вызываться то количество раз, которое будет
+вызываться рекурсия
+"""
