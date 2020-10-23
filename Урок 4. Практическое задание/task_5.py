0694e8f9c4862c5d9b721825dda2bb196069ae03
leonardar <leonardar88@gmail.com>
2020-10-23 13:52:29 +0300

    Доделанное задание 5 из урока 4 и урок 5

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -12,11 +12,9 @@
 10, 100, 1000
 Опишите результаты, сделайте выводы, где и какой алгоритм эффективнее
 Подумайте и по возможности определите сложность каждого алгоритма
-"""
-
 
 def simple(i):
-    """Без использования «Решета Эратосфена»"""
+
     count = 1
     n = 2
     while count <= i:
@@ -33,7 +31,38 @@ def simple(i):
             count += 1
         n += 1
     return n
+"""
+
+from math import log
 
 
-i = int(input('Введите порядковый номер искомого простого числа: '))
-print(simple(i))
+def brut_len(number):
+    _n = 10
+    k = _n / log(_n)
+    while k < number:
+        _n *= 10
+        k = _n / log(_n)
+    return _n
+
+
+if __name__ == '__main__':
+    n = int(input('Введите порядковый номер искомого простого числа: '))
+
+    cache_len = brut_len(n)
+    cache = {i: True for i in range(2, cache_len)}
+
+    count = 0
+    p = 2
+
+    simples = []
+
+    while count != n:
+        if cache[p]:
+            count += 1
+            simples.append(p)
+            for i in range(2*p, cache_len, p):
+                cache[i] = False
+        p += 1
+
+    print(simples)
+    print(cache)
