30c7e797ed9514291f4bbb2f63b4f10e2bdde8fb
nat0t <nataniel1987@gmail.com>
2020-10-25 11:29:25 +0300

    Added tasks1-3

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,39 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+from random import uniform
+
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+    for i in range(len(left_list) + len(right_list)):
+        if left_list_index < len(left_list) and right_list_index < len(right_list):
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+        elif left_list_index == len(left_list):
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        elif right_list_index == len(right_list):
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+    return sorted_list
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left_list = merge_sort(array[:mid])
+    right_list = merge_sort(array[mid:])
+    return merge(left_list, right_list)
+
+if __name__ == '__main__':
+    random_array = [uniform(0, 50) for i in range(1000)]
+    sorted_array = merge_sort(random_array)
+    print('Исходный массив:')
+    print(random_array)
+    print('Отсортированный массив:')
+    print(sorted_array)
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,51 @@
 arr[m]
 from statistics import median
 """
+from statistics import median
+import random
+from timeit import timeit
+
+def quickselect(l, k, pivot_func=random.choice):
+    # Базовый случай.
+    if len(l) == 1:
+        return l[0]
+    # Выбираем индекс опорного элемента.
+    pivot = pivot_func(l)
+    # Разбиваем исходный список на больше/меньше опорного элемента.
+    lows = [el for el in l if el < pivot]
+    highs = [el for el in l if el > pivot]
+    pivots = [el for el in l if el == pivot]
+    # Ищем медиану.
+    if k < len(lows):
+        return quickselect(lows, k, pivot_func)
+    elif k < len(lows) + len(pivots):
+        return pivots[0]
+    else:
+        return quickselect(highs, k - len(lows) - len(pivots), pivot_func)
+
+if __name__ == '__main__':
+    m = input('Введите m: ')
+    l = [random.uniform(-1000, 1000) for i in range(2*int(m) + 1)]
+    print('Исходный список:')
+    print(l)
+    print('Поиск медианы "быстрым выбором".')
+    print(f'Медиана: {quickselect(l, len(l)/2)}')
+    print('Время нахождения медианы "быстрым выбором": ',
+          timeit(f'quickselect({l}, {len(l) / 2})',
+                 setup='from __main__ import quickselect, l',
+                 number=1000))
+    print('Поиск медианы с помощью модуля statistics.')
+    print(f'Медиана: {median(l)}')
+    print('Время нахождения медианы встроенными средствами: ',
+          timeit(f'median({l})',
+                 setup='from __main__ import median, l',
+                 number=1000))
+
+"""
+Несмотря на теоретическое превосходство "быстрого выбора" перед Timsort,
+который используется в функции statistics.median (O(n) в лучшем случае
+и O(n^2) в худшем), множественные запуски программы однозначно указывают
+на превосходство встроенных средств. Могу объяснить это только лучшей
+оптимизацией встроенных алгоритмов в python. Всё равно опыт был очень
+интересным.
+"""
