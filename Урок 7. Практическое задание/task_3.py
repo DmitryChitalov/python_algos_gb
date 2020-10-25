55f980f0d775f55494c94105433ccfe3636506dc
alex <schegoleff@gmail.com>
2020-10-25 23:49:48 +0300

    HW_7

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,41 @@
 arr[m]
 from statistics import median
 """
+
+from random import randint
+from statistics import median
+
+
+def gnome_sort(lst_obj):
+    i, size = 1, len(lst_obj)
+    while i < size:
+        if lst_obj[i - 1] <= lst_obj[i]:
+            i += 1
+        else:
+            lst_obj[i - 1], lst_obj[i] = lst_obj[i], lst_obj[i - 1]
+            if i > 1:
+                i -= 1
+    return f'Гномья: {lst_obj}, Медиана {median(lst_obj)}'
+
+
+def shell_sort(lst_obj):
+    inc = len(lst_obj) // 2
+    while inc:
+        for i, el in enumerate(lst_obj):
+            while i >= inc and lst_obj[i - inc] > el:
+                lst_obj[i] = lst_obj[i - inc]
+                i -= inc
+            lst_obj[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+    return f'Шелла: {lst_obj}, Медиана {median(lst_obj)}'
+
+
+def get_median(lst_obj):
+    return f'Человеческая: {sorted(lst_obj)}, Медиана {median(lst_obj)}'
+
+
+lst = [randint(-100, 100) for i in range(2 * int(input('Введите m для построения списка 2m + 1: ')) + 1)]
+print(f'Исходный список: {lst}')
+print(gnome_sort(lst.copy()))
+print(shell_sort(lst.copy()))
+print(get_median(lst.copy()))
