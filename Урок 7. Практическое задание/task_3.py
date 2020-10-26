678a73f22adfea360111107e9c4d8c2b5ce8dca6
mrwindmark <agentstorm@ya.ru>
2020-10-26 23:20:20 +0300

    За основу брал метод из сети https://www.cyberforum.ru/post10582447.html Дальше производил доработку по части повторяющихся значений

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,46 @@
 arr[m]
 from statistics import median
 """
+from random import randint
+
+
+test_data = [randint(0, 10) for _ in range(15)]
+print(test_data)
+
+# в такой реализации возникают проблемы с неуникальными элементами
+def med_check(data):
+    for step in range(len(data)):
+        more = less = 0
+        for step2 in range(len(data)):
+            if data[step] != data[step2]:
+                if data[step] < data[step2]:
+                    more += 1
+                else:
+                    less += 1
+        if more == less:
+            return f"Медианное значение: {data[step]}"
+        # таким костыликом решил часть проблем с повторяющимися значениями
+        # однако если повторы уходят в обе стороны - будет ошибка
+        # возможно подошёл бы метод посчёта вхождений элемента и ...
+        elif more + less + data.count(data[step]) >= len(data):
+            # я забыл как побороть "ленивые" or и and, поэтому написал всё... прямолинейно
+            # максимальный элемент не может быть медианой
+            if more != 0:
+                # как и минимальный
+                if less != 0:
+                    # если разница между кол-вом меньших и больших элементов не превышает число вхождений элемента
+                    # то данный элемент является медианным
+                    if abs(more - less) < data.count(data[step]):
+                        return f"Медианное значение = {data[step]}"
+        # elif more + data.count(data[step]) - 1 == less:
+        #     return f"Медианное значение: {data[step]}"
+        # elif less + data.count(data[step]) - 1 == more:
+        #     return f"Медианное значение: {data[step]}"
+
+        # использовалась для отлова значений
+        # print(data[step], "-", less, more)
+
+print(med_check(test_data))
+test2 = sorted(test_data)
+print(test2)
+print("Check: ", test2[len(test2)//2])
