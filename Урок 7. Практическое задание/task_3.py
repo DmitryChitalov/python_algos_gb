90a5401c1fda442afed16f82457350b9138be6d4
anatolytsi <tsirkunenkoat@gmail.com>
2020-10-28 20:57:25 +0100

    added time measurements and conclusion

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -16,6 +16,7 @@ from statistics import median
 
 from statistics import median
 from random import randint
+from timeit import timeit
 
 random_list = [randint(0, 10) for _ in range(9)]
 
@@ -32,3 +33,16 @@ print(f'Исходный массив: {random_list}')
 print(f'Медиана от встроенной функции: {median(random_list)}')
 print(f'Медиана от кастомной функции: {find_median(random_list[:])}')
 print(f'Отсортированный массив: {sorted(random_list)}')
+
+print(f'Время выполнения встроенной функции: '
+      f'{timeit(f"median({random_list})", setup="from statistics import median", number=10000)}')
+print(f'Время выполнения кастомной функции: '
+      f'{timeit(f"find_median({random_list})", setup="from __main__ import find_median", number=10000)}')
+
+"""
+Время выполнения встроенной функции: 0.009680699999999987
+Время выполнения кастомной функции: 0.06809379999999998
+
+Вывод: кастомная функция выполняется хоть и без сортировки, но все равно намного медленнее чем встроенная функция
+(в ~7 раз)
+"""
