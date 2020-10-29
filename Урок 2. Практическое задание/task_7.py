d1c92149f5916e43c2bb252b6e7f9313c5434865
Mary <ulanovamary@yandex.ru>
2020-10-29 15:26:08 +0300

    2 updated py-files
    2 updated py-files

--- Урок 2. Практическое задание/task_7.py
+++ Урок 2. Практическое задание/task_7.py
@@ -7,11 +7,16 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
 
-def count_nat(num, sum_part=0, mul_part=1):
-    if sum_part==mul_part:
-        return (f' {sum_part==mul_part}')
-    elif sum_part < mul_part:
-        return num, sum_part+1, num(num+1)/2
 
-count_nat(3);
+def count_nat(num, sum_part=0, mul_part=1):
+    if sum_part == mul_part:
+        return f' {sum_part == mul_part}'
+    elif sum_part < mul_part:
+        return count_nat(num, sum_part + 1, num*(num + 1)//2)
+
+
+print(count_nat(3));
+
+
+#def
 
