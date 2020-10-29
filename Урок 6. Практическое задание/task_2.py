60fa85cfb11715bf50e04d5903ff7977be248d9a
anrii <andreydatsenko777@gmail.com>
2020-10-30 00:58:12 +0100

    completed 1 and 2 tasks

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -5,3 +5,84 @@
 
 Например, один из вариантов, использование генераторов
 """
+import sys
+import copy
+from memory_profiler import profile
+
+
+class HelloWorld:
+	def __init__(self):
+		self.hello = "Hello World!!"
+
+	def __call__(self):
+		print(self.hello)
+
+
+def main():
+	hi = HelloWorld()
+	hi()
+
+	if __name__ == "__main__":
+		main()
+
+
+el = input("Введите елемент: ")
+
+if el.isdigit():
+	el = int(el)
+
+print(f"количество ссылов на елемент '{el}'(тип елемента: '{type(el)}'): ", sys.getrefcount(el))
+print("1 = Присваивание переменной \n"
+      "2 = функция подсчета сылок")
+print('size: ', sys.getsizeof(el))
+
+
+@profile
+def func_1():
+	x = list(range(100000))
+	y = copy.deepcopy(x)
+	return y
+
+
+func_1()
+
+print('#\n' * 3)
+
+
+@profile
+def func_2():
+	x = list(range(100000))
+	print('size: ', sys.getrefcount(x))
+	y = copy.deepcopy(x)
+	print('size: ', sys.getrefcount(y))
+	del x
+	y = None
+	return y
+
+
+func_2()
+
+
+class Point:
+	def __init__(self, x=0, y=0, lst=[]):
+		self.x = x
+		self.y = y
+		self.lst = list(range(100000))
+
+	def __del__(self):
+		class_name = self.__class__.__name__
+		print(f'{class_name} kill')
+
+
+@profile
+def func():
+	pt1 = Point()
+	pt2 = pt1
+	pt3 = pt1
+	print(id(pt1), id(pt2), id(pt3))
+
+	del pt1
+	del pt2
+	del pt3
+
+func()
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,74 @@
 arr[m]
 from statistics import median
 """
+from random import randint
+from timeit import timeit, Timer
+from statistics import median
+
+
+def gnome(data):
+	i, size = 1, len(data)
+	while i < size:
+		if data[i - 1] <= data[i]:
+			i += 1
+		else:
+			data[i - 1], data[i] = data[i], data[i - 1]
+			if i > 1:
+				i -= 1
+	return data
+
+
+arr1 = [randint(0, 1000) for _ in range(100)]
+print(arr1)
+arr1 = gnome(arr1)
+print('### :', arr1)
+
+print('функция гномьей сортировки 1 варриант: ', timeit('gnome(arr1[:])',
+                                                        'from __main__ import gnome, arr1',
+                                                        number=100000), 'seconds')
+
+def with_sort(lst_obj):
+	temp = lst_obj
+	left_list = []
+	right_list = []
+
+	for i in range(len(temp)):
+
+		for j in range(len(temp)):
+			if temp[i] > temp[j]:
+				left_list.append(temp[j])
+			if temp[i] < temp[j]:
+				right_list.append(temp[j])
+			if temp[i] == temp[j] and i > j:
+				left_list.append(temp[j])
+			if temp[i] == temp[j] and i < j:
+				right_list.append(temp[j])
+		if len(left_list) == len(right_list):
+			return temp[i]
+
+	left_list.clear()
+	right_list.clear()
+
+
+arr1 = [randint(0, 100) for _ in range(10)]
+print(arr1)
+print('медиана списка: ', median(arr1))
+arr1 = with_sort(arr1)
+print('Медиана: ', arr1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
