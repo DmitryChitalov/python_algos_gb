8b5f94ce5710d28bf19dba9d11bb02a70c8a2a27
nat0t <nataniel1987@gmail.com>
2020-10-30 04:10:54 +0300

    Added task1

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,26 @@
 ВНИМАНИЕ: примеры заданий будут размещены в последний день сдачи.
 Но постарайтесь обойтись без них.
 """
+from heapq import heappush, heappop, heapify
+from collections import Counter
+
+def huffman_code(string):
+    heap = [[weight, [symbol, '']] for symbol, weight in string.items()]
+    heapify(heap)
+    while len(heap) > 1:
+        left = heappop(heap)
+        right = heappop(heap)
+        for couple in left[1:]:
+            couple[1] += '0'
+        for couple in right[1:]:
+            couple[1] += '1'
+        heappush(heap, [left[0] + right[0]] + left[1:] + right[1:])
+    return sorted(heappop(heap)[1:], key=lambda x: (len(x[-1]), x))
+
+text = "Пробная строка для кодирования."
+print(text)
+string = Counter(text)
+code_table = huffman_code(string)
+print(f'Символ  Вес  Код Хаффмана')
+for el in code_table:
+    print(f'{el[0]:^6}  {string[el[0]]:^3}  {el[1]:^12}')
