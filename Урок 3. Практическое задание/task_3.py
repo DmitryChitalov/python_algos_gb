38d4c2df14bb6df5ec7a21f208f7601e734ab0ac
Oleg Upovalov <2987026@mail.ru>
2020-10-30 07:09:03 +0300

    Урок 3. Задания 1-4

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,19 @@
 р
 а
 """
+import hashlib
+
+substring_set = set()
+string = input('Введите строку из строчных латинских букв: ')
+str_len = len(string)
+for i in range(str_len):
+    for j in range(str_len):
+        if j >= i:
+            substr = string[i:j + 1]
+            if substr != string:
+                hash_substr = hashlib.md5(substr.encode('utf-8')).hexdigest()
+                if hash_substr not in substring_set:
+                    print(substr)
+                substring_set.add(hash_substr)
+
+print(f'\nКоличество подстрок - {len(substring_set)}')
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,24 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+import os, hashlib, urllib3
+cache_table = {}
+def cache_check(url):
+
+    content = ''
+    hasher = hashlib.new('md5')
+    http = urllib3.PoolManager()
+    req = http.request('GET', url)
+    with open('cache', 'wb+') as f:
+        f.write(req.data)
+        content = f.read()
+        hasher.update((str(content) + url).encode('utf-8'))
+        if hasher.hexdigest() not in cache_table:
+            cache_table[url] = hasher.hexdigest()
+            # ну и здесь тогда можно добавить, например сохранения кэшированного файла в папку кэша
+        os.remove('cache')
+
+cache_check('http://rostovmeteo.ru/index.php')
+cache_check('https://161.ru/index.php')
+cache_check('http://rostovmeteo.ru/index.php')
+print(cache_table)
