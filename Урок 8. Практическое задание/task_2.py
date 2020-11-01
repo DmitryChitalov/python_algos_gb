e5c4d083bc2bbfa8d7aaf0efbc860967764e2b75
tryVellum <amigodrigo@gmail.com>
2020-11-01 21:40:44 +0300

    Lesson 8: task 2 done

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,70 +10,87 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных.
 """
 
+
 class BinaryTree:
-    def __init__(self, root_obj):
-        # корень
-        self.root = root_obj
-        # левый потомок
-        self.left_child = None
-        # правый потомок
-        self.right_child = None
+    def __init__(self, root, left=None, right=None, level=1):
+        self.root = root
+        self.left = left
+        self.right = right
+        self.level = level
 
-    # добавить левого потомка
-    def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
+    def add_to_left_new_branch(self):
+        """
+        Делаем из левой ветки корень, инициируем класс. Левое значение становится корнем для нового дерева
+        """
+        self.left = BinaryTree(self.left, level=self.level+1)
+
+    def add_to_right_new_branch(self):
+        """
+        Делаем из правой ветки корень, инициируем класс. Правое значение становится корнем для нового дерева
+        """
+        self.right = BinaryTree(self.right, level=self.level+1)
+
+    def add_left_value(self, left):
+        """
+        Изменяем левое значение, даже если "лево" это "дерево", то меняем корень
+        """
+        if isinstance(self.left, BinaryTree):
+            self.left.root = left
+            print(f'На уровне {self.level} изменено левое значение на ', left)
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+            self.left = left
+            print(f'На уровне {self.level} изменено левое значение на ', left)
 
-    # добавить правого потомка
-    def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
+    def add_right_value(self, right):
+        """
+        Изменяем правое значение, даже если "право" это "дерево", то меняем корень
+        """
+        if isinstance(self.right, BinaryTree):
+            self.right.root = right
+            print(f'На уровне {self.level} изменено правое значение на ', right)
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+            self.right = right
+            print(f'На уровне {self.level} изменено правое значение на ', right)
 
-    # метод доступа к правому потомку
-    def get_right_child(self):
-        return self.right_child
+    def add_value_way(self, value, way):
+        """
+        С помощью бинарного пути добавляем заначени на определённое место
+        """
+        if len(way) > 1:
+            if int(way[0]):
+                try:
+                    self.right.add_value_way(value, way[1:])
+                except AttributeError:  # отлавливаем ошибку, если путь длинее дерева
+                    self.add_to_right_new_branch()
+                    self.right.add_value_way(value, way[1:])
+            else:
+                try:
+                    self.left.add_value_way(value, way[1:])
+                except AttributeError:  # отлавливаем ошибку, если путь длинее дерева
+                    self.add_to_left_new_branch()
+                    self.left.add_value_way(value, way[1:])
+        else:
+            if int(way):
+                self.add_right_value(value)
+            else:
+                self.add_left_value(value)
 
-    # метод доступа к левому потомку
-    def get_left_child(self):
-        return self.left_child
-
-    # метод установки корня
-    def set_root_val(self, obj):
-        self.root = obj
-
-    # метод доступа к корню
-    def get_root_val(self):
-        return self.root
+    def __str__(self):
+        return f'level-{self.level}\n (root-{self.root} left-{self.left}|right-{self.right})'
 
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(4)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+if __name__ == '__main__':
+    tree = BinaryTree(8)
+    tree.add_left_value(6)
+    tree.add_right_value(11)
+    tree.add_to_right_new_branch()
+    tree.add_to_left_new_branch()
+    tree.add_value_way(5, '00')
+    tree.add_value_way(7, '01')
+    tree.add_value_way(9, '10')
+    tree.add_value_way(12, '11')
+    tree.add_right_value(10)    # заменяем значение заданное на 85 строчке
+    tree.add_value_way(11, '110')
+    tree.add_value_way(13, '111')
+    tree.add_value_way(55, '0101')
+    print(tree)
