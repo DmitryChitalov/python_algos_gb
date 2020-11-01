a3dcafa47341365d17e6907b02272dc1df86b951
pkrivtsov <krivtsov@live.com>
2020-11-02 02:13:45 +0300

    Поправил ПЗ ко второму уроку, в соответствии с PEP-8 и выполнил ПЗ к третьему

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -6,5 +6,26 @@
 есть ли в кэше соответствующая страница, если нет, то вносит ее в кэш
 
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
-Можете условжнить задачу, реализовав ее через ООП
+Можете усложнить задачу, реализовав ее через ООП
 """
+import hashlib
+from uuid import uuid4
+
+
+class Cache:
+    def __init__(self):
+        self.cache_obj = {}
+
+    def get_page(self, url):
+        salt = uuid4().hex
+        if self.cache_obj.get(url):
+            return f'Данный адрес: {url} присутствует в кэше'
+        else:
+            res = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+            self.cache_obj[url] = res
+            return self.cache_obj
+
+
+c = Cache()
+c.get_page('https://geekbrains.ru/')
+print(c.get_page('https://geekbrains.ru/'))
