aab09c8849f49f6b940d1ec46b42faabd30720fe
Pavel <mirprost@gmail.com>
2020-11-03 21:27:54 +0300

    Урок 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,17 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_my(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+nums_data = [i for i in range(1, 100)]
+
+print(timeit(f"func_1({nums_data})", setup="from __main__ import func_1", number=1000))
+print(timeit(f"func_my({nums_data})", setup="from __main__ import func_my", number=1000))
+
+"""
+В новой функции использовал генератор списка, получил небольшой прирост. 
+"""
--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -13,6 +13,8 @@
 Опишите результаты, сделайте выводы, где и какой алгоритм эффективнее
 Подумайте и по возможности определите сложность каждого алгоритма
 """
+import cProfile
+from timeit import timeit
 
 
 def simple(i):
@@ -35,5 +37,41 @@ def simple(i):
     return n
 
 
+def gen_simple():
+    """Решение через генератор с применением решета"""
+    d = {}
+    q = 2
+    while True:
+        if q not in d:
+            yield q
+            d[q * q] = [q]
+        else:
+            for p in d[q]:
+                d.setdefault(p + q, []).append(p)
+            del d[q]
+        q += 1
+
+
+def eratosfen(i):
+    gen = gen_simple()
+    num = 1
+    for _ in range(0, i):
+        num = gen.__next__()
+    return num
+
+
 i = int(input('Введите порядковый номер искомого простого числа: '))
 print(simple(i))
+print(eratosfen(i))
+
+cProfile.run('simple')
+cProfile.run('eratosfen')
+
+print(timeit("simple", setup="from __main__ import simple", number=10000))
+print(timeit("eratosfen", setup="from __main__ import eratosfen", number=10000))
+
+"""
+Решение через генератор работает быстрее при большом количестве итераций.
+simple(i) - квадратичная сложность O(n**2)
+eratosfen(i) - линейно-логарифмическая сложность O(n log n) 
+"""
