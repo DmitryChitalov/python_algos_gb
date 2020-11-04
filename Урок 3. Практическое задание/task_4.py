97f7fe5d846dff1003ebf0483b44e521baf8f699
evgeniy_prokofyev <prokofyev.evg@yandex.ru>
2020-11-04 14:52:42 +0300

    Lesson3

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,23 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+import hashlib
+from uuid import uuid4
+
+salt = uuid4().hex
+pages_cache = set()
+
+def page_caching(url):
+    hash_url = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+    if hash_url in pages_cache:
+        print(f'Адрес {url} присутствует в кэше')
+    else:
+        pages_cache.add(hash_url)
+        print(f'Адрес {url} добавлен в кэш')
+
+page_caching("http://yandex.ru")
+page_caching("http://vk.ru")
+page_caching("http://yandex.ru")
+page_caching("http://google.com")
+page_caching("http://google.com")
+page_caching("http://vk.com")
