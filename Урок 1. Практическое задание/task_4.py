d64af86345a1623e3d0f6ecf81a57bc7d20eb5c9
Vladimir_Novikov <vovasnew@mail.ru>
2020-11-04 21:27:59 +0500

    Добрый день!
    Сделал все задания

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -27,3 +27,57 @@
 Для реализации хранилища можно применить любой подход,
 который вы придумаете, например, реализовать словарь.
 """
+
+user_log = {'login': ['Nik', 'John'], 'password': ['123', '444'], 'is_active': [False, True]}
+
+"""T(n) = 6n + 7
+Общая сложность O(n)"""
+
+
+def validation():
+    log_in = input('Введите логин: ').capitalize()  # O(1)
+    if log_in in user_log['login']:  # O(n)
+        password = input(f'Пользователь {log_in} введите пароль: ')  # O(1)
+        login_id = user_log['login'].index(log_in)  # получаем индекс элемента (логина для сравнения с паролем) # O(1)
+        if password == user_log['password'][login_id]:  # O(n)
+            if user_log['is_active'][login_id]:  # O(n)
+                print('Добро пожаловать!')  # O(1)
+            else:  # O(n)
+                print('Ваша учетная запись не активирована\nПожалуйста, пройдите валидацию')  # O(1)
+        else:  # O(n)
+            print('*** Пароль не подходит ***')  # O(1)
+    else:  # O(n)
+        print('*** Такой пользователь не зарегистрирован ***')  # O(1)
+
+
+validation()
+print()
+
+"""T(n) = 5n + 12
+Общая сложность O(n)"""
+"""В первом решении если логин верный, то получаем индекс логина в словаре, и запрашиваем пароль 
+Во втором случае получаем индексы логина и пароля, и уже сравниваем эти цифры между собой
+Считаю первое решение более эффективным, т.к. если логина нет в словаре, то и искать дальше нет смысла и требуется 
+ на 5 констант меньше"""
+
+def validation_2():
+    log_in = input('Введите логин и пароль через пробел: ').split()  # O(1)
+    if len(log_in) < 2:  # O(n)
+        print('Вы ввели не все данные')  # O(1)
+    else:  # O(n)
+        user_name = log_in[0].capitalize()  # O(1)
+        password = log_in[1]  # O(1)
+        try:  # O(1)
+            login_id = user_log['login'].index(user_name)  # O(1)
+            password_id = user_log['password'].index(password)  # O(1)
+            if (login_id == password_id) and user_log['is_active'].index(login_id):  # O(n)
+                print('Добро пожаловать!')  # O(1)
+            elif (login_id == password_id) and not user_log['is_active'].index(login_id):  # O(n)
+                print('Ваша учетная запись не активирована')  # O(1)
+            else:  # O(n)
+                print('Логин или пароль указаны не верно')  # O(1)
+        except ValueError:  # O(1)
+            print('Логин или пароль указаны не верно')  # O(1)
+
+
+validation_2()
--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,57 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+"""К имеющемуся коду добавил ограничение высоты стопки тарелок и добавление новых списков - стопок
+Добавил методы для просмотра общего количества стопок и просмотра всех стопок"""
+
+
+class StackClass:
+    def __init__(self):
+        self.stack = 0
+        self.elems = [[]]
+
+    def is_empty(self):
+        return self.elems[self.stack] == []
+
+    def push_in(self, el):
+        if len(self.elems[self.stack]) < 2:  # указываем высоты стопки тарелок
+            self.elems[self.stack].append(el)
+        else:
+            self.stack += 1
+            self.elems.append([])
+            self.elems[self.stack].append(el)
+
+    def pop_out(self):
+        return self.elems[self.stack].pop()
+
+    def get_val(self):
+        return self.elems[len(self.elems) - 1]
+
+    def stack_size(self):
+        return len(self.elems[self.stack])
+
+    def stack_quantity(self):
+        return len(self.elems)
+
+    def view_stacks(self):
+        return self.elems
+
+
+SC_OBJ = StackClass()
+print(SC_OBJ.is_empty())  # -> стек пустой
+SC_OBJ.push_in(10)
+SC_OBJ.push_in('code')
+SC_OBJ.push_in(False)
+SC_OBJ.push_in(5.5)
+print(SC_OBJ.get_val())
+print(SC_OBJ.stack_size())
+print(SC_OBJ.is_empty())
+SC_OBJ.push_in(4.4)
+print(SC_OBJ.pop_out())
+print(SC_OBJ.stack_size())
+SC_OBJ.push_in(True)
+SC_OBJ.push_in(45)
+print(SC_OBJ.stack_size())
+print(SC_OBJ.stack_quantity())
+print(SC_OBJ.view_stacks())
