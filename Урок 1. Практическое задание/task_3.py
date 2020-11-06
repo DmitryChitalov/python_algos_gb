3510ca771e6b713813d4b51d5358ab0942b18ae8
avsuv <suverin.av@gmail.com>
2020-11-06 18:01:14 +0300

    3

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -22,3 +22,26 @@
 Реализуйте поиск трех компаний с наибольшей годовой прибылью.
 Выведите результат.
 """
+# Первый вариант предпочтительнее, т.к. сложность меньше
+# Первый вариант: O(n)
+def search_1(info):
+    names = list(info.keys())       # O(n) + O(1) = O(n)
+    profits = list(info.values())   # O(n) + O(1) = O(n)
+    max_profit_companies = {}       # O(1)
+    while len(max_profit_companies) < 3:    # O(1) - цикл всегда делает три прохода
+        el = profits.index(max(profits))    # O(n)
+        max_profit_companies[names[el]] = profits[el]   # O(1)
+        names.pop(el), profits.pop(el)  # 2 * O(n)
+    return max_profit_companies
+
+# Второй вариант: O(N log N)
+def search_2(info):
+    new_lst = list(info.items())   # O(N) + O(1)
+    new_lst.sort(key=lambda x: x[1]) # O(N logN)
+    return new_lst[:3:-1] # O(N)
+
+company_info = {'Рога': 500000, 'Копыта': 345000, 'Рога и Копыта': 800000, 'ПАО Газпром': 5000001,
+    'ООО Ритейл': 1300000, 'ИП Заваркин': 180000, 'ПАО Лукойл': 5000002}
+
+print(search_1(company_info))
+print(search_2(company_info))
