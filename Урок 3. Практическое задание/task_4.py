505224af7c0f62a06f28173932d3290c935dd58a
maffiKo <konyakova.marya@gmail.com>
2020-11-09 15:22:44 +0300

    finished all ex's

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,21 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+import hashlib
+from uuid import uuid4
+
+salt = uuid4().hex
+kesh = {}
+
+
+def check_site(url):
+    if kesh.get(url):
+        print('Этот адрес есть в кэше')
+    else:
+        hash_site = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+        kesh[url] = hash_site
+        print(f'Сайт {url} добавлен в кэш')
+
+
+check_site('https://geekbrains.ru/lessons/94281')
+check_site('https://geekbrains.ru/lessons/94281')
