a82e5c227606fc0a9ccfd7eb415c0f269be0d0dc
Sharp_Mind <redlance988@gmail.com>
2020-11-09 21:06:38 +0300

    Homework 3 completed

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -10,3 +10,98 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+from time import time
+from random import randint
+
+dict_1, list_1 = {}, []
+
+
+def time_count(func):
+    def wrapper(*args, **kwargs):
+        start_time = time()
+        out = func(*args, **kwargs)
+        end_time = time()
+        print(f'Время выполнения: {end_time - start_time} секунд.')
+        return out
+    return wrapper
+
+
+@time_count
+def dict_filling(dict_obj):
+    for i in range(3000):
+        dict_obj[f'Element_{i}'] = randint(1, 100)
+    print(dict_1)
+
+
+@time_count
+def dict_out(dict_obj):
+    for k in dict_obj.keys():
+        print(f'{k}: {dict_obj[k]}.', end=' ')
+    print()
+
+
+@time_count
+def dict_search_by_val(dict_obj, val):
+    found = False
+    for k in dict_obj.keys():
+        if dict_obj[k] == val:
+            print(f'Значение {val} найдено по ключу {k}.')
+            found = True
+    if not found:
+        print('Искомое значение не найдено.')
+
+
+@time_count
+def list_filling(list_obj):
+    for i in range(3000):
+        list_obj.append(randint(1, 100))
+    print(list_1)
+
+
+@time_count
+def list_out(list_obj):
+    for i in list_obj:
+        print(i, end=' ')
+    print()
+
+
+@time_count
+def list_search(list_obj, val):
+    found = False
+    for i in range(len(list_obj)):
+        if list_obj[i] == val:
+            print(f'Значение {val} найдено на позиции {i} в списке.')
+            found = True
+    if not found:
+        print('Искомое значение не найдено.')
+
+
+# Словари:
+print(f'Заполнение словаря:')
+dict_filling(dict_1)
+
+print(f'\nВывод из словаря:')
+dict_out(dict_1)
+
+print(f'\nПоиск значения "9" по словарю:')
+dict_search_by_val(dict_1, 9)
+
+print()
+
+# Списки:
+print(f'Заполнение списка:')
+list_filling(list_1)
+
+print(f'\nВывод из списка:')
+list_out(list_1)
+
+print(f'\nПоиск значения "9" по списку:')
+list_search(list_1, 9)
+
+print()
+
+'''
+Наблюдения и выводы:
+Операции по заполнению и выводу элементов со словарями занимают больше времени, чем со списками.
+Операция поиска занимает примерно одинаковое количество времени.
+'''
--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,21 @@
 р
 а
 """
+from hashlib import sha256
+
+input_string = input('Введите строку: ')
+substring_hash_list = []
+
+
+def rec_substring(c=0, h=1):
+    if c < len(input_string):
+        for i in range((len(input_string))):
+            if len(input_string[i:h + i]) == c + 1:
+                substring_hash_list.append(sha256(input_string[i:h + i].encode("utf-8")).hexdigest())
+        return rec_substring(c + 1, h + 1)
+    else:
+        return
+
+
+rec_substring()
+print(f'Количество уникальных подстрок в строке: {len(set(substring_hash_list))} варианта(ов).')
