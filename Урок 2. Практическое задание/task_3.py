93eb80b656ad3bdac4685ddc75df9ec66234ce67
Николай Гаркушенко <nekdfl@gmail.com>
2020-11-10 20:58:14 +0300

    lesson 2 task3

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -15,3 +15,37 @@
 Введите число, которое требуется перевернуть: 123
 Перевернутое число: 321
 """
+
+
+def reverse_number(number, postion):
+    pass
+    res = ""
+    postion -= 1
+    if postion < 0:
+        return res
+
+    res = number[postion]
+    res += reverse_number(number, postion)
+
+    return res
+
+
+def input_number(message):
+    pass
+    res = input(message).strip()
+    if not res.isdigit():
+        print("Недопустимый ввод!")
+        res = input_number()
+
+    return res
+
+
+def main():
+    pass
+    number = input_number("Введите число, которое требуется перевернуть: ")
+    res = reverse_number(number, len(number)).strip()
+    print(f"Перевернутое число: {res}")
+
+
+if __name__ == "__main__":
+    main()
