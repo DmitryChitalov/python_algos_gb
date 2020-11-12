8b12836f9c15a89e758a82de8879689d1dc7c33b
avsuv <suverin.av@gmail.com>
2020-11-12 12:49:35 +0300

    4

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,24 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+import hashlib
+
+
+def link_to_sha256(lnk):
+    salt = lnk + lnk[::-1]
+    return hashlib.sha256(lnk.encode() + salt.encode()).hexdigest()
+
+
+links = {}
+k = True
+while k:
+    link = input('Введите ссылку. Для выхода введите пустую строку: ')
+    if link == '':
+        for key in links.keys():
+            print(f'{key} - {links[key]}')
+        k = False
+    else:
+        if link_to_sha256(link) not in links:
+            links[link_to_sha256(link)] = link
+        else:
+            print('Такая ссылка уже существует в списке.')
