fd6812d4765667b7a8972c423312794c701fb600
Svetlana <svetlana77ekb@yandex.ry>
2020-11-13 00:34:31 +0500

    Задание сделано.

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -10,3 +10,70 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+from random import randint, random
+import time
+
+# 1 вариант: работает, но без декоратора
+def filling_list(n, my_list):
+    if n == 1:
+        return my_list
+    else:
+        my_list.append(randint(0, 100))
+        return filling_list(n - 1, my_list)
+
+def check_1():
+    start_val = time.time()
+    print(filling_list(500, []))
+    end_val = time.time()
+    return f'Операция заняла {round((end_val - start_val), 10)} сек.'
+
+
+def filling_dict(n, my_dict):
+    if n == 1:
+        return my_dict
+    else:
+        my_dict[round(random(), 2)] = randint(0, 100)
+        return filling_dict(n - 1, my_dict)
+
+def check_2():
+    start_val = time.time()
+    print(filling_dict(500, {}))
+    end_val = time.time()
+    return f'Операция заняла {round((end_val - start_val), 10)} сек.'
+
+print(check_1())
+print(check_2())
+# Выводы: словарь выполняется немного дольше.
+
+# вариант 2 с декоратором, не работает, не могу понять ошибки
+
+# def check(func):
+#     def wrapper(*args, **kwargs):
+#         start_val = time.time()
+#
+#         func(*args, **kwargs)
+#
+#         end_val = time.time()
+#         print(f'Операция заняла {end_val - start_val} сек.')
+#     return wrapper
+#
+# @check
+# def filling_list(n, my_list):
+#     if n == 1:
+#         return my_list
+#     else:
+#         my_list.append(randint(0, 100))
+#         return filling_list(n - 1, my_list)
+#
+# @check
+# def filling_dict(n, my_dict):
+#     if n == 1:
+#         return my_dict
+#     else:
+#         my_dict[round(random(), 2)] = randint(0, 100)
+#         return filling_dict(n - 1, my_dict)
+#
+# filling_list(50, [])
+# filling_dict(50, [])
+
+
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,21 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+import hashlib
+
+def caching_web_pages(wp, cash):
+    salt = 'salt'
+    res_wp = hashlib.sha256(salt.encode() + (wp).encode()).hexdigest()
+    if res_wp in cash.values():
+        return f'Страница {wp} уже есть в кэше'
+    else:
+        cash[wp] = res_wp
+        return cash
+wp = 'https://geekbrains.ru/'
+cash = {'https://intuit.ru/': '8bbd3c9b62d191b0a3170bf8acfa08500038d9af5c12ec40e48076bd70478daa',
+        'https://geekbrains.ru/': 'b38e009334a29a3c0fbce7cff86c4d63669f894dcc6fd30f0d06f440a04db864',
+        'https://pythonworld.ru/': '47b9b2984d3bd6806ef905c4bae48a21779ef43e9daea4896ffd9132293e8fbe'}
+print(caching_web_pages(wp, cash))
+
+
+
