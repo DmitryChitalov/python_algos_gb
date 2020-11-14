c9606e296e54dbe4ad099ea28039803cb3bbb35c
Николай Гаркушенко <nekdfl@gmail.com>
2020-11-14 15:32:50 +0300

    lesson 3 task2

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -15,3 +15,51 @@
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
+
+import hashlib
+import uuid
+
+
+
+def get_hash(passwd_obj, hash_salt):
+    pass_and_solt = hash_salt.encode("utf-8") + passwd_obj.encode("utf-8")
+    pass_hash = hashlib.sha256(pass_and_solt)
+    pass_hash_hex = pass_hash.hexdigest()
+    return pass_hash_hex
+
+
+def check_hash(old_hash, new_hash):
+    return old_hash == new_hash
+
+
+def do_ask_password(message):
+    pass
+    res = input(message)
+    return res
+
+
+def main():
+    pass
+    try:
+        hash_salt = uuid.uuid4().hex
+        password = do_ask_password("Введите пароль: ")
+        hashed_pass = get_hash(password, hash_salt)
+
+        retype_pass = do_ask_password("Введите пароль еще раз: ")
+        hashed_newpass = get_hash(retype_pass, hash_salt)
+
+        if check_hash(hashed_pass, hashed_newpass):
+            print("Пароли совпали.")
+
+        else:
+            print("Пароли не совпали!")
+        print(f"Первый пароль в виде хэша: {hashed_pass}")
+        print(f"Второй пароль в виде хэша: {hashed_newpass}")
+
+        print("\nПрограмма завершена!")
+    except Exception as ex:
+        print(f"Fatal error: {ex}")
+
+
+if __name__ == "__main__":
+    main()
