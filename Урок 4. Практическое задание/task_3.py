90887df2103e15c42548455c7851a79a9a4e5704
Vladimir_Novikov <vovasnew@mail.ru>
2020-11-14 21:55:48 +0500

    Добрый день! Сделал 4 из 5 задач

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -9,6 +9,17 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+"""Из трех реализаций самая эффективная - slice - реверс на месте, т.к. имеет константую сложность O(1)
+У цикла сложность O(n) и он на втором месте
+И самое долгое - рекурсия
+В cProfile цифры все 0, т.к. задача простая, но в рекурсии видно, что кол-во вызовов функции 
+ncalls - меняется в зависимости от длины числа, переданного функции."""
+
+from timeit import timeit
+from random import randint
+import cProfile
+
+num_1000 = randint(1000000, 10000000)
 
 
 def revers(enter_num, revers_num=0):
@@ -34,3 +45,16 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+functions = ["revers(num_1000)", "revers_2(num_1000)", "revers_3(num_1000)"]
+setup_data = "from __main__ import revers, revers_2, revers_3, num_1000"
+
+for fn in functions:
+    print(f'Время выполнения функции {fn}',
+          timeit(fn, setup=setup_data, number=10000))
+
+print()
+
+cProfile.run('revers(num_1000)')
+cProfile.run('revers_2(num_1000)')
+cProfile.run('revers_3(num_1000)')
