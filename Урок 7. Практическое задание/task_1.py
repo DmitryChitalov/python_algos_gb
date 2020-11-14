0385584d403bff561192e5c06f15078d07855fca
Oleg Upovalov <2987026@mail.ru>
2020-11-14 07:24:01 +0300

    Урок 7. Задания 1-3

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -12,3 +12,49 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+import timeit
+import random
+
+
+def bubble_sort(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i] < lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+def bubble_sort_optim(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj) - n):
+            flag = 0
+            if lst_obj[i] < lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+                flag = 1
+        if flag == 1:
+            n += 1
+        else:
+            return lst_obj
+
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+print(orig_list)
+print(bubble_sort(orig_list[:]))
+print(bubble_sort_optim(orig_list[:]))
+
+# замеры
+print(timeit.repeat("bubble_sort(orig_list[:])",
+                    setup="from __main__ import bubble_sort, orig_list", repeat=5, number=1000))
+print(timeit.repeat("bubble_sort_optim(orig_list[:])",
+                    setup="from __main__ import bubble_sort_optim, orig_list", repeat=5, number=1000))
+
+"""
+Один из результатов замеров:
+[0.009837838995736092, 0.014766625012271106, 0.015082622005138546, 0.014815785019891337, 0.013687147991731763]
+[0.003437837993260473, 0.0032412879809271544, 0.0033189249807037413, 0.003383176022907719, 0.00317583599826321]
+Замеры показывают что скорость сортировки в отдельных случаях увеличилась
+в четыре раза. А иногда прирост не наблюдается вовсе. Результат зависит от исходного списка.
+"""
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,57 @@
 arr[m]
 from statistics import median
 """
+import random
+from statistics import median
+
+
+def gnome_sort(lst):
+    i = 1
+    while i < len(lst):
+        if (lst[i - 1] <= lst[i]):
+            i += 1
+        else:
+            lst[i - 1], lst[i] = lst[i], lst[i - 1]
+            if i > 1:
+                i -= 1
+    return lst
+
+
+"""
+попробовал сделать без сортировки, но алгоритм периодически дает сбой на одну позицию, не могу
+найти причину, точнее подозреваю что некорректно брать в качестве стартовой точки (переменная
+temp_med) среднее значение ряда, в некоторых случаях разбиение на левый и правый список происходит
+некорректно
+"""
+
+
+def lst_median(lst):
+    temp_med = sum(lst) / len(lst)
+    left = []
+    right = []
+    for el in lst:
+        if el <= temp_med:
+            left.append(el)
+        else:
+            right.append(el)
+    if (len(left) - len(right)) > 1:
+        temp_left = left[-1:-(len(left) - len(right))]
+        right.extend(temp_left)
+        return max(left)
+    elif (len(right) - len(left)) > 1:
+        temp_right = right[:(len(right) - len(left))]
+        left.extend(temp_right)
+        return min(right)
+    elif (len(left) - len(right)) == 1:
+        return max(left)
+    elif (len(right) - len(left)) == 1:
+        return min(right)
+
+
+m = int(input('Введите натруальное число: '))
+orig_list = [random.randint(-100, 100) for _ in range(2 * m + 1)]
+print(f'Полученный массив размером 2m+1 - {orig_list}')
+print(f'Отсортированный массив {gnome_sort(orig_list[:])}')
+print(f'Медиана массива, полученная методом сортировки - {gnome_sort(orig_list[:])[m]}')
+print(f'Медиана массива - {lst_median(orig_list)}')
+print(f'Проверка - {median(orig_list)}')
