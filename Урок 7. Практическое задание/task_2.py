8d22cb4fc419b33ce2e77a42dfa19338eac2fa37
Ivan <silencer1588@yandex.ru>
2020-11-14 06:16:46 +0300

    lesson 7 done

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,45 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+import random
+
+def merge_sort(oList, start, end):
+    if end - start > 1:
+        mid = (start + end)//2
+        merge_sort(oList, start, mid)
+        merge_sort(oList, mid, end)
+        merge_list(oList, start, mid, end)
+    return oList
+
+def merge_list(oList, start, mid, end):
+    left = oList[start:mid]
+    right = oList[mid:end]
+    k = start
+    i = 0
+    j = 0
+    while (start + i < mid and mid + j < end):
+        if (left[i] <= right[j]):
+            oList[k] = left[i]
+            i = i + 1
+        else:
+            oList[k] = right[j]
+            j = j + 1
+        k = k + 1
+    if start + i < mid:
+        while k < end:
+            oList[k] = left[i]
+            i = i + 1
+            k = k + 1
+    else:
+        while k < end:
+            oList[k] = right[j]
+            j = j + 1
+            k = k + 1
+
+def gogo():
+    original_lst = [random.random() + random.randint(0, 100) for _ in range(int(input('Кол-во эл-ов: ')))]
+    print(f'Отсортированный - {merge_sort(original_lst[:], 0, len(original_lst))}')
+    print(f'исходный - {original_lst}')
+
+gogo()
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,57 @@
 arr[m]
 from statistics import median
 """
+
+import numpy, random, timeit
+
+original_lst = [random.randint(0,9) for _ in range(2 * random.randint(2, 9) + 1)]
+print(original_lst)
+
+def shell_(oList):
+    inc = len(oList) // 2
+    while inc:
+        for i, el in enumerate(oList):
+            while i >= inc and oList[i - inc] > el:
+                oList[i] = oList[i - inc]
+                i -= inc
+            oList[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+    return oList
+
+print('shell time: ', timeit.timeit('shell_(original_lst[:])', \
+                   setup='from __main__ import shell_, original_lst', \
+                    number=100))
+
+print(f'median = {numpy.median(original_lst)}')
+print(f'my median = {shell_(original_lst[:])[len(original_lst) // 2]}')
+
+# без сортировки
+def no_sort(oLst, n=0):
+    left = []
+    right = []
+    tmp = oLst[:]
+    a = tmp.pop(n)
+    for i in range(len(tmp)):
+        if a > tmp[i]:
+            left.append(tmp[i])
+        else:
+            right.append(tmp[i])
+    if len(left) != len(right):
+        no_sort(oLst, n=n+1)
+    else:
+        return a
+
+print('no sort time: ', timeit.timeit('no_sort(original_lst[:])', \
+                   setup='from __main__ import no_sort, original_lst', \
+                    number=100))
+
+print(f'no sort median = {no_sort(original_lst)}')
+
+# не всегда корректно работает no_sort(), не пойму...
+# и я не понимаю, почему return a возвращает "None"?
+# если вместо return делать print(a), то всё нормально,
+# но я не хочу писать print, иначе в замере он вернёт его 100 раз
+
+# По замерам:
+# чаще всего Шелл лучше, примерно, в 10 раз.
+# Но бывают случаи, когда это не так. Мне пока не удалось выяснить закономерность.
