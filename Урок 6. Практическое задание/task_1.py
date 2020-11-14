2f4cf2c71c7b7899ed6cbf76ab8cba5ad4453f4f
maffiKo <konyakova.marya@gmail.com>
2020-11-14 17:09:33 +0300

    finished all ex's

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -13,3 +13,65 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+import collections
+from memory_profiler import profile
+
+
+@profile
+def calc():
+    companies_count = int(input("Введите количество предприятий: "))
+    companies = collections.defaultdict()
+    for i in range(companies_count):
+        name = input(f"Введите название предприятия {i + 1}: ")
+        profit = [int(input(f"Введите прибыль за {j + 1}-й квартал: ")) for j in range(4)]
+        companies[name] = sum(profit)
+        print(companies)
+
+    avg = sum(companies.values()) / companies_count
+    print(f"Средняя прибыль всех компаний за год: {avg}")
+
+    for company in companies:
+        print(company)
+        if companies[company] >= avg:
+            print(f"Компания: {company} имееет прибыль выше средней: {companies[company]}")
+    for company in companies:
+        if companies[company] < avg:
+            print(f"Компания: {company} имеет прибыль ниже среднего: {companies[company]}")
+
+
+@profile
+def rec(n):
+    if n < 10:
+        return n
+    return int(str(n % 10) + str(rec(n // 10)))
+
+
+## эта же задача на переверни число не рекурсией
+@profile
+def reverse():
+    n1 = input("Введите целое число: ")
+    n2 = n1[::-1]
+    return print('"Обратное" ему число:', n2)
+
+
+## тоже самое вайлом
+@profile
+def reverse_2():
+    n1 = int(input("Введите целое число: "))
+    n2 = 0
+    while n1 > 0:
+        digit = n1 % 10
+        n1 = n1 // 10
+        n2 = n2 * 10
+        n2 = n2 + digit
+    return print('"Обратное" ему число:', n2)
+
+
+if __name__ == "__main__":
+    calc()  ##функция крайне эффективная, использует всего 18.3 Mib без дальнейшего увеличения
+
+    ## ниже видно, что вторая функция наиболее эффективная
+    ##при вычислении огромных значений, Occurences не меняется
+rec(2323131232131233232313123213123323231312321312332323131232131233)
+reverse()
+reverse_2()
--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -5,3 +5,49 @@
 
 Например, один из вариантов, использование генераторов
 """
+# использование форматирования строк вместо конкатенации(внутри цикла)
+#  избегать использование глобальных переменных
+#  использовать встроенные функции-они уже оптимизированны
+
+
+##пример генератора
+
+from memory_profiler import profile, memory_usage
+
+
+@profile
+def work_with_items(items):
+    result = []
+    for item in items:
+        result.append('Changed {}'.format(item))
+    return result
+
+
+@profile
+def yield_items(items):
+    for item in items:
+        yield 'Changed {}'.format(item)
+
+
+m1 = memory_usage()
+for i in work_with_items(range(10)):
+    print(i)
+m2 = memory_usage()
+mem_diff = m2[0] - m1[0]
+print(f"Выполнение заняло {mem_diff} Mib")
+
+m1 = memory_usage()
+for i in yield_items(range(10)):
+    print(i)
+m2 = memory_usage()
+mem_diff = m2[0] - m1[0]
+print(f"Выполнение заняло {mem_diff} Mib")
+
+
+"""
+генератор yield_items возвращает элементы по одному, 
+а функция work_with_items - собирает их в список, а потом возвращает. 
+Если количество элементов небольшое, это не существенно. 
+Но, при обработке больших объемов данных, лучше работать с элементами по одному.
+Например, при обработке большого log-файла, лучше обрабатывать его построчно, не выгружая все содержимое в память.
+"""
