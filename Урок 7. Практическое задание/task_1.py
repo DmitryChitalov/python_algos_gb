7724465563bbc4a18baf684ac248d3a1e424b404
evgeniy_prokofyev <prokofyev.evg@yandex.ru>
2020-11-18 17:54:12 +0300

    lesson7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -3,12 +3,33 @@
 заданный случайными числами на промежутке [-100; 100). Выведите на экран
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. Обязательно доработайте алгоритм (сделайте его умнее).
-
 Идея доработки: если за проход по списку не совершается ни одной сортировки,
 то завершение
 Обязательно сделайте замеры времени обеих реализаций
 и обосновать дала ли оптимизация эффективность
-
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+
+def bubble_sort(input_array):
+    iter_count = 0
+    for i in range(len(input_array)):
+        array_sorted = True
+        for j in range(len(input_array) - i - 1):
+            iter_count += 1
+            if input_array[j] < input_array[j + 1]:
+                input_array[j], input_array[j + 1] = input_array[j + 1], input_array[j]
+                array_sorted = False
+            if array_sorted:
+                print(f"Iterations count - {iter_count}")
+                return
+    print(f"Iterations count - {iter_count}")
+
+
+arr = [1, 7, 6, 5, 4, 3, 2]
+
+print(arr)
+bubble_sort(arr)
+print(arr)
+bubble_sort(arr)
+print(arr)
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -3,13 +3,53 @@
 Найдите в массиве медиану. Медианой называется элемент ряда, делящий его на
 две равные части: в одной находятся элементы, которые не меньше медианы,
 в другой – не больше медианы.
-
 Задачу можно решить без сортировки исходного
 массива.
-
 Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках: Шелла, Гномья, ...
-
 arr[m]
 from statistics import median
+"""
+
+from statistics import median
+from random import randint, choice
+from timeit import timeit
+
+
+# Использован алгоритм quickselect, разработанный Тони Хоаром
+def median_search(input_array, m):
+    if len(input_array) == 1:
+        return input_array[0]
+
+    pivot = choice(input_array)
+    lower_values = [x for x in input_array if x < pivot]
+    higher_values = [x for x in input_array if x > pivot]
+    pivot_values = [x for x in input_array if x == pivot]
+
+    if m < len(lower_values):
+        return median_search(lower_values, m)
+    elif m < len(lower_values) + len(pivot_values):
+        return pivot_values[0]
+    else:
+        return median_search(higher_values, m - len(lower_values) - len(pivot_values))
+
+
+m = 1000
+arr = list([randint(-100, 100) for _ in range(2 * m + 1)])
+print(arr)
+print(f"Результат median_search - {median_search(arr, m)}")
+print(f"Результат median - {median(arr)}")
+
+print(
+    f"Время median_search - {timeit('median_search(arr, m)', 'from __main__ import arr, m, median_search', number=1000)}")
+print(f"Время median - {timeit('median(arr)', 'from __main__ import arr, m, median', number=1000)}")
+
+"""
+Результат median_search - 4
+Результат median - 4
+Время median_search - 10.617405501001485
+Время median - 0.18911677199866972
+
+Алгоритм работает верно, однако встроеная функция работает на порядок быстрее. Возможно, следует поэкспериментировать
+с выбором опорного элемента для улучшения быстродействия
 """
