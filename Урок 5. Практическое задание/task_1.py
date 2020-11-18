4c71cc4058897dcddca2fafaaef12870e4e259d9
Vladimir_Novikov <vovasnew@mail.ru>
2020-11-18 21:35:21 +0500

    Добрый день!

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,68 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+from collections import namedtuple
+
+company = namedtuple('company', 'Name quarters total')
+name = ''
+quarters = []
+total = 0
+company_list = []
+while True:
+    try:
+        quantity = int(input('Введите количество предприятий для расчета прибыли: '))
+        if quantity <= 0:
+            print('Введите положительное число')
+            continue
+        break
+    except ValueError:
+        print('Нужно вводить число')
+for i in range(quantity):
+    space = False
+    while not space:
+        name = input(f'Введите название предприятия {i + 1}: ')
+        if len(name) < 1 or name == ' ' * len(name):
+            print('Название не может быть пустым')
+            continue
+        space = True
+    correct = False
+    while not correct:
+        while True:
+            quarters = input(
+                f'Через пробел введите прибыль предприятия "{name}" за каждый квартал(Всего 4 квартала): ').split()
+            if len(quarters) != 4:
+                print('Введите 4 числа')
+            else:
+                break
+        try:
+            total = sum([float(item) for item in quarters])
+        except ValueError:
+            print('Вводить можно только числа')
+            continue
+        correct = True
+    data = company(Name=name, quarters=quarters, total=total)
+    company_list.append(data)
+
+total_annual_profit = 0
+for i in company_list:
+    total_annual_profit += i.total
+
+avg = total_annual_profit / quantity
+print('+' * 100)
+print(f'Средняя годовая прибыль всех предприятий: {avg}')
+
+high = {}
+low = {}
+middle = {}
+
+for i in company_list:
+    if i.total > avg:
+        high[i.Name] = i.total
+    elif i.total == avg:
+        middle[i.Name] = i.total
+    else:
+        low[i.Name] = i.total
+
+print(f'Предприятия, с прибылью выше среднего значения: {high}')
+print(f'Предприятия, с прибылью ниже среднего значения: {low}')
+print(f'Предприятия, с прибылью равной среднему значению: {middle}')
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,59 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+
+from collections import deque
+
+num_1 = deque(input('Введите первое число для сложения: '))
+num_2 = deque(input('Введите второе число для сложения: '))
+num_3 = deque(input('Введите первое число для умножения: '))
+num_4 = deque(input('Введите второе число для умножения: '))
+
+hex_sum = deque()
+
+
+def addition_hex(a, b, transfer='0'):
+    if len(a) == 0 and len(b) == 0:
+        if transfer == '1':
+            hex_sum.appendleft('1')
+        return hex_sum
+    key_a = '0' if len(a) == 0 else a.pop()
+    key_b = '0' if len(b) == 0 else b.pop()
+    formula_1 = hex(int(key_a, 16) + int(key_b, 16) + int(transfer, 16))
+    integer_remainder = hex(int(formula_1, 16) % 16)[2:]
+    transfer = '0' if hex(int(formula_1, 16) // 16) == '0x0' else '1'
+    hex_sum.appendleft(integer_remainder.upper())
+    return addition_hex(a, b, transfer)
+
+
+print(addition_hex(num_1, num_2))
+
+
+def func_2(a, b, multiplier=1):
+    total_sum = ''
+    subtotal = ''
+    full_number = ''
+    a = deque(a)
+    b = deque(b)
+    if len(a) < len(b):
+        a, b = b, a
+    if len(a) >= len(b):
+        for i in range(len(a)):
+            key_a = a.popleft()
+            full_number += key_a
+            a.append(key_a)
+        for i in range(len(b)):
+            key_b = b.pop()
+            multiplication = (hex(int(full_number, 16) * int(key_b, 16) * int(multiplier)))[2:]
+            b.appendleft(key_b)
+            multiplier *= 16
+            if i == 0:
+                subtotal = multiplication
+                total_sum = subtotal
+            else:
+                total_sum = addition_hex(deque(subtotal), deque(multiplication))
+                subtotal = total_sum
+    return total_sum
+
+
+print(func_2(num_3, num_4))
