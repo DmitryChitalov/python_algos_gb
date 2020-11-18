9e63c5c7f25a64c42bcbacfc9608b818ca18f247
Aleksei.Polianskii <aleksei.polianskii@mail.ru>
2020-11-18 21:15:27 +0700

    Add task

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -9,6 +9,9 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+from timeit import timeit
+from random import randint
+import cProfile
 
 
 def revers(enter_num, revers_num=0):
@@ -34,3 +37,23 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+print('Через Timeit')
+nums = randint(10000,1000000)
+revers(nums)
+revers_2(nums)
+revers_3(nums)
+
+
+# print(f'Функция с использованием рекурсии: '
+#       f'{timeit("revers(nums)", setup="from __main__ import revers_2", number=100000)})
+# print(f'Функция с использованием рекурсии: '
+#       f'{timeit("revers_2(nums)", setup="from __main__ import revers_2", number=100000)})
+# print(f'Функция с использованием рекурсии: '
+#       f'{timeit("revers_3(nums)", setup="from __main__ import revers_3", number=100000)})
+
+cProfile.run('revers(10000000000)')
+cProfile.run('revers_2(10000000000)')
+cProfile.run('revers_3(10000000000)')
+
+# Как и ожидалось самый быстрый способ это срез, но у меня не получается запустить функцию
