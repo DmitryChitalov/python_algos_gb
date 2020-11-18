e5b617104ed2e9f2617293486b9b17ac3f60ce2e
Aleksei.Polianskii <aleksei.polianskii@mail.ru>
2020-11-18 21:25:11 +0700

    Add task

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,6 +11,11 @@
 """
 
 
+from timeit import timeit
+from random import randint
+import cProfile
+
+
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
         return
@@ -34,3 +39,23 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+print('Через Timeit')
+nums = randint(10000,1000000)
+revers(nums)
+revers_2(nums)
+revers_3(nums)
+
+
+# print(f'Функция с использованием рекурсии: '
+#       f'{timeit("revers(nums)", setup="from __main__ import revers_2", number=100000)})
+# print(f'Функция с использованием рекурсии: '
+#       f'{timeit("revers_2(nums)", setup="from __main__ import revers_2", number=100000)})
+# print(f'Функция с использованием рекурсии: '
+#       f'{timeit("revers_3(nums)", setup="from __main__ import revers_3", number=100000)})
+
+cProfile.run('revers(10000000000)')
+cProfile.run('revers_2(10000000000)')
+cProfile.run('revers_3(10000000000)')
+
+# Как и ожидалось самый быстрый способ это срез, но у меня не получается запустить функцию
