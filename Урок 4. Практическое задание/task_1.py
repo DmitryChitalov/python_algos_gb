17b10a89f8bd256323f121550c59718110512468
Aleksei.Polianskii <aleksei.polianskii@mail.ru>
2020-11-18 21:00:45 +0700

    Add task

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -8,8 +8,6 @@
 
 Попробуйте оптимизировать код, чтобы снизить время выполнения
 Проведите повторные замеры
-
-Добавьте аналитику: что вы сделали и почему
 """
 
 from timeit import timeit
@@ -21,3 +19,22 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [i for i, el in enumerate(nums) if el % 2 == 0]
+
+
+nums = [el for el in range(1000)]
+print(
+    timeit(
+        "func_1(nums)",
+        setup="from __main__ import func_1, nums",
+        number=10000))
+
+nums_2 = [el for el in range(1000)]
+print(
+    timeit(
+        "func_2(nums_2)",
+        setup="from __main__ import func_2, nums_2",
+        number=10000))
