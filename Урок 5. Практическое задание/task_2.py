7ce667d29f104bb25398796289a2ef605e6f5769
Shaumik <a.v-suv@yandex.ru>
2020-11-19 19:22:24 +0500

    Домашнее задание по 5 уроку

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,60 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+
+from collections import deque
+
+HEX_NUM = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+               'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
+HEX_NUM_RES = {
+               0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
+               10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+
+#Переводим в десятиричную систему и выводим число
+def translit_10(lst):
+    deq_10 = deque()
+    degree = len(lst) - 1
+    res = 0
+    for i in lst:
+        if degree >= 0:
+            if i in HEX_NUM.keys():
+                res = HEX_NUM[i] * (16 ** degree)
+                deq_10.append(res)
+                degree -= 1
+            else:
+                continue
+        else:
+            continue
+    return sum(deq_10)
+
+# Подготавливаем к переводу в 16-ричную систему
+def translit_16(num):
+    deq_16 = deque()
+    q = num
+    res_1 = q // 16
+    while res_1 % 16 != 0:
+        res_n = res_1 // 16
+        res = res_1 % 16
+        deq_16.appendleft(res)
+        res_1 = res_n
+    return deq_16
+
+# Переводим в 16 и ричную систему
+def trans(deq):
+    res = []
+    for i in deq:
+        if i in HEX_NUM_RES.keys():
+            res.append(HEX_NUM_RES[i])
+    return res
+
+x = list(input('Введите 1-е шестнадцатиричное число: ').upper())
+y = list(input('Введите 2-е шестнадцатиричное число: ').upper())
+
+composition_10 = translit_10(x) * translit_10(y)
+amount_10 = translit_10(x) + translit_10(y)
+
+composition_16 = translit_16(composition_10)
+amount_16 = translit_16(amount_10)
+
+print(f'сумма шестнадцатеричных чисел = {trans(amount_16)}')
+print(f'Произведение шестнадцатеричных чисел = {trans(composition_16)}')
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,87 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+import random
+from collections import deque
+from timeit import timeit
+
+deq_new = deque(random.sample(range(100), 10))
+print(deq_new)
+
+#Создаем очередь в deque
+def completion_dq():
+    deq = deque(random.sample(range(100), 5))
+    return deq
+
+# добавляем элемент в начало очереди deque
+def app_dq():
+    deq_new.appendleft('999')
+    return deq_new
+
+# перемещаем  элементы с конца очереди в начало deque
+def rotat_dq():
+    deq_new.rotate(2)
+    return deq_new
+
+# пoдсчитываем количесвто эллементов = 5
+def count_dq():
+    deq_new.count(5)
+    return deq_new
+
+# разворачиваем очередь
+def reverse_dq():
+    deq_new.reverse()
+    return deq_new
+
+###################################################
+
+list_new = random.sample(range(100), 5)
+print((list_new))
+
+# создаем список
+def completion_lst():
+    lst = random.sample(range(100), 5)
+    return lst
+
+# добавляем элемент в начало списка
+def app_lst():
+    list_new.insert(6, 999)
+    return deq_new
+
+# перемещаем  элементы c начала в конец списка
+def rotat_lst():
+    new_1 = list_new[:2]
+    del list_new[:2]
+    list_new.append(new_1)
+    return list_new
+
+# пoдсчитываем количесвто эллементов = 5
+def count_lst():
+    list_new.count(5)
+    return list_new
+
+# разворачиваем список
+def reverse_lst():
+    list_new.reverse()
+    return list_new
+
+#################################################
+print('сравнительная скорость работы по заполениею очереди и списка')
+print(timeit('completion_dq()', setup='from __main__ import completion_dq', number=10000))
+print(timeit('completion_lst()', setup='from __main__ import completion_lst', number=10000))
+
+print('сравнительная скорость работы по добавлению элементов очереди и списка')
+print(timeit('app_dq()', setup='from __main__ import app_dq', number=1000))
+print(timeit('app_lst()', setup='from __main__ import app_lst', number=1000))
+
+print('сравнительная скорость работы по перемещению элементов очереди и списка')
+print(timeit('rotat_dq()', setup='from __main__ import rotat_dq', number=1000))
+print(timeit('rotat_lst()', setup='from __main__ import rotat_lst', number=1000))
+
+print('сравнительная скорость работы по подсчету определенных элементов очереди и списка')
+print(timeit('count_dq()', setup='from __main__ import count_dq'))
+print(timeit('count_lst()', setup='from __main__ import count_lst'))
+
+print('сравнительная скорость работы по развороту элементов очереди и списка')
+print(timeit('reverse_dq()', setup='from __main__ import reverse_dq', number=1000))
+print(timeit('reverse_lst()', setup='from __main__ import reverse_lst', number=1000))
