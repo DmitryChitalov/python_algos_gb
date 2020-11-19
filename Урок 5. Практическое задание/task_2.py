ec1b0ecd147916b4b3bc5f5c698fdb07d4dc126b
Sharp_Mind <redlance988@gmail.com>
2020-11-19 18:05:58 +0300

    Homework 5 completed.

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,78 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+from collections import deque
+
+
+# вариант с деком, с переворотом вводимых данных по ходу выполнения программы, по сути просто так
+def variant_1():
+    def is_it_int(data):
+        while not data.isdigit():
+            print('Ошибка ввода. Пожалуйста, введите одно целое натуральное число.')
+            data = input('Введите число: ')
+        return int(data)
+
+    input_list, output_list = deque(), deque()
+
+    def check_symbol(data):
+        while True:
+            for i in data:
+                if i.lower() not in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f'):
+                    print('Ошибка ввода.')
+                    data = input('Пожалуйста, введите шестнадцатеричное число: ')
+                    break
+            return data
+
+    for i in range(2):
+        input_list.append(deque())
+        output_list.append(deque())
+        user_input = check_symbol(input(f'Введите число {i + 1}: '))
+        for j in user_input:
+            input_list[i].append(j)
+
+    nums_list = deque()
+
+    for i in range(len(input_list) - 1, -1, -1):
+        nums = ''
+        for j in input_list[i]:
+            nums += j
+        nums_list.appendleft(nums)
+
+    nums_summ = hex(int(nums_list[0], 16) + int(nums_list[1], 16)).split('x')[-1]
+    nums_mult = hex(int(nums_list[0], 16) * int(nums_list[1], 16)).split('x')[-1]
+
+    for i in nums_summ:
+        output_list[0].append(i)
+
+    for i in nums_mult:
+        output_list[1].append(i)
+
+    for n in range(len(output_list)):
+        if n == 0:
+            print('Сумма введённых чисел: ')
+        else:
+            print('Произведение введённых чисел: ')
+        for d in output_list[n]:
+            print(str(d).upper(), end='')
+        print()
+
+
+# вариант на ООП:
+def variant_2():
+    class MyDigit:
+        def __init__(self, num):
+            self.num = int(num, 16)
+
+        def __add__(self, other):
+            return hex(self.num + other.num).split('x')[-1].upper()
+
+        def __mul__(self, other):
+            return hex(self.num * other.num).split('x')[-1].upper()
+
+    aa = MyDigit('a2')
+    bb = MyDigit('c4f')
+    print(aa + bb)
+    print(aa * bb)
+
+# variant_1()
+# variant_2()
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,131 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+
+from timeit import timeit
+from collections import OrderedDict
+from random import randint
+
+# заполнение случайными заглавными английскими (или латинскими) буквами:
+list_1 = [chr(randint(65, 90)) for i in range(10)]
+dict_1 = {}
+dict_1 = dict_1.fromkeys(list_1)
+
+
+for k in dict_1.keys():
+    dict_1[k] = randint(1, 100)
+
+dict_2 = OrderedDict(dict_1.items())
+
+item_d1 = chr(randint(65, 90))
+while item_d1 not in dict_1.keys():
+    item_d1 = chr(randint(65, 90))
+
+item_d2 = chr(randint(65, 90))
+while item_d2 not in dict_1.keys():
+    item_d2 = chr(randint(65, 90))
+
+
+def dict_out():
+    for key, value in dict_1.items():
+        a = key
+        b = value
+
+
+def ordered_dict_out():
+    for key, value in dict_2.items():
+        c = key
+        d = value
+
+
+def add_el_to_dict():
+    dict_1['x'] = randint(101, 200)
+
+
+def add_el_to_ordered_dict():
+    dict_2['y'] = randint(101, 200)
+
+
+def dict_del_el():
+    dict_1 = {}.fromkeys(list_1)
+    dict_1.popitem()
+
+
+def ordered_dict_del_el():
+    dict_1 = {}.fromkeys(list_1)
+    dict_2 = OrderedDict(dict_1.items())
+    dict_2.popitem()
+
+
+def dict_move_to_end():
+    def give_me_el():
+        for i in dict_1.keys():
+            yield i
+
+    el = next(give_me_el())
+    dict_1[el] = dict_1.pop(el)
+
+
+def dict_move_to_start():
+    dict_1 = {}.fromkeys(list_1)
+    new_dict_1 = {}
+    last_item = dict_1.popitem()
+    new_dict_1 = new_dict_1.fromkeys(last_item[0], last_item[1])
+    new_dict_1.update(dict_1)
+
+
+def dict_move_to_end_2():
+    temp_dict, temp_dict_2 = dict(), dict()
+    needed_val = {}
+
+    next_stage = False
+    for k in dict_1.keys():
+        if k == item_d1:
+            needed_val[k] = dict_1[k]
+            next_stage = True
+        if next_stage is False:
+            temp_dict[k] = dict_1[k]
+        else:
+            if k != item_d1:
+                temp_dict_2[k] = dict_1[k]
+    temp_dict.update(temp_dict_2)
+    temp_dict.update(needed_val)
+
+
+def ordered_dict_move_to_end():
+    dict_2.move_to_end(item_d2)
+
+
+def ordered_dict_move_to_start():
+    dict_2.move_to_end(item_d2, False)
+
+
+print(f'Перебор dict:'
+      f'\n{timeit("dict_out()", setup="from __main__ import dict_out", number=10000)}\n'
+      f'Перебор OrderedDict:\n'
+      f'{timeit("ordered_dict_out()", setup="from __main__ import ordered_dict_out", number=10000)}\n'
+      f'Добавление элемента в dict:\n'
+      f'{timeit("add_el_to_dict()", setup="from __main__ import add_el_to_dict", number=10000)}\n'
+      f'Добавление элемента в OrderedDict:\n'
+      f'{timeit("add_el_to_ordered_dict()", setup="from __main__ import add_el_to_ordered_dict", number=10000)}\n'
+      f'Удаление элемента из dict:\n'
+      f'{timeit("dict_del_el()", setup="from __main__ import dict_del_el", number=10000)}\n'
+      f'Удаление элемента из Ordered dict:\n'
+      f'{timeit("ordered_dict_del_el()", setup="from __main__ import ordered_dict_del_el", number=10000)}\n'
+      f'Перемещение элемента в начало в dict:\n'
+      f'{timeit("dict_move_to_start()", setup="from __main__ import dict_move_to_start", number=10000)}\n'
+      f'Перемещение элемента в конец в dict:\n'
+      f'{timeit("dict_move_to_end()", setup="from __main__ import dict_move_to_end", number=10000)}\n'
+      f'Перемещение элемента в конец с любой позиции в dict:\n'
+      f'{timeit("dict_move_to_end_2()", setup="from __main__ import dict_move_to_end_2", number=10000)}\n'
+      f'Перемещение первого элемента в конец в OrderedDict:\n'
+      f'{timeit("ordered_dict_move_to_end()", setup="from __main__ import ordered_dict_move_to_end", number=10000)}\n'
+      f'Перемещение элемента в начало с любой позиции в OrderedDict:\n'
+      f'{timeit("ordered_dict_move_to_start()", setup="from __main__ import ordered_dict_move_to_start", number=10000)}\n')
+
+'''
+Наблюдения и выводы:
+использовать OrderedDict нет необходимости ввиду упорядоченности словарей в новой версии Python;
+если в проекте нужно периодически перемещать быстро (во всех смыслах) элементы в начало или в
+конец словаря, то OrderedDict делает это одной строкой кода и за короткое время.
+'''
