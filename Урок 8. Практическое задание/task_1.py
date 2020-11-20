dd4dddacf5bc81e6840ede858e26ccb18a32e44b
pkrivtsov <krivtsov@live.com>
2020-11-20 23:51:23 +0300

    My HW for Lesson 8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,69 @@
 ВНИМАНИЕ: примеры заданий будут размещены в последний день сдачи.
 Но постарайтесь обойтись без них.
 """
+from collections import Counter, deque
+import time
+
+
+class MyNode:
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+
+def haffman_tree(s):
+    count_s = Counter(s)
+
+    sorted_s = deque(sorted(count_s.items(), key=lambda item: item[1]))
+
+    while len(sorted_s) > 1:
+
+        weight = sorted_s[0][1] + sorted_s[1][1]
+        node = MyNode(left=sorted_s.popleft()[0], right=sorted_s.popleft()[0])
+
+        for i, item in enumerate(sorted_s):
+            if weight > item[1]:
+                continue
+            else:
+                sorted_s.insert(i, (node, weight))
+                break
+        else:
+            sorted_s.append((node, weight))
+
+    return sorted_s[0][0]
+
+
+def memoize(f):
+    cache = {}
+
+    def decorate(*args, **kwargs):
+
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = f(*args, **kwargs)
+            return cache[args]
+
+    return decorate
+
+
+code_table = dict()
+
+
+@memoize
+def haffman_code(tree, path=''):
+    if not isinstance(tree, MyNode):
+        code_table[tree] = path
+
+    else:
+        haffman_code(tree.left, path=f'{path}0')
+        haffman_code(tree.right, path=f'{path}1')
+
+
+s = "beep boop beer beep boop beer beep boop beer beep boop beer beep boop beer beep boop beer beep boop beer beep boop beer beep boop beer!"
+
+haffman_code(haffman_tree(s))
+
+for i in s:
+    print(code_table[i], end=' ')
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -8,7 +8,21 @@
 (например, валидация значений узлов в соответствии с требованиями для бинарного дерева)
 
 Поработайте с доработанной структурой, позапускайте на реальных данных.
+
 """
+
+
+def on_err(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            print(e)
+        except AttributeError:
+            print('Ошибка доступа к атрибуту класса')
+
+    return wrapper
+
 
 class BinaryTree:
     def __init__(self, root_obj):
@@ -19,41 +33,55 @@ class BinaryTree:
         # правый потомок
         self.right_child = None
 
+    def __str__(self):
+        return str(self.root)
+
     # добавить левого потомка
+    @on_err
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
+        # валидация значений узлов в соответствии с требованиями для бинарного дерева
+        if new_node < self.root:
+            # если у узла нет левого потомка
+            if self.left_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.left_child = BinaryTree(new_node)
+            # если у узла есть левый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+            raise Exception('Ошибка вставки!')
 
     # добавить правого потомка
+    @on_err
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
+        if new_node >= self.root:
+            # если у узла нет правого потомка
+            if self.right_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.right_child = BinaryTree(new_node)
+            # если у узла есть правый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+            raise Exception('Ошибка вставки!')
 
     # метод доступа к правому потомку
+    @on_err
     def get_right_child(self):
         return self.right_child
 
     # метод доступа к левому потомку
+    @on_err
     def get_left_child(self):
         return self.left_child
 
@@ -62,6 +90,7 @@ class BinaryTree:
         self.root = obj
 
     # метод доступа к корню
+    @on_err
     def get_root_val(self):
         return self.root
 
@@ -70,6 +99,9 @@ r = BinaryTree(8)
 print(r.get_root_val())
 print(r.get_left_child())
 r.insert_left(4)
+print(r.get_left_child())
+print(r.get_left_child().get_root_val())
+r.insert_left(9)
 print(r.get_left_child())
 print(r.get_left_child().get_root_val())
 r.insert_right(12)
