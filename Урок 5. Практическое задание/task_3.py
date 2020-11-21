6d8f88d0a7c1d0d5af6ee6d4ffa71ce22ba60e6c
Wolfy2604 <60422125+Wolfy2604@users.noreply.github.com>
2020-11-21 15:47:16 +0400

    HW6

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,51 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+from timeit import timeit
+import random
+
+setup = """
+import collections as coll
+import random
+
+
+list1 = [random.choice(range(1001)) for _ in range(10000)]
+deque = coll.deque([random.choice(range(1001)) for _ in range(10000)])
+
+
+def sum_first_last(some_list, n):
+    res = 0
+    for _ in range(n):
+        try:
+            res += (some_list.pop() + some_list.popleft())
+        except AttributeError:
+            res += (some_list.pop() + some_list.pop(0))
+    return res
+
+
+def sum_random(some_list, n):
+    res = 0
+    for _ in range(n):
+        rand = random.choice(range(len(some_list) - 1))
+        res += some_list[rand]
+        del some_list[rand]
+    return res
+
+
+n = 234
+"""
+
+check_list = 'sum_first_last(list1, n)'
+check_deque = 'sum_first_last(deque, n)'
+check2_list = 'sum_random(list1, n)'
+check2_deque = 'sum_random(deque, n)'
+print('Время выполнения суммы n чисел с начала и конца: ')
+print(f'Время выполнения списка - {timeit(check_list, setup, number=10)}')
+print(f'Время выполнения колоды - {timeit(check_deque, setup, number=10)}')
+
+print('Время выполнения суммы n случайных чисел: ')
+print(f'Время выполнения списка - {timeit(check2_list, setup, number=10)}')
+print(f'Время выполнения колоды - {timeit(check2_deque, setup, number=10)}')
+
+
+
