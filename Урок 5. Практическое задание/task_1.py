e591f308c380cdfd3a4287b5adb35ef0a50c6509
Николай Гаркушенко <nekdfl@gmail.com>
2020-11-21 09:58:06 +0300

    lesson 5 task1

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,68 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+
+from collections import namedtuple
+
+
+def ask_number(message):
+    pass
+    res = input(message).strip()
+    if not res.isdigit():
+        print("Недопустимый ввод! Разрешены к вводу только цифры!")
+        return ask_number(message)
+
+    return int(res)
+
+
+def ask_company_name(message):
+    res = input(message).strip()
+
+    return res
+
+
+def ask_company_profit(message):
+    res = input(message).strip()
+
+    if len(res.split()) < 4:
+        res = ask_company_profit(message)
+
+    return res.split()
+
+
+def main():
+    pass
+    try:
+        total_profit = {}
+        below_average = []
+        above_average = []
+        companies = namedtuple("company", "q1 q2 q3 q4")
+        company_counter = ask_number("Введите количество компаний ")
+
+        for i in range(1, company_counter + 1):
+            print(f"Ввод данных {i}-й компании")
+            name = ask_company_name("Введите название компании ")
+            all_quatter_profit = ask_company_profit("Введите прибыль за 4 квартала через пробел ")
+            company = companies(q1=int(all_quatter_profit[0]), q2=int(all_quatter_profit[1]),
+                                q3=int(all_quatter_profit[2]), q4=int(all_quatter_profit[3]))
+            total_profit[name] = (company.q1 + company.q2 + company.q3 + company.q4) / len(all_quatter_profit)
+
+        average_profit_all_companies = sum(total_profit.values()) / len(total_profit.values())
+        print(f"Средняя прибыль всех компаний за 4 квартала - {average_profit_all_companies}")
+
+        for key, value in total_profit.items():
+            if value < average_profit_all_companies:
+                below_average.append(key)
+            else:
+                above_average.append(key)
+
+        print(f"Компаний с прибылью ниже среднего - {', '.join(below_average)}")
+        print(f"Компаний с прибылью выше среднего - {', '.join(above_average)}")
+
+        print("\nПрограмма завершена!")
+    except Exception as ex:
+        print(f"Fatal error: {ex}")
+
+
+if __name__ == "__main__":
+    main()
