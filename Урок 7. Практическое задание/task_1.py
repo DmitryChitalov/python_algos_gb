fb5e53144af2ea067efe5512131ac6d4f4fcc205
Shaumik <a.v-suv@yandex.ru>
2020-11-25 20:12:56 +0500

    Задание к 7 уроку

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -12,3 +12,55 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+import timeit
+import random
+
+def bubble_sort(lst_obj):
+    lst_obj_p = orig_list
+    n = 1
+    while n < len(lst_obj_p):
+        for i in range(len(lst_obj_p)-n):
+            if lst_obj_p[i] < lst_obj_p[i+1]:
+                lst_obj_p[i], lst_obj_p[i+1] = lst_obj_p[i+1], lst_obj_p[i]
+        n += 1
+    return lst_obj_p
+
+def bubble_sort_up(lst_obj):
+    n = 1
+    lst_obj_up = orig_list
+    lst_obj_n = [1]
+    while n < len(lst_obj):
+        if n < len(lst_obj_up) and len(lst_obj_n) != 0:
+            lst_obj_n.clear()
+            for i in range(len(lst_obj_up) - n):
+                if lst_obj_up[i] < lst_obj_up[i + 1]:
+                    lst_obj_up[i], lst_obj_up[i + 1] = lst_obj_up[i + 1], lst_obj_up[i]
+                    lst_obj_n.append(lst_obj_up[i])
+                    lst_obj_n.append(lst_obj_up[i + 1])
+        elif n < len(lst_obj_up) and len(lst_obj_n) == 0:
+            return lst_obj_up
+        n += 1
+    return lst_obj_up
+
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+print(orig_list)
+print(bubble_sort(orig_list))
+print(timeit.timeit("bubble_sort(orig_list)",
+                    setup="from __main__ import bubble_sort, orig_list", number=1))
+
+#print(bubble_sort_up(orig_list))
+
+#print(timeit.timeit("bubble_sort_up(orig_list)",
+#                    setup="from __main__ import bubble_sort_up, orig_list", number=1))
+
+''' Доработанная сортировка работает как минимум не мендленее, а чаще всего быстрее. Это обусловленно тем, что мы 
+прекращаем вызывать лишние итерации, и сразу выдаем результат. 
+[52, -25, 40, 34, 51, 93, 20, -7, 100, -94]
+[100, 93, 52, 51, 40, 34, 20, -7, -25, -94]
+3.100000000000325e-05
+[-29, -67, -41, -51, 100, 47, 7, -24, -82, -96]
+[100, 47, 7, -24, -29, -41, -51, -67, -82, -96]
+1.6399999999999748e-05
+
+'''
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -11,5 +11,44 @@
 который не рассматривался на уроках: Шелла, Гномья, ...
 
 arr[m]
-from statistics import median
+
 """
+import random
+import statistics
+import numpy as np
+
+a = random.randint(5, 20)
+size = 2 * a + 1
+ar = [random.randint(0, 100) for i in range(size)]
+print(ar)
+#1
+print(statistics.median(ar))
+#2
+a = np.array(ar)
+median_value = np.percentile(a, 50)
+print(median_value)
+#3
+b = sum(ar) / len(ar)
+print(b)
+left = []
+right = []
+for i in range(len(ar)):
+    if ar[i] > b:
+        right.append(ar[i])
+    else:
+        left.append(ar[i])
+c = (max(left))
+
+d = (min(right))
+
+print(max(left))
+print(min(right))
+print(1 - c / b)
+print(d % b)
+if (1 - c / b) > d % b or d / b == 1:
+    print(d)
+else:
+    print(c)
+
+''' Третий способ с математической точки зрения более точный, чем метод statistics.median, при этом совершенно
+странно, что их результаты переодически не совпадают'''
