84717e1e8e38621913c65d355fd7dcb6c902840c
sofya-stepanova <66839199+sofya-stepanova@users.noreply.github.com>
2020-11-27 13:56:42 +0300

    Add files via upload

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -12,3 +12,42 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+import random
+import timeit
+
+my_list = [random.randint(-100, 100) for _ in range(100)]
+
+
+def bubble(any_list):
+    n = 1
+    flag = True
+    while n < len(any_list):
+        for i in range(len(any_list) - n):
+            if any_list[i] > any_list[i+1]:
+                any_list[i], any_list[i+1] = any_list[i+1], any_list[i]
+                flag = False
+        if flag is True:
+            break
+        n += 1
+    return any_list
+
+
+print(my_list)
+print(bubble(my_list[:]))
+print(my_list)
+print(timeit.timeit("bubble(my_list[:])", setup="from __main__ import bubble, my_list", number=100))
+
+
+def bubble1(any_list):
+    n = 1
+    while n < len(any_list):
+        for i in range(len(any_list) - n):
+            if any_list[i] > any_list[i+1]:
+                any_list[i], any_list[i+1] = any_list[i+1], any_list[i]
+        n += 1
+    return any_list
+
+print(timeit.timeit("bubble1(my_list[:])", setup="from __main__ import bubble1, my_list", number=100))
+
+
+'''Улучшенный вариант сортировки дает несущественное улучшение'''
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,53 @@
 arr[m]
 from statistics import median
 """
+from statistics import median
+import random
+
+
+
+my_list = [random.randint(0, 40) for i in range(5)]
+
+
+print(my_list)
+print(median(my_list))
+
+
+def my_median(any_list):
+    my_set = set(my_list)
+    avg = sum(my_set) / len(my_set)
+    result_list = []
+
+    for i in my_set:
+        result_list.append(abs(avg - i))
+
+    x = min(result_list)
+
+    for i in my_set:
+        if i == avg:
+            result_list.remove(min(result_list))
+            y = min(result_list)
+            if (avg - y) in my_list:
+                my_med = avg - y
+            else:
+                my_med = y - avg
+
+        elif x == avg - i:
+            my_med = i
+        elif x == i - avg:
+            my_med = i
+
+    return my_med
+
+print(my_median(my_list)) #Cначала мне казалось, что это сработает, но потом я поняла, что слишком много исключений
+
+
+def my_median1(any_list):
+    temprorary_copy = any_list
+    for i in range(len(any_list) // 2):
+        temprorary_copy.remove(min(temprorary_copy))
+    return min(temprorary_copy)
+
+
+print(my_median1(my_list))
+
