d03d991f93abadc377307b9b808e4441c614297c
Aleksei.Polianskii <aleksei.polianskii@mail.ru>
2020-11-28 19:22:50 +0700

    Add task

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -13,3 +13,64 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+from memory_profiler import profile
+
+
+class Matrix:
+    def __init__(self, list_lists):
+        self._list_lists = list_lists
+
+    def __str__(self):
+        str_list = ''
+        for el in self._list_lists:
+            for el_2 in el:
+                str_list += f'{el_2}  '
+            str_list += '\n'
+        return str_list[:-1]
+
+    def __add__(self, other):
+        sum_mat = []
+        x = 0
+        while x < len(self._list_lists):
+            sum_mat.append([])
+            y = 0
+            while y < len(self._list_lists[x]):
+                sum_el = self._list_lists[x][y] + other._list_lists[x][y]
+                sum_mat[x].append(sum_el)
+                y += 1
+            x += 1
+        return Matrix(sum_mat)
+
+
+@profile
+def sum_string():
+    sum_user = 0
+    flag = True
+    while flag:
+        string = ' '.join([str(el) for el in range(10000)])
+        list_string = string.split()
+        list_string.append('&')
+        for el in list_string:
+            if flag:
+                if el != '&':
+                    sum_user += int(el)
+                else:
+                    flag = False
+    else:
+        return sum_user
+
+
+@profile
+def sum_matrix():
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    b = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
+    matrix_a = Matrix(a)
+    matrix_b = Matrix(b)
+    print(matrix_a + matrix_b)
+
+
+print(sum_string())
+sum_matrix()
+'''
+Для этого задания взял из основ несколько заданий
+'''
--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -5,3 +5,48 @@
 
 Например, один из вариантов, использование генераторов
 """
+from memory_profiler import profile
+
+
+@profile
+def simple(i):
+    count = 1
+    n = 2
+    while count <= i:
+        t = 1
+        is_simple = True
+        while t <= n:
+            if n % t == 0 and t != 1 and t != n:
+                is_simple = False
+                break
+            t += 1
+        if is_simple:
+            if count == i:
+                break
+            count += 1
+        n += 1
+    return n
+
+
+@profile
+def eratosfen(number, l=100000):
+    a = [el for el in range(l + 1)]
+    a[1] = 0
+    i = 2
+    while i <= l:
+        if a[i] != 0:
+            j = i + i
+            while j <= l:
+                a[j] = 0
+                j += i
+        i += 1
+    return [el for el in a if el != 0][number - 1]
+
+
+i = 1
+print(simple(i))
+print(eratosfen(i))
+
+'''
+Решил использовать задачу из предыдущих уроков. Сразу видно что в циклах использование памяти меньше.
+'''
