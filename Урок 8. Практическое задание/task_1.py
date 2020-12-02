f570499e2e0c379e72bb22b0e7821f16ce5e1fe4
Aleksey Khlybov <halexx7@gmail.com>
2020-12-02 06:53:42 +0300

    --less_8-- hw added solution

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,71 @@
 ВНИМАНИЕ: примеры заданий будут размещены в последний день сдачи.
 Но постарайтесь обойтись без них.
 """
+from collections import deque, defaultdict
+
+
+class HuffmanCode:
+    def __init__(self, some_str):
+        self.some_str = some_str
+        self.count = defaultdict(int)
+        self.counter_sort()
+        self.tree_str = self.tree()[0][0]
+        self.code_tree = dict()
+        self.code(self.tree_str)
+
+    def __str__(self):
+        rez = [self.code_tree[i] for i in self.some_str]
+        return f'STR = {self.some_str}\n' \
+               f'TREE = {str(self.tree_str)}\n' \
+               f'HUFFMAN CODING = {" ".join(rez)}'
+
+    def counter_sort(self):
+        """
+        Метод считает кол-во символов в строке, и возвращает отсортированную очередь из уникальных символов
+        """
+        some_lst = list(self.some_str)
+        for el in some_lst:
+            self.count[el] += 1
+        self.tree_str = deque(sorted(self.count.items(), key=lambda item: item[1]))
+        return self.tree_str
+
+    def tree(self):
+        """
+        Строим наше дерево
+        """
+        if len(self.tree_str) != 1:
+            while len(self.tree_str) > 1:
+                weight = self.tree_str[0][1] + self.tree_str[1][1]
+                comb = {0: self.tree_str.popleft()[0],
+                        1: self.tree_str.popleft()[0]}
+                for i, _count in enumerate(self.tree_str):
+                    if weight > _count[1]:
+                        continue
+                    else:
+                        self.tree_str.insert(i, (comb, weight))
+                        break
+                else:
+                    self.tree_str.append((comb, weight))
+        else:
+            weight = self.tree_str[0][1]
+            comb = {0: self.tree_str.popleft()[0], 1: None}
+            self.tree_str.append((comb, weight))
+        return self.tree_str
+
+    def code(self, tree=None, path=''):
+        """
+        Кодируем нашу строку
+        """
+        if tree is None:
+            tree = self.tree_str
+            self.code(tree)
+        else:
+            if not isinstance(tree, dict):
+                self.code_tree[tree] = path
+            else:
+                self.code(tree[0], path=f'{path}0')
+                self.code(tree[1], path=f'{path}1')
+
+
+obj = HuffmanCode('beep boop beer!')
+print(obj)
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,44 +10,36 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных.
 """
 
+
 class BinaryTree:
     def __init__(self, root_obj):
-        # корень
         self.root = root_obj
-        # левый потомок
         self.left_child = None
-        # правый потомок
         self.right_child = None
 
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
+        if new_node < self.root:
+            if self.left_child is None:
+                self.left_child = BinaryTree(new_node)
+            else:
+                tree_obj = BinaryTree(new_node)
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+            return print(f'Левый потомок не может быть больше корня!')
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
+        if new_node >= self.root:
+            if self.right_child == None:
+                self.right_child = BinaryTree(new_node)
+            else:
+                tree_obj = BinaryTree(new_node)
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+            return print(f'Правый потомок не может быть меньше корня!')
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -66,14 +58,19 @@ class BinaryTree:
         return self.root
 
 
-r = BinaryTree(8)
+r = BinaryTree(16)
+r.insert_left(10)
+r.insert_right(20)
+r.get_left_child().insert_right(30)
+
 print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(4)
-print(r.get_left_child())
 print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+# print(r.get_left_child().get_left_child().get_root_val())
+print(r.get_left_child().get_right_val())
+# r.insert_right(12)
+# print(r.get_right_child())
+# print(r.get_right_child().get_root_val())
+# r.get_right_child().set_root_val(16)
+# print(r.get_right_child().get_root_val())
+
+
