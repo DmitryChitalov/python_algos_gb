886c6890dc956371fe89296dd5bfda396d87ff6c
Valeriya <vagaganova@gmail.com>
2020-12-20 14:49:49 +0300

    Revert "Кроме пятой и седьмой"
    This reverts commit e6151424

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -9,33 +9,3 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
-from random import randint
-
-
-def natural_input():
-    __natural = input('Введите число от 0 до 100: ')
-    try:
-        __natural = int(__natural)
-        assert __natural>0 and __natural<100
-        return __natural
-    except ValueError:
-        print('Вы ввели не число')
-        return natural_input()
-    except AssertionError:
-        print('Вы ввели число вне диапазона')
-        return natural_input()
-
-def guess_game(riddle,answer,tries = 10):
-    if riddle == answer:
-        print(f'Вы отгадали число {riddle}')
-    elif tries == 0:
-        print(f'Вы не отгадали число {riddle}')
-    else:
-        if(riddle>answer):
-            print('Загаданное число больше чем предложенное')
-        else:
-            print('Загаданное число меньше чем предложенное')
-        print(f'У вас осталось попыток: {tries-1}')
-        guess_game(riddle, natural_input(), tries-1)
-
-guess_game(randint(0,100),natural_input())
