0b093f1b27b35b8582bce65eaddddfcc9edfdd7d
alek99 <alekseibarannikov@mail.ru>
2020-12-21 01:00:48 +0800

    Homework #1

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,39 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+
+class Stack:
+    def __init__(self, max_size):
+        self.items = [[]]
+        self.max_size = max_size
+
+    def isEmpty(self):
+        return self.items == [[]]
+
+    def push(self, element):
+        if len(self.items[len(self.items)-1]) < self.max_size:
+            self.items.append(element)
+        else:
+            self.items.append([])
+            self.items[len(self.items)-1].append(element)
+
+    def pop(self):
+        result = len(self.items[len(self.items)-1]).pop()
+        if len(self.items[len(self.items)-1]) == 0:
+            self.items.pop()
+        return result
+
+    def __str__(self):
+        return str(self.items)
+
+    def get_value(self):
+        return self.items[len(self.items) - 1][len(self.items[len(self.items)-1])-1]
+
+    def size(self):
+        items_summary = 0
+        for stack in self.items:
+            items_summary += len(stack)
+        return items_summary
+    def stack_count(self):
+        return len(self.items)
