3aa4371de692ab0fa456d3fecdfdf2b553080e20
philippov <bervr@mail.ru>
2020-12-22 13:38:08 +0400

    6 done

---
+++ Урок 1. Практическое задание/queue.py
@@ -0,0 +1,16 @@
+class QueueClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def to_queue(self, item):
+        self.elems.insert(0, item)
+
+    def from_queue(self):
+        return self.elems.pop()
+
+    def size(self):
+        return len(self.elems)
+# хороший код, не будем его ломать
