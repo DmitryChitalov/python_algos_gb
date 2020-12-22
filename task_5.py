460c57da357b323ca1ec5ba3036e380e878b3c71
volga52 <volga52@bk.r>
2020-12-22 23:36:39 +0300

    Lesson1

---
+++ task_5.py
@@ -0,0 +1,62 @@
+"""
+Задание 6.
+Задание на закрепление навыков работы со стеком
+
+Примечание: в этом задании вспомните ваши знания по работе с ООП
+и опирайтесь на пример урока
+
+Реализуйте структуру "стопка тарелок".
+
+Мы можем складывать тарелки в стопку и при превышении некоторого значения
+нужно начать складывать тарелки в новую стопку.
+
+Структура должна предусматривать наличие нескольких стеков.
+Создание нового стека происходит при достижении предыдущим стеком порогового значения.
+Реализуйте по аналогии с примером, рассмотренным на уроке, необходимые методы,
+для реализации это структуры, добавьте новые методы (не рассмотренные в примере с урока)
+для реализации этой задачи.
+
+После реализации структуры, проверьте ее работу на различных сценариях
+"""
+
+
+class PlateStackClass:
+    def __init__(self, max_size):
+        self.elems = [[]]
+        self.max_size = max_size    # размер стопки
+
+    def __str__(self):
+        return str(self.elems)
+
+    def is_empty(self):
+        return self.elems == [[]]
+
+    def push_in(self, el):
+        """Предполагаем, что верхний элемент стека находится в конце списка
+        если размер стопки равен пороговому значению то создается новая стопка и туда кладется значние"""
+        if len(self.elems[len(self.elems)-1]) < self.max_size:
+            self.elems[len(self.elems)-1].append(el)
+        else:
+            self.elems.append([])
+            self.elems[len(self.elems) - 1].append(el)
+
+    def pop_out(self):
+        """Берем тарелку из крайней стопки, если она пустая удаляем ее"""
+        result = self.elems[len(self.elems) - 1].pop()
+        if len(self.elems[len(self.elems) - 1]) == 0:
+            self.elems.pop()
+        return result
+
+    def get_val(self):
+        return self.elems[len(self.elems) - 1][len(self.elems[len(self.elems) - 1])-1]
+
+    def stack_size(self):
+        """Общее колличество тарелок"""
+        elem_sum = 0
+        for stack in self.elems:
+            elem_sum += len(stack)
+        return elem_sum
+
+    def stack_count(self):
+        """Количество стоек"""
+        return len(self.elems)
