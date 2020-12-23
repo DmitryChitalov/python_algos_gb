478f296743975a9ba094e300e17de5fc8df70b2d
Andrey <pranehs@mail.ru>
2020-12-24 00:20:49 +0300

    Домащнее задание № 7

--- Урок 2. Практическое задание/task_7.py
+++ Урок 2. Практическое задание/task_7.py
@@ -6,3 +6,22 @@
  Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+import sys
+
+sys.setrecursionlimit(10000)
+
+
+# Переменная i нужна для того, чтобы значение left result оставалось постоянным
+# Не могу понять, за счет чего у меня переполняется стек
+def check_equality(n, right_result=0, left_result=1, i=0):
+    i += 1
+    if right_result == left_result:
+        print(f'{right_result} = {left_result}')
+    else:
+        i += 1
+        return check_equality(n - 1, right_result + n, (n + i) * (n + i + 1) / 2)
+
+
+print(check_equality(3))
+print(check_equality(4))
