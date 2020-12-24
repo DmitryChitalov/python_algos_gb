c05be31f464f1cee58cdbecc1bdf4fa110dad57d
Andrey <pranehs@mail.ru>
2020-12-24 21:41:27 +0300

    Задание № 7

--- Урок 2. Практическое задание/task_7.py
+++ Урок 2. Практическое задание/task_7.py
@@ -7,21 +7,14 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
 
-import sys
-
-sys.setrecursionlimit(10000)
-
 
 # Переменную i использую для того, чтобы значение left result оставалось постоянным
-# Не могу понять, за счет чего у меня переполняется стек
-def check_equality(n, right_result=0, left_result=1, i=0):
+def check_equality(n, right_result=0, left_result=1, i=-1):
     i += 1
     if right_result == left_result:
         print(f'{right_result} = {left_result}')
     else:
-        i += 1
-        return check_equality(n - 1, right_result + n, (n + i) * (n + i + 1) / 2)
+        return check_equality(n - 1, right_result + n, (n + i) * (n + i + 1) / 2, i)
 
 
-print(check_equality(3))
-print(check_equality(4))
+print(check_equality(15))
