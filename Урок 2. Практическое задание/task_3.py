67499f79e31ad0c7a08aa46c14e4fb0e3cd23100
AlexeyVertoprakhov <64571185+AlexeyVertoprakhov@users.noreply.github.com>
2020-12-24 15:53:03 +1000

    Update task_3.py

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -17,8 +17,8 @@
 """
 
 
-def rec(n, i):
-    return i if (n == 0) else rec(n // 10, i * 10 + n % 10)
+def rec(a, i):
+    return i if (a == 0) else rec(a // 10, i * 10 + a % 10)
 
 
 print(rec(int(input("Введите любое целое число : ")), 0))
