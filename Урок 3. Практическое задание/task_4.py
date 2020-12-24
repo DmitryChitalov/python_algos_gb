a67d7e90902a876b2857f4db96ebac381fcc4e44
AlexeyVertoprakhov <64571185+AlexeyVertoprakhov@users.noreply.github.com>
2020-12-24 15:59:23 +1000

    Update task_4.py

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,21 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+
+class cache(object):
+    def __init__(self, fun):
+        self.fun = fun
+        self.cache = {}
+
+    def __call__(self, *args, **kwargs):
+        key  = str(args) + str(kwargs)
+        try:
+            return self.cache[key]
+        except KeyError:
+            self.cache[key] = rval = self.fun(*args, **kwargs)
+            return rval
+        except TypeError: # incase key isn't a valid key - don't cache
+            return self.fun(*args, **kwargs)
+@cache
+def get_url_src(url):
+    return urllib.urlopen(url).read()
