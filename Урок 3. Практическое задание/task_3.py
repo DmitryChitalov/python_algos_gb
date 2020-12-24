58a0726da3cf02ca40991a94aac586d8b928d88a
philippov <bervr@mail.ru>
2020-12-24 17:27:36 +0400

    3 done

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,40 @@
 р
 а
 """
+import hashlib
+
+
+def cut_string(my_str):
+    hlist = []
+    rlist = []
+    l = len(my_str) + 1
+    for i in range(l):
+        for j in range(l):
+            under_str = my_str[i:j]
+            if under_str != '' and under_str != my_str:
+                hlist.append(hashlib.sha256(under_str.encode()).hexdigest())
+                rlist.append(under_str)
+    # uniqs = len(list(set(reslt.keys())))
+    return rlist, hlist
+
+
+def count_set(my_str):
+    my_str = cut_string(my_str)[0]
+    uniqs = len(list(set(my_str)))
+    return uniqs
+
+
+def count_hash(my_str):
+    my_str = cut_string(my_str)[1]
+    uniqs = len(list(set(my_str)))
+    return uniqs
+
+
+if __name__ == '__main__':
+    print(count_set('pарa'))
+    print(count_set('papa'))
+
+    print(count_hash('pарa'))
+    print(count_hash('papa'))
+
+# И все же - зачем здесь хеш?  множества же сделали бы тоже самое
