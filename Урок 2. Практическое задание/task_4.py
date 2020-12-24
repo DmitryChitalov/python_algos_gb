7a079ac66dd91b56d831ff4ddceef189cc902b08
AlexeyVertoprakhov <64571185+AlexeyVertoprakhov@users.noreply.github.com>
2020-12-24 15:53:39 +1000

    Update task_4.py

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -9,19 +9,18 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
-def sum_arr(arr, size):
-    if (size == 0):
+def sum_digit(digit, lenth):
+    if (lenth == 0):
         return 0
     else:
-        return arr[size - 1] + sum_arr(arr, size - 1)
-n = int(input("Введите длину ряда чисел для вычисления : "))
+        return digit[lenth - 1] + sum_digit(digit, lenth - 1)
+dig_lenth = int(input("Введите длину ряда чисел для вычисления : "))
 a = []
-for i in range(0, n):
+for i in range(0, dig_lenth):
     element = float(input("Введите любое число ( вместо знака ',' используется '.' : "))
     a.append(element)
 print("Все введенные числа : ")
 print(a)
 print("Сумма всех введенных чисел равна : ")
-b = sum_arr(a, n)
+b = sum_digit(a, dig_lenth)
 print(b)
-
