d33382309f65f8e33a3f0b4424f8fe8c0d16e62e
Vitaly <kazancevvitallijj@gmail.com>
2020-12-25 16:23:03 +0800

    start task3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,22 @@
 р
 а
 """
+import hashlib
+
+
+def substring(string):
+    substring_set = set()
+    for i in range(len(string)):
+        for j in range(len(string)-1 if i == 0 else len(string), i, -1):
+            # print(string[i], string[j])
+            # print(string[i:j])
+            substring_set.add(hashlib.sha256(bytes(string[i:j], 'cp1251')))
+    return substring_set
+
+
+# строка filder
+# строка filfil
+amount_substing = len(substring(input('Введите строку:\n')))
+print(f'Количество различных подстрок в строке: {amount_substing}')
+
+
