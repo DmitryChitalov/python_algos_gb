71f2b4697ef4083ec6d5fe39e4473a242b3dedc0
Anna <sem.mm@mail.ru>
2020-12-25 13:22:27 +1000

    lesson 2

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -9,3 +9,26 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+<<<<<<< Updated upstream
+=======
+from random import randint
+
+def random_number(numb, count=10):
+    rand_number = int(input('Enter number: '))
+    count -= 1
+    if count == 0:
+        return f'Game over: the right number is : {numb}'
+    elif numb == rand_number:
+        return f'You won! Game over'
+    elif numb > rand_number:
+        print(f'The number is bigger, you have {count} tries')
+        return random_number(numb, count)
+    elif numb < rand_number:
+        print(f'The number is  smaller, you have {count} tries')
+        return random_number(numb, count)
+    
+numb = randint(1, 100)
+print(random_number(numb))
+
+
+>>>>>>> Stashed changes
