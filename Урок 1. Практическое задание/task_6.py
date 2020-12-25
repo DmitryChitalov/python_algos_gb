1fa3be54d4c7f3d729916d9af9bcc5072b775acb
Temujen <70853088+Temujen@users.noreply.github.com>
2020-12-25 08:50:12 +0300

    Add task_6
    Solve task_6

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -15,3 +15,64 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+class QueueClass:
+    def __init__(self):
+    	self.base = []
+    	self.solve = []
+    	self.reject = []
+			
+    def is_empty_base(self):
+    	return self.base == []
+
+    def to_queue_base(self, item):
+    	self.base.insert(0, item)
+
+    def from_queue_base(self, to_queue):
+    	if to_queue == 'solve':
+    		task = self.base.pop()
+    		self.solve.append(task)
+    		return task
+    	elif to_queue == 'reject':
+    		task = self.base.pop()
+    		self.reject.insert(0, task)
+    		return task
+    	else:
+    		print('Выберите очередь solve или reject')
+			
+    def list_queue(self, queue):
+    	if queue == 'base':
+    		print(f"Очередь задач {self.base}")
+    	elif queue == 'solve':
+    		print(f"Список решенных задач {self.solve}")
+    	elif queue == 'reject':
+    		print(f"Список на доработку {self.reject}")
+    	else:
+    		print('Выберите один из списков: base, solve, reject')
+    
+    def from_queue_reject(self):
+    	task = self.reject.pop()
+    	self.solve.append(task)
+    	return task
+    			
+    def size(self):
+      return len(self.base)
+
+
+ToDoList = QueueClass()
+ToDoList.to_queue_base('1')
+ToDoList.to_queue_base('2')
+ToDoList.to_queue_base('3')
+ToDoList.to_queue_base('4')
+
+
+ToDoList.from_queue_base('solve')
+ToDoList.from_queue_base('reject')
+ToDoList.from_queue_base('reject')
+ToDoList.from_queue_reject()
+ToDoList.from_queue_reject()
+
+ToDoList.list_queue('base')
+ToDoList.list_queue('reject')
+ToDoList.list_queue('solve')
+
