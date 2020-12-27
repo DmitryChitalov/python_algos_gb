0245bcd5e51d5f2124e07d85d1b225cf9d70f9e3
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2020-12-27 17:11:53 +0300

    task 6

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,5 +7,30 @@
 
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
-Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+import random
+
+
+def guess_num(number, n):
+    if n == 10:
+        print(f'Вы не смогли угадать число за 10 попыток. Загаданное число: {number}')
+        return
+    else:
+        try:
+            user_number = int(input('Введите число:'))
+            if number > user_number:
+                print(f'Ваше число {user_number} меньше загаданного')
+                guess_num(number, n + 1)
+            elif number < user_number:
+                print(f'Ваше число {user_number} больше загаданного')
+                guess_num(number, n + 1)
+            else:
+                print(f'Вы угадали число! {number}')
+                return
+        except ValueError:
+            print('Вы ввели не число! Попробуйте еще раз')
+            guess_num(number, n)
+
+
+rand_number = random.randint(0, 100)
+guess_num(rand_number, 1)
