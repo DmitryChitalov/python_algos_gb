a31c40ad00b9be351cfebd96c9309d480d1b93e9
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2020-12-27 17:18:54 +0300

    task 7

--- Урок 2. Практическое задание/task_7.py
+++ Урок 2. Практическое задание/task_7.py
@@ -6,3 +6,27 @@
  Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+def left_sum(n, start, result):
+    if start > n:
+        return result
+    else:
+        result += start
+        return left_sum(n, start + 1, result)
+
+
+def right_sum(n):
+    return n * (n + 1) / 2
+
+
+def check_form(n):
+    left = left_sum(n, 1, 0)
+    right = right_sum(n)
+    if left == right:
+        print('Левая и правая части равны')
+    else:
+        print('Левая и правая части неравны')
+
+
+check_form(10)
