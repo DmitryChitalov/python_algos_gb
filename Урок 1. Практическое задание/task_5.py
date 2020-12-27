1ddab9f0d047df17a496af4fe092cd5c3b41eb31
Sergey Pedchenko <pedchenko.sergey@gmail.com>
2020-12-27 13:33:19 +0300

    The solution for DZ1

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,93 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+
+class Plates:
+    def __init__(self):
+        self.plates = []
+
+    def __str__(self):
+        return str(self.plates)
+
+    def __len__(self):
+        return len(self.plates)
+
+    def push_in(self, el):
+        self.plates.append(el)
+
+    def push_out(self):
+        return self.plates.pop()
+
+    def get_val(self):
+        return self.plates[len(self.plates) - 1]
+
+
+class StackClass:
+    def __init__(self, max_size):
+        self.elems = []
+        self.max_size = max_size
+
+    def is_empty(self):
+        return self.elems == []
+
+    def __str__(self):
+        my_str = ''
+        for el in self.elems:
+            my_str += str(el)
+        return my_str
+
+    def push_in(self, el):
+        if len(self.elems) == 0 or len(self.elems[len(self.elems) - 1]) >= self.max_size:
+            self.elems.append(Plates())
+            self.elems[len(self.elems) - 1].push_in(el)
+        else:
+            self.elems[len(self.elems) - 1].push_in(el)
+
+    def pop_out(self):
+        if len(self.elems[len(self.elems) - 1]) > 0:
+            buf = self.elems[len(self.elems) - 1].push_out()
+            if len(self.elems[len(self.elems) - 1]) == 0:
+                self.elems.pop()
+            return buf
+
+    def get_val(self):
+        return self.elems[len(self.elems) - 1].get_val()
+
+    def stack_size(self):
+        return len(self.elems)
+
+
+SC_OBJ = StackClass(2)
+
+print(SC_OBJ.is_empty())
+
+
+SC_OBJ.push_in('Plate1')
+SC_OBJ.push_in('Plate2')
+SC_OBJ.push_in('Plate3')
+print(SC_OBJ)
+SC_OBJ.pop_out()
+print(SC_OBJ)
+SC_OBJ.push_in('Plate4')
+SC_OBJ.push_in('Plate5')
+SC_OBJ.push_in('Plate6')
+SC_OBJ.push_in('Plate7')
+print(SC_OBJ)
+print(SC_OBJ.get_val())
+SC_OBJ.pop_out()
+print(SC_OBJ)
+SC_OBJ.pop_out()
+SC_OBJ.pop_out()
+print(SC_OBJ)
+SC_OBJ.push_in('Plate8')
+SC_OBJ.push_in('Plate9')
+SC_OBJ.push_in('Plate10')
+SC_OBJ.push_in('Plate11')
+print(SC_OBJ)
+SC_OBJ.pop_out()
+SC_OBJ.push_in('Plate12')
+print(SC_OBJ)
+
+print(SC_OBJ.get_val())
+
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -15,3 +15,76 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+
+class QueueClass:
+    def __init__(self):
+        self.elems = []
+
+    def __str__(self):
+        return str(self.elems)
+
+    def is_empty(self):
+        return self.elems == []
+
+    def to_queue(self, item):
+        self.elems.insert(0, item)
+
+    def from_queue(self):
+        return self.elems.pop()
+
+    def size(self):
+        return len(self.elems)
+
+
+class Board:
+    def __init__(self):
+        self.tasks = {
+            'initial': QueueClass(),
+            'resolving': QueueClass(),
+            'solved': QueueClass(),
+        }
+
+    def __str__(self):
+        my_str = ''
+        for key, value in self.tasks.items():
+            my_str += str(key) + ':' + str(value) + '\n'
+        return my_str
+
+    def to_board(self, queue, el):
+        self.tasks[queue].to_queue(el)
+
+    def to_solved(self):
+        self.tasks['solved'].to_queue(self.tasks['initial'].from_queue())
+
+    def to_resolving(self):  # Здесь можно отдавать в порядке общей очереди задачи на доработку,
+        # что не совсем эффективно, но соответствует условиям задачи
+        self.tasks['resolving'].to_queue(self.tasks['solved'].from_queue())
+
+
+if __name__ == '__main__':
+    qc_obj = Board()
+
+    qc_obj.to_board('initial', 'task1')
+    qc_obj.to_board('initial', 'task2')
+    qc_obj.to_board('initial', 'task2')
+    print(qc_obj)
+    qc_obj.to_solved()
+    print(qc_obj)
+    qc_obj.to_board('initial', 'task3')
+    qc_obj.to_board('initial', 'task4')
+    qc_obj.to_board('initial', 'task5')
+    print(qc_obj)
+    qc_obj.to_solved()
+    qc_obj.to_solved()
+    print(qc_obj)
+    qc_obj.to_resolving()
+    print(qc_obj)
+    qc_obj.to_resolving()
+    print(qc_obj)
+    qc_obj.to_board('initial', 'task6')
+    qc_obj.to_board('initial', 'task7')
+    qc_obj.to_board('initial', 'task8')
+    print(qc_obj)
+
+
