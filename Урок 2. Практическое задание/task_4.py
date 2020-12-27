35cc0b580e3d77ea29d50c32b4676553de9b3017
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2020-12-27 16:35:38 +0300

    task 4

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -9,3 +9,19 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+def sum_row(n, number, result):
+    if n == 0:
+        return result
+    else:
+        new_number = (number / 2) * -1
+        result += number
+        return sum_row(n - 1, new_number, result)
+
+
+try:
+    number_from_input = int(input('Введите количество элементов:'))
+    print(sum_row(number_from_input, 1, 0))
+except ValueError:
+    print('Вы ввели не число')
