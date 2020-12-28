4a21eeefb71cb83e5aedfeeb0644e863fd60e7af
NaumovAleksei <naumov2407@gmail.com>
2020-12-29 00:50:15 +0300

    lesson 3 done

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,20 @@
 р
 а
 """
+import hashlib
+
+my_string = input('Введите строку: ')
+result = set()
+
+elem_string = len(my_string)
+for i in range(elem_string):
+    if i == 0:
+        elem_string = len(my_string) - 1
+    else:
+        elem_string = len(my_string)
+    for j in range(elem_string, i, -1):
+        # print(my_string[i:j])
+        result.add(hashlib.sha1(my_string[i:j].encode('utf-8')).hexdigest())
+# print(result)
+
+print(f'Количество уникальных вхождений в строке \"{my_string}\" равно {len(result)}')
