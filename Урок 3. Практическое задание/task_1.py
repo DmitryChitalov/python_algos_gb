e307006e6cbbb1117598dbfbecee61a99c5c9ab4
mifrok <mifra@mail.ru>
2020-12-28 09:00:58 +0700

    lesson_3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -10,3 +10,59 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+from time import time
+
+
+def show_operation_time(func):
+    def g(data):
+        start = time()
+        func(data)
+        stop = time()
+        print(stop - start)
+        return
+
+    return g
+
+
+start = time()
+my_list = [i for i in range(999999)]
+stop = time()
+print(stop - start)  # 0.06697845458984375
+start = time()
+my_dict = {f'{i}': i for i in range(999999)}
+stop = time()
+print(stop - start)  # 0.14095330238342285
+
+
+start = time()
+for i in my_list:
+    my_list[i] = 555
+stop = time()
+print(stop - start)  # 0.07097673416137695
+
+start = time()
+for i in my_dict.keys():
+    my_dict[i] = 555
+stop = time()
+print(stop - start)  # 0.20893168449401855
+
+
+@show_operation_time
+def my_list_sort(list):
+    sorted(list, reverse=True)
+    return
+
+
+@show_operation_time
+def my_dict_sort(dict):
+    sorted(dict.items(), key=lambda item: item[1], reverse=True)
+    return
+
+
+my_list_sort(my_list)
+my_dict_sort(my_dict)
+
+# По всем операциям время выполнения меньше со списками.
+# По причине того, что в словаре используется дополнительно хеш
+# это объясняет более долгое создание словаря
+# При вводе нового значения хеш так же пересчитывается для элемента хеш таблицы
--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,21 @@
 р
 а
 """
+import hashlib as h
+s = 'рара'
+my_set = set()
+
+def my_hash(s):
+    return h.sha1(s.encode('utf-8')).hexdigest()
+
+def check_str(string):
+    for i in range(1, len(string)):
+        for j in range(len(string)):
+            if my_hash(string[j:i+j]) in my_set:
+                continue
+            else:
+                my_set.add(my_hash(string[j:i+j]))
+                print(string[j:i+j])
+    return
+
+check_str(s)
