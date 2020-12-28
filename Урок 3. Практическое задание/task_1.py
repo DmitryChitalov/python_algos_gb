ea44e31793b7b79b6b22e0ca1015cf7566b788d4
Kllraz <kllraz@yandex.ru>
2020-12-28 13:56:11 +0300

    HW for lesson3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -10,3 +10,66 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+from time import time
+
+
+def time_dec(func):
+    def g(*args, **kwargs):
+        start = time()
+        result = func(*args, **kwargs)
+        stop = time()
+        print(f"Время выполнения - {stop - start}")
+
+        return result
+
+    return g
+
+
+@time_dec
+def fill_list(num_elems):
+    return list(i for i in range(num_elems))
+
+
+@time_dec
+def fill_dict(num_elems):
+    return {i: i for i in range(num_elems)}
+
+
+"""
+Время заполнения словаря больше, чем списка, т.к. при создании ключа в словаре генерируется его хеш
+и добавляется в хеш-таблицу
+"""
+fill_list = fill_list(100)
+fill_dict = fill_dict(100)
+
+
+@time_dec
+def from_dict(dict: dict, key):
+    return dict[key]
+
+
+@time_dec
+def from_list(list: list, key):
+    return list[key]
+
+
+@time_dec
+def getall_from_dict(dict: dict):
+    for key, value in dict.items():
+        print(f"{key}:{value}")
+
+
+@time_dec
+def getall_from_list(list: list):
+    for i in list:
+        print(i)
+
+
+print(from_dict(fill_dict, 1))
+print(from_list(fill_list, 2))
+print(getall_from_dict(fill_dict))
+print(getall_from_list(fill_list))
+
+"""
+По результатам получение данных из словарей быстрее, чем из списков, т.к. в словарях используется хещ-дерево
+"""
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,27 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+import hashlib
+
+
+class Cash(object):
+    salt = "This is Salt"
+
+    def __init__(self):
+        self.cash = {}
+
+    def add_url(self, url: str):
+        hash = hashlib.sha256(self.salt.encode() + url.encode())
+
+        if self.cash.get(url):
+            return False
+
+        self.cash[url] = hash
+
+        return True
+
+
+if __name__ == "__main__":
+    cash = Cash()
+    print(cash.add_url("github.com"))
+    print(cash.add_url("github.com"))
