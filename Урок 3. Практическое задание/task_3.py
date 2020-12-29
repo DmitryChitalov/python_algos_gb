7fd90c16d71b2a3606a300a850b20ba5e8a6b743
Temujen <70853088+Temujen@users.noreply.github.com>
2020-12-29 21:11:24 +0300

    Add task_3
    Solve task_3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,20 @@
 р
 а
 """
+import hashlib
+
+def check_string():
+	string = input('Введите строку:')
+	count = 0
+	a = set()
+	for i in range(len(string) - 1):
+		for j in range(i+1, len(string) + 1):
+			if len(string[i:j]) < len(string):
+				print(string[i:j])
+				a.add(hashlib.sha256(string[i:j].encode('utf-8')).hexdigest())
+				count += 1
+	print(f'Из {count} сочетаний, уникальных {len(a)}')
+	return a
+
+a = check_string()
+
