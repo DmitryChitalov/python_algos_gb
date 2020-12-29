f9ce53488a56c889aff128488167ae986dac73db
Temujen <70853088+Temujen@users.noreply.github.com>
2020-12-29 21:17:01 +0300

    Update task_4

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -14,13 +14,14 @@ import hashlib
 
 def check_url():
 	a = []
+	salt = b'coding web pages'
 	while True:
 		url = input('Enter url or q for exit: ')
 		if url == 'q':
 			print('Finish programm\n')
 			return 
 		else:
-			h = hashlib.sha256(url.encode('utf-8')).hexdigest()
+			h = hashlib.sha256(url.encode('utf-8') + salt).hexdigest()
 			if h in a:
 				print('Web site open\n')
 			else:
