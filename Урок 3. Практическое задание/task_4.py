d066739af3a9ae388fa3d3e1782cace2bbc1ba6c
Temujen <70853088+Temujen@users.noreply.github.com>
2020-12-29 21:12:49 +0300

    Add task_4
    Solve task_4

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,24 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+
+import hashlib
+
+
+def check_url():
+	a = []
+	while True:
+		url = input('Enter url or q for exit: ')
+		if url == 'q':
+			print('Finish programm\n')
+			return 
+		else:
+			h = hashlib.sha256(url.encode('utf-8')).hexdigest()
+			if h in a:
+				print('Web site open\n')
+			else:
+				a.append(h)
+				print("Web site added to chache\n")
+				
+				
+check_url()
