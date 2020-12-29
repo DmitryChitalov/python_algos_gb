0292fb8227bd73ebca2b77356ddd28d7154e9722
MerezhkOlga <MerezhkOly@gmail.com>
2020-12-29 19:50:29 +0300

    4 задания к уроку 3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,19 @@
 р
 а
 """
+
+import hashlib
+
+s = 'papa'
+substrings = set()
+
+for i in range(len(s)):
+    for j in range(i+1, len(s)+1):
+        srez = hashlib.md5(s[i:j].encode()).hexdigest()
+        # if srez !=s : # Никак не пойму, как сделать так, чтобы вcя строка нe бралась тоже.
+        # Если убираю +1 от len(s)+1, то теряю значение...
+        substrings.add(srez)
+
+substrings.remove(hashlib.md5(s.encode()).hexdigest())  # Наверное, с точки зрения оптимизации так лучше, чем лишний if
+print(substrings)
+print(f'В строке {len(substrings)} уникальных подстрок.')
