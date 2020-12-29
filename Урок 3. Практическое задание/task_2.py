72e6ace55a6b5cde9f6abac867320b13f52f8620
Екатерина Осенина <ekaterinaosenina@macbook.osenin.me>
2020-12-29 17:43:22 +0300

    homework3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -15,3 +15,49 @@
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
+import hashlib
+from uuid import uuid4
+
+
+def check_user(user_name, password):
+    user_data = users.get(user_name)
+    if user_data is None:
+        print('Такого пользователя нет')
+        return False
+    check_pass = hashlib.sha256(user_data[1].encode() + password.encode()).hexdigest()
+    if user_data[0] == check_pass:
+        return True
+    return False
+
+
+def add_user(name, password):
+    user_data = users.get(name)
+    if user_data is None:
+        salt = uuid4().hex
+        pass_hash = hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+        print(f'хеш: {pass_hash}\tсоль: {salt}')
+        users.update({name: [pass_hash, salt]})
+        return True
+    return False
+
+
+users = {}
+for i in range(10):
+    add_user('user' + str(i), str(i)*4)
+while True:
+    ans = input('Выберите действие (+ - добавить пользователя, * - проверить пароль пользователя, 0 - выход):')
+    if ans == '0':
+        break
+    if ans == '+' or ans == '*':
+        u_name = input('Введите имя пользователя: ')
+        u_pass = input('Введите пароль: ')
+        if ans == '+':
+            if add_user(u_name, u_pass):
+                print('Пользователь добавлен!')
+            else:
+                print('Ошибка! Не удалось добавить пользователя!')
+        else:
+            if check_user(u_name, u_pass):
+                print('Пароль верный!')
+            else:
+                print('Пароль неверный!')
