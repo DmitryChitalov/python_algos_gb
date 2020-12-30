4e24edd504eb8829368859270f3035917f094a27
Sergey Pedchenko <pedchenko.sergey@gmail.com>
2020-12-30 13:39:40 +0300

    The solution for DZ2

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -9,3 +9,31 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+import random
+
+
+def guess_rec(number, TTL=0, my_try=1):
+    try:
+        if my_try > TTL:
+            return f"У Вас не получилось угадать - закончились попытки, число: {number}"
+        else:
+            answer = int(input("Введите целое число от 0 до 100 "))
+            if answer == number:
+                return f'Вы угадали число - {number}, Поздравляю!'
+            else:
+                if answer < number:
+                    print('Число больше!')
+                    return guess_rec(number, TTL, my_try + 1)
+                else:
+                    print('Число меньше!')
+                    return guess_rec(number, TTL, my_try + 1)
+
+    except ValueError:
+        return guess_rec(number, TTL, my_try + 1)
+
+
+TOTAL_TRIES = 10
+
+print(guess_rec(random.randint(0, 100), TOTAL_TRIES))
+
