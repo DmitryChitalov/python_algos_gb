a320a561e96d4590f29990147f4abb810fda842a
vagaganova <69433218+vagaganova@users.noreply.github.com>
2020-12-30 14:22:14 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,22 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = []
+    index = 0
+    for i in nums:
+        if i % 2 == 0:
+            new_arr.append(index)
+        index += 1
+    return new_arr
+
+
+if __name__ == '__main__':
+    print(timeit("func_1([41,42,43,45,44,53,34,35,54,67,80,90,97,102])", setup="from __main__ import func_1"))
+    print(timeit("func_2([41,42,43,45,44,53,34,35,54,67,80,90,97,102])", setup="from __main__ import func_2"))
+
+'''
+Убрала генерацию дополнительного массива и вычисление длины массива заменив на доп переменную и инкремент после каждого цикла
+'''
