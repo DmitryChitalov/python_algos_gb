3b3b4c97a887abe6fb06a207388294af13bf788a
SergeyLomakin <sergeylomakinaals@mail.ru>
2020-12-30 14:25:11 +0500

    lesson 4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -9,16 +9,18 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+import cProfile
+import timeit
 
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return revers_num
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        return revers(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -34,3 +36,25 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+n = 1234567890
+
+print(cProfile.run('revers(n)'))
+print(cProfile.run('revers_2(n)'))
+print(cProfile.run('revers_3(n)'))
+''' Через cProfile все функции дали хороший результат,
+в функции revers идёт большее количество вызовов из-за рекурсии.
+'''
+
+print(timeit.timeit('revers(n)', setup='from __main__ import revers, n', number=100000))
+print(timeit.timeit('revers_2(n)', setup='from __main__ import revers_2, n', number=100000))
+print(timeit.timeit('revers_3(n)', setup='from __main__ import revers_3, n', number=100000))
+'''Через модуль timeit видно, что:
+фун revers_3 быстрее всех, т.к. используется срез строки
+фун revers_2 средняя по скорости из-за вычислений в цикле
+а фун revers медленнее из-за использования рекурсии
+Вывод для решения такой задачи использовать функцию revers_3, со срезом строки
+revers    # 0.29408631600017543
+revers_2  # 0.18526349599960668
+revers_3  # 0.040194991000134905
+'''
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -9,6 +9,7 @@
 Попытайтесь написать третью версию, которая будет самой быстрой.
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
 """
+import timeit
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -37,5 +38,23 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    max_el = max(array, key=array.count)
+    return f'Чаще всего встречается число {max_el}, ' \
+           f'оно появилось в массиве {array.count(max_el)} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+
+print(timeit.timeit('func_1()', setup='from __main__ import func_1', number=100000))
+print(timeit.timeit('func_2()', setup='from __main__ import func_2', number=100000))
+print(timeit.timeit('func_3()', setup='from __main__ import func_3', number=100000))
+
+''' Быстрее всего работает фун func_3,
+с использование встроенных функций (max) и методов (.count)
+func_1  # 0.17179322900028637
+func_2  # 0.1957220350004718
+func_3  # 0.1567900660002124
+'''
