5d70793513056d6dbdc60ddc11e9509a20dda925
Belyaev Mikhail <68550956+BelyaevMikhail@users.noreply.github.com>
2020-12-30 16:51:37 +0300

    Update task_4.py

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -9,3 +9,16 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+def sum_numbers(i, number, counting, numbers_sum):
+
+    if i == counting:
+        print(f"Количество элементов - {counting}, их сумма - {numbers_sum}")
+    elif i < counting:
+        return sum_numbers(i + 1, number / 2 * -1, counting, numbers_sum+number)
+
+
+try:
+    result_counting = int(input("Введите количество элементов: "))
+    sum_numbers(0, 1, result_counting, 0)
+except ValueError:
+    print("Вместо числа вы ввели строку")
