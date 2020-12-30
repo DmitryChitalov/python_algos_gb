a37ecca9e04fb19273242f014ae99d5ec6f989b0
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2020-12-30 16:01:41 +0300

    task 1 with decorator

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -13,16 +13,58 @@
 import time
 
 
-start = time.time()
-lst = []
-for i in range(1000):
-    lst.append(i)
-end = time.time()
-print('list test =>>>', end-start)
+def benchmark(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name__, end - start)
+        return res
 
-start = time.time()
-dict = {}
-for i in range(1000):
-    dict[i] = i
-end = time.time()
-print('dict test =>>>', end - start)
+    return wrapper
+
+
+@benchmark
+def init_list():
+    lst = []
+    for i in range(100000):
+        lst.append(i)
+    return lst
+
+
+@benchmark
+def init_dict():
+    dict = {}
+    for i in range(100000):
+        dict[i] = i
+    return dict
+
+
+@benchmark
+def add_to_list(lst):
+    for i in range(100000):
+        lst.append(i)
+
+
+@benchmark
+def add_to_dict(dict):
+    for i in range(100000, 200000):
+        dict[i] = i
+
+
+@benchmark
+def get_from_list(lst):
+    return lst[100000]
+
+
+@benchmark
+def get_from_dict(dict):
+    return dict.get(100000)
+
+
+list = init_list()
+dict = init_dict()
+add_to_list(list)
+add_to_dict(dict)
+list_el = get_from_list(list)
+dict_el = get_from_dict(dict)
