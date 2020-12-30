1351c2ec85d6438ce0a7c53bc7dbaf7f996d7613
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2020-12-30 16:49:28 +0300

    task 4

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,30 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете усложнить задачу, реализовав ее через ООП
 """
+import hashlib
+from uuid import uuid4
+
+salt = uuid4().hex
+cache = {}
+
+
+def add_to_cache(url):
+    hash = hashlib.sha256(url.encode() + salt.encode()).hexdigest()
+    if hash not in cache:
+        cache[hash] = url
+
+
+urls = [
+    'www.yandex.ru',
+    'www.yandex.ru/test',
+    'www.yandex.ru/test',
+    'www.yandex.ru/test',
+    'www.yandex.ru/test',
+    'www.yandex.ru/test1',
+    'www.yandex.ru/test2',
+]
+
+for url in urls:
+    add_to_cache(url)
+
+print(cache)
