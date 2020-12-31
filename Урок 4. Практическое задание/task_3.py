0cb7427c59bf68d85088596e0e9fe8680d3a0006
IgoryanM <69872101+IgoryanM@users.noreply.github.com>
2020-12-31 23:00:07 +0300

    hw_4
    hw_4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,6 +11,10 @@
 """
 
 
+import timeit
+import cProfile
+
+
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
         return
@@ -34,3 +38,21 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+def main():
+    revers(12345678901)
+    revers_2(12345678901)
+    revers_3(12345678901)
+
+
+cProfile.run('main()')
+cProfile.run('revers(12345678901)')
+
+print('revers   ', timeit.timeit("revers(12345678901)", "from __main__ import revers", number=1000))
+print('revers_2 ', timeit.timeit("revers_2(12345678901)", "from __main__ import revers_2", number=1000))
+print('revers_3 ', timeit.timeit("revers_3(12345678901)", "from __main__ import revers_3", number=1000))
+
+# cProfile показал 0.000 для всех функций, но timeit показал, что самая быстрая функция revers_3
+# revers    0.005522200000000005
+# revers_2  0.005540599999999979
+# revers_3  0.0006726999999999983
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -10,6 +10,10 @@
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
 """
 
+
+import timeit
+
+
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -37,5 +41,26 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    array_count = [array.count(el) for el in array]
+    max_count = max(array_count)
+    max_count_elem = array[array_count.index(max_count)]
+    return f'Чаще всего встречается число {max_count_elem}, ' \
+           f'оно появилось в массиве {max_count} раз(а)'
+
 print(func_1())
+print(max(timeit.repeat('func_1()', "from __main__ import func_1", repeat=10, number=1000)))
 print(func_2())
+print(max(timeit.repeat('func_2()', "from __main__ import func_2", repeat=10, number=1000)))
+print(func_3())
+print(max(timeit.repeat('func_3()', "from __main__ import func_3", repeat=10, number=1000)))
+
+
+# Добавил func_3, которая быстрее остальных за счет использования генератора списков
+#
+# Чаще всего встречается число 1, оно появилось в массиве 3 раз(а)
+# 0.005888600000000001
+# Чаще всего встречается число 1, оно появилось в массиве 3 раз(а)
+# 0.005440299999999995
+# Чаще всего встречается число 1, оно появилось в массиве 3 раз(а)
+# 0.004370700000000005
