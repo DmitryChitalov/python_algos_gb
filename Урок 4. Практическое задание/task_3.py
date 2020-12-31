996cc0adf1388b07993be0297c281d4b55032b23
Vitaly <kazancevvitallijj@gmail.com>
2020-12-31 19:08:09 +0800

    complete

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -36,3 +36,40 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+enter_user_num = 716362327915
+
+# 1b
+print('Профилировка через timeit:')
+print(timeit('revers(enter_user_num)',
+             setup='from __main__ import revers, enter_user_num',
+             number=1000),
+      'секунд для первой реализации')
+print(timeit('revers_2(enter_user_num)',
+             setup='from __main__ import revers_2, enter_user_num',
+             number=1000),
+      'секунд для второй реализации')
+print(timeit('revers_3(enter_user_num)',
+             setup='from __main__ import revers_3, enter_user_num',
+             number=1000),
+      'секунд для третьей реализации')
+
+"""
+результат:
+0.004503299999999995 секунд для первой реализации
+0.0026931999999999998 секунд для второй реализации
+0.00036289999999999933 секунд для третьей реализации
+значит третья реализация эффективнее второй и первой.
+"""
+
+# 1a
+
+
+def profile_func():
+    res_1 = revers(enter_user_num)
+    res_2 = revers_2(enter_user_num)
+    res_3 = revers_3(enter_user_num)
+
+
+print('Профилировка через cProfile')
+cProfile.run('profile_func()')
