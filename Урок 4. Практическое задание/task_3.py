290e31016c3e83507c366b9ef10d0f8407c2150d
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2020-12-31 17:57:25 +0300

    task 3

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -9,6 +9,8 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+import cProfile
+from timeit import timeit
 
 
 def revers(enter_num, revers_num=0):
@@ -34,3 +36,53 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+number = 352352352
+
+print(
+    timeit(
+        "revers(number)",
+        setup='from __main__ import revers, number',
+        number=10000))
+print(
+    timeit(
+        "revers_2(number)",
+        setup='from __main__ import revers_2, number',
+        number=10000))
+print(
+    timeit(
+        "revers_3(number)",
+        setup='from __main__ import revers_3, number',
+        number=10000))
+
+
+def main():
+    number = 352352352
+    revers(number)
+    revers_2(number)
+    revers_3(number)
+
+
+cProfile.run('main()')
+
+"""
+Результаты timeit:
+0.030475350000000012
+0.018989254999999983
+0.004533014000000002
+
+Результаты cProfile:
+    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+     10/1    0.000    0.000    0.000    0.000 task_3.py:16(revers)
+        1    0.000    0.000    0.000    0.000 task_3.py:26(revers_2)
+        1    0.000    0.000    0.000    0.000 task_3.py:34(revers_3)
+        1    0.000    0.000    0.000    0.000 task_3.py:58(main)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+        
+Выводы:
+первый метод медленный, т.к. использует рекурсию
+второй вариант быстрее первого, т.к. работает через цикл
+третий вариант быстрее, т.к. работает с переворотом строки вместо арифметических операций
+"""
