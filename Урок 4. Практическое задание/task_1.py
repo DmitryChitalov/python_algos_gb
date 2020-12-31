c0c1dee6990353b55e30476d748f31bdee655574
Georgiy <georgy.plastinin@yandex.ru>
2020-12-31 13:17:05 +0300

    ДЗ 4 Пластинин
    ДЗ 4 Пластинин

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -14,6 +14,8 @@
 
 from timeit import timeit
 
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 def func_1(nums):
     new_arr = []
@@ -21,3 +23,30 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [el for el, elem in enumerate(nums) if el % 2 == 0]
+
+
+def func_3(nums):
+    for elem in range(len(nums)):
+        if nums[elem] % 2 == 0:
+            yield elem
+
+
+print(timeit('func_1(nums)', 'from __main__ import func_1, nums'))
+print(timeit('func_2(nums)', 'from __main__ import func_2, nums'))
+print(timeit('func_3(nums)', 'from __main__ import func_3, nums'))
+
+
+"""
+Результат при 1 000 000 запусков:
+
+1.1189160999999999
+0.9828839
+0.16879460000000002
+
+Вывод. Генератор выполняется быстрее, чем генераторное выражение,
+а генераторное выражение быстрее чем цикл.
+"""
