f3870cb5e3af1995acdcd2dd245660408ab55858
Valentine <dialog.valentine@gmail.com>
2021-01-01 23:52:00 +0800

    variant1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,6 +13,7 @@
 """
 
 from timeit import timeit
+from random import randint
 
 
 def func_1(nums):
@@ -21,3 +22,18 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(obj: list) -> list:
+    # Списковое включение работает быстрее, чем обычный цикл, к тому же мы избавились от доплнительного действия в виде
+    # добавления элемента в список (append). Так же большую прибавку в скорости дает использование оператора not вместо
+    # оператора сравнения.
+    return [i for i in range(len(obj)) if not obj[i] % 2]
+
+
+if __name__ == '__main__':
+    a = [randint(1, 100) for i in range(100)]
+    b = timeit('func_1(a)', number=100000, globals=globals())
+    c = timeit('func_2(a)', number=100000, globals=globals())
+    print(b)
+    print(c)
