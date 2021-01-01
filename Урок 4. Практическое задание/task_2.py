b5dbdca67744d896111039af1f73b890425d4c14
valentinafomina <69861285+valentinafomina@users.noreply.github.com>
2021-01-01 19:27:31 +0300

    Update task_2.py

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -1,81 +1,50 @@
 """
-Задание 2.
+Задание 1.
 
-Приведен код, который формирует из введенного числа
-обратное по порядку входящих в него цифр.
-Задача решена через рекурсию
-Выполнена попытка оптимизировать решение через мемоизацию.
-Сделаны замеры обеих реализаций.
+Приведен код, который позволяет сохранить в
+массиве индексы четных элементов другого массива
 
-Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?
-Если у вас есть идеи, предложите вариант оптимизации.
+Сделайте замеры времени выполнения кода с помощью модуля timeit
+
+Попробуйте оптимизировать код, чтобы снизить время выполнения
+Проведите повторные замеры
+
+Добавьте аналитику: что вы сделали и почему
 """
 
 from timeit import timeit
-from random import randint
 
 
-def recursive_reverse(number):
-    if number == 0:
-        return str(number % 10)
-    return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+def func_1(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    return new_arr
 
 
-num_100 = randint(10000, 1000000)
-num_1000 = randint(1000000, 10000000)
-num_10000 = randint(100000000, 10000000000000)
-
-print('Не оптимизированная функция recursive_reverse')
-print(
-    timeit(
-        "recursive_reverse(num_100)",
-        setup='from __main__ import recursive_reverse, num_100',
-        number=10000))
-print(
-    timeit(
-        "recursive_reverse(num_1000)",
-        setup='from __main__ import recursive_reverse, num_1000',
-        number=10000))
-print(
-    timeit(
-        "recursive_reverse(num_10000)",
-        setup='from __main__ import recursive_reverse, num_10000',
-        number=10000))
+def func_2(nums):
+    new_arr = []
+    for i in nums:
+        if i % 2 == 0:
+            new_arr.append(i)
+    return new_arr
 
 
-def memoize(f):
-    cache = {}
-
-    def decorate(*args):
-
-        if args in cache:
-            return cache[args]
-        else:
-            cache[args] = f(*args)
-            return cache[args]
-    return decorate
+nums = range(1000)
 
 
-@memoize
-def recursive_reverse_mem(number):
-    if number == 0:
-        return ''
-    return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
+print(timeit("func_1(nums)", setup="from __main__ import func_1, nums", number=1000))
+print(timeit("func_2(nums)", setup="from __main__ import func_2, nums", number=1000))
 
 
-print('Оптимизированная функция recursive_reverse_mem')
-print(
-    timeit(
-        'recursive_reverse_mem(num_100)',
-        setup='from __main__ import recursive_reverse_mem, num_100',
-        number=10000))
-print(
-    timeit(
-        'recursive_reverse_mem(num_1000)',
-        setup='from __main__ import recursive_reverse_mem, num_1000',
-        number=10000))
-print(
-    timeit(
-        'recursive_reverse_mem(num_10000)',
-        setup='from __main__ import recursive_reverse_mem, num_10000',
-        number=10000))
+'''
+
+Время выполнения функций (1000 повторений):
+
+func_1:     0.0015 сек
+func_2:     0.0007 сек
+
+В func_2 доступ к элементу по индексу заменен итерированием, что привело к ускорению.
+
+'''
