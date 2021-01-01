efd0ed4aa26f075d14f0ad4ad5683f231af36e7c
valentinafomina <69861285+valentinafomina@users.noreply.github.com>
2021-01-01 19:27:02 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,30 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = []
+    for i in nums:
+        if i % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+
+nums = range(1000)
+
+
+print(timeit("func_1(nums)", setup="from __main__ import func_1, nums", number=1000))
+print(timeit("func_2(nums)", setup="from __main__ import func_2, nums", number=1000))
+
+
+'''
+
+Время выполнения функций (1000 повторений):
+
+func_1:     0.0015 сек
+func_2:     0.0007 сек
+
+В func_2 доступ к элементу по индексу заменен итерированием, что привело к ускорению.
+
+'''
