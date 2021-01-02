6c0e1816fb48360e2d8bad707afe947fdb42787e
MikeGer92 <mikeman@inbox.ru>
2021-01-02 21:03:19 +0300

    added all

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -1,3 +1,4 @@
+
 """
 Задание 1.
 
@@ -10,3 +11,34 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+
+import time
+
+test_dic = {}
+test_list = []
+
+def excute_time(func):
+    def wrapped(*args):
+        start_time = time.time()
+        result = func(*args)
+        end_time = time.time()
+        print(end_time - start_time)
+    return wrapped
+
+
+@excute_time
+def insert_dic(dic):
+    for a in range(1000):
+        k=str(a+1)+' '+'val'
+        test_dic[k] = a
+    print(test_dic)
+
+@excute_time
+def insert_list(list):
+    for i in range(1000):
+        test_list.append(i)
+    print(test_list)
+
+
+insert_dic(test_dic)
+insert_list(test_list)
