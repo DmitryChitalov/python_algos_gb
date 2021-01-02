02c4a5c007187df57efdc3c0ad4bb2f9c638eb07
MerezhkOlga <MerezhkOly@gmail.com>
2021-01-02 17:35:36 +0300

    ДЗ 4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -9,6 +9,10 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+import cProfile
+from timeit import timeit
+
+enter_num = 1234567890123456789
 
 
 def revers(enter_num, revers_num=0):
@@ -34,3 +38,31 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+def mein():
+    num = 1234567890123456789
+    revers(num)
+    revers_2(num)
+    revers_3(num)
+
+
+cProfile.run('mein()')
+cProfile.run('revers(1234567890123456789)')
+cProfile.run('revers_2(1234567890123456789)')
+cProfile.run('revers_3(1234567890123456789)')
+
+# Через cProfile ничего не видно, везде только 0.
+
+num = 1234567890123456789
+
+print('revers : ', timeit(f'revers({num})', setup='from __main__ import revers', number=10000))
+print('revers_1 : ', timeit(f'revers_2({num})', setup='from __main__ import revers_2', number=10000))
+print('revers_2 : ', timeit(f'revers_3({num})', setup='from __main__ import revers_3', number=10000))
+
+# Результаты через timeit
+# revers :  0.10920490000000001
+# revers_1 :  0.06533070000000002
+# revers_2 :  0.006191900000000028
+# Видно, что саммый эффективный способ revers_2, через срез. Оно объясняется тем, что тут нет циклов, еслить только
+# присвоение одного символа. Самый неэффективный, как всегда, рекурсия, revers. Занимает больше памяти чтобы пройти
+# туда-обратно по сравнению с циклом.
