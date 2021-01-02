6a20b4e0d4b2be4abfd96a0cc1904b986010cf3d
Toras <efpan@mail.ru>
2021-01-02 20:35:37 +0700

    дз к третьему занятию

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -10,3 +10,54 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+import time
+from random import randint
+
+
+def stopwatch(func):
+    def wrapper(*args, **kwargs):
+        t_ns = time.perf_counter_ns()
+        t = time.time()
+        res = func(*args, **kwargs)
+        print(f'{func.__name__} выполнялась {time.time()-t} секунд, {time.perf_counter_ns()-t_ns} наносекунд')
+        return res
+    return wrapper
+
+
+@stopwatch
+def fill_dict(d, n):
+    for i in range(n):
+        d[i] = i
+    return d
+
+
+@stopwatch
+def fill_list(l, n):
+    for i in range(n):
+        l.append(i)
+    return l
+
+
+@stopwatch
+def dict_get(d, i):
+    return d[i]
+
+
+@stopwatch
+def list_get(l, i):
+    return l[i]
+
+
+if __name__ == '__main__':
+    my_dict = {}
+    my_list = []
+    max_items = 10000000
+    my_dict = fill_dict(my_dict, max_items)
+    my_list = fill_list(my_list, max_items)
+    search_n = randint(0, max_items+1)
+    print(dict_get(my_dict, search_n))
+    print(list_get(my_list, search_n))
+
+# заполнение словаря происходит меделенне, чем списка из-за того, что для добавления элемента в словар необходимо
+# рассчитать значение хэша для ключа, добавление в список же не требует каки-либо дополнительных расчетов
+
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,29 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+from uuid import uuid4
+import hashlib
+
+
+class UrlStorage:
+    def __init__(self):
+        self.storage = []
+        self.salt = uuid4().hex
+
+    def push(self, url):
+        url = hashlib.sha256(self.salt.encode() + url.encode()).hexdigest()
+        if not self.check(url):
+            self.storage.append(url)
+        else:
+            print('url уже есть в кэше')
+
+    def check(self, url):
+        return url in self.storage
+
+
+if __name__ == '__main__':
+    my_url_storage = UrlStorage()
+    my_url_storage.push('https://ya.ru')
+    my_url_storage.push('https://ya.ru')
+    my_url_storage.push('http://mail.ru')
+    print(len(my_url_storage.storage))
