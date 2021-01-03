7dff728d14b738bf409793f5fa1378b652be1cf1
Erofeev Vladislav <vlad27-27@mail.ru>
2021-01-03 17:01:50 +0300

    lesson 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -1,18 +1,16 @@
 """
 Задание 1.
-
 Приведен код, который позволяет сохранить в
 массиве индексы четных элементов другого массива
-
 Сделайте замеры времени выполнения кода с помощью модуля timeit
-
 Попробуйте оптимизировать код, чтобы снизить время выполнения
 Проведите повторные замеры
-
 Добавьте аналитику: что вы сделали и почему
 """
 
-from timeit import timeit
+import timeit
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 def func_1(nums):
@@ -21,3 +19,27 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+#print(func_1(nums))
+
+print('Время для 1 функции составило:') ## 0.004379270001663826
+print(
+    timeit.timeit(
+        f"func_1({nums})", setup="from __main__ import func_1", number=1000))
+
+
+def func_2(nums):
+    new_arr_1 = [elem for elem in nums if elem % 2 != 0]
+    return new_arr_1
+
+
+#print(func_2(nums))
+
+print('Время для 2 функции составило:') ## 0.0026921539974864572
+print(
+    timeit.timeit(
+        f"func_2({nums})", setup="from __main__ import func_2", number=1000))
+
+
+##Благодаря тому что использовали listcomprehension для 2 функции вместо append, удалось оптимизировать код и сократить время.
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,14 +1,15 @@
 """
 Задание 3.
-
 Приведен код, формирующий из введенного числа
 обратное по порядку входящих в него
 цифр и вывести на экран.
-
 Сделайте профилировку каждого алгоритма через cProfile и через timeit
-
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+
+import timeit, cProfile
+
+enter_num = 12345
 
 
 def revers(enter_num, revers_num=0):
@@ -34,3 +35,39 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+def main():
+    revers(enter_num)
+    revers_2(enter_num)
+    revers_3(enter_num)
+
+
+## через timeit
+print('Время для 1 функции составило:')
+print(
+    timeit.timeit(
+        "revers(enter_num)",
+        setup="from __main__ import revers, enter_num",
+        number=1000))  ## 0.0035573980057961307
+
+print('Время для 2 функции составило:')
+print(
+    timeit.timeit(
+        "revers_2(enter_num)",
+        setup="from __main__ import revers_2, enter_num",
+        number=1000))  ## 0.0021882380024180748
+
+print('Время для 3 функции составило:')
+print(
+    timeit.timeit(
+        "revers_3(enter_num)",
+        setup="from __main__ import revers_3, enter_num",
+        number=1000))  ## 0.0007074290042510256
+
+## через cProfile
+
+cProfile.run('main()')
+"""
+Проанализировав функции разными способами видно, что функция revers_3 выполняется быстрее остальных. Первая функция затрачивает много времени, так как используется рекурсия (нужно оптимизировать). 
+Во 2 функции используется цикл, что усложняет код. В 3 варианте используются срезы, а так как это встроенная функция, время затрачивается меньше остальных.
+"""
