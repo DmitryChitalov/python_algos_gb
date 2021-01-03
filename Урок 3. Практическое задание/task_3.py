c457459755db22ca2ab8e9c94af9620453f5c903
Sergey Pedchenko <pedchenko.sergey@gmail.com>
2021-01-03 12:42:21 +0300

    The solution for DZ3 - Algorithms

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,20 @@
 р
 а
 """
+
+
+import hashlib
+
+
+def check_substrings(lcl_str=''):
+    lcl_set = set()
+    for i in range(len(lcl_str)):
+        for j in range(i + 1, len(lcl_str) + 1):
+            if lcl_str[i:j] != lcl_str:
+                lcl_set.add(hashlib.sha256(lcl_str[i:j].encode('utf-8')).hexdigest())
+    return lcl_set
+
+
+my_str = input('Ввдеите строку для проверки: ')
+print(check_substrings(my_str))
+
