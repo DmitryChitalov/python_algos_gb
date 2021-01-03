1ba9b45a28a2cde2919f60e9bc78e54261abd13c
Nikita <frommyhdd@gmail.com>
2021-01-03 14:59:30 +0300

    Lesson 5

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,31 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+from collections import defaultdict
+
+"""С применением defaultdict"""
+d = defaultdict(list)
+d["first"] = list(input("Введите первое шеснадцатиричное число: ").upper())
+d["second"] = list(input("Введите второе шеснадцатиричное число: ").upper())
+d['sum'] = hex(int(''.join(d['first']),16) + int(''.join(d['second']),16))[2:].upper()
+d['mul'] = hex(int(''.join(d['first']),16) * int(''.join(d['second']),16))[2:].upper()
+print("Сумма равна: ", d['sum'])
+print("Произведение равно: ", d['mul'])
+
+"""С применением ООП"""
+class HexNumber:
+    def __init__(self, number):
+        try:
+            self._from_hex = int(number, 16)
+        except Exception as e:
+            print("Неверное значение: ", e)
+            self._from_hex = 0
+    def __add__(self, other):
+        return hex(self._from_hex + other._from_hex).upper()[2:]
+    def __mul__(self, other):
+        return hex(self._from_hex * other._from_hex).upper()[2:]
+
+h1 = HexNumber('A2')
+h2 = HexNumber('c4f')
+print("Сумма равна: ", h1 + h2)
+print("Произведение равно: ", h1 * h2)
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,48 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+
+from collections import OrderedDict
+from timeit import timeit
+
+def fill_simple_dict():
+    simple_dict = dict()
+    for i in range(10000):
+        simple_dict[i] = i
+
+def fill_ordered_dict():
+    ordered_dict = OrderedDict()
+    for i in range(10000):
+        ordered_dict[i]= i
+
+s = dict()
+o = OrderedDict()
+for i in range(10000):
+    s[i] = i
+    o[i] = i
+
+def find_in_simple_dict(simple_dict : dict):
+    for i in range(10000):
+        if i in simple_dict:
+            pass
+
+def find_in_ordered_dict(ordered_dict : OrderedDict):
+    for i in range(10000):
+        if i in ordered_dict:
+            pass
+
+
+print("fill_simple_dict: ", timeit("fill_simple_dict()","from __main__ import fill_simple_dict",number=1000))
+print("fill_ordered_dict: ",timeit("fill_ordered_dict()","from __main__ import fill_ordered_dict",number=1000))
+
+print("find_in_simple_dict: ", timeit("find_in_simple_dict(s)","from __main__ import find_in_simple_dict, s",number=10000))
+print("find_in_ordered_dict: ",timeit("find_in_ordered_dict(o)","from __main__ import find_in_ordered_dict, o",number=10000))
+
+"""Вывод: целесообразнее использовать обычный dict, т.к при операциях заполнения словаря, он работает на 30% быстрее OrderedDict
+Что касается операции поиска ключа, то оба контейнера работают примерно одинаково(но обычный словарь тут тоже опережает)
+Вывод программы:
+fill_simple_dict:  1.3148751760000001
+fill_ordered_dict:  2.039563823
+find_in_simple_dict:  8.821126876000001
+find_in_ordered_dict:  8.844777422
+"""
