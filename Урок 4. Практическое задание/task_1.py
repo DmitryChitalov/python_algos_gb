a46a43701a0f2fe490322774bb9d562dadf4e2f2
dmitry <evsvald@gmail.com>
2021-01-04 19:39:12 +0300

    lesson4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,22 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+arr = [n for n in range(1, 11111) if n % 2 == 0]
+
+print(timeit("func_1(arr)", setup="from __main__ import func_1, arr", number=1000))
+print(timeit("func_2(arr)", setup="from __main__ import func_2, arr", number=1000))
+
+"""
+результат:
+0.47990150000000004
+0.3471668
+
+Заменил цикл генераторным выражением, достаточно быстрый способ создания списка.
+"""
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -9,6 +9,11 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+import cProfile
+from timeit import timeit
+from random import randint
+
+num_100 = randint(10000, 1000000)
 
 
 def revers(enter_num, revers_num=0):
@@ -34,3 +39,45 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+print('revers')
+print(
+    timeit(
+        'revers(num_100)',
+        setup='from __main__ import revers, num_100',
+        number=10000))
+
+print('revers_2')
+print(
+    timeit(
+        'revers_2(num_100)',
+        setup='from __main__ import revers_2, num_100',
+        number=10000))
+
+print('revers_3')
+print(
+    timeit(
+        'revers_3(num_100)',
+        setup='from __main__ import revers_3, num_100',
+        number=10000))
+
+
+def main():
+    revers(num_100),
+    revers_2(num_100),
+    revers_3(num_100)
+
+
+cProfile.run('main()')
+
+"""
+revers
+0.012249699999999999
+revers_2
+0.008478499999999996
+revers_3
+0.002529499999999997
+  
+3 алгоритм самый эффективный исходя из результатов timeit, в нем используетсе встроенный метод
+среза, когда в первых двух используется рекурсия и цикл.
+"""
