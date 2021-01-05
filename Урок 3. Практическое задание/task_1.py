6828368c61706a74c54a460ef516dd237f52afab
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2021-01-05 21:11:15 +0300

    task 1 analytic

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -27,7 +27,7 @@ def benchmark(func):
 @benchmark
 def init_list():
     lst = []
-    for i in range(100000):
+    for i in range(1000000):
         lst.append(i)
     return lst
 
@@ -35,31 +35,31 @@ def init_list():
 @benchmark
 def init_dict():
     dict = {}
-    for i in range(100000):
+    for i in range(1000000):
         dict[i] = i
     return dict
 
 
 @benchmark
 def add_to_list(lst):
-    for i in range(100000):
+    for i in range(len(lst), len(lst) + 1000000):
         lst.append(i)
 
 
 @benchmark
 def add_to_dict(dict):
-    for i in range(100000, 200000):
+    for i in range(1000000, 2000000):
         dict[i] = i
 
 
 @benchmark
 def get_from_list(lst):
-    return lst[100000]
+    return lst[1000000]
 
 
 @benchmark
 def get_from_dict(dict):
-    return dict.get(100000)
+    return dict.get(1000000)
 
 
 list = init_list()
@@ -68,3 +68,17 @@ add_to_list(list)
 add_to_dict(dict)
 list_el = get_from_list(list)
 dict_el = get_from_dict(dict)
+
+"""
+init_list 0.09682416915893555
+init_dict 0.1258707046508789
+add_to_list 0.08980917930603027
+add_to_dict 0.13010907173156738
+get_from_list 6.9141387939453125e-06
+get_from_dict 1.0967254638671875e-05
+
+Выводы:
+1. инициализация быстрее у списка, медленнее у словаря - для словаря требуются расчеты хешей для ключей
+2. добавление элементов в список быстрее, медленнее у словаря - для словаря требуются расчеты хешей для ключей
+3. выбора элемента по ключу быстрее в словаре, т.к. поиск по ключу(хешу) быстрее, чем в списке 
+"""
