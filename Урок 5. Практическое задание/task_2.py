86a39864b9bf599cab91ca629cf45b93f60d0525
philippov <bervr@mail.ru>
2021-01-06 13:39:41 +0400

    2 done

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -26,79 +26,68 @@ def get_str(a1):
     my_hex = ''.join(a1)
     return my_hex
 
+
 def get_hex(a1):
     return list(map(lambda x: format(x, 'x'), a1))
 
-def summ_hex(a1,a2):
-    return get_list(format((int(get_str(a1),16)+int(get_str(a2),16)),'x'))
 
-def mult_hex(a1,a2):
-    return get_list(format((int(get_str(a1),16)*int(get_str(a2),16)),'x'))
+def summ_hex(a1, a2):
+    return get_list(format((int(get_str(a1), 16) + int(get_str(a2), 16)), 'x'))
 
 
-"""
-# тут пытался реализовать поэлементное сложение 16-ричных чисел в массивах, но что-то зарылся и не получилось
-def diff_rank(up_rank, result_summ, i=1):
-    l = len(result_summ)
-    if i <= l:
-        n = l - i
-        new_el = result_summ.pop(n) + up_rank
-        if new_el > 15:
-            up_rank = new_el // 16
-            down_rank = new_el % 16
-            result_summ.insert(n, down_rank)
-            i += 1
-            return diff_rank(up_rank, result_summ, i)
-        else:
-            result_summ.insert(n, new_el)
-            return result_summ
-    else:
-        result_summ.insert(0, up_rank)
-    return result_summ
+def mult_hex(a1, a2):
+    return get_list(format((int(get_str(a1), 16) * int(get_str(a2), 16)), 'x'))
 
 
-def sum_list(a1, a2, result_summ=[]):
-    # len_diff = len(a1) - len(a2)
-    # if len_diff > 0:
-    #     for i in range(len_diff):
-    #         result_summ.append(a1.pop(i))
-    #     return sum_list(a1, a2, result_summ)
-    # elif len_diff < 0:
-    #     for i in range(abs(len_diff)):
-    #         result_summ.append(a2.pop(i))
-    #     return sum_list(a1, a2, result_summ)
-    # else:
-    print(result_summ)
+def get_summ(a1, a2):
+    a1 = get_deci(a1)
+    a2 = get_deci(a2)
+    result_summ = []
+    rank = 0
+    for i in range((max(len(a1), len(a2))) + 1):
+        flag = 0
+        try:
+            el1 = a1.pop()
+        except IndexError:
+            el1 = 0
+            flag += 1
+        try:
+            el2 = a2.pop()
+        except IndexError:
+            el2 = 0
+            flag += 1
+        el_summ = el1 + el2 + rank
+        rank = 0
+        if el_summ > 15:
+            rank = el_summ // 16
+            el_summ = el_summ % 16
+        result_summ.insert(0, el_summ)
+    if flag > 2 and rank != 0:
+        result_summ.insert(0, rank)
+    if result_summ[0] == 0:
+        result_summ.pop(0)  # костыль от первого нуля
+    return list(map(lambda x: format(x, 'x'), result_summ))
 
-    for i in reversed(range(len(a1))):
-        l = len(result_summ)-1 if len(result_summ) != 0 else 0
-        up_rank = 0
-        # print(a1, a2)
-        my_summ = a1.pop() + a2.pop()
-        print(my_summ)
-        if my_summ < 15:
-            result_summ.insert(i, my_summ)
-        else:
-            up_rank = my_summ // 16
-            down_rank = my_summ % 16
-            result_summ.append(down_rank)
-            # print(result_summ)
-            result_summ = diff_rank(up_rank, result_summ)
-    return result_summ
-"""
+
+def get_mult(a1, a2, mult=[], counter=0):
+    for counter in range(int(get_str(a1), 16)):
+        mult = get_summ(mult, a2)
+    return mult
+
 
 if __name__ == '__main__':
     # first = get_list(input('Введите шестнадцатиричное число: '))
     # second = get_list(input('Введите шестнадцатиричное число: '))
     first = get_list('A2')
     second = get_list('C4F')
-    # second = get_list('eec')
-    # first = get_list('111')
-    # print(get_hex(get_deci(first)))
-    # print(sum_list(get_deci(first), get_deci(second)))
-    # print(diff_rank(1, get_deci(second)))
-    # print(get_hex(first))
-    print(summ_hex(first,second))
-    print(mult_hex(first, second))
 
+    # # Чит-вариант:
+    # print(summ_hex(first,second))
+    # print(mult_hex(first, second))
+    # # строго говоря в тз не сказано что мы должны делать вычисления с массивами - нужно умножать и складывать
+    # # шестнадцатеричные числа, и хранить их в виде массива.
+    # #
+    # # нормальный вариант:
 
+    print(get_summ(first, second))
+    print(get_mult(first, second))
