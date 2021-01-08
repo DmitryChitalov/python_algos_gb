d44fb0299b24236ecebb29e6414666426b977136
Pafnodeveloper <51054260+Pafnodeveloper@users.noreply.github.com>
2021-01-08 12:55:55 +0300

    Add files via upload

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -17,3 +17,32 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 """
+
+
+def search_for_min1(target_list):
+    min_number = target_list[0]
+    for number in target_list:
+        if number < min_number:
+            min_number = number
+    return min_number
+
+
+x = [2, 465, 9, 4, 1, 3]
+
+y = search_for_min1(x)
+print(y)
+
+
+def search_for_min2(target_list):
+    min_number = target_list[-1]
+    for number in target_list:
+        for again_number in target_list:
+            if again_number < number and again_number < min_number:
+                min_number = again_number
+    return min_number
+
+
+x = [2, 465, 9, 4, 1, 3]
+
+y = search_for_min2(x)
+print(y)
