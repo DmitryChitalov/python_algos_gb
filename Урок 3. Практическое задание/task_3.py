a42ecf8f9c8afc362c76917a1974f18cc9223913
BrueShavrk <borisshavrin@yandex.ru>
2021-01-10 00:48:13 +0300

    Lesson 3 completed

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -1,3 +1,4 @@
+import hashlib
 """
 Задание 3.
 Определить количество различных подстрок с использованием хеш-функции.
@@ -14,3 +15,29 @@
 р
 а
 """
+
+
+def papa(string, start=0, end=1):       # изначально подстрока равна первому символу
+    n = len(string)
+    if start > n:                       # окончание рекурсии
+        return string
+    else:
+        if end <= n:                            # пока правая граница не достигнет крайнего правого положения...
+            substring = string[start:end]       # подстрока
+            if len(substring) > 0 and substring != string:
+                substring_hash = hashlib.sha256(substring.encode('utf-8')).hexdigest()    # хеширование
+                set_res.add(substring_hash)                                             # добавление ко множеству
+            return papa(string, start, end+1)       # ...рекурсивный вызов ф-ии с увеличением правой границы
+        else:                                       # иначе...
+            return papa(string, start+1, end=1)     # ...рекурсивный вызов ф-ии с увеличением левой границы,
+                                                    # возвращение правой границы к первоначальному значению
+
+
+string = input('Введите строку, состоящую только из строчных латинских букв: ').lower()
+set_res = set()
+papa(string)
+
+for i, substring in enumerate(set_res, 1):          # вывод внесенных хешей
+    print(f'{i}: {substring}')
+
+print(f'{len(set_res)} - общее количество подстрок в строке <<{string}>>')
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -1,3 +1,6 @@
+import hashlib
+from uuid import uuid4
+import requests
 """
 Задание 4.
 Реализуйте скрипт "Кэширование веб-страниц"
@@ -8,3 +11,31 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+
+cache = dict()
+salt = uuid4().hex      # генерируем соль
+
+
+def get_url(url):
+    if cache.get(url):                              # если страница уже имеется в кеше, то возвращается ее хэш
+        print('Страница была загружена из кэша.')
+        return cache[url]
+    else:
+        print(f'Подождите. Страница прогружается.')
+        data = get_data_from_server(url)                # если нет, то url страницы хэшируется
+        cache[url] = data                               # и заносится в кэш
+        return data
+
+
+def get_data_from_server(url):
+    data_hash = hashlib.sha1(salt.encode() + url.encode()).hexdigest()      # хешируем адрес страницы
+    return data_hash
+
+
+get_url('https://mail.ru/')             # загружаем новые страницы
+get_url('https://love.mail.ru/ru')
+get_url('https://ok.ru/')
+
+get_url('https://mail.ru/')             # загружаем уже имеющуюся в кеше страницу
+
+print(cache)
