91cbea6d91e93a46b002f81b157b181292674e5e
maximchernets <max.pochta@list.ru>
2021-01-11 18:22:39 +0300

    Домашнее задание к уроку 3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -15,3 +15,29 @@
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
+import hashlib
+
+
+login = hashlib.sha256(input('Введите логин:  ').encode('utf-8'))
+password = hashlib.sha256(input('Пароль:  ').encode('utf-8'))
+hash_obj = login.hexdigest()+password.hexdigest()
+print(hash_obj)
+file = open('password.txt', 'w')
+file.write(hash_obj)
+file.close()
+
+def check_pass():
+    password_rep = hashlib.sha256(input('Повторите пароль:  ').encode('utf-8'))
+    hash_obj_rep = login.hexdigest() + password_rep.hexdigest()
+    file = open('password.txt')
+    pass_checking = file.read()
+    print(pass_checking)
+    file.close()
+    if hash_obj_rep == pass_checking:
+        print('Вы ввели верный пароль!')
+    else:
+        print('Пароль не верный!')
+        check_pass()
+
+
+check_pass()
--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,19 @@
 р
 а
 """
+import hashlib
+
+
+string = input('Введите строку:  ')
+result = set()
+length = len(string)
+for i in range(length):
+    if i == 0:
+        length = len(string) - 1
+    else:
+        length = len(string)
+    for j in range(length, i, -1):
+        print(string[i:j])
+        result.add(hashlib.sha1(string[i:j].encode('utf-8')).hexdigest())
+print(f'Количество уникальных подстрок {len(result)}')
+
