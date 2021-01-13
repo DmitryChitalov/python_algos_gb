d568d61221f1b3453065c3835fcb9ce5406a9f6d
Pafnodeveloper <51054260+Pafnodeveloper@users.noreply.github.com>
2021-01-13 21:03:01 +0300

    Add files via upload

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,25 @@
 р
 а
 """
+
+
+import hashlib
+
+user_str = 'papa'
+my_set = set()
+
+try:
+    for x in range(len(user_str)):
+        word = ''
+        for i in user_str[x:]:
+            word += i
+            if word == user_str:
+                continue
+            hash_obj = hashlib.sha256(word.encode('utf-8'))
+            res = hash_obj.hexdigest()
+            my_set.add((res, word))
+except IndexError:
+    my_set.add(user_str[-1])
+
+for i in my_set:
+    print(i)
