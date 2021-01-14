7e667dc27709e8956b02c5bafbdf7531d658c897
maximchernets <max.pochta@list.ru>
2021-01-14 10:51:33 +0300

    Домашнее задание к уроку 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,22 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+nums = [i for i in range(1000)]
+print(timeit('func_1(nums)', setup='from __main__ import func_1, nums', number = 100000))
+
+def func_2(nums):
+    res = [i for i, y in enumerate(nums) if y % 2 == 0]
+    return res
+
+print(timeit('func_2(nums)', setup='from __main__ import func_2, nums', number = 100000))
+
+
+'''
+В результате вычислений получилось
+func_1 - 14.995340363
+func_2 - 9.512429995
+
+Генераторные выражения работают быстрее, хоть и сложность одинакова O(n)
+'''
