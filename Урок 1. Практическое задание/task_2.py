5a4d4c5e4c0353724a0ad7e583b000ccbd2c2b11
Nikita_Kulkov <kulkovnik93@gmail.com>
2021-01-16 15:58:46 +0300

    add hw_1-5 ex

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -7,6 +7,23 @@
 В основе алгоритма должно быть сравнение каждого числа со всеми другими элементами списка.
 Сложность такого алгоритма: O(n^2) - квадратичная.
 
+"""
+import time
+
+check_1 = time.time
+
+# def search_min_lst_quad (lst_test_min_1): 
+def search_min_lst_qud (ltmq): # ltmq = lst_test_min_quad
+    for i in ltmq:
+        var_min = True
+        for j in ltmq:
+            if i > j:
+                var_min = False
+        if var_min:
+            return i
+test_lst = [35,21,34,58,54,34] 
+print(search_min_lst_qud(test_lst))
+"""
 Второй, в виде функции, должен обеспечивать поиск минимального значения для списка.
 Сложность такого алгоритма: O(n) - линейная.
 
@@ -17,3 +34,13 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 """
+
+def search_min_lst_line (ltml): # ltml == list_test_min_2
+    elem_1 = ltml[0]
+    for i in ltml:
+        if i < elem_1:
+            elem_1 = i
+    return elem_1
+
+test_lst_2 = [35,21,34,58,54,34] 
+print (search_min_lst_line(test_lst_2))
--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -1,5 +1,5 @@
 """
-Задание 6.
+Задание 5.
 Задание на закрепление навыков работы со стеком
 
 Примечание: в этом задании вспомните ваши знания по работе с ООП
@@ -18,3 +18,54 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+class StackOfPlates:
+    def __init__(self, size_stack):
+        self.elems = [[]]
+        self.size_stack = size_stack
+    def empty(self):
+        return self.elems == [[]]
+    def that_push(self, elem):
+        if len(self.elems[len(self.elems) - 1]) < self.size_stack:
+            self.elems[len(self.elems) - 1].append(elem)
+        else:
+            self.elems.append([])
+            self.elems[len(self.elems) - 1].append(elem)
+    def pop_from(self):
+        deleted_item = self.elems[len(self.elems) - 1].pop()
+        if len(self.elems[len(self.elems) - 1]) == 0:
+            self.elems.pop()
+        return deleted_item
+    def get_val(self):
+        return self.elems[len(self.elems) - 1][len(self.elems[len(self.elems) - 1]) - 1]
+    def num_plates(self):
+        sum_plates = 0
+        for stack in self.elems:
+            sum_plates += len(stack)
+        return sum_plates
+    def num_stacks(self):
+        return len(self.elems)
+    def __str__(self):
+        for stack in self.elems:
+            print(stack)
+        print('\n')
+    def __repr__(self):
+        return repr(self.elems)
+test_stack = StackOfPlates(3)
+print(test_stack.empty())
+test_stack.__str__()
+test_stack.that_push(1)
+print(test_stack.empty())
+test_stack.__str__()
+test_stack.that_push(2)
+test_stack.__str__()
+test_stack.that_push('tst_1')
+test_stack.__str__()
+test_stack.that_push('tst_2')
+test_stack.__str__()
+test_stack.that_push(10)
+test_stack.__str__()
+test_stack.pop_from()
+test_stack.__str__()
+print(test_stack.__repr__())
+print(test_stack.num_stacks())
+print(test_stack.num_plates())
