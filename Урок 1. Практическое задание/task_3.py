ec8d1c81fc059c88f13b5e49a78c4c538283f21c
KIRILLNAX <03-033@mail.ru>
2021-01-17 16:54:07 +0300

    Выполнил 6 заданий

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -22,3 +22,63 @@
 Реализуйте поиск трех компаний с наибольшей годовой прибылью.
 Выведите результат.
 """
+base_company = {
+    'yandex': 2000,
+    'mail': 5000,
+    'microsoft': 1234,
+    'perecrestok': 3000,
+    'pyterochka': 706,
+    'appel': 3388,
+    'apteka': 299,
+    'sumsung': 23599,
+    'xiaomi': 888900,
+    'NB': 9088,
+    'honda': 46883
+}
+
+# O(N)
+def max_1(list_input):
+    input_max = {}
+    list_d = dict(list_input)
+    for i in range(3):
+        maximum = max(list_d.items(), key=lambda k_v: k_v[1])
+        del list_d[maximum[0]]
+        input_max[maximum[0]] = maximum[1]
+    return input_max
+
+
+print(max_1(base_сompany))
+
+
+
+# O(N^2)
+def sorted_1(random_list):
+    for i in range(len(random_list)):
+        lowest_value_index = i
+        for j in range(i + 1, len(random_list)):
+            if random_list[j][1] > random_list[lowest_value_index][1]:
+                lowest_value_index = j
+        random_list[i], random_list[lowest_value_index] = random_list[lowest_value_index], random_list[i]
+    return random_list[0:3]
+
+list_from_dictionary = list(base_сompany.items())
+for i in sorted_1(list_from_dictionary):
+    print(i[0], ':', i[1])
+
+
+
+
+#  O(N*logN)
+list_from_dictionary = list(base_сompany.items())
+list_from_dictionary.sort(key=lambda i: i[1], reverse=True)
+for i in range(3):
+    print(list_from_dictionary[i][0], ':', list_from_dictionary[i][1])
+
+
+
+
+
+
+
+
+"Лучшим вариантом будет первый, т.к. имеет минимальную временную сложность "
