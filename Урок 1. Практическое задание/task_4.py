6223311dbbb1c617ac2083129e4c43798bd22768
Lajilit <lajilit@gmail.com>
2021-01-17 21:17:56 +0300

    task_4_2nd_var

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -33,27 +33,20 @@ def authentication_func(login:str, password:str, base:list) -> bool:
     '''
     Вариант 1 - Сложность O(N)
 
+    Функция проверяет наличие пользователя в базе, правильность ввода пароля,
+    статус активации аккаунта, а также предлагает активировать аккаунт,
+    если активация не выполнена
+
     Функция принимает на вход 3 параметра:
     - логин пользователя
     - пароль пользователя
-    список словарей, содержащих информацию о пользователе
-    Алгоритм работы функции:
-    1) Проверка словарей с информацией о пользователе на соответствие
-    параметра login значению словаря с ключом 'login'.
-    2) Если нужное значение найдено, в словаре, содержащем это значение,
-    функция проверяет соответствие параметра password значению поля по ключу
-    'password'. Если значение не верное, функция возвращает False
-    3) Если пароль правильный, функция проверяет значение по ключу
-    'is_active', и если оно == False, предлагает активировать учетную запись,
-    - в случае согласия меняет значение по ключу 'is_active' на True и
-    возвращает True,
-    - в случае отказа возвращает False
-    4) Если значение по ключу 'is_active' = True, функция возвращает True
-    5) Если в списке словарей не найден словарь, у которого значение по
-    ключу login == параметру login, функция возвращает False
+    - список словарей, содержащих информацию о пользователе
+
+    Функция возвращает значение True в случае успешной авторизации и
+    False, если авторизация не удалась
     '''
     for user_dict in base:                                              # O(N)
-        if user_dict['login'] == login.lower():                         # O(1)
+        if user_dict['login'] == login:                         # O(1)
             if user_dict['password'] != password:                       # O(1)
                 print('Неверный пароль')                                # O(1)
                 return False                                            # O(1)
@@ -73,49 +66,52 @@ def authentication_func(login:str, password:str, base:list) -> bool:
     return False                                                        # O(1)
 
 
-def authentication_func_2(login: str, password: str, base: list) -> bool:
+def authentication_func_2(login: str, password: str, base: dict) -> bool:
     '''
-    Вариант 1 - Сложность O(N)
+    Вариант 2 - Сложность O(1)
+
+    Функция проверяет наличие пользователя в базе, правильность ввода пароля,
+    статус активации аккаунта, а также предлагает активировать аккаунт,
+    если активация не выполнена
 
     Функция принимает на вход 3 параметра:
     - логин пользователя
     - пароль пользователя
-    список словарей, содержащих информацию о пользователе
-    Алгоритм работы функции:
-    1) Проверка словарей с информацией о пользователе на соответствие
-    параметра login значению словаря с ключом 'login'.
-    2) Если нужное значение найдено, в словаре, содержащем это значение,
-    функция проверяет соответствие параметра password значению поля по ключу
-    'password'. Если значение не верное, функция возвращает False
-    3) Если пароль правильный, функция проверяет значение по ключу
-    'is_active', и если оно == False, предлагает активировать учетную запись,
-    - в случае согласия меняет значение по ключу 'is_active' на True и
-    возвращает True,
-    - в случае отказа возвращает False
-    4) Если значение по ключу 'is_active' = True, функция возвращает True
-    5) Если в списке словарей не найден словарь, у которого значение по
-    ключу login == параметру login, функция возвращает False
+    - двухуровневый словарь, содержащий информацию о пользователе
+
+    Функция возвращает значение True в случае успешной авторизации и
+    False, если авторизация не удалась
     '''
-    for user_dict in base:  # O(N)
-        if user_dict['login'] == login.lower():  # O(1)
-            if user_dict['password'] != password:  # O(1)
-                print('Неверный пароль')  # O(1)
-                return False  # O(1)
-            is_active = user_dict['is_active']  # O(1)
-            if not is_active:  # O(1)
-                print('Необходимо активировать аккаунт')  # O(1)
-                activation = input('Введите "Y" чтобы активировать: ')  # O(1)
-                if activation.upper() != 'Y':  # O(1)
-                    print('Аккаунт не активирован')  # O(1)
-                    return False  # O(1)
-                user_dict['is_active'] = True  # O(1)
-                print('Аккаунт активирован')  # O(1)
-            print('Аутентификация пройдена')
-            return True  # O(1)
+    try:
+        user_info = base[login]                                         # O(1)
+    except KeyError:
+        print('Неверное имя пользователя')                              # O(1)
+        return False                                                    # O(1)
+    else:
+        if user_info['password'] != password:                           # O(1)
+            print('Неверный пароль')                                    # O(1)
+            return False                                                # O(1)
+        is_active = user_info['is_active']                              # O(1)
+        if not is_active:                                               # O(1)
+            print('Необходимо активировать аккаунт')                    # O(1)
+            activation = input('Введите "Y" чтобы активировать: ')      # O(1)
+            if activation.upper() != 'Y':                               # O(1)
+                print('Аккаунт не активирован')                         # O(1)
+                return False                                            # O(1)
+            user_info['is_active'] = True                               # O(1)
+            print('Аккаунт активирован')                                # O(1)
+        print('Аутентификация пройдена')                                # O(1)
+        return True                                                     # O(1)
 
-    print('Неверное имя пользователя')
-    return False  # O(1)
 
+# На мой взгляд более быстрым выглядит второй вариант функции, так как его
+# алгоритмическая скорость (если я все верно посчитала) = O(1), тогда как в 1
+# варианте она = O(N). Однако я также видела мнение, что словари работают в
+# каких-то случаях медленнее, чем списки, а также я не уверена, что обработка
+# исключений не влияет на скорость выполнения кода. Также можно было
+# попробовать написать ещё более медленный вариант функции, где несколько раз
+# перебирались бы одни и те же словари или списки, но у меня не хватает на
+# это времени и фантазии.
 
 users_list = [
     {'login': 'anna', 'password': 'key', 'is_active': True},
@@ -124,11 +120,29 @@ users_list = [
     {'login': 'xiaomi', 'password': 'redmi note IV', 'is_active': False}
 ]
 
-# login_input = input('Имя пользователя: ')
+# login_input = input('Имя пользователя: ').lower()
 # password_input = input('Пароль: ')
-# authentication_func(login_input, password_input, users_list)
+
 
 authentication_func('anna', 'wrong', users_list)
 authentication_func('japan', 'tokyo', users_list)
 authentication_func('second', 'qwertY', users_list)
+# authentication_func(login_input, password_input, users_list)
 
+print('----------------------')
+
+users_dict = {
+    'anna': {'password': 'key', 'is_active': True},
+    'second': {'password': 'qwertY', 'is_active': False},
+    'japan': {'password': 'tokyo', 'is_active': True},
+    'xiaomi': {'password': 'redmi note IV', 'is_active': False}
+}
+
+
+# login_input = input('Имя пользователя: ').lower()
+# password_input = input('Пароль: ')
+
+authentication_func_2('anna', 'wrong', users_dict)
+authentication_func_2('japan', 'tokyo', users_dict)
+authentication_func_2('second', 'qwertY', users_dict)
+# authentication_func(login_input, password_input, users_dict)
