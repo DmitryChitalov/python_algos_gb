e1b3314c29086732cd89b9128586953d6cf14390
Nikita <frommyhdd@gmail.com>
2021-01-17 21:50:22 +0300

    Lesson 7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,57 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+from random import random
+from timeit import timeit
+
+n = int(input("Введите число элементов: "))
+arr = [random()*50 for _ in range(n)]
+
+print("Исходный массив: ", arr)
+
+def merge(left: list, right: list):
+    i,j = 0 , 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] > right[j]:
+            result.append(right[j])
+            j = j + 1
+            if j == len(right):
+                result.extend(left[i:])
+        else:
+            result.append(left[i])
+            i = i + 1
+            if i == len(left):
+                result.extend(right[j:])
+    return result
+
+
+def merge_sort(arr : list):
+    if len(arr) == 1:
+        return arr
+    else:
+        middle = len(arr)//2
+        left_part = merge_sort(arr[:middle])
+        right_part = merge_sort(arr[middle:])
+        return merge(left_part,right_part)
+
+print("Отсортированный массив: ", merge_sort(arr))
+
+
+arr_10 = [random()*50 for _ in range(10)]
+arr_100 = [random()*50 for _ in range(100)]
+arr_1000 = [random()*50 for _ in range(1000)]
+print("10 элементов: ", timeit("merge_sort(arr_10)","from __main__ import arr_10, merge_sort, merge",number=100))
+print("100 элементов: ", timeit("merge_sort(arr_100)","from __main__ import arr_100, merge_sort, merge",number=100))
+print("1000 элементов: ", timeit("merge_sort(arr_1000)","from __main__ import arr_1000, merge_sort, merge",number=100))
+
+"""Вывод работы программы:
+Введите число элементов: 5
+Исходный массив:  [19.497989090052037, 43.77655946998179, 21.688223447019343, 7.172632888204622, 10.59084880044846]
+Отсортированный массив:  [7.172632888204622, 10.59084880044846, 19.497989090052037, 21.688223447019343, 43.77655946998179]
+
+10 элементов:  0.009429169999999765
+100 элементов:  0.06599494999999989
+1000 элементов:  0.6784994410000005
+Вывод: Очень полезный алгоритм сортировки, т.к работает гораздо быстрее пузырька и шейкерного алгоритма.
+"""
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,63 @@
 arr[m]
 from statistics import median
 """
+from statistics import median
+import random
+
+arr = [3,4,3,3,5,3,3]
+arr_2 = [random.randint(0,100) for _ in range(11)]
+left = []
+right = []
+mediana = 0
+
+def search_median(array : list):
+    for i in range(len(arr)):
+        left_count = 0
+        right_count = 0
+        left.clear()
+        right.clear()
+        for j in range(len(array)):
+            if j == i:
+                continue
+            elif array[j] <= array[i]:
+                left.append(array[j])
+            elif array[j] >= array[i]:
+                right.append(array[j])
+        left.extend(right)
+        for j in range(len(left)//2):
+            if left[j] <= array[i]:
+                left_count = left_count + 1
+        for j in range(len(left) // 2, len(left)):
+            if left[j] >= array[i]:
+                right_count = right_count + 1
+        if left_count == right_count:
+            mediana = array[i]
+            break
+    print("Медиана найдена и равна: ", mediana)
+
+print(arr)
+print(median(arr))  #Для проверки
+search_median(arr)
+
+print(arr_2)
+print(median(arr_2))
+search_median(arr_2)
+"""Работа выполнена: 
+    1-й прогон программы:
+[3, 4, 3, 3, 5, 3, 3]
+3
+Медиана найдена и равна:  3
+[19, 55, 78, 7, 16, 9, 35, 46, 48, 96, 2]
+35
+Медиана найдена и равна:  35
+
+    2-й прогон программы:
+[3, 4, 3, 3, 5, 3, 3]
+3
+Медиана найдена и равна:  3
+[5, 100, 28, 33, 15, 16, 78, 15, 72, 74, 39]
+33
+Медиана найдена и равна:  33   
+
+Вывод: моя реализация сходится со встроенной ф-й median.
+"""
