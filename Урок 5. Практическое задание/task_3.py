9735467eab193b90fc9301e22b35c26952b3c097
Temujen <talalayartem@mail.ru>
2021-01-18 01:55:38 +0300

    add task_3

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,19 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+import timeit
+from collections import deque
+
+int_list = list(range(100))
+int_deque = deque(range(100))
+
+print(f'pop_end_list - {timeit.timeit(lambda: int_list.pop(), number=100)}')
+print(f'pop_end_deque - {timeit.timeit(lambda: int_deque.pop(), number=100)}')
+print(f'append_end_list - {timeit.timeit(lambda: int_list.append(11), number=100)}')
+print(f'append_end_deque - {timeit.timeit(lambda: int_deque.append(11), number=100)}')
+print(f'del_first_list - {timeit.timeit(lambda: int_list.pop(0), number=100)}')
+print(f'del_first_deque - {timeit.timeit(lambda: int_deque.popleft(), number=100)}')
+print(f'reverse_list - {timeit.timeit(lambda: int_list.reverse(), number=100)}')
+print(f'reverse_deque - {timeit.timeit(lambda: int_deque.reverse(), number=100)}')
+print(f'append_begin_list - {timeit.timeit(lambda: int_list.insert(0, 11), number=100)}')
+print(f'append_begin_deque - {timeit.timeit(lambda: int_deque.appendleft(11), number=100)}')
