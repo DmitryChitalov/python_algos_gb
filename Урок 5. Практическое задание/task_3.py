01d53b894a3070186b230a9dc5ad23d14cfaf56a
BrueShavrk <borisshavrin@yandex.ru>
2021-01-17 20:38:48 +0300

    Lesson_5 completed

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,82 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+from timeit import timeit
+from collections import deque
+
+
+def func_list_insert(lst):          # Добавление в list элементов слева
+    for i in range(1000):
+        lst.insert(0, i)
+    return lst
+
+
+def func_list_append(lst):          # Добавление в list элементов справа
+    for i in range(1000):
+        lst.append(i)
+    return lst
+
+
+def func_deque_appendleft(deq):     # Добавление в deque элементов слева
+    for i in range(1000):
+        deq.appendleft(i)
+    return deq
+
+
+def func_deque_append(deq):         # Добавление в deque элементов справа
+    for i in range(1000):
+        deq.append(i)
+    return deq
+
+
+def func_list_cycle(lst):           # перебор всех элементов из list и добавление их в новый list
+    res = []
+    for el in lst:
+        res.append(el)
+    return res
+
+
+def func_deque_cycle(deq):          # перебор всех элементов из deque и добавление их в новый deque/list
+    # res = []                      # добавление в list
+    res = deque(list())             # добавление в deque
+    for el in deq:
+        res.append(el)
+    return res
+
+
+lst_work = [range(1, 101)]
+new_deque = deque(lst_work)
+
+time_1 = timeit('func_list_insert(lst_work)',
+                setup='from __main__ import func_list_insert, lst_work', number=100)
+time_2 = timeit('func_list_append(lst_work)',
+                setup='from __main__ import func_list_append, lst_work', number=100)
+time_3 = timeit('func_deque_appendleft(new_deque)',
+                setup='from __main__ import func_deque_appendleft, new_deque', number=100)
+time_4 = timeit('func_deque_append(new_deque)',
+                setup='from __main__ import func_deque_append, new_deque', number=100)
+time_5 = timeit('func_list_cycle(lst_work)',
+                setup='from __main__ import func_list_cycle, lst_work', number=100)
+time_6 = timeit('func_deque_cycle(new_deque)',
+                setup='from __main__ import func_deque_cycle, new_deque', number=100)
+
+print('Время добавления элементов в начало list: {:.2f}'.format(time_1))
+print('Время добавления элементов в начало deque: {:.2f}'.format(time_3))
+
+print('\nВремя добавления элементов в конец list: {:.2f}'.format(time_2))
+print('Время добавления элементов в конец deque: {:.2f}'.format(time_4))
+
+print('\nВремя выполнения цикла в list: {:.2f}'.format(time_5))
+print('Время выполнения цикла в deque: {:.2f}'.format(time_6))
+
+"""Выводы: выполнение функции добавления элементов в начало простого списка (func_list_insert)
+   занимает гораздо больше времени, нежели аналогичная функция (func_deque_appendleft), 
+   использующая оптимизированный список deque (что и следовало ожидать).
+   
+   Операции добавления элементов в конец простого или оптимизированного списка deque занимают
+   относительно одно и то же время.
+   
+   Время выполнения цикла перебора всех элементов list и поочередное добавление каждого этого
+   элемента в новый list (res) занимает немного больше времени, чем аналогичный перебор элементов
+   оптимизированного списка deque с добавлением этих элементов в новый список deque (res).
+   Но если добавлять элементы из deque в обычный list, то время выполнения такое же как из list в list"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,66 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+import collections
+from timeit import timeit
+
+
+def func_with_dict():               # Заполнение словаря dict (простой словарь)
+    d = dict()
+    for i in range(1, 1000):
+        name = f'key_{i}'
+        d[name] = i
+    return d
+
+
+def func_with_ordereddict():        # Заполнение словаря OrderedDict
+    d = collections.OrderedDict()   # Словарь с памятью порядка добавления элементов
+    for i in range(1, 1000):
+        name = f'key_{i}'
+        d[name] = i
+    return d
+
+
+def cycle_dict():                   # Перебор пар простого словаря и сохранение в новом простом словаре
+    d = func_with_dict()            # только четных ключей и их значений
+    new_d = dict()
+    for key, value in d.items():
+        if int(key[-1]) % 2 == 0:
+            new_d[key] = value * 2
+    return new_d
+
+
+def cycle_ordereddict():            # Перебор пар словаря OrderedDict и сохранение в новом словаре OrderedDict
+    d = func_with_ordereddict()         # только четных ключей и их значений
+    new_d = collections.OrderedDict()
+    for key, value in d.items():
+        if int(key[-1]) % 2 == 0:
+            new_d[key] = value * 2
+    return new_d
+
+
+time_1 = timeit('func_with_dict()',
+                setup='from __main__ import func_with_dict', number=1000)
+time_2 = timeit('func_with_ordereddict()',
+                setup='from __main__ import func_with_ordereddict', number=1000)
+
+time_3 = timeit('cycle_dict()',
+                setup='from __main__ import cycle_dict', number=1000)
+time_4 = timeit('cycle_ordereddict()',
+                setup='from __main__ import cycle_ordereddict', number=1000)
+
+print(f'Заполненный список dict (простой словарь):\n {func_with_dict()}')
+print(f'Заполненный список OrderedDict:\n {func_with_ordereddict()}')
+
+print('\nВремя заполнения пустого dict ключами со значениями: {:.2f}'.format(time_1))
+print('Время заполнения пустого OrderedDict ключами со значениями: {:.2f}'.format(time_2))
+
+print('\nВремя заполнения и перебора пар из dict, а также добавление только \n'
+      'четных ключей со значениями в новый словарь dict (new_d): {:.2f}'.format(time_3))
+print('\nВремя заполнения и перебора пар из OrderedDict, а также добавление только \n'
+      'четных ключей со значениями в новый словарь OrderedDict (new_d): {:.2f}'.format(time_4))
+
+"""Выводы: Заполнение и перебор пар в простом словаре (dict) занимает немного меньше времени,
+   чем аналогичные операции со словарем OrderedDict. К тому же, при выводе двух параллельно созданных
+   словарей видно, что оба словаря имеют память порядка добавления элементов.
+   Поэтому использование OrderedDict перестает быть необходимым."""
