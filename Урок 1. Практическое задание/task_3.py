365d714515c5925b04a24441c01808e5f4bbc91c
Vasiliy <vasiliypoddubny@gmail.com>
2021-01-18 17:07:21 +0300

    Lesson_1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -2,7 +2,7 @@
 Задание 3.
 
 Для этой задачи:
-1) придумайте 1-3 решения (желательно хотя бы два)
+1) придумайте 2-3 решения (желательно хотя бы два)
 2) оцените сложность каждого решения в нотации О-большое
 3) сделайте вывод, какое решение эффективнее и почему
 
@@ -14,7 +14,6 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 
-
 Сама задача:
 Имеется хранилище с информацией о компаниях: название и годовая прибыль.
 Для реализации хранилища можно применить любой подход,
@@ -22,3 +21,44 @@
 Реализуйте поиск трех компаний с наибольшей годовой прибылью.
 Выведите результат.
 """
+import operator
+
+company = {
+    'Salesforce': 800,
+    'Adobe': 700,
+    'NVIDIA': 1200,
+    'Amazon': 900
+}
+
+# Вариант 1
+# O(N log N)
+sorted_one = sorted(company.items(), key=operator.itemgetter(1))
+
+# или
+sorted_two = sorted(company.items(), key=lambda x: x[1])
+
+
+def best_company(*args):
+    for i, value in enumerate(sorted_one[::-1]):
+        if i < 3:
+            print(value)
+        else:
+            break
+
+
+best_company(sorted_one)
+best_company(sorted_two)
+
+
+# Вариант 2
+# O(N) - самый лучший вариант т.к быстрее и по графику роста линия плавно и медленно поднимается вверх
+def three_max(list_input):
+    input_max = {}
+    list_d = dict(list_input)
+    for i in range(3):
+        maximum = max(list_d.items(), key=lambda k_v: k_v[1])
+        del list_d[maximum[0]]
+        input_max[maximum[0]] = maximum[1]
+    return input_max
+
+print(three_max(company))
--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -1,5 +1,5 @@
 """
-Задание 6.
+Задание 5.
 Задание на закрепление навыков работы со стеком
 
 Примечание: в этом задании вспомните ваши знания по работе с ООП
@@ -17,4 +17,71 @@
 для реализации этой задачи.
 
 После реализации структуры, проверьте ее работу на различных сценариях
+
+Подсказка:
+Отдельне стопки можно реализовать через:
+# 1) созд-е экземпляров стека (если стопка - класс)
+# 2) lst = [[], [], [], [],....]
 """
+
+
+class PlateStack:
+
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.stacks = []
+
+    def push_in(self, el):
+        """ Добавить тарелку в стопку"""
+        if self.stacks: # проверяем наличие хотя бы одной стопки
+            if len(self.stacks[0]) < self.max_size:
+                self.stacks[0].insert(0, el)
+            else:
+                self.stacks.insert(0, list())
+                self.stacks[0].insert(0, el)
+        else:
+            self.stacks.insert(0, list())
+            self.stacks[0].insert(0, el)
+
+    def pop_out(self):
+        """ Убрать тарелку из стопки"""
+        if self.stacks: # удалить можно только при наличии хотя бы одной стопки
+            self.stacks[0].pop(0)
+            if len(self.stacks[0]) == 0:
+                self.stacks.pop(0)
+
+    def count_stacks(self):
+        """ Посчитать количество стопок тарелок"""
+        return len(self.stacks)
+
+    def get_plate(self):
+        return self.stacks[0][0]
+
+    def __str__(self):
+        return str(self.stacks)
+
+if __name__ == '__main__':
+
+    a = PlateStack(3)
+    a.push_in('Тарелка 1')
+    a.push_in('Тарелка 2')
+    a.push_in('Тарелка 3')
+    a.push_in('Тарелка 4')
+    a.push_in('Тарелка 5')
+
+    print(a.count_stacks())
+    print(a)
+
+    a.pop_out()
+    a.pop_out()
+
+    print(a)
+    print(a.count_stacks())
+
+    print(a.get_plate())
+
+    a.pop_out()
+    a.pop_out()
+    a.pop_out()
+    print(a)
+    a.pop_out()
