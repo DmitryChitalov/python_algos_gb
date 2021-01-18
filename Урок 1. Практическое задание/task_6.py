7fa7eb18800c00ad678d8ea88da552076f64c8a4
Lajilit <lajilit@gmail.com>
2021-01-18 14:20:42 +0300

    task_6 update

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -18,6 +18,10 @@
 
 
 class QueueObj:
+    """
+    Очередь элементов, представляющая собой список, где элементы добавляются
+    в начало списка, а удаляются с конца
+    """
     def __init__(self):
         self._queue_items = []
 
@@ -43,41 +47,65 @@ class QueueObj:
 
 
 class TaskBoard:
+    """
+    Доска задач, представляющая собой словарь, где ключ - название очереди
+    задач, а значение - объект класса QueueObj.
+    Задания с доски задач выполняются в порядке приоритета - чем раньше
+    очередь добавлена на доску задач, тем выше у неё приоритет.
+    """
+
     def __init__(self):
         self._queues = {}
         self._priority = []
         self._executed = []
 
     def is_empty(self):
+        """Метод проверяет, есть ли очереди на доске задач,
+        и возвращает True, если очередь пуста, или False, если в очереди
+         есть задачи"""
         return self._queues == {}
 
     def add_queue(self, name: str):
+        """ Метод принимает в качестве параметра название очереди задач и
+        создаёт новую очередь, если на доске задач нет очереди с таким
+        названием"""
         if name in self._queues.keys():
-            print('Такая очередь задач уже существует')
+            print('Очередь задач с таким названием уже существует')
         self._queues[name] = QueueObj()
         self._priority.append(name)
 
     def new_task(self, name: str, item: str):
+        """ Метод принимает в качестве параметра название очереди задач и
+        саму задачу. Если такая очередь задач не существует, она будет
+        создана. Метод добавляет задачу в указанную очередь задач"""
         if name not in self._queues.keys():
             self.add_queue(name)
         self._queues[name].item_to_queue(item)
 
     def execute_task(self):
+        """ Метод перемещает следующую в порядке приоритета очередей задачу
+        в список выполненных задач"""
         for el in self._priority:
             if self._queues[el].is_empty():
                 continue
             self._executed.append(self._queues[el].item_from_queue())
             break
+        print('Нет доступных заданий')
 
     def change_queue(self, old: str, new: str):
-        self._queues[new].item_to_queue(
-            self._queues[old].item_from_queue()
-        )
+        """ Метод позволяет перенести следующее задание из одной очереди в
+        конец другой очереди, либо в конец той же самой очереди"""
+        self._queues[new].item_to_queue(self._queues[old].item_from_queue())
 
-    def show_queue(self, name):
+    def show_queue(self, name: str):
+        """ Метод принимает в качестве параметра название очереди задач и
+        возвращает саму очередь"""
         return self._queues[name].queue_list()
 
     def show_next_task(self, name=None):
+        """ Метод принимает в качестве необязательного параметра название
+        очереди задач и возвращает следующий её элемент. Если параметр не
+        указан, метод возвращает задачу, которая будет выполена следующей"""
         if name is None:
             for el in self._priority:
                 if self._queues[el].is_empty():
@@ -87,12 +115,18 @@ class TaskBoard:
         return self._queues[name].queue_list()[-1]
 
     def queue_is_empty(self, name):
-        return self._queues[name].is_empty()
+        """ Метро принимает в качестве параметра название очереди возвращает
+        True, если в очереди нет задач или False, если очередь не пуста"""
+        if name in self._queues.keys():
+            return self._queues[name].is_empty()
+        print('На доске задач нет очереди с таким названием')
 
     def executed(self):
+        """ Метод возвращает список выполненных задач со всей доски задач"""
         return self._executed
 
     def task_queues(self):
+        """ Метод возвращает список имеющихся очередей задач"""
         return self._queues.keys()
 
 
@@ -157,6 +191,9 @@ if __name__ == '__main__':
     print('Очередь задач "basic" пуста:', NEW_B.queue_is_empty('basic'))
     print('Очередь задач "deferred" пуста:', NEW_B.queue_is_empty('deferred'))
 
-
     # Смотрим список выполненных заданий
-    print(NEW_B.executed())
+    print('Были выполнены задачи:', NEW_B.executed())
+
+    #Порядок приоритета очередей мне не очень нравится, хотелось бы,
+    # чтобы его можно было указывать явно, но мне не хватило времени это
+    # доработать
