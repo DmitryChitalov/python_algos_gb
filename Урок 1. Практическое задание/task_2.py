742ad9c8040df7e6c525ab94c71532bf3be20dc8
aleksei@mazur <mazuralexey@gmail.com>
2021-01-18 14:24:30 +0300

    lesson 1 tasks

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,11 +9,45 @@
 
 Второй, в виде функции, должен обеспечивать поиск минимального значения для списка.
 Сложность такого алгоритма: O(n) - линейная.
-
-Примечание:
-Построить список можно через генератор списка.
-Если у вас возникают сложности, постарайтесь подумать как можно решить задачу,
-а не писать "мы это не проходили)".
-Алгоритмизатор должен развивать мышление, а это прежде всего практика.
-А без столкновения со сложностями его не развить.
 """
+from random import randint
+
+# вложенный цикл O(n^2)
+# lst = [2, 1, 3]
+
+
+def search_min_value(lst):
+    for i in lst:
+        min_value = True
+        for j in lst:
+            if i > j:
+                min_value = False
+        if min_value:
+            return i
+
+# sort O(NlogN)
+# в задании этого нет, но как вариант решения
+
+
+def min_elem(lst):
+    result_list = sorted(lst)
+    return result_list[0]
+
+
+# O(n) линейная сложность
+
+def min_in_list(lst):
+    min_value_n = lst[0]
+    for i in lst:
+        if i < min_value_n:
+            min_value_n = i
+    return min_value_n
+
+
+lst2 = [randint(-10, 10) for i in range(10)]
+
+print(lst2)
+# print(search_min_value(lst))
+print(search_min_value(lst2))
+print(min_elem(lst2))
+print(min_in_list(lst2))
