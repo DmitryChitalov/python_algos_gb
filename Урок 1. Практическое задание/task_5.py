7337a6669e07f762db64988467fb3eb4b6ddfe7d
Роман Тетерин <rateterin@yandex.ru>
2021-01-18 15:32:03 +0300

    tasks 1 - 7 for lesson 1 Algs

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -1,5 +1,5 @@
 """
-Задание 6.
+Задание 5.
 Задание на закрепление навыков работы со стеком
 
 Примечание: в этом задании вспомните ваши знания по работе с ООП
@@ -17,4 +17,74 @@
 для реализации этой задачи.
 
 После реализации структуры, проверьте ее работу на различных сценариях
+
+Подсказка:
+Отдельне стопки можно реализовать через:
+# 1) созд-е экземпляров стека (если стопка - класс)
+# 2) lst = [[], [], [], [],....]
 """
+
+
+class MyStackClass:
+    def __init__(self, group_size):
+        if type(group_size) != int:
+            self.group_size = 10
+        else:
+            self.group_size = group_size
+        self.groups = []
+        self.current_group = 0
+        self.elems = []
+
+    @property
+    def is_empty(self):
+        return not bool(self.size)
+
+    @property
+    def size(self):
+        return len(self.elems) + self.current_group * self.group_size
+
+    def push(self, el):
+        if el is not None:
+            if self.is_empty:
+                self.groups.append([])
+                self.elems = self.groups[self.current_group]
+            if len(self.elems) == self.group_size:
+                self.current_group += 1
+                self.groups.append([])
+                self.elems = self.groups[self.current_group]
+            self.elems.append(el)
+
+    def pop(self):
+        if self.is_empty:
+            return 'Тарелок больше нет, стек пуст'
+        r = self.elems.pop()
+        if not len(self.elems):
+            self.groups.pop()
+            if self.is_empty:
+                self.elems = []
+            else:
+                self.current_group -= 1
+                self.elems = self.groups[self.current_group]
+        return r
+
+    def get_val(self):
+        if not self.is_empty:
+            return self.elems[-1]
+
+    def __str__(self):
+        if self.is_empty:
+            return 'Тарелок больше нет, стек пуст'
+        else:
+            return f'Стопок: {len(my_stack.groups)}, Тарелок в последней стопке: {len(my_stack.elems)}'
+
+
+my_stack = MyStackClass(10)
+while True:
+    ans = input('(+) - добавить тарелку, (-) - убрать тарелку, (q) - выход: ').lower()
+    if ans == 'q':
+        break
+    if ans == '+':
+        my_stack.push('тарелка')
+    elif ans == '-':
+        my_stack.pop()
+    print(my_stack)
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -15,3 +15,70 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+
+class MyQueueClass:
+    def __init__(self):
+        self.q_tasks = []  # Очередь задач, отсюда будем их брать
+        self.d_tasks = []  # Решенные задачи будем помещать сюда
+        self.pd_tasks = []  # Частично решенные "на доработку"
+        self.current = 0
+
+    @property
+    def q_empty(self):
+        return not bool(self.q_size)
+
+    @property
+    def q_size(self):
+        return len(self.q_tasks)
+
+    @property
+    def p_empty(self):
+        return not bool(self.p_size)
+
+    @property
+    def p_size(self):
+        return len(self.pd_tasks)
+
+    def put(self):
+        self.current += 1
+        self.q_tasks.insert(0, f'Задача {self.current}')
+
+    def done(self):
+        if not self.q_empty:
+            self.d_tasks.insert(0, self.q_tasks.pop())
+        else:
+            print('Задач в очереди нет!')
+
+    def done_partial(self):
+        if not self.p_empty:
+            self.d_tasks.insert(0, self.pd_tasks.pop())
+        else:
+            print('Задач "на доработку" нет!')
+
+    def to_partial(self):
+        if not self.q_empty:
+            self.pd_tasks.insert(0, self.q_tasks.pop())
+        else:
+            print('Задач в очереди нет!')
+
+    def __str__(self):
+        return f'Текущие задачи: {self.q_tasks}\n' \
+               f'На доработку: {self.pd_tasks}\n' \
+               f'Решенные задачи: {self.d_tasks}'
+
+
+tasks_board = MyQueueClass()
+while True:
+    ans = input('(+) - добавить задачу, (v) - решить задачу, (-) - на доработку, (=) - дорешать, (q) - выход: ').lower()
+    if ans == 'q':
+        break
+    if ans == '+':
+        tasks_board.put()
+    elif ans == 'v':
+        tasks_board.done()
+    elif ans == '=':
+        tasks_board.done_partial()
+    elif ans == '-':
+        tasks_board.to_partial()
+    print(tasks_board)
