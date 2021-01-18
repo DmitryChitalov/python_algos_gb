92162063d9b290ca281399e7d29cde6cede771cd
Lajilit <lajilit@gmail.com>
2021-01-18 03:34:22 +0300

    task_5

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,76 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+import random
+
+class Plate_storage:
+    def __init__(self):
+        self._stacks_of_plates = []
+
+    def is_empty(self):
+        return self._stacks_of_plates == []
+
+    def add_plate(self):
+        """Добавляем тарелку в стопку, пока тарелок не станет 10,
+        иначе начинаем новую стопку"""
+        if self.is_empty() or len(self._stacks_of_plates[-1]) == 10:
+            self._stacks_of_plates.append(['plate'])
+        else:
+            self._stacks_of_plates[-1].append('plate')
+
+    def remove_plate(self):
+        if not self.is_empty():
+            self._stacks_of_plates[-1].pop()
+            if len(self._stacks_of_plates[-1]) == 0:
+                self._stacks_of_plates.pop()
+
+
+    def last_stack_amount(self):
+        return len(self._stacks_of_plates[-1])
+
+    def plates_amount(self):
+        return (len(self._stacks_of_plates) - 1) * 10 + \
+               len(self._stacks_of_plates[-1])
+
+    def stacks_amount(self):
+        return len(self._stacks_of_plates)
+
+# создаём хранилище тарелок (объект класса Plate_storage())
+cupboard = Plate_storage()
+
+# определяем, есть ли тарелки в хранилище
+print(cupboard.is_empty())  # -> тарелок нет
+
+# наполняем хранилище
+plates_amount = random.randint(1,100)
+for i in range(plates_amount):
+    cupboard.add_plate()
+
+# Определяем общее количество тарелок в хранилище
+print(cupboard.plates_amount())
+
+# Определяем количество стопок в хранилище
+print(cupboard.stacks_amount())
+
+# определяем количество тарелок в последней стопке
+print(cupboard.last_stack_amount())
+
+# определяем, есть ли тарелки в хранилище
+print(cupboard.is_empty())  # -> тарелки есть
+
+# Добавляем тарелку
+cupboard.add_plate()
+print(cupboard.last_stack_amount())
+
+# Убираем тарелку
+cupboard.remove_plate()
+print(cupboard.last_stack_amount())
+
+# Убираем тарелку
+cupboard.remove_plate()
+print(cupboard.last_stack_amount())
+
+# Определяем общее количество тарелок в хранилище
+print(cupboard.plates_amount()) 
+
+
