3b9cf4fa0c7e7d292da06bb3b4f8f7a3f9c0fcd1
Lajilit <lajilit@gmail.com>
2021-01-18 14:38:30 +0300

    task_7 and Reformat Code

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -7,8 +7,8 @@
 3) сделайте вывод, какое решение эффективнее и почему
 
 Примечание:
-Без выполнения пунктов 2 и 3 задание считается нерешенным. Пункты 2 и 3 можно выполнить
-через строки документации в самом коде.
+Без выполнения пунктов 2 и 3 задание считается нерешенным. Пункты 2 и 3
+можно выполнить через строки документации в самом коде.
 Если у вас возникают сложности, постарайтесь подумать как можно решить задачу,
 а не писать "мы это не проходили)".
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
@@ -24,56 +24,56 @@
 """
 
 
-def top_profit_1(dict_obj:dict, count:int) -> list:
+def top_profit_1(dict_obj: dict, count: int) -> list:
     """ Функция принимает на вход два параметра: словарь и целое число,
     и возвращает 'count' пар (ключ, значение) с максимальным значением
 
     Вариант 1 - сложность O(N log N)
     """
     result_list = []
-    dict_items = list(dict_obj.items())                                 # O(N)
-    dict_items.sort(key=lambda i: i[1], reverse=True)                   # О(N log N)
-    for i in range(count):                                              # O(3)
-        result_list.append(dict_items[i])                               # O(1)
+    dict_items = list(dict_obj.items())                 # O(N)
+    dict_items.sort(key=lambda i: i[1], reverse=True)   # О(N log N)
+    for i in range(count):                              # O(3)
+        result_list.append(dict_items[i])               # O(1)
     return result_list
 
 
-def top_profit_2(dict_obj:dict, count:int) -> list:
+def top_profit_2(dict_obj: dict, count: int) -> list:
     """ Функция принимает на вход два параметра: словарь и целое число,
     и возвращает 'count' пар (ключ, значение) с максимальным значением
 
     Вариант 2 - сложность O(N^2)
     """
-    dict_copy = dict_obj.copy()                                         # O(N)
-    result_list = []                                                    # O(1)
-    for i in range(count):                                              # O(1)
-        for k,v in dict_copy.items():                                   # O(N)
-            if v == max(dict_copy.values()):                            # O(N)
-                result_list.append((k,v))                               # O(1)
-                dict_copy.pop(k)                                        # O(1)
+    dict_copy = dict_obj.copy()                         # O(N)
+    result_list = []                                    # O(1)
+    for i in range(count):                              # O(1)
+        for k, v in dict_copy.items():                  # O(N)
+            if v == max(dict_copy.values()):            # O(N)
+                result_list.append((k, v))              # O(1)
+                dict_copy.pop(k)                        # O(1)
                 break
-    return result_list                                                  # O(1)
+    return result_list                                  # O(1)
 
 
-def top_profit_3(dict_obj:dict, count:int) -> list:
+def top_profit_3(dict_obj: dict, count: int) -> list:
     """ Функция принимает на вход два параметра: словарь и целое число,
     и возвращает 'count' пар (ключ, значение) с максимальным значением
 
     Вариант 3 - сложность O(N) - наиболее предпочтительный вариант
     """
-    dict_copy = dict_obj.copy()                                         # O(N)
-    result_list = []                                                    # O(1)
-    for i in range(count):                                              # O(1)
-        max_profit_key = ''                                             # O(1)
-        max_profit_value = 0                                            # O(1)
-        for k, v in dict_copy.items():                                  # O(N)
-            if v > max_profit_value:                                    # O(1)
-                max_profit_key = k                                      # O(1)
-                max_profit_value = v                                    # O(1)
+    dict_copy = dict_obj.copy()                         # O(N)
+    result_list = []                                    # O(1)
+    for i in range(count):                              # O(1)
+        max_profit_key = ''                             # O(1)
+        max_profit_value = 0                            # O(1)
+        for k, v in dict_copy.items():                  # O(N)
+            if v > max_profit_value:                    # O(1)
+                max_profit_key = k                      # O(1)
+                max_profit_value = v                    # O(1)
 
-        result_list.append((max_profit_key, max_profit_value))          # O(1)
-        dict_copy.pop(max_profit_key)                                   # O(1)
-    return result_list                                                  # O(1)
+        result_list.append((max_profit_key, max_profit_value))  # O(1)
+        dict_copy.pop(max_profit_key)                   # O(1)
+    return result_list                                  # O(1)
 
 
 companies = {
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -6,13 +6,11 @@
 2) оцените сложность каждого решения в нотации О-большое
 3) сделайте вывод, какое решение эффективнее и почему
 
-Примечание:
-Без выполнения пунктов 2 и 3 задание считается нерешенным. Пункты 2 и 3 можно выполнить
-через строки документации в самом коде.
-Если у вас возникают сложности, постарайтесь подумать как можно решить задачу,
-а не писать "мы это не проходили)".
-Алгоритмизатор должен развивать мышление, а это прежде всего практика.
-А без столкновения со сложностями его не развить.
+Примечание: Без выполнения пунктов 2 и 3 задание считается нерешенным. Пункты
+2 и 3 можно выполнить через строки документации в самом коде. Если у вас
+возникают сложности, постарайтесь подумать как можно решить задачу,
+а не писать "мы это не проходили)". Алгоритмизатор должен развивать мышление,
+а это прежде всего практика. А без столкновения со сложностями его не развить.
 
 
 Сама задача:
@@ -23,14 +21,15 @@
 При этом его учетка должна быть активирована.
 А если нет, то польз-лю нужно предложить ее пройти.
 
-Приложение должно давать ответы на эти вопросы и быть реализовано в виде функции.
+Приложение должно давать ответы на эти вопросы и быть реализовано
+в виде функции.
 Для реализации хранилища можно применить любой подход,
 который вы придумаете, например, реализовать словарь.
 """
 
 
-def authentication_func(login:str, password:str, base:list) -> bool:
-    '''
+def authentication_func(login: str, password: str, base: list) -> bool:
+    """
     Вариант 1 - Сложность O(N)
 
     Функция проверяет наличие пользователя в базе, правильность ввода пароля,
@@ -44,30 +43,30 @@ def authentication_func(login:str, password:str, base:list) -> bool:
 
     Функция возвращает значение True в случае успешной авторизации и
     False, если авторизация не удалась
-    '''
-    for user_dict in base:                                              # O(N)
-        if user_dict['login'] == login:                         # O(1)
-            if user_dict['password'] != password:                       # O(1)
-                print('Неверный пароль')                                # O(1)
-                return False                                            # O(1)
-            is_active = user_dict['is_active']                          # O(1)
-            if not is_active:                                           # O(1)
-                print('Необходимо активировать аккаунт')                # O(1)
+    """
+    for user_dict in base:  # O(N)
+        if user_dict['login'] == login:  # O(1)
+            if user_dict['password'] != password:  # O(1)
+                print('Неверный пароль')  # O(1)
+                return False  # O(1)
+            is_active = user_dict['is_active']  # O(1)
+            if not is_active:  # O(1)
+                print('Необходимо активировать аккаунт')  # O(1)
                 activation = input('Введите "Y" чтобы активировать: ')  # O(1)
-                if activation.upper() != 'Y':                           # O(1)
-                    print('Аккаунт не активирован')                     # O(1)
-                    return False                                        # O(1)
-                user_dict['is_active'] = True                           # O(1)
-                print('Аккаунт активирован')                            # O(1)
+                if activation.upper() != 'Y':  # O(1)
+                    print('Аккаунт не активирован')  # O(1)
+                    return False  # O(1)
+                user_dict['is_active'] = True  # O(1)
+                print('Аккаунт активирован')  # O(1)
             print('Аутентификация пройдена')
-            return True                                                 # O(1)
+            return True  # O(1)
 
     print('Неверное имя пользователя')
-    return False                                                        # O(1)
+    return False  # O(1)
 
 
 def authentication_func_2(login: str, password: str, base: dict) -> bool:
-    '''
+    """
     Вариант 2 - Сложность O(1)
 
     Функция проверяет наличие пользователя в базе, правильность ввода пароля,
@@ -81,27 +80,27 @@ def authentication_func_2(login: str, password: str, base: dict) -> bool:
 
     Функция возвращает значение True в случае успешной авторизации и
     False, если авторизация не удалась
-    '''
+    """
     try:
-        user_info = base[login]                                         # O(1)
+        user_info = base[login]  # O(1)
     except KeyError:
-        print('Неверное имя пользователя')                              # O(1)
-        return False                                                    # O(1)
+        print('Неверное имя пользователя')  # O(1)
+        return False  # O(1)
     else:
-        if user_info['password'] != password:                           # O(1)
-            print('Неверный пароль')                                    # O(1)
-            return False                                                # O(1)
-        is_active = user_info['is_active']                              # O(1)
-        if not is_active:                                               # O(1)
-            print('Необходимо активировать аккаунт')                    # O(1)
-            activation = input('Введите "Y" чтобы активировать: ')      # O(1)
-            if activation.upper() != 'Y':                               # O(1)
-                print('Аккаунт не активирован')                         # O(1)
-                return False                                            # O(1)
-            user_info['is_active'] = True                               # O(1)
-            print('Аккаунт активирован')                                # O(1)
-        print('Аутентификация пройдена')                                # O(1)
-        return True                                                     # O(1)
+        if user_info['password'] != password:  # O(1)
+            print('Неверный пароль')  # O(1)
+            return False  # O(1)
+        is_active = user_info['is_active']  # O(1)
+        if not is_active:  # O(1)
+            print('Необходимо активировать аккаунт')  # O(1)
+            activation = input('Введите "Y" чтобы активировать: ')  # O(1)
+            if activation.upper() != 'Y':  # O(1)
+                print('Аккаунт не активирован')  # O(1)
+                return False  # O(1)
+            user_info['is_active'] = True  # O(1)
+            print('Аккаунт активирован')  # O(1)
+        print('Аутентификация пройдена')  # O(1)
+        return True  # O(1)
 
 
 # На мой взгляд более быстрым выглядит второй вариант функции, так как его
@@ -137,7 +136,6 @@ users_dict = {
     'japan': {'password': 'tokyo', 'is_active': True},
     'xiaomi': {'password': 'redmi note IV', 'is_active': False}
 }
-
 
 # login_input = input('Имя пользователя: ').lower()
 # password_input = input('Пароль: ')
