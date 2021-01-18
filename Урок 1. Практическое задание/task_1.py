21545ef2c0833689624f629106c861eb396684dc
RinatKhanov <rinat.hanoff@icloud.com>
2021-01-18 17:24:15 +0300

    К сожалению не со всем справился(

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -26,10 +26,10 @@ def check_1(lst_obj):
     Алгоритм 3:
     Создать множество из списка
 
-    Сложность: !!!.
+    Сложность: O(n)
     """
-    lst_to_set = set(lst_obj)  # !!!
-    return lst_to_set
+    lst_to_set = set(lst_obj)  # O(n)
+    return lst_to_set  # O(1)
 
 
 #############################################################################################
@@ -41,12 +41,12 @@ def check_2(lst_obj):
     что такой элемент отстутствует
     в оставшихся справа элементах
 
-    Сложность: !!!.
+    Сложность: O(n), хотя возможно O(n^2) тут не уверен)
     """
-    for j in range(len(lst_obj)):          # !!!
-        if lst_obj[j] in lst_obj[j+1:]:    # !!!
-            return False                   # !!!
-    return True                            # !!!
+    for j in range(len(lst_obj)):          # O(n)
+        if lst_obj[j] in lst_obj[j+1:]:    # O(n)
+            return False                   # O(1)
+    return True                            # O(1)
 
 
 #############################################################################################
@@ -57,14 +57,14 @@ def check_3(lst_obj):
     Вначале выполним для списка сортировку, далее, сравниваем элементы попарно
     Если присутствуют дубли, они будут находиться рядом.
 
-    Сложность: !!!
+    Сложность: O(nLogn)
     """
-    lst_copy = list(lst_obj)                 # !!!
-    lst_copy.sort()                          # !!!
-    for i in range(len(lst_obj) - 1):        # !!!
-        if lst_copy[i] == lst_copy[i+1]:     # !!!
-            return False                     # !!!
-    return True                              # !!!
+    lst_copy = list(lst_obj)                 # O(n)
+    lst_copy.sort()                          # O(nLogn)
+    for i in range(len(lst_obj) - 1):        # O(n)
+        if lst_copy[i] == lst_copy[i+1]:     # O(1)
+            return False                     # O(1)
+    return True                              # O(1)
 
 #############################################################################################
 
@@ -77,3 +77,4 @@ for j in (50, 500, 1000, 5000, 1000):
 print(check_1(lst))
 print(check_2(lst))
 print(check_3(lst))
+
--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -17,3 +17,34 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 """
+
+from random import randint
+
+#O(n^2)
+def smallestElement(list):
+    for i in list:
+        smallest = True
+        for j in list:
+            if i > j:
+                smallest = False
+        if smallest:
+            return i      
+
+
+list = [randint(1, 50) for x in range(10)]
+print(list)
+print(smallestElement(list))
+
+#O(n)
+def smallestElement1(list):
+    smallest = list[0]
+    for i in list:
+        if i < smallest:
+            smallest = i
+    return smallest
+
+
+list1 = [randint(1, 50) for x in range(10)]
+
+print(list1)
+print(smallestElement1(list1))
