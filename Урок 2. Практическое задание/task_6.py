edb01d7ee04b8a95ad15356782ea735a80b456e0
Роман Тетерин <rateterin@yandex.ru>
2021-01-19 10:53:13 +0300

    tasks 1 - 7 for lesson 2 Algs

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -9,3 +9,36 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+from random import randint
+
+
+def check_num(number, attempts):
+    try:
+        check_num.i += 1
+    except AttributeError:
+        check_num.i = 1
+    while True:
+        try:
+            ans = int(input(f'Отгадайте число от 1 до 100 (Попытка {check_num.i} из {attempts}): '))
+        except ValueError:
+            print('Нужно ввести целое число от 1 до 100!')
+            ans = 101
+        if 1 <= ans <= 100:
+            break
+    if check_num.i == attempts:
+        return number
+    else:
+        if ans < number:
+            print('Больше!')
+
+        elif ans > number:
+            print('Меньше!')
+        if ans == number:
+            print('Верно!')
+            return number
+        return check_num(number, attempts)
+
+
+print(f'Загаданное число: {check_num(randint(1, 100), 10)}')
