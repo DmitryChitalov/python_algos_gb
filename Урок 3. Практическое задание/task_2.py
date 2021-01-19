c122bde4253de68faed28915e64ddf4ee2427cdc
ShepLL <pavelpopov98@mail.ru>
2021-01-19 17:53:57 +0300

    Lesson 3. Popov

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -15,3 +15,69 @@
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
+import hashlib
+from uuid import uuid4
+import os
+
+
+class UserManager:
+    """
+    Класс для создания пользователей и аутентификации
+    """
+    @staticmethod
+    def create_usr(usr_id, passwd):
+        """
+        Метод для создания нового юзера
+        """
+        salt = usr_id
+        if os.path.isfile('save_pass'):
+            with open('save_pass', 'r') as f_in:
+                lin = f_in.readlines()
+            for elem in lin:
+                if elem.find(usr_id) >= 0:
+                    raise Exception('Пользователь с таким ником уже существует')
+        else:
+            open('save_pass', 'w')
+        hash_obj = hashlib.new('sha512')
+        hash_obj.update((passwd + salt).encode('utf-8'))
+        with open('save_pass', 'a') as f_out:
+            f_out.write(f'{usr_id} {hash_obj.hexdigest()}\n')
+        print('Пользователь успешно создан!')
+
+
+    @staticmethod
+    def auth_usr(user_id, passwd):
+        """
+        Метрод аутентификации юзера
+        """
+        salt = user_id
+        if os.path.isfile('save_pass'):
+            cnt = -1
+            with open('save_pass', 'r') as f_in:
+                lin = f_in.readlines()
+            for i, elem in enumerate(lin):
+                if elem.find(user_id) >= 0:
+                    cnt = i
+                    break
+            if cnt == -1:
+                print('Аутентификация не пройдена!')
+                return
+        else:
+            raise Exception('Файл с данными пользователей не найден')
+        passwd_ident = lin[i].split(' ')[1][:-1]
+        hash_obj = hashlib.new('sha512')
+        hash_obj.update((passwd + salt).encode('utf-8'))
+        if str(hash_obj.hexdigest()) == passwd_ident:
+            print(f'Добро пожаловать {user_id} !')
+        else:
+            print('Аутентификация не пройдена!')
+
+
+if __name__ == '__main__':
+    usr = UserManager()
+    usr.create_usr('kk', '123qwe')
+    usr.auth_usr('kk', '123qwe')
+    usr.auth_usr('LLgg', '123qwen')
+
+
+
