3931e2302c33ba6a4cbe975dc0264f940e26be58
timonen87 <71833451+timonen87@users.noreply.github.com>
2021-01-19 13:29:32 +0300

    Update tack_5.py

--- Урок 1. Практическое задание/tack_5.py
+++ Урок 1. Практическое задание/tack_5.py
@@ -23,3 +23,54 @@
 # 1) созд-е экземпляров стека (если стопка - класс)
 # 2) lst = [[], [], [], [],....]
 """
+
+
+
+
+class StackClass:
+    def __init__(self, max_size=10):
+        self.elems = [[]]
+        self.max_size = max_size
+
+    def __str__(self):
+        return str(self.elems)
+
+    def is_empty(self):
+        return self.elems == [[]]
+
+    def push_in(self, el):
+        if len(self.elems[len(self.elems) - 1]) < self.max_size:
+            self.elems[len(self.elems) - 1].append(el)
+        else:
+            self.elems.append([])
+            self.elems[len(self.elems) - 1].append(el)
+
+    def pop_out(self):
+        out = self.elems[len(self.elems) - 1].pop()
+        if len(self.elems[len(self.elems) - 1]) == 0:
+            self.elems.pop()
+        return out
+
+    def get_val(self):
+        return self.elems[len(self.elems) - 1][len(self.elems[len(self.elems) - 1]) - 1]
+
+    def stack_size(self):
+        size = 0
+        for i in self.elems:
+            size += len(i)
+        return size
+
+
+StackPlates = StackClass()
+
+for i in range(20):
+    StackPlates.push_in(i)
+print(StackPlates)
+
+for i in range(5):
+    StackPlates.pop_out()
+print(StackPlates)
+
+for i in range(11):
+    StackPlates.pop_out()
+print(StackPlates)
