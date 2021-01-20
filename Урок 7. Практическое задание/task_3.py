ec2ba9dda57985bd521d9a904dcab025bb98ca50
SergeyLomakin <sergeylomakinaals@mail.ru>
2021-01-20 17:29:41 +0500

    lesson 7

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,35 @@
 arr[m]
 from statistics import median
 """
+import random
+
+# Просто поиск медианы
+m = int(input('введите длину массива: '))
+lst_obj = [random.randint(-100, 100) for i in range(2 * m + 1)]
+print(f'исходный массив: {lst_obj}')
+
+while len(lst_obj) != 1:
+    lst_obj.remove(max(lst_obj))
+    lst_obj.remove(min(lst_obj))
+
+print(f'медиана массива равна: {lst_obj[0]}')
+
+#######################################################
+
+print()
+# Поиск медианы с созданием двух списков, с элементами больше и меньше медианы.
+lst_obj = [random.randint(-100, 100) for i in range(2 * m + 1)]
+print(f'исходный массив: {lst_obj}')
+
+more_median = []
+less_median = []
+while len(lst_obj) != 1:
+    more_median.append(max(lst_obj))
+    lst_obj.remove(max(lst_obj))
+    less_median.append(min(lst_obj))
+    lst_obj.remove(min(lst_obj))
+
+print(f'медиана массива равна: {lst_obj[0]}')
+print(f'список элементов больше медианы: {more_median}')
+print(f'список элементов меньше медианы: {less_median}')
+
