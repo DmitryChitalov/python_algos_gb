6447d50b3aee23120d309d623aaebe14b0c49d45
dmitry <evsvald@gmail.com>
2021-01-20 16:36:55 +0300

    lesson5

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -15,3 +15,84 @@
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
+
+from hashlib import sha256
+from uuid import uuid4
+import sqlite3  # импорт библиотеки sqlite
+from sqlite3 import Error  # импорт класса
+
+salt = uuid4().hex
+
+
+#  print(salt)
+
+
+def create_connection(path):  # подключение к базе данных
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Соединение успешно")
+    except Error as E:
+        print(f" Произошла ошибка '{E}'!")
+
+    return connection
+
+
+connect = create_connection("E:\\gb\example.sqlite")  # создает файл базы данных по указанному пути
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Добавление в БД успешно")
+    except Error as e:
+        print(f"Ошибка '{e}' !")
+
+
+def read_query(connection, query):  # чтение из БД
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return print(result)
+    except Error as e:
+        print(f" Ошибка '{e}'! ")
+
+
+# оздание простой таблицы с двумя столбцами id и pass
+create_hash_pass_table = """                    
+CREATE TABLE IF NOT EXISTS hashed_pass (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pass BLOB 
+);
+"""
+execute_query(connect, create_hash_pass_table)
+
+
+def hash_pass(password):
+    psswrd = sha256(salt.encode() + password.encode()).hexdigest() + '/' + salt
+    execute_query(connect, f"insert into hashed_pass(pass) values ('{psswrd}')")
+    # read_query(connect, "SELECT * FROM hashed_pass")
+    return psswrd
+
+
+def check_hashed_pass():
+    check_pass = input('Введите пароль для проверки: ')
+    check_pass_h = sha256(salt.encode() + check_pass.encode()).hexdigest() + '/' + salt
+    psswrd = read_query(connect, "SELECT pass FROM hashed_pass WHERE id = 1")
+    if check_pass_h == psswrd:
+        print('Введен правильный пароль ')
+    else:
+        print('Пароли не совпадают')
+
+
+passwd = input('Введите пароль: ')
+hash_pass(passwd)
+# execute_query(connect, hash_pass(passwd))
+check_hashed_pass()
+# execute_query(connect, "DROP TABLE IF EXISTS hashed_pass")
+#abc = read_query(connect, "SELECT pass FROM hashed_pass WHERE id = 1 ")
+#print(type(abc))
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,67 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+
+from collections import OrderedDict
+import timeit
+
+abc = {}
+xyz = OrderedDict(abc)
+
+
+def create_dict():
+    abc = {i: i / 2 for i in range(10000)}
+    return abc
+
+
+def create_order_dict():
+    xyz = {i: i / 2 for i in range(10000)}
+    return xyz
+
+
+def pop_dict():
+    for i in abc:
+        abc.pop(i)
+    return abc
+
+
+def pop_order_dict():
+    for i in xyz:
+        xyz.pop(i)
+    return xyz
+
+
+def dict_items():
+    for i in abc:
+        abc.items()
+
+
+def order_dict_items():
+    for i in xyz:
+        xyz.items()
+
+
+print('CREATE:')
+print(timeit.timeit('create_dict', setup='from __main__ import create_dict', number=10000))
+print(timeit.timeit('create_order_dict', setup='from __main__ import create_order_dict', number=10000))
+print('POP')
+print(timeit.timeit('pop_dict', setup='from __main__ import pop_dict', number=10000))
+print(timeit.timeit('pop_order_dict', setup='from __main__ import pop_order_dict', number=10000))
+print('ITEMS')
+print(timeit.timeit('dict_items', setup='from __main__ import dict_items', number=10000))
+print(timeit.timeit('order_dict_items', setup='from __main__ import order_dict_items', number=10000))
+
+'''
+CREATE:
+0.0005156000000000605
+0.0005797000000000718
+POP
+0.0005182000000000242
+0.0005213000000000578
+ITEMS
+0.0005139000000000671
+0.0005140999999999618
+
+получились идентичные результаты, каких либо преимуществ, кроме отслеживания порядка ввода значений в OrderedDict
+(при случае может оказаться полезным инструментом), я не увидел 
+'''
