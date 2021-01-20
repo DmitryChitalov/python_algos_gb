82183eb63b2b6959864eb845d16c0edc4a1dd48a
etery <shadowX110@yandex.ru>
2021-01-20 22:30:52 +0300

    added homework to lesson 7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,59 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import random
+from timeit import timeit
+
+
+def merge_sort(lst_obj):
+    if len(lst_obj) > 1:
+        center = len(lst_obj) // 2
+        left = lst_obj[:center]
+        right = lst_obj[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst_obj[k] = left[i]
+                i += 1
+            else:
+                lst_obj[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            lst_obj[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            lst_obj[k] = right[j]
+            j += 1
+            k += 1
+        return lst_obj
+
+
+if __name__ == "__main__":
+    test_list = [random.triangular(0, 50) for elem in range(100)]
+    test_list_int = [random.randint(0, 50) for _ in range(100)]
+    print(f'исходный массив: {test_list}')
+    print(f'Отсортированный массив: {merge_sort(test_list[:])}')
+    print('Профилировка времени сортировки слиянием')
+    print(timeit(
+        "merge_sort(test_list[:])",
+        setup="from __main__ import merge_sort, test_list", number=1000)
+    )
+    print(timeit(
+        "merge_sort(test_list_int[:])",
+        setup="from __main__ import merge_sort, test_list_int", number=1000)
+    )
+
+"""
+Выводы: Сортировка слиянием работает в несколько раз эффективнее, чем сотрировка пузырьком.
+Время, затраченное на сортировку массива, почти в 4 раза меньше.
+"""
+
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,41 @@
 arr[m]
 from statistics import median
 """
+import random
+from statistics import median
+from timeit import timeit
+
+
+def gnome_sort(list_obj):
+    i = 1
+    size = len(list_obj)
+    while i < size:
+        if list_obj[i - 1] <= list_obj[i]:
+            i += 1
+        else:
+            list_obj[i], list_obj[i - 1] = list_obj[i - 1], list_obj[i]
+            i -= 1
+            if i == 0:
+                i = 1
+    return list_obj
+
+
+def get_list_median(list_obj, m):
+    return gnome_sort(list_obj)[m]
+
+
+if __name__ == "__main__":
+    m = 50
+    test_list = [random.randint(0, 100) for i in range(2 * m + 1)]
+    print(median(test_list[:]))
+    print(get_list_median(test_list[:], m))
+    print('Профилировка времени гномьей сортировки')
+    print(timeit(
+        "gnome_sort(test_list[:])",
+        setup="from __main__ import gnome_sort, test_list", number=1000)
+    )
+
+"""
+Выводы: Гномья сортировка обладает худшей эффективностью по времени в сравнении с пузырьковой сортировкой
+и сортировкой слиянием.
+"""
