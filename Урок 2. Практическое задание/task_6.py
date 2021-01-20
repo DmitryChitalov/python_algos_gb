6ca09b976f4d9fb5f9cda6e8a7d3c5909a6325fc
kfotkin <68823654+kfotkin@users.noreply.github.com>
2021-01-20 22:06:43 +0300

    Update task_6.py

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -9,3 +9,28 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+import random
+random_num = random.randint(0, 100)
+count = 0
+
+
+def guess():
+    global random_num, count
+    if count <10:
+        user_num = int(input(f'введите число: '))
+        if user_num == random_num:
+            print(f'победа!')
+        elif user_num < random_num:
+            print(f'бери больше!')
+            count += 1
+            guess()
+        else:
+            print(f'бери меньше!')
+            count += 1
+            guess()
+    else:
+        print(f'попытки закончились :( загаданное число было {random_num}')
+        
+        
+   guess()
