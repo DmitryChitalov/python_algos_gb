01a91e702b5919293417be3f1706c14d029db2f2
kfotkin <68823654+kfotkin@users.noreply.github.com>
2021-01-20 23:17:33 +0300

    Update task_7.py

--- Урок 2. Практическое задание/task_7.py
+++ Урок 2. Практическое задание/task_7.py
@@ -6,3 +6,20 @@
  Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+def sum(num):
+    if num == 0:
+        return num
+    else:
+        return num + sum(num - 1)
+
+
+try:
+    num = int(input(f'введите целое число: '))
+    if sum(num) == num * (num + 1) / 2:
+        print(f'{sum(num)} = {int(num * (num + 1) / 2)} доказано!')
+    else:
+        print(f'где-то затаилась ошибка...')
+except ValueError:
+    print(f'введите все-таки число...')
