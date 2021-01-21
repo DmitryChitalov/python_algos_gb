850f7b09c4694ca80c21e93920bdfd12c504710e
Dmitriy <71927035+Velhsq@users.noreply.github.com>
2021-01-21 21:33:34 +0700

    HW2

--- task_5.py
+++ task_5.py
@@ -1,70 +1,36 @@
 """
-Задание 5.
-Задание на закрепление навыков работы со стеком
+5.	Вывести на экран коды и символы таблицы ASCII, начиная с символа
+под номером 32 и заканчивая 127-м включительно.
+Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
 
-Примечание: в этом задании вспомните ваши знания по работе с ООП
-и опирайтесь на пример урока
+Пример:
+32 -   33 - ! 34 - " 35 - # 36 - $ 37 - % 38 - & 39 - ' 40 - ( 41 - )
+42 - * 43 - + 44 - , 45 - - 46 - . 47 - / 48 - 0 49 - 1 50 - 2 51 - 3
+52 - 4 53 - 5 54 - 6 55 - 7 56 - 8 57 - 9 58 - : 59 - ; 60 - < 61 - =
+62 - > 63 - ? 64 - @ 65 - A 66 - B 67 - C 68 - D 69 - E 70 - F 71 - G
+72 - H 73 - I 74 - J 75 - K 76 - L 77 - M 78 - N 79 - O 80 - P 81 - Q
+82 - R 83 - S 84 - T 85 - U 86 - V 87 - W 88 - X 89 - Y 90 - Z 91 - [
+92 - \ 93 - ] 94 - ^ 95 - _ 96 - ` 97 - a 98 - b 99 - c 100 - d 101 - e
+102 - f 103 - g 104 - h 105 - i 106 - j 107 - k 108 - l 109 - m 110 - n 111 - o
+112 - p 113 - q 114 - r 115 - s 116 - t 117 - u 118 - v 119 - w 120 - x 121 - y
+122 - z 123 - { 124 - | 125 - } 126 - ~ 127 - 
 
-Реализуйте структуру "стопка тарелок".
-
-Мы можем складывать тарелки в стопку и при превышении некоторого значения
-нужно начать складывать тарелки в новую стопку.
-
-Структура должна предусматривать наличие нескольких стеков.
-Создание нового стека происходит при достижении предыдущим стеком порогового значения.
-Реализуйте по аналогии с примером, рассмотренным на уроке, необходимые методы,
-для реализации это структуры, добавьте новые методы (не рассмотренные в примере с урока)
-для реализации этой задачи.
-
-После реализации структуры, проверьте ее работу на различных сценариях
+Решите через рекурсию. Решение через цикл не принимается.
+Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 
 Подсказка:
-Отдельне стопки можно реализовать через:
-# 1) созд-е экземпляров стека (если стопка - класс)
-# 2) lst = [[], [], [], [],....]
+Допускается исп-е встроенных ф-ций
+и допускается комб-е - цикл и рекурсия
 """
 
-class PlatesStacks():
-
-    def __init__(self):
-        self.plates_in_stack = []
-        self.stacks_of_plates = []
-
-    def plate_add(self):
-        if len(self.plates_in_stack) < 5:
-            self.plates_in_stack.append('plate')
+def ascii_symbols(num=32, counter=0):
+    if num > 127:
+        return ''
+    else:
+        if counter == 10:
+            counter = 0
+            return f'\n  {num}-{chr(num)}' + ascii_symbols(num + 1, counter + 1)
         else:
-            self.stacks_of_plates.append(self.plates_in_stack)
-            self.plates_in_stack = []
-            self.plates_in_stack.append('plate')
+            return f'  {num}-{chr(num)}' + ascii_symbols(num + 1, counter + 1)
 
-    def plate_take_away(self):
-        if self.plates_in_stack:
-            self.plates_in_stack.pop(-1)
-        elif not self.stacks_of_plates:
-            print('тарелок нет')
-        else:
-            self.plates_in_stack = self.stacks_of_plates[-1]
-            self.stacks_of_plates.pop(-1)
-            self.plates_in_stack.pop(-1)
-
-    def show_parametres(self):
-        print(f'тарелки в стеке {self.plates_in_stack}')
-        print(f'заполненные стеки {self.stacks_of_plates}')
-
-
-plt = PlatesStacks()
-plt.plate_add()
-plt.plate_add()
-plt.plate_add()
-plt.plate_add()
-plt.plate_add()
-plt.plate_add()
-plt.plate_add()
-plt.plate_add()
-plt.plate_add()
-plt.plate_take_away()
-plt.plate_take_away()
-plt.plate_take_away()
-plt.plate_take_away()
-plt.show_parametres()
+print(ascii_symbols())
