5fe6d736672f4cc1b540558dd0742bb689f9917b
Valentine <dialog.valentine@gmail.com>
2021-01-21 12:44:06 +0800

    variant1

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -11,4 +11,52 @@
 
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
+
+Ответ: используем цикл for вместо while и отказываемся от счетчика,
+такая оптимизация даст небольшой прирост скорости.
+Так же во второй функции реализована проверка на отсортированность массива,
+где в подавляющем большинстве случаев не придется проходить массив
+полностью, если массив неотсортирован и достаточно будет всего нескольких
+шагов для проверки.
 """
+from random import randint
+from timeit import timeit
+
+
+def bubble_sort1(arr):
+    n = 1
+    while n < len(arr):
+        for i in range(len(arr)-n):
+            if arr[i] < arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+        n += 1
+    return arr
+
+
+def bubble_sort2(arr):
+    n = len(arr)
+
+    # Проверка отсортирован ли массив
+    for i in range(0, n-1):
+        if arr[i] < arr[i+1]:
+            break
+    else:
+        return arr
+
+    for i in range(1, n):
+        for k in range(n-i):
+            if arr[k] < arr[k+1]:
+                arr[k], arr[k+1] = arr[k+1], arr[k]
+    return arr
+
+
+if __name__ == '__main__':
+    unsorted = [randint(-100, 100) for _ in range(2000)]
+
+    print(timeit(f'bubble_sort1(j)', setup='j = unsorted.copy()', number=1, globals=globals()))
+    print(timeit(f'bubble_sort2(j)', setup='j = unsorted.copy()', number=1, globals=globals()))
+    unsorted1 = unsorted.copy()
+    unsorted2 = unsorted.copy()
+    print(bubble_sort1(unsorted1))
+    print(bubble_sort2(unsorted2))
+
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,30 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+from heapq import merge
+from timeit import timeit
+import random
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+    left = arr[:middle]
+    right = arr[middle:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return list(merge(left, right))
+
+
+if __name__ == '__main__':
+    a = int(input('Введите число элементов: '))
+    b = [random.uniform(0, 50) for _ in range(a)]
+    print('Исходный - ', b)
+    print('Отсортированный - ', merge_sort(b))
+    print('\nЗамеры на массивах разной длины:')
+    print(timeit('merge_sort(c)', 'c = [random.uniform(0, 50) for _ in range(10)]', number=1, globals=globals()))
+    print(timeit('merge_sort(c)', 'c = [random.uniform(0, 50) for _ in range(100)]', number=1, globals=globals()))
+    print(timeit('merge_sort(c)', 'c = [random.uniform(0, 50) for _ in range(1000)]', number=1, globals=globals()))
