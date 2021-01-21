d4c28a3be33e78538a4436683616db980ecd15bf
Lajilit <lajilit@gmail.com>
2021-01-21 22:54:39 +0300

    lesson_3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,14 @@
 р
 а
 """
+result_set = set()
+s = 'verylongstring'
+for sym in range(len(s)):
+    last_str = s[sym:]
+    for length in range(1, len(last_str) + 1):
+        sub_str = s[sym:sym + length]
+        if s != sub_str:
+            hash_sub_str = hash(sub_str)
+            result_set.add(hash_sub_str)
+
+print(len(result_set))
