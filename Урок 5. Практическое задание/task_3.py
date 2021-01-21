6b8308cfbf18fb29e74fd48cf07ff8d902b89ebe
Pafnodeveloper <51054260+Pafnodeveloper@users.noreply.github.com>
2021-01-21 14:50:40 +0300

    Add files via upload

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,42 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+
+from collections import deque
+
+import timeit
+
+list_example = [x for x in range(10)]
+deque_example = deque()
+[deque_example.append(x) for x in range(10)]
+
+print(list_example, deque_example)
+help(deque)
+
+print("Вставка справа в list занимает",
+      timeit.timeit("list_example.append(1)", 'from __main__ import list_example', number=100))
+print("Вставка справа в deque занимает:",
+      timeit.timeit("deque_example.append(1)", 'from __main__ import deque_example', number=100))
+print("Вставка слева в list занимает:",
+      timeit.timeit("list_example.insert(0, 1)", 'from __main__ import list_example', number=100))
+print("Вставка справа в deque занимает:",
+      timeit.timeit("deque_example.appendleft(1)", 'from __main__ import deque_example', number=100))
+
+print("Извлечение справа из list занимает:",
+      timeit.timeit("list_example.pop()", 'from __main__ import list_example', number=100))
+print("Извлечение справа из deque занимает:",
+      timeit.timeit("deque_example.pop()", 'from __main__ import deque_example', number=100))
+print("Извлечение слева из list занимает:",
+      timeit.repeat("list_example.pop(0)", 'from __main__ import list_example', timeit.default_timer, 3, number=3))
+print("Извлечение слева из deque занимает:",
+      timeit.repeat("deque_example.popleft()", 'from __main__ import deque_example', timeit.default_timer, 3, number=3))
+
+print("Доступ к случайному элементу из list:",
+      timeit.timeit("list_example[0]", 'from __main__ import list_example', number=100))
+print("Доступ к случайному элементу из deque:",
+      timeit.timeit("deque_example[0]", 'from __main__ import deque_example', number=100))
+
+"""
+Как видно из чисел доступ к случайному элементу быстрее происходит из list, 
+вставка быстрее у deque, скорость извлечения зависит от стороны извлечения
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,38 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+
+from collections import OrderedDict
+import timeit
+
+dict_example = {x: str(x) for x in range(10)}
+ordered_dict_example = OrderedDict([(0,'0'), (1,'1'), (2,'2'), (3,'3'), (4,'4'), (5,'5'),
+                                    (6,'6'), (7,'7'), (8,'8'), (9,'9')])
+
+
+help(ordered_dict_example)
+
+print("Время ordered_dict_example.items()",
+      timeit.timeit("ordered_dict_example.items()", "from __main__ import ordered_dict_example", number=100))
+print("Время dict_example.items()",
+      timeit.timeit("dict_example.items()", "from __main__ import dict_example", number=100))
+
+print("Время ordered_dict_example.values()",
+      timeit.timeit("ordered_dict_example.values()", "from __main__ import ordered_dict_example", number=100))
+print("Время dict_example.values()",
+      timeit.timeit("dict_example.values()", "from __main__ import dict_example", number=100))
+
+print("Время ordered_dict_example.keys()",
+      timeit.timeit("ordered_dict_example.keys()", "from __main__ import ordered_dict_example", number=100))
+print("Время dict_example.keys()",
+      timeit.timeit("dict_example.keys()", "from __main__ import dict_example", number=100))
+
+print("Время ordered_dict_example.update()",
+      timeit.timeit("ordered_dict_example.update(a=1)", "from __main__ import ordered_dict_example", number=100))
+print("Время dict_example.update()",
+      timeit.timeit("dict_example.update(a=1)", "from __main__ import dict_example", number=100))
+
+"""
+В целом dict из стандартной библиотеки работает или быстрее или с тем же временем, что и OrderedDict
+из модуля collections
+"""
