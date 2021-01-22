926808410146eb5a2180edf989419626924de4a1
invays <ayan.paul1234@yandex.ru>
2021-01-22 15:58:09 +0300

    Lesson 2 tasks - 1,2,3,4,6

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -9,3 +9,21 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+def number_sum(**data):
+    try:
+        data_mass = data['mass']
+        if len(data['mass']) == data['limit']:
+            return f'Количество элементов - {data["limit"]}, их сумма - {sum(data_mass)}'
+        else:
+            user_values = float(input('values: '))
+            data_mass.append(user_values)
+            return number_sum(limit=user_inp, mass=data_mass)
+
+    except ValueError:
+        print('Error')
+
+
+user_inp = int(input('Введите желаемое количество элементов: '))
+print(number_sum(limit=user_inp, mass=[]))
--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -9,3 +9,36 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+import random
+
+rand_num = random.randint(0, 100)
+print('Введите числа от 0 до 100')
+
+
+def random_value(**data):
+    try:
+        data_rand = data['rand_num']
+        data_value = data['value']
+        data_mass = data['mass']
+        if len(data_mass) == 10:
+            return f'Вы не отгадали число {data_rand}'
+        else:
+            if data_value != data_rand:
+                data_mass.append(data_value)
+                if data_value > data_rand:
+                    print('Число больше рандомного')
+                else:
+                    print('Число меньше рандомного')
+                data_value = int(input('Value: '))
+                return random_value(rand_num=data_rand, value=data_value, mass=data_mass)
+            else:
+                return f'Обалдеть вы угадали число {data_rand} с попытки номер {len(data_mass) + 1}'
+    except ValueError:
+        print('Only int!')
+try:
+    user_inp = int(input('Value: '))
+    print(random_value(rand_num=rand_num, value=user_inp, mass=[]))
+except ValueError:
+    print('Only int! Try again!')
+    user_inp = int(input('Value: '))
+    print(random_value(rand_num=rand_num, value=user_inp, mass=[]))
