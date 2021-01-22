0d9906cf487805cdb3541c73552646786dc0d54d
Luna Tek <34731676+luunatek@users.noreply.github.com>
2021-01-22 07:17:17 -0600

    Update task_1.py

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -28,3 +28,49 @@
 Вы вместо трехзначного числа ввели строку (((. Исправьтесь
 Введите операцию (+, -, *, / или 0 для выхода):
 """
+
+def rec_calc():
+    user_input = input("Please enter operation type +, -, *, / or enter 0 to exit: ")
+
+    if user_input == '0':
+        return "Exit program"
+    else:
+        if user_input in '+-*/':
+
+            try:
+                num1 = int(input("Enter first number: "))
+                num2 = int(input("Enter second number: "))
+
+                if user_input == '+':
+                    result = num1 + num2
+                    print(f"Results will be {result}")
+                    return rec_calc()
+
+                elif user_input == '-':
+                    result = num1 - num2
+                    print(f"Results will be {result}")
+                    return rec_calc()
+
+                elif user_input == '*':
+                    result = num1 * num2
+                    print(f"Results will be {result}")
+                    return rec_calc()
+
+                elif user_input == '/':
+                    if num2 != 0:
+                        result = num1 / num2
+                        print(f"Results will be {result}")
+                    else:
+                        print("Can not divide by 0")
+                    return rec_calc()
+
+            except ValueError:
+                print("Please enter correct symbol")
+                return rec_calc()
+
+        else:
+            print("You entered incorrect symbol, please try again")
+            return rec_calc()
+
+
+rec_calc()
