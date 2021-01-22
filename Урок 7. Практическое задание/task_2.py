70113435bb31004ae317dc2d9c3232791b751a09
volga52 <volga52@bk.r>
2021-01-22 07:57:55 +0300

    Lesson_7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,62 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import timeit
+import random
+
+
+def merge_sort(test_list):
+    if len(test_list) > 1:
+        center = len(test_list) // 2
+        left = test_list[:center]
+        right = test_list[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                test_list[k] = left[i]
+                i += 1
+            else:
+                test_list[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            test_list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            test_list[k] = right[j]
+            j += 1
+            k += 1
+        return test_list
+
+
+def bubble_sort(lst_obj):
+    """Функция сортировки 'пузырьком' """
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] > lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+test_list = [random.random()*50 for _ in range(300)]
+test_list1 = test_list.copy()
+
+print("Исходный массив ", test_list)
+# merge_sort(test_list)
+print("Функция сортировки слиянием ", (timeit.timeit("merge_sort(test_list)", \
+    setup="from __main__ import merge_sort, test_list", number=1)))
+print("Отсортированный массив ", test_list)
+print("Функция сортировки 'пузырком' ", (timeit.timeit("bubble_sort(test_list1)", \
+    setup="from __main__ import bubble_sort, test_list1", number=1)))
+
+"""По замерам Сортировка 'слиянием' заметно быстрее сортировки 'пузырьком' """
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,137 @@
 arr[m]
 from statistics import median
 """
+import random
+from statistics import median
+import timeit
+""" Пять функций нахождения медианы без сортировки.
+ 'share' функция содержит мой алгоритм нахождения медианы"""
+
+
+def median_nature(arr):
+    """Функция для проверки результата"""
+    return median(arr)
+
+
+def search_med0(arr):
+    """Эта функция как образец для всех остальных"""
+    if len(arr) == 2:
+        # print(sum(arr)/2)
+        return sum(arr)/2
+    elif len(arr) ==1:
+        # print(*arr)
+        return arr[0]
+    arr.remove(max(arr))
+    arr.remove(min(arr))
+    return search_med0(arr)
+
+
+def search_med1(arr):
+    # Принцип функции search_med0
+    # Применен 'пузырьковый метод' поиска min, max
+    if len(arr) == 2:
+        # print(sum(arr)/2)
+        return sum(arr)/2
+    elif len(arr) == 1:
+        # print(*arr)
+        return arr[0]
+
+    for i in range(1, len(arr)):
+        if arr[i] < arr[i-1]:
+            arr[i], arr[i-1] = arr[i-1], arr[i]
+    arr.remove(arr[-1])
+    for i in range(len(arr)-1, 0, -1):
+        if arr[i] < arr[i-1]:
+            arr[i], arr[i - 1] = arr[i - 1], arr[i]
+    arr.remove(arr[0])
+    return search_med1(arr)
+
+
+def search_med2(arr):
+    # Применен 'пузырьковый метод' поиска max
+    # Одновременно за проход фиксируется min
+    if len(arr) == 2:
+        return sum(arr)/2
+    elif len(arr) == 1:
+        return arr[0]
+    minimum = arr[0]
+
+    for i in range(1, len(arr)):
+        if arr[i] < minimum:
+            minimum = arr[i]
+        if arr[i] < arr[i - 1]:
+            arr[i], arr[i - 1] = arr[i - 1], arr[i]
+    arr.remove(arr[-1])
+    arr.remove(minimum)
+    return search_med2(arr)
+
+
+def share(test_list, d_l=0, d_r=0):
+    """Функция делит массив на 2 части 'больше' 'меньше' относительно случайного
+    (первого) элемента до нахождения искомого элемента
+    Работает только для нечетного количества элементов """
+    left = list()
+    right = list()
+    test_i = test_list[0]       # элемент основа для деления на два массива
+
+    for i in test_list[1:]:     # Деление на две части больше, меньше
+        if i > test_i:
+            right.append(i)
+        else:
+            left.append(i)
+    right.append(test_i)        # Основу определяем в правый массив, для избежания зацикливания
+
+    len_left = len(left) + d_l  # Длина (количество элементов) слева + величина изменения
+    len_right = len(right) + d_r
+    if len_left == (len_right - 1):     # Проверка количества элементов слева и справа от основы деления
+        return test_i                   # искомый элемент
+    elif len_right > len_left:
+        test_list = right       # Отсечение элеменов
+        d_l += len(left)        # Вместо удаленныхэлементов: величина измениния 'слева' (здесь)
+    else:
+        test_list = left
+        d_r += len(right)
+
+    return share(test_list, d_l, d_r)
+
+
+array = [random.randint(0, 10) for _ in range(1101)]
+
+arr1 = array.copy()
+
+print("Идеал", timeit.timeit("median_nature(arr1)", setup="from __main__ import median_nature, arr1", number=1)*1000)
+print(arr1)
+
+print("min, max", timeit.timeit("search_med0(arr1)", setup="from __main__ import search_med0, arr1", number=1)*1000)
+
+arr2 = array.copy()
+print("Пузырек", timeit.timeit("search_med1(arr2)", setup="from __main__ import search_med1, arr2", number=1)*1000)
+
+arr3 = array.copy()
+print("New", timeit.timeit("search_med2(arr3)", setup="from __main__ import search_med2, arr3", number=1)*1000)
+
+arr4 = array.copy()
+print("share", timeit.timeit("share(arr4)", setup="from __main__ import share, arr4", number=1)*1000)
+
+arrN = array.copy()
+print(median_nature(arrN))
+arr00 = array.copy()
+print(search_med0(arr00))
+arr11 = array.copy()
+print(search_med1(arr11))
+array22 = array.copy()
+print(search_med2(array22))
+array33 = array.copy()
+print(share(array33))
+
+""" По результатам замеров
+Функция share() самая быстрая, так как она отсекает часть массива,
+поэтому проходов по элементам меньше.
+Умножение на 1000 для визуализации результатов замеров 
+
+Идеал 0.11010000000000186
+min, max 22.639899999999997
+Пузырек 185.8028
+New 145.14560000000003
+share 1.3586999999999905
+"""
