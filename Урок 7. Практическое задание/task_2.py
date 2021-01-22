db8358dbdd30edbecf0220b7b6e456e60295d252
Anna <sem.mm@mail.ru>
2021-01-22 22:30:01 +1000

    Lesson 7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,48 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+from random import uniform
+import copy
+def merge():
+    count = int(input('Введите число элементов: '))
+    flist = [uniform(0, 50) for _ in range(count)]
+    alist = copy.copy(flist)
+
+    def mergeSort(alist):
+        if len(alist)>1:
+            mid = len(alist)//2
+            lefthalf = alist[:mid]
+            righthalf = alist[mid:]
+
+            mergeSort(lefthalf)
+            mergeSort(righthalf)
+        
+            i=0
+            j=0
+            k=0
+            while i<len(lefthalf) and j<len(righthalf):
+                if lefthalf[i]<righthalf[j]:
+                    alist[k]=lefthalf[i]
+                    i=i+1
+                else:
+                    alist[k]=righthalf[j]
+                    j=j+1
+                k=k+1
+
+            while i<len(lefthalf):
+                alist[k]=lefthalf[i]
+                i=i+1
+                k=k+1
+
+            while j<len(righthalf):
+                alist[k]=righthalf[j]
+                j=j+1
+                k=k+1
+        return alist
+    return f'{flist}\n{mergeSort(alist)}'
+
+print(merge())
+
+""" 
+[2.288533865546194, 35.03376149450966, 8.755906180401102, 16.344339564039, 47.58069473003281]
+[2.288533865546194, 8.755906180401102, 16.344339564039, 35.03376149450966, 47.58069473003281] """
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -11,5 +11,31 @@
 который не рассматривался на уроках: Шелла, Гномья, ...
 
 arr[m]
-from statistics import median
 """
+from statistics import median
+import random
+import copy
+
+
+def gnome(data):
+    i, size = 1, len(data)
+    while i < size:
+        if data[i - 1] <= data[i]:
+            i += 1
+        else:
+            data[i - 1], data[i] = data[i], data[i - 1] 
+            if i > 1:
+                i -= 1    
+
+    index = (len(data) - 1) // 2    
+    if len(data) % 2 == 1:
+        median = data[index]
+    else:
+        median = (data[index] + data[index + 1])/2.0
+    return median
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+new_list = copy.copy(orig_list)
+print(orig_list)
+print(gnome(new_list))
+print(median(new_list))
