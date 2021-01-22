f2b90f948b6a6ce310a5ce0abfcb92bf18d8f87e
Екатерина Осенина <ekaterinaosenina@macbook.osenin.me>
2021-01-22 19:40:47 +0300

    homework7

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,39 @@
 arr[m]
 from statistics import median
 """
+from statistics import median
+from random import randint, choice
+
+
+# Решение на основе рекурсивного алгоритма quickselect,
+# который позволяет находить любой заданный элемент по индексу (не только медиану) без сортировки
+def quick_select(array, k):
+    if len(array) == 1:
+        return array[0]
+    base_el = choice(array)
+    less = [el for el in array if el < base_el]
+    more = [el for el in array if el > base_el]
+    bases = [el for el in array if el == base_el]
+    if k < len(less):
+        return quick_select(less, k)
+    elif k < len(less) + len(bases):
+        return bases[0]
+    else:
+        return quick_select(more, k - len(less) - len(bases))
+
+
+while True:
+    try:
+        m = int(input('Введите натуральное число m: '))
+        if m < 1:
+            raise ValueError
+    except ValueError:
+        print('Ошибка ввода!')
+    else:
+        arr = [randint(1, 100) for _ in range(2 * m + 1)]
+        print(f'Исходный массив:\n{arr}')
+        med = median(arr)
+        med2 = quick_select(arr, m)
+        print(f'Медиана statistics: {median(arr)}')
+        print(f'Медиана quick_search: {quick_select(arr, m)}')
+        break
