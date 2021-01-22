dba9573c1a2fd663d58fbca932e0d5d56472faa2
Nooby <chuva4ok12@yandex.ru>
2021-01-22 16:28:13 +0300

    lesson_7_homework

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -12,3 +12,63 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+
+import timeit
+import random
+
+
+def bubble_sort(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+def bubble_sort_mod(lst_obj):
+    n = 1
+    x = 0
+    while n < len(lst_obj):
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+                x = 1
+        if x == 0:
+            break
+        n += 1
+    return lst_obj
+
+
+lst_obj = [random.randint(-100, 100) for _ in range(1000)]
+
+
+print('bubble_sort_time: ', timeit.timeit("bubble_sort(lst_obj[:])", \
+                                          setup="from __main__ import bubble_sort, lst_obj", number=1000))
+
+print('bubble_sort_mod_time: ',timeit.timeit("bubble_sort_mod(lst_obj[:])", \
+                                              setup="from __main__ import bubble_sort_mod, lst_obj", number=1000))
+
+print(lst_obj)
+
+bubble_sort(lst_obj)
+print(lst_obj)
+
+bubble_sort_mod(lst_obj)
+print(lst_obj)
+
+
+'''Как видим, доработка ускорила работу функции. Особенно хорошо заметно на больших аргументах. Неоптимизированная 
+функция не останавливала работу цикла и проходила по всему массиву в то время, как модифицированная функция останавли-
+вала дальнейшее течение цикла, когда в списке встречались повторяющиеся значения. 
+
+Результаты по времени на range(100):
+bubble_sort_time:  1.1237306999999999
+bubble_sort_mod_time:  1.0968498999999998
+
+Результаты по времени на range(1000):
+bubble_sort_time:  114.7681566
+bubble_sort_mod_time:  112.30503319999998
+ '''
+
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,56 @@
 arr[m]
 from statistics import median
 """
+from statistics import median
+from random import randint
+
+# воспользуемся гномьей сортировкой для списка
+
+
+def gnome_sort(lst, size):
+    i = 1
+    while i < size:
+        if (lst[i - 1] <= lst[i]):
+            i += 1
+        else:
+            tmp = lst[i]
+            lst[i] = lst[i - 1]
+            lst[i - 1] = tmp
+            i -= 1
+            if (i == 0):
+                i = 1
+    return lst
+
+# воспользуемся сортировкой Шелла для списка
+
+def shell(lst):
+    inc = len(lst) // 2
+    while inc:
+        for i, el in enumerate(lst):
+            while i >= inc and lst[i - inc] > el:
+                lst[i] = lst[i - inc]
+                i -= inc
+            lst[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+    return lst
+
+
+m = int(input('Please, insert m: '))
+my_lst = [randint(0, 100) for i in range(2 * m + 1)]
+
+new_lst_1 = gnome_sort(my_lst, len(my_lst))
+print(f'Sorted list by gnome method: {new_lst_1}')
+
+median_search_gnome = new_lst_1[len(new_lst_1) // 2]
+print(f'The median of gnome sorted list: {median_search_gnome}')
+
+
+new_lst_2 = shell(my_lst)
+print(f'Sorted list by shell method: {new_lst_2}')
+
+median_search_shell = new_lst_2[len(new_lst_2) // 2]
+print(f'The median of shell sorted list: {median_search_shell}')
+
+# Проверим методом statistics, правильно ли рассчитана медиана списка:
+
+print('The median calculated by statistics module is: ', median(my_lst))
