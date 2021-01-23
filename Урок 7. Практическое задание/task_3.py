d1cc72313c0a55a46bb5ac7b93d056de9f63b16d
ColdyRide <coldyride@gmail.com>
2021-01-23 16:03:31 +0300

    Third task of lesson 7 was done.

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,50 @@
 arr[m]
 from statistics import median
 """
+
+from random import randint, choice
+from statistics import median
+from collections import deque
+from timeit import timeit
+
+
+def median_cust_1(m, arr):
+    result = deque(arr)
+    while len(result) > m:
+        med = max(result)
+        result.remove(med)
+    return med
+
+
+def median_cust_2(arr, k=0):
+    if len(arr) == 1:
+        return arr[0]
+    if isinstance(arr, list):
+        arr = deque(arr)
+        k = len(arr) / 2
+    piv = choice(arr)
+    left, right, same = deque(), deque(), deque()
+    while arr:
+        el = arr.pop()
+        if el < piv:
+            left.append(el)
+        if el > piv:
+            right.append(el)
+        if el == piv:
+            same.append(el)
+    if k < len(left):
+        return median_cust_2(left, k)
+    elif k < len(left) + len(same):
+        return same[0]
+    else:
+        return median_cust_2(right, k - len(left) - len(same))
+
+
+m = int(input('Введите m: '))
+arr = [randint(-10, 10) for _ in range(2 * m + 1)]
+
+print(f'Standart median : {median(arr)}')
+print(f'Custom median first : {median_cust_1(m, arr)}')
+print(timeit("median_cust_1(m, arr)", "from __main__ import median_cust_1, arr, m", number=100))
+print(f'Custom median second : {median_cust_2(arr)}')
+print(timeit("median_cust_2(arr)", "from __main__ import median_cust_2, arr", number=100))
