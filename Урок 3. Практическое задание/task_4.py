16a894ff298562c406654ae29ff44a8733a34faa
AlexeyVertoprakhov <64571185+AlexeyVertoprakhov@users.noreply.github.com>
2021-01-23 23:48:35 +1000

    Update task_4.py

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -1,28 +1,29 @@
 """
 Задание 4.
 Реализуйте скрипт "Кэширование веб-страниц"
-
 Функция должна принимать url-адрес и проверять
 есть ли в кэше соответствующая страница, если нет, то вносит ее в кэш
-
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
 
-class cache(object):
-    def __init__(self, fun):
-        self.fun = fun
-        self.cache = {}
+from uuid import uuid4
+import hashlib
 
-    def __call__(self, *args, **kwargs):
-        key  = str(args) + str(kwargs)
-        try:
-            return self.cache[key]
-        except KeyError:
-            self.cache[key] = rval = self.fun(*args, **kwargs)
-            return rval
-        except TypeError: # incase key isn't a valid key - don't cache
-            return self.fun(*args, **kwargs)
-@cache
-def get_url_src(url):
-    return urllib.urlopen(url).read()
+salt = uuid4().hex  # -> 952604f24d9f4cd0b515a39c73657027
+cache_obj = {}
+
+
+def get_page(url):
+    if cache_obj.get(url):
+        print(f'Данный адрес: {url} присутствует в кэше')
+    else:
+        res = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+        cache_obj[url] = res
+        print(cache_obj)
+
+
+get_page('https://mail.ru/')
+get_page('https://mail.ru/')
+get_page('https://mail.ru/')
+
