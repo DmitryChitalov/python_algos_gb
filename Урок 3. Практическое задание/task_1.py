9c5f688fee572ff105c142055fbb5f41d9779f0b
AlexeyVertoprakhov <64571185+AlexeyVertoprakhov@users.noreply.github.com>
2021-01-23 23:44:13 +1000

    Update task_1.py

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -1,28 +1,66 @@
 """
-7.	Напишите программу, доказывающую или проверяющую, что для множества
-натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
- где n - любое натуральное число.
-
- Решите через рекурсию. Решение через цикл не принимается.
-Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
+Задание 1.
+Реализуйте заполнение списка и словаря, сделайте замеры и сделайте выводы, обоснуйте результат.
+Сделайте несколько операций с каждым из объектов, сделайте замеры и сделайте выводы, обоснуйте результат.
+Подсказка: для замеров воспользуйтесь модулем time (см. примеры урока 1)
+Примечание: eсли вы уже знаете, что такое декоратор и как его реализовать,
+то реализуйте ф-цию-декоратор и пусть она считает время
+И примените ее к двум своим функциям.
 """
-def first(n):
-    if n == 1:
-        return n
-    elif n > 0:
-        return n + first(n-1)
+'''
+n = int(input('Размер списка: '))
+j = input("Введите элимент списка: ")
+i = 0
+
+user_sp = []
+for i in range(n):
+    user_sp.append(j)
+
+print(user_sp)
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+n = int(input('Размер списка: '))
+j = input("Введите элимент списка: ")
+i = 0
+
+user_sp = []
+for i in range(n):
+    user_sp.extend(j)
+
+print(user_sp)
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+'''
+
+import time
 
 
-def second(n):
-    return n * (n + 1) // 2
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        func(args[0])
+        print(time.time() - start_time)
+    return wrapper
 
 
-n = 1
+@time_it
+def test_list(n):
+    list_obj = []
+    for i in range(n):
+        list_obj.append(i)
+        list_obj.index(i)
+    return list_obj
 
-while True:
-    if first(n) == second(n):
-        print(f'Для n={n} - True')
-    else:
-        print(f'Для n={n} - False')
-        break
-    n += 1
+
+@time_it
+def test_dict(n):
+    dict_obj = dict()
+    for i in range(n):
+        dict_obj[i] = i
+        dict_obj.get(i)
+    return dict_obj
+
+
+test_list(10000)
+test_dict(10000)
