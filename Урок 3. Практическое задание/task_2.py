844e5773136ce71edcc4029f428f2973be62c66b
AlexeyVertoprakhov <64571185+AlexeyVertoprakhov@users.noreply.github.com>
2021-01-23 23:45:29 +1000

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -1,62 +1,39 @@
-"""
+""
 Задание 2.
 Ваша программа должна запрашивать пароль
 Для этого пароля вам нужно получить хеш, используя функцию sha256
 Для генерации хеша обязательно нужно использовать криптографическую соль
 Обязательно выведите созданный хеш
-
 Далее программа должна запросить пароль повторно
 Вам нужно проверить, совпадает ли пароль с исходным
 Для проверки необходимо сравнить хеши паролей
-
 ПРИМЕР:
 Введите пароль: 123
 В базе данных хранится строка: 555a3581d37993843efd4eba1921f1dcaeeafeb855965535d77c55782349444b
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
-import os
-
-salt = os.urandom(32)
-
-import hashlib
-import os
-
-     #salt = os.urandom(32)  # Запомните это
-     #password = 'password123'
-     #100000,  # Рекомендуется использоваться по крайней мере 100000 итераций SHA-256
-     # dklen=128  # Получает ключ в 128 байтов
-
-#import os
-#import hashlib
-
-# Пример генерации
-salt = os.urandom(32)
-key = hashlib.pbkdf2_hmac('sha256', 'mypassword'.encode('utf-8'), salt, 100000)
-
-# Хранение как
-storage = salt + key
-
-# Получение значений обратно
-salt_from_storage = storage[:32]  # 32 является длиной соли
-key_from_storage = storage[32:]
-
+from uuid import uuid4
 import hashlib
 
-salt = b''  # Получение соли, сохраненной для *этого* пользователя
-key = b''  # Получение рассчитанного ключа пользователя
+salt = uuid4().hex  # -> <class 'str'> - 8d43a764ef0f4c059fa40af85d8e2923
 
-password_to_check = 'password246'  # Пароль, предоставленный пользователем, проверяется
 
-# Используется та же настройка для генерации ключа, только на этот раз вставляется для проверки настоящий пароль
-new_key = hashlib.pbkdf2_hmac(
-    'sha256',
-    password_to_check.encode('utf-8'),  # Конвертирование пароля в байты
-    salt,
-    100000
-)
+def get_hash(passwd_obj):
+    return hashlib.sha256(salt.encode() + passwd_obj.encode()).hexdigest()
 
-if new_key == key:
-    print('Пароль правильный')
+
+def hash_check(hashed_password, user_password):
+    return hashed_password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+
+new_passwd = input('Введите пароль: ')
+hash_obj = get_hash(new_passwd)
+print(f'В базе данных хранится строка: {hash_obj}')
+
+old_pass = input('Введите пароль еще раз: ')
+
+if hash_check(hash_obj, old_pass):
+    print('Пароль верный')
 else:
-    print('Пароль неправильный')
+    print('Внимание! Пароль не верный.')
