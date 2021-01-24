424bd8c9fe2179d57535cc67fe119b113c06fb84
Роман Тетерин <rateterin@yandex.ru>
2021-01-24 19:28:15 +0300

    Fix to tasks 3 for lesson 3 Algs

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -16,11 +16,7 @@
 """
 
 
-def all_substr(s):
-    l = len(s)
-    return set([s[b:b + a] for a in range(1, l) for b in range(l + 1 - a)])
-
-
 test_str = 'papa'
-for sub in all_substr(test_str):
-    print(sub)
+l = len(test_str)
+print(f'Количество подстрок в строке "{test_str}": '
+      f'{len(set([test_str[b:b + a] for a in range(1, l) for b in range(l + 1 - a)]))}')
