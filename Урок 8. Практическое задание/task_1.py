75272359e0e7732cc646c48fde6241f669b8f47e
sergey-vol <sergey.volnukhin@timebook.ru>
2021-01-25 17:34:26 +0300

    HW_8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,75 @@
 ВНИМАНИЕ: примеры заданий будут размещены в последний день сдачи.
 Но постарайтесь обойтись без них.
 """
+from collections import deque, Counter
+
+
+class HaffmanTree:
+    def __init__(self, string):
+        self.string = string
+        self.tree = None
+        self.count = None
+        self.root = None
+        self.comb = None
+
+    def counter_str(self):
+        self.count = Counter(self.string)
+
+    def deq_sort(self):
+        self.tree = deque(sorted(self.count.items(), key=lambda el: el[1]))
+
+    def step(self):
+        self.comb = {'0': self.tree.popleft()[0], '1': self.tree.popleft()[0]}
+
+    def construction_tree(self):
+        HaffmanTree.counter_str(self)
+        HaffmanTree.deq_sort(self)
+        while len(self.tree) > 1:
+            root = self.tree[0][1] + self.tree[1][1]
+            HaffmanTree.step(self)
+            for i, el in enumerate(self.tree):
+                if el[1] < root:
+                    continue
+                else:
+                    self.tree.insert(i, (self.comb, root))
+                    break
+            else:
+                self.tree.append((self.comb, root))
+
+    def get_tree(self):
+        return self.tree[0][0]
+
+
+class HaffmanCode:
+    def __init__(self, tree):
+        self.tree = tree
+        self.dict_code = {}
+
+    def collect_code(self, tree,  path=''):
+        if isinstance(tree, str):
+            self.dict_code[tree] = path
+        else:
+            self.collect_code(tree['0'], path=f'{path}0')
+            self.collect_code(tree['1'], path=f'{path}1')
+
+    def get_dict_code(self):
+        HaffmanCode.collect_code(self, self.tree)
+        return self.dict_code
+
+
+def haffman_code(words):
+    tree = HaffmanTree(words)
+    tree.construction_tree()
+    code = HaffmanCode(tree.get_tree())
+    dict_code = code.get_dict_code()
+    code_str = ''
+    for i in words:
+        code_str += (dict_code[i] + ' ')
+    return code_str
+
+
+if __name__ == '__main__':
+    word = "beep boop beer!"
+    print(haffman_code(word))
+
+
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -22,32 +22,39 @@ class BinaryTree:
     # добавить левого потомка
     def insert_left(self, new_node):
         # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
+        if new_node < self.root:
+            if self.left_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.left_child = BinaryTree(new_node)
+            # если у узла есть левый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+            raise Exception('The left child cannot be larger than the root')
+
 
     # добавить правого потомка
     def insert_right(self, new_node):
         # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
+        if new_node > self.root:
+            if self.right_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.right_child = BinaryTree(new_node)
+            # если у узла есть правый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+            raise Exception('The right child cannot be less than the root')
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -66,14 +73,32 @@ class BinaryTree:
         return self.root
 
 
-r = BinaryTree(8)
+r = BinaryTree(10)
 print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(4)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
+
+r.insert_left(5)
+r.get_left_child().insert_left(2)
+r.get_left_child().insert_right(7)
+
+print(r.get_left_child().get_left_child().get_root_val())
 print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+r.insert_right(15)
+r.get_right_child().insert_left(12)
+r.get_right_child().insert_right(17)
+print(r.get_root_val())
+print(r.get_right_child().get_left_child().get_root_val())
+r.get_right_child().get_left_child().insert_left(8) # -- вот тут вопрос, может быть так?
+
+"""
+возможно ли такое дерево:
+           10
+        |       |
+        5      15
+      |  |    |   |
+      2  7    8   17
+
+Вопрос про 8, если да то все ок
+если нет, то получается потомков нужно проверять дальше, с основным корнем
+"""
+
+
