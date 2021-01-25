b15f81bb5fb2429a01ac8aef3a1923ddbb55b0ff
Dmitriy <71927035+Velhsq@users.noreply.github.com>
2021-01-25 20:08:50 +0700

    HW3

--- task_4.py
+++ task_4.py
@@ -1,24 +1,31 @@
 """
-4.	Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
-Количество элементов (n) вводится с клавиатуры.
+Задание 4.
+Реализуйте скрипт "Кэширование веб-страниц"
 
-Пример:
-Введите количество элементов: 3
-Количество элементов: 3, их сумма: 0.75
+Функция должна принимать url-адрес и проверять
+есть ли в кэше соответствующая страница, если нет, то вносит ее в кэш
 
-Решите через рекурсию. Решение через цикл не принимается.
-Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
-
-Подсказка:
-Каждый очередной элемент в 2 раза меньше предыдущего и имеет противоположный знак
+Подсказка: задачу решите обязательно с применением 'соленого' хеширования и хеш-таблиц
+Можете условжнить задачу, реализовав ее через ООП
+Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
 
-def n_sum(n, first_num=1):
-    n = int(n)
-    if not n:
-        return 0
-    else:
-        return first_num + n_sum(n - 1, first_num / -2)
+import hashlib
 
-user_input = input('введите число ')
-print(n_sum(user_input))
+cache_lst = []
+def cache(url_adress='geekbrains.ru', salt='name'):
+    url_adress = hashlib.sha256(url_adress.encode() + salt.encode())
+    url_adress = url_adress.hexdigest()
+    if url_adress not in cache_lst:
+        cache_lst.append(url_adress)
+        print('added')
+        return
+
+url_input = input('url adress ')
+salt = input('salt ')
+while url_input and salt:
+    cache(url_input, salt)
+    url_input = input('url adress ')
+    salt = input('salt ')
+print(cache_lst)
+
