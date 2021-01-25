4223f5096dbcce3b7adf3a775529f27fd34e11d1
KIRILLNAX <03-033@mail.ru>
2021-01-25 19:33:29 +0300

    выполнил 4 задания

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -10,3 +10,30 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+import time
+
+def time_f(function):
+    def time_score(*args):
+        start_time = time.time()
+        function(args[0])
+        print(time.time() - start_time)
+    return time_score
+
+@time_f
+def list_f(n):
+    list_obj = []
+    for i in range(n):
+        list_obj.append(i)
+    return list_obj
+
+@time_f
+def dict_f(n):
+    dict_obj = dict()
+    for i in range(n):
+        dict_obj[i] = i
+    return dict_obj
+
+list_f(10000000)
+dict_f(10000000)
+
+# Список заполняется дольше чем словарь
