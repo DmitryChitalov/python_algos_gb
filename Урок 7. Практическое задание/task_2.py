8f2c75d92af13ab3a7af0582447d961552684349
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2021-01-25 11:26:30 +0300

    task 2 default solution

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,42 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import random
+
+
+def merge_sort(orig_list):
+    while len(orig_list) > 1:
+        center = len(orig_list) // 2
+        left = orig_list[:center]
+        right = orig_list[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                orig_list[k] = left[i]
+                i += 1
+            else:
+                orig_list[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            orig_list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            orig_list[k] = right[j]
+            j += 1
+            k += 1
+
+        return orig_list
+
+
+count = int(input('Введите число элементов: '))
+orig_list = [random.randint(-100, 100) for _ in range(count)]
+print(f'Исходный: {orig_list}\nОтсортированный: {merge_sort(orig_list)}')
+
