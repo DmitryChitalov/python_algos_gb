41aa35a4fc929871acd88e3d4237695f5a9519ff
timonen87 <71833451+timonen87@users.noreply.github.com>
2021-01-25 11:16:57 +0300

    Update task_4.py

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,22 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+
+import hashlib
+from uuid import uuid4
+
+url = ''
+url_cash = {}
+
+salt = uuid4().hex
+
+hash_obj = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+
+def url_check(url):
+    if url not in url_cash:
+        url_cash[url] = hash_obj
+
+    return url, hash_obj
+
+print(url_check('https://ozon.ru'))
+print(url_check('https://yandex.ru'))
