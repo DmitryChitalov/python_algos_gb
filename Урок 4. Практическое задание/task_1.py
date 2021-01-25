a46237bbe6baf5f860701e8cdd9ee662f2176ccc
Arkasha <ar246@yandex.ru>
2021-01-25 19:52:19 +0300

    HW4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,6 +13,9 @@
 """
 
 from timeit import timeit
+from random import randint
+
+num_list = [randint(1, 150) for i in range(1000)]
 
 
 def func_1(nums):
@@ -21,3 +24,24 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+print(
+    timeit(
+        "func_1(num_list)",
+        globals=globals(),
+        number=10000),)
+
+
+def func_2(nums):
+    return [el for el in nums if el % 2 == 0]
+
+
+print(
+    timeit(
+        "func_2(num_list)",
+        globals=globals(),
+        number=10000))
+
+"""Использовал генератор списка, так как он, по найденной мною информации, является самым быстрым способом создания
+ списка"""
