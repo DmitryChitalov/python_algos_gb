f0167b4c4a57edf20cf5ebaeb6b6fab9814c14d4
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2021-01-26 09:47:41 +0300

    task 3

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -16,7 +16,7 @@ from statistics import median
 import random
 
 """
-вариант решения без сортитровки
+вариант решения без сортировки
 """
 m = 5
 orig_list = [random.randint(0, 50) for _ in range(2 * m + 1)]
@@ -37,9 +37,37 @@ for i in range(len(orig_list)):
                 right.append(orig_list[k])
             else:
                 left.insert(0, orig_list[k])
-    print(f'Итерация: {i+1}\n\tЛевая часть:  {left}\n\tПравая часть: {right}\n\n')
+    print(f'Итерация: {i + 1}\n\tЛевая часть:  {left}\n\tПравая часть: {right}\n\n')
     if len(left) == len(right) == m:
         print(f'Медиана ====> {orig_list[i]}')
         break
     left.clear()
     right.clear()
+
+"""
+вариант с использованием гномьей сортировки
+"""
+
+
+def gnome_sort(orig_list):
+    i = 1
+    j = 2
+    while i < len(orig_list):
+        if orig_list[i - 1] < orig_list[i]:
+            i = j
+            j += 1
+        else:
+            orig_list[i], orig_list[i - 1] = orig_list[i - 1], orig_list[i]
+            i -= 1
+            if i == 0:
+                i = j
+                j += 1
+    return orig_list
+
+
+m = 5
+orig_list = [random.randint(0, 50) for _ in range(2 * m + 1)]
+sorted_list = gnome_sort(orig_list)
+print(f'Оригинальный массив: {orig_list}\n\tОтсортированный массив: {sorted_list}')
+if sorted_list[m - 1] <= sorted_list[m] <= sorted_list[m + 1]:
+    print(f'Медиана ====> {sorted_list[m]}')
