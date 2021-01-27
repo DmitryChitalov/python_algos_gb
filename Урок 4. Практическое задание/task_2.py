0b579292ed7d81aba6ed3edd22ea0aaa4d3a0b67
alex <sasha@prikhod.ru>
2021-01-27 16:09:47 +0300

    add lesson4

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,8 +7,9 @@
 Выполнена попытка оптимизировать решение через мемоизацию.
 Сделаны замеры обеих реализаций.
 
-Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?
-Если у вас есть идеи, предложите вариант оптимизации.
+Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?!!!
+Если у вас есть идеи, предложите вариант оптимизации, если мемоизация не имеет смысла.
+Без аналитики задание считается не принятым
 """
 
 from timeit import timeit
@@ -23,24 +24,25 @@ def recursive_reverse(number):
 
 num_100 = randint(10000, 1000000)
 num_1000 = randint(1000000, 10000000)
-num_10000 = randint(100000000, 10000000000000)
+num_10000 = randint(100000000000000, 10000000000000000000)
 
+n = 10000
 print('Не оптимизированная функция recursive_reverse')
 print(
     timeit(
         "recursive_reverse(num_100)",
         setup='from __main__ import recursive_reverse, num_100',
-        number=10000))
+        number=n))
 print(
     timeit(
         "recursive_reverse(num_1000)",
         setup='from __main__ import recursive_reverse, num_1000',
-        number=10000))
+        number=n))
 print(
     timeit(
-        "recursive_reverse(num_10000)",
-        setup='from __main__ import recursive_reverse, num_10000',
-        number=10000))
+        "recursive_reverse(randint(100000000000000, 10000000000000000000))",
+        globals=globals(),
+        number=n))
 
 
 def memoize(f):
@@ -49,6 +51,7 @@ def memoize(f):
     def decorate(*args):
 
         if args in cache:
+            # print(f'from cache{args}')
             return cache[args]
         else:
             cache[args] = f(*args)
@@ -59,7 +62,7 @@ def memoize(f):
 @memoize
 def recursive_reverse_mem(number):
     if number == 0:
-        return ''
+        return str(number % 10)
     return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
 
 
@@ -68,14 +71,47 @@ print(
     timeit(
         'recursive_reverse_mem(num_100)',
         setup='from __main__ import recursive_reverse_mem, num_100',
-        number=10000))
+        number=n))
 print(
     timeit(
         'recursive_reverse_mem(num_1000)',
         setup='from __main__ import recursive_reverse_mem, num_1000',
-        number=10000))
+        number=n))
 print(
     timeit(
-        'recursive_reverse_mem(num_10000)',
-        setup='from __main__ import recursive_reverse_mem, num_10000',
-        number=10000))
+        'recursive_reverse_mem(randint(100000000000000, 10000000000000000000))',
+        globals=globals(),
+        number=n))
+
+
+"""
+Мемоизация помогает при замерах timeit'ом получить ускорение на порядок. при единичном вызове функции мемоизация 
+не сработает. так же мемоизация необходима при частых разворотах чисел (причем с одинаковыми остатками от деления)
+в рамках одного запуска. в данном примере показывает хороший результат изза того, что одно и тоже число разворачиваем 
+n раз
+Это результат выполнения, в которой при вызове каждом вызове timeit'ом генерируется рандомное число для разворота (для 
+чистоты эксперимента - чтоб расходы были одинаковыми, генерируем рандомное число и в неопримизированной функции)
+Мемоизация замедляет выполнения вдвое.
+
+n = 10000
+Не оптимизированная функция recursive_reverse
+0.012219133030157536
+0.017079835990443826
+0.053420550015289336
+Оптимизированная функция recursive_reverse_mem
+0.0013272379874251783
+0.0013425510260276496
+0.09855322702787817
+
+n = 1000000
+Не оптимизированная функция recursive_reverse
+1.5048250610125251
+1.7265853220014833
+5.280357568990439
+Оптимизированная функция recursive_reverse_mem
+0.13589507300639525
+0.14156196097610518
+11.005577815987635
+
+Вывод: мемоизация может ускорить при работе с одинаковыми числами
+"""
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -7,18 +7,23 @@
 
 Сделайте профилировку каждого алгоритма через cProfile и через timeit
 
-Сделайте вывод, какая из трех реализаций эффективнее и почему
+Сделайте вывод, какая из трех реализаций эффективнее и почему!!!
+
+И можете предложить еще свой вариант решения!
+Без аналитики задание считается не принятым
 """
+from cProfile import run
+from timeit import timeit
 
 
-def revers(enter_num, revers_num=0):
+def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return revers_num
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        return revers_1(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -34,3 +39,65 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+
+number = 122344345442334385
+run('revers_1(number)')
+run('revers_2(number)')
+run('revers_3(number)')
+n = 1000
+print(timeit(
+        'revers_1(number)',
+        globals=globals(),
+        number=n))
+print(timeit(
+        'revers_2(number)',
+        globals=globals(),
+        number=n))
+print(timeit(
+        'revers_3(number)',
+        globals=globals(),
+        number=n))
+"""
+cProfile округляет время до 0, в рекурсивном алгоритме (n + 3) вызова функций, где n - количество цифр в числе
+
+timeit нам показывает, что срезом развернуть число быстрее всего, предварительно преобразовать его в строку
+рекурсивные вызовы выполняются дольше всего.
+цикл дольше встроенных функций, т.к. встроенные оптимизированны по скорости
+
+         22 function calls (4 primitive calls) in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+     19/1    0.000    0.000    0.000    0.000 task_3.py:19(revers_1)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:29(revers_2)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:37(revers_3)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+0.0033967139897868037
+0.002242734015453607
+0.0002506900345906615
+"""
