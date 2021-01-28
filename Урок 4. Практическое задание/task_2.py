d8eaa9d25a94a068b76388416bc76cb9cd7e68ef
NataliyaU <Nolace@yandex.ru>
2021-01-28 23:13:19 +0300

    Урок 4 Задача 2

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -17,7 +17,7 @@ from random import randint
 
 def recursive_reverse(number):
     if number == 0:
-        return str(number % 10)
+        return '' # str(number % 10) -> исправлен код, функция добавляла 0 в конце реверса.
     return f'{str(number % 10)}{recursive_reverse(number // 10)}'
 
 
@@ -31,6 +31,7 @@ print(
         "recursive_reverse(num_100)",
         setup='from __main__ import recursive_reverse, num_100',
         number=10000))
+
 print(
     timeit(
         "recursive_reverse(num_1000)",
@@ -79,3 +80,36 @@ print(
         'recursive_reverse_mem(num_10000)',
         setup='from __main__ import recursive_reverse_mem, num_10000',
         number=10000))
+
+
+def reverse(number):
+    rev_number = str(number)[::-1]
+    return rev_number
+
+print('Реверс через строку reverse')
+print(timeit('reverse(num_100)', globals=globals(), number=10000))
+print(timeit('reverse(num_1000)', globals=globals(), number=10000))
+print(timeit('reverse(num_10000)', globals=globals(), number=10000))
+
+"""
+Результаты замеров:
+Не оптимизированная функция recursive_reverse
+0.026580600000000003
+0.02971130000000001
+0.0606825
+
+Оптимизированная функция recursive_reverse_mem
+0.0024153999999999842
+0.00273000000000001
+0.0033473000000000253
+
+После мемоизации время значительно сократилось.
+Попыталась решить задачу через ревес строк (результаты замеров ниже). 
+Время выполнения гораздо ниже чем при рекурсии, но все равно уступают мемоизации.
+Мемоизация в данной задаче оправдана.  
+
+Реверс через строку reverse
+0.004578500000000013
+0.005385799999999996
+0.004432400000000003
+"""
