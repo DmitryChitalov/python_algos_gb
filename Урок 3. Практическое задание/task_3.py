c5977cacef84d6be44373b910a03f084798fdd97
JuliaRaylyan <74094012+JuliaRaylyan@users.noreply.github.com>
2021-01-29 16:02:52 +0300

    Add files via upload

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,19 @@
 р
 а
 """
+
+
+def substrings(s):
+    res_hash = set()
+    res = set()
+
+    for i in range(len(s)):
+        for j in range(i + 1, len(s) + 1):
+            print(s[i:j])
+            res_hash.add(hash_the_object(s[i:j]).decode())
+            res.add(s[i:j])
+
+    res_hash.remove(hash_the_object(s).decode())
+    res.remove(s)
+    return res_hash, res
+
