d56a0c29688c5f0042b9cd3b2b58acb6618d0852
KIRILLNAX <03-033@mail.ru>
2021-01-29 19:51:52 +0300

    Выполнил 4, с Эратосфеном разбираюсь

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,7 +12,7 @@
 Добавьте аналитику: что вы сделали и почему
 """
 
-from timeit import timeit
+import timeit
 
 
 def func_1(nums):
@@ -21,3 +21,120 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [i for i, el in enumerate(nums) if el % 2 == 0]
+
+def dict_f1(n):
+    dict_obj = dict()
+    for i in range(n):
+        dict_obj[i] = i
+    return dict_obj
+
+def dict_f2(n):
+    return(i for i, j in n.items() if n.keys() % 4 != 0 )
+
+# 1000
+NUMS = [el for el in range(1000)]
+n = {i: {j**2 % i for j in range(1, 1000)} for i in range(1, 100)}
+print(
+    timeit.timeit(
+        "func_1(NUMS)",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "func_2(NUMS)",
+        globals=globals(),
+        number=1000))
+print(
+    timeit.timeit(
+        "dict_f1",
+        globals=globals(),
+        number=100000))
+
+print(
+    timeit.timeit(
+        "dict_f2",
+        globals=globals(),
+        number=100000))
+
+# 10000
+NUMS = [el for el in range(10000)]
+n = {i: {j**2 % i for j in range(1, 10000)} for i in range(1, 100)}
+print(
+    timeit.timeit(
+        "func_1(NUMS)",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "func_2(NUMS)",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "dict_f1",
+        globals=globals(),
+        number=100000))
+
+print(
+    timeit.timeit(
+        "dict_f2",
+        globals=globals(),
+        number=100000))
+
+# 100000
+NUMS = [el for el in range(100000)]
+n = {i: {j**2 % i for j in range(1, 100000)} for i in range(1, 100)}
+print(
+    timeit.timeit(
+        "func_1(NUMS)",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "func_2(NUMS)",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "dict_f1",
+        globals=globals(),
+        number=1000000))
+
+print(
+    timeit.timeit(
+        "dict_f2",
+        globals=globals(),
+        number=1000000))
+
+"""
+Замер времени:
+---1000---
+0.16842050000000003
+0.12216520000000003
+0.0033374999999999932
+0.003080499999999986
+
+---10000---
+1.7433271000000001
+1.2771898
+0.003204899999999622
+0.0030032000000002057
+
+---100000---
+22.853218599999998
+16.928565
+0.03406710000000146
+0.031103999999999132
+
+Использование enumirate() ускоряет работу функций, по моему, тк после генерации значений становятся пустыми, 
+когда range() можно воспользоваться повторно
+"""
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -9,16 +9,18 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+from timeit import timeit
+import cProfile
 
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return revers_num
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        return revers(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -34,3 +36,71 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+enter_num = int(input('Введите число: '))
+
+print(revers(enter_num, revers_num=0))
+print(
+    'Рекурсия ',
+    timeit(
+        'revers(enter_num, revers_num=0)',
+        globals=globals(),
+        number=10000))
+print(revers_2(enter_num, revers_num=0))
+print(
+    'Цикл ',
+    timeit(
+        'revers_2(enter_num, revers_num=0)',
+        globals=globals(),
+        number=10000))
+print(revers_3(enter_num))
+print(
+    'Срез',
+    timeit(
+        'revers_3(enter_num)',
+        globals=globals(),
+        number=10000))
+
+cProfile.run('revers(10000000000)')
+cProfile.run('revers_2(10000000000)')
+cProfile.run('revers_3(10000000000)')
+
+"""
+Функция с использованием среза выполнилась быстрее всех
+
+Введите число: 312564684844631321
+Рекурсия  0.09290440000000055
+Цикл  0.06379779999999968
+Срез 0.008160799999999746
+         15 function calls (4 primitive calls) in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+     12/1    0.000    0.000    0.000    0.000 task_3.py:16(revers)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:26(revers_2)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:34(revers_3)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+"""
