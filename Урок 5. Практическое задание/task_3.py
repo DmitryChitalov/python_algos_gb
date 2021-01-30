edd51023d52b453c4db3228e7cf7e3babc7cbe7e
kfotkin <68823654+kfotkin@users.noreply.github.com>
2021-01-30 12:03:12 +0300

    Update task_3.py

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,26 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+
+from timeit import timeit
+from random import randint
+from collections import deque
+test_list = [randint(1, 100) for num in range(1000)]
+deq = deque(test_list)
+print('Вставка в начало списка:')
+print(timeit('test_list.insert(0,"abc")', globals=globals(), number=10000))
+print(timeit('deq.appendleft("abc")', globals=globals(), number=10000))
+print("Удаление первого элемента списка:")
+print(timeit('test_list.pop(0)', globals=globals(), number=10000))
+print(timeit('deq.popleft()', globals=globals(), number=10000))
+print("Вставка последнего элемента списка:")
+print(timeit('test_list.append(1)', globals=globals(), number=10000))
+print(timeit('deq.append(1)', globals=globals(), number=10000))
+print("доступ к случайному элементу:")
+print(timeit('test_list[randint(0, 999)]', globals=globals(), number=10000))
+print(timeit('deq[randint(0, 999)]', globals=globals(), number=10000))
+
+"""
+Использование дека дает преимущества при работе с начальнами элементами.
+При работе с последними элементами время выполнения примерно равно, как и при доступе к случайному элементу.
+"""
