9abbb9cddc8aa1e583dfbcc0614a6965020fc15f
Vasiliy <vasiliypoddubny@gmail.com>
2021-01-31 22:19:17 +0300

    Lesson_2_3_4
    Задания с 2 по 4 урок включительно

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -5,6 +5,34 @@
 Функция должна принимать url-адрес и проверять
 есть ли в кэше соответствующая страница, если нет, то вносит ее в кэш
 
-Подсказка: задачу решите обязательно с применением 'соленого' хеширования
+Подсказка: задачу решите обязательно с применением 'соленого' хеширования и хеш-таблиц
 Можете условжнить задачу, реализовав ее через ООП
+Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
+
+
+import hashlib
+from uuid import uuid4
+
+salt = uuid4().hex
+url_store = {}
+
+
+def cash_webpages(url):
+    res = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+    if len(url_store) == 0:
+        url_store.update({url: res})
+        return "Добавлен первый адрес."
+    else:
+        for items in url_store.values():
+            if items == res:
+                return "Адрес уже существует!"
+            else:
+                url_store.update({url: res})
+                return "Новый адрес добавлен!"
+
+
+print(cash_webpages('https://geekbrains.ru'))
+print(cash_webpages('https://geekbrains.ru'))
+print(cash_webpages('https://www.codecademy.com'))
+print(url_store)
