cc1e96c3f1b49330be776e9c084e8746536629c2
CHIZHOVA <chizhova@amdocs.com>
2021-02-01 16:38:41 +0300

    lesson 1 done

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -17,3 +17,35 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 """
+import random
+
+
+def min_quad(lst_target):
+    if lst_target is None:
+        return
+    for i in range(len(lst_target)):
+        for j in range(len(lst_target)):
+            if lst_target[i] > lst_target[j]:
+                break
+            if j == len(lst_target)-1:
+                return lst_target[i]
+
+
+def min_lin(lst_target):
+    if lst_target is None:
+        return
+    curr_min = lst_target[0]
+    for i in range(1, len(lst_target)):
+        if lst_target[i] < curr_min:
+            curr_min = lst_target[i]
+    return curr_min
+
+
+for k in (50, 500, 1000, 5000, 1000):
+    # Из 100000 чисел возьмем 'k' случайно выбранных
+    # Всего 10 тыс. чисел
+    lst = random.sample(range(-100000, 100000), k)
+
+    print(min_quad(lst))
+    print(min_lin(lst))
+    print(min(lst))
