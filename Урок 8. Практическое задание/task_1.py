3ab197ac63959a3386300b72e11eaf57071a4aba
Pafnodeveloper <51054260+Pafnodeveloper@users.noreply.github.com>
2021-02-01 12:56:03 +0300

    Add files via upload

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,80 @@
 ВНИМАНИЕ: примеры заданий будут размещены в последний день сдачи.
 Но постарайтесь обойтись без них.
 """
+
+from collections import Counter, deque
+
+
+class Tree:
+    def __init__(self, any_deque=deque(), any_dict={}):
+        self.dict = any_dict
+        self.deque = any_deque
+        self.dict_for_code = {}
+
+    # Изначально планировал сделать класс для каждой пары буква-частота и добавлять его в deq
+    def insert_node(self, node):
+        self.deque.appendleft(node)
+
+    def get_dict(self):
+        while len(self.deque) > 1:
+            left = self.deque.popleft()
+            right = self.deque.popleft()
+            frequency = left[1] + right[1]
+
+            self.dict = {0: left[0],
+                         1: right[0]}
+
+            for index, value in enumerate(self.deque):
+                if index == len(self.deque) - 1:
+                    self.deque.insert(index + 1, (self.dict, frequency))
+                    break
+                elif frequency > value[1]:
+                    continue
+                else:
+                    self.deque.insert(index, (self.dict, frequency))
+                    break
+            if not self.deque:
+                self.deque.append((self.dict, frequency))
+                break
+
+    def get_encoded(self, random_dict={}, path=''):
+        if not isinstance(random_dict, dict):
+            my_tree.dict_for_code[random_dict] = path
+        else:
+            my_tree.get_encoded(random_dict[0], path=f'{path}0')
+            my_tree.get_encoded(random_dict[1], path=f'{path}1')
+
+# Рудимент, к сожалению
+class Node:
+    def __init__(self, node):
+        self.node = node
+
+
+my_tree = Tree()
+
+str_to_code = "beep boop beer!"
+str_like_dict = Counter(str_to_code)
+reversed_list_in_deque = deque(sorted(str_like_dict.items(), key=lambda item: item[1]))
+
+my_tree = Tree(reversed_list_in_deque)
+
+# Пережиток от классов, хотел таким образом сделать deq по возрастанию,
+# но метод items почему-то возвращает первым элементом пару ('b', 3), а не ('e', 4) из словаря Counter
+# for key, value in str_like_dict.items():
+#     new_node = Node((key, value))
+#     my_tree.insert_node(new_node.node)
+
+my_tree.get_dict()
+print(my_tree.dict)
+
+my_tree.get_encoded(my_tree.dict)
+print(my_tree.dict_for_code)
+
+for letter in my_tree.dict_for_code:
+    print(my_tree.dict_for_code[letter], end=' ')
+
+"""
+Итоговая строка отличается от примера на уроке т.к. 
+при заполнении deque по частоте элемент ({0: {0: 'r', 1: '!'}, 1: 'p'}, 4)
+встает не перед ('e', 4), а после
+"""
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,6 +10,8 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных.
 """
 
+from collections import deque
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -18,36 +20,70 @@ class BinaryTree:
         self.left_child = None
         # правый потомок
         self.right_child = None
+        self.all_right_nodes = []
+        self.all_left_nodes = []
 
     # добавить левого потомка
     def insert_left(self, new_node):
+        # если узел должен быть справа
+        if new_node > self.root:
+            self.insert_right(new_node)
         # если у узла нет левого потомка
-        if self.left_child == None:
+        elif not self.left_child:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.left_child = BinaryTree(new_node)
+            self.all_left_nodes.append(self.left_child)
         # если у узла есть левый потомок
         else:
             # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+            for index, node in enumerate(self.all_left_nodes):
+                if new_node > node.root:
+                    if index == 0:
+                        tree_obj.left_child = node
+                        self.left_child = tree_obj
+                        self.all_left_nodes.insert(index, tree_obj)
+                        break
+                    else:
+                        tree_obj.left_child = node
+                        self.all_left_nodes.insert(index, tree_obj)
+                        break
+                elif node is self.all_left_nodes[-1]:
+                    self.all_left_nodes[len(self.all_left_nodes) - 1].left_child = tree_obj.root
+                    self.all_left_nodes.append(tree_obj)
+                    break
 
     # добавить правого потомка
     def insert_right(self, new_node):
+        if new_node < self.root:
+            self.insert_left(new_node)
         # если у узла нет правого потомка
-        if self.right_child == None:
+        elif not self.right_child:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
+            self.all_right_nodes.append(self.right_child)
         else:
             # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+            for index, node in enumerate(self.all_right_nodes):
+                if new_node < node.root:
+                    if index == 0:
+                        tree_obj.right_child = node
+                        self.right_child = tree_obj
+                        self.all_right_nodes.insert(index, tree_obj)
+                        break
+                    else:
+                        tree_obj.right_child = self.all_right_nodes[index]
+                        self.all_right_nodes[index - 1].right_child = tree_obj
+                        self.all_right_nodes.insert(index, tree_obj)
+
+                        break
+                elif node is self.all_right_nodes[-1]:
+                    self.all_right_nodes[len(self.all_right_nodes) - 1].right_child = tree_obj.root
+                    self.all_right_nodes.append(tree_obj)
+                    break
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -59,7 +95,36 @@ class BinaryTree:
 
     # метод установки корня
     def set_root_val(self, obj):
-        self.root = obj
+        if obj == self.root:
+            pass
+        elif obj > self.root:
+            # self.root = obj
+            for index, node in enumerate(self.all_right_nodes):
+                if obj < node.root:
+                    self.root = obj
+                    self.right_child = node
+
+                    for x in range(index):
+                        self.all_left_nodes.insert(0, self.all_right_nodes[x])
+                        self.all_left_nodes[0].left_child = self.all_left_nodes[1]
+                        self.all_left_nodes[0].right_child = None
+
+                    self.left_child = self.all_left_nodes[0]
+                    self.all_right_nodes = self.all_right_nodes[index:]
+                    break
+        else:
+            for index, node in enumerate(self.all_left_nodes):
+                if obj > node.root:
+                    self.root = obj
+                    self.left_child = node
+
+                    for x in range(index):
+                        self.all_right_nodes.insert(0, self.all_left_nodes[x])
+                        self.all_right_nodes[0].left_child = None
+
+                    self.right_child = self.all_right_nodes[0]
+                    self.all_left_nodes = self.all_left_nodes[index:]
+                    break
 
     # метод доступа к корню
     def get_root_val(self):
@@ -69,11 +134,45 @@ class BinaryTree:
 r = BinaryTree(8)
 print(r.get_root_val())
 print(r.get_left_child())
+
+r.insert_left(40)
 r.insert_left(4)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+r.insert_left(-2)
+r.insert_left(6)
+print(r.all_left_nodes)
+for i in r.all_left_nodes:
+    print(i.root, end=" ")
+print("\n")
+print(r.left_child.root)
+
+r.insert_right(20)
+r.insert_right(16)
+r.insert_right(24)
+print(r.all_right_nodes)
+for i in r.all_right_nodes:
+    print(i.root)
+
+print("Меняю значение главного узла на 22")
+r.set_root_val(22)
+
+for node in r.all_right_nodes:
+    print("Я правый",node.root)
+for node in r.all_left_nodes:
+    print("Я левый", node.root)
+print(f"Первый левый {r.left_child.root}")
+print(f"Первый правый {r.right_child.root}")
+
+print("Меняю значение главного узла на 8")
+r.set_root_val(8)
+
+for node in r.all_right_nodes:
+    print("Я правый",node.root)
+for node in r.all_left_nodes:
+    print("Я левый", node.root)
+print(f"Первый левый {r.left_child.root}")
+print(f"Первый правый {r.right_child.root}")
+
+"""
+Добавил валидацию при вставках справа и слева неподоходящих значений, 
+а также реформат при замене значения главного узла
+"""
