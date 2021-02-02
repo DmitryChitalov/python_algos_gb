b67f4d99e79b6232bf8440c24ff234d2bbad14a7
nik1tam <48382667+nik1tam@users.noreply.github.com>
2021-02-02 22:53:47 +0300

    lesson2
    homework for lesson2

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -9,3 +9,16 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+def arr_avg(in_index, in_val):
+    v_sum = 0
+    if in_index > 0:
+        v_sum = in_val + arr_avg((in_index-1), (-1)*in_val/2)
+    return v_sum
+
+
+if __name__ == '__main__':
+    print('Enter number of values:')
+    v_num = input()
+    v_sum = arr_avg(int(v_num), 1)
+    print('Average value: ' + str(v_sum))
