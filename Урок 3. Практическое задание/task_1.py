435c8573f22b2aa2d3f8b67bf98fdc3ea123558e
Vitalii Gulin <gvitaly@mail.ru>
2021-02-03 22:00:53 +0300

    Lesson 3.

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -10,3 +10,58 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+import time
+
+def elapse_list(func):
+    def measure(i_a, i_b, i_c):
+        l_start = time.time()
+        func(i_a, i_b, i_c)
+        l_end = time.time()
+        l_elapsed = (l_end - l_start)*1000000
+        print(f'Elapsed time(ns) for <{func.__name__}>: {l_elapsed}.')
+    return measure
+
+def elapse_dict(func):
+    def measure(i_a, i_b, i_c, i_d):
+        l_start = time.time()
+        func(i_a, i_b, i_c, i_d)
+        l_end = time.time()
+        l_elapsed = (l_end - l_start)*1000000
+        print(f'Elapsed time(ns) for <{func.__name__}>: {l_elapsed}.')
+    return measure
+
+@elapse_list
+def add_list(i_lst, i_item, i_cnt=0):
+    # time.sleep(0.3)
+    if i_cnt == 0:
+        i_cnt = 1
+
+    for i in range(i_cnt):
+        i_lst.append(i_item)
+
+@elapse_dict
+def add_dict(i_dct, i_start, i_value, i_cnt=0):
+    # time.sleep(0.3)
+    if i_cnt == 0:
+        i_cnt = 1
+
+    for i in range(i_cnt):
+        i_dct[i_start+i] = i_value
+
+
+g_lst = []
+g_dct = {}
+
+add_list(g_lst, 'item_1', 50500)
+add_dict(g_dct, 0, 'item_1', 50500)
+
+print(len(g_lst))
+print(len(g_dct))
+######################################################################
+# Время, потраченное на заполнении списка или словаря объектами в
+# количестве до 5-10 тысяч, равно нулю, хотя уж наносекунды должно длиться.
+# Вывод 1: функция time.time() имеет, видимо, како-то лаг и
+# для замеров быстрых операций не подходит.
+# Вывод 2: заполнение словаря занимает по времени несколько раз больше,
+# чем списка.
+######################################################################
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,30 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+import hashlib
+
+g_cash = {}
+
+class PageClass:
+    def __init__(self, i_url, i_hash, i_salt):
+        self.url  = i_url
+        self.hash = i_hash
+        self.salt = i_salt
+
+    def __str__(self):
+        return self.url + ', ' + self.hash + ', ' + self.salt
+
+def get_from_cash(i_url, i_salt):
+    l_hash = hashlib.sha256(i_salt.encode() + i_url.encode('utf-8')).hexdigest()
+
+    l_obj = g_cash.get(l_hash)
+
+    if l_obj is None:
+        l_obj = PageClass(i_url, l_hash, i_salt)
+        g_cash[l_hash] = l_obj
+    else:
+        return l_obj
+
+
+print(get_from_cash('url1', 'salt1'))
+print(get_from_cash('url1', 'salt1'))
