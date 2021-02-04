9a6dc68020c93dbe9f29bd00ec977e593f0ba085
pingvvin <71318818+pingvvin@users.noreply.github.com>
2021-02-04 10:55:45 +0100

    Add files via upload
    2е задание не осилил

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -9,4 +9,62 @@ deque – это обобщение стеков и очередей.
 Выполните различные операции с каждым из объектов.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
+
+Операции равные по семантике (по смыслу)
+Но разные по используемым ф-циям
+
+И добавить аналитику, так ли это или нет.!
+"""
+
+import collections
+import timeit
+
+
+def l():
+    ll = list()
+
+    for i in range(1000):
+        ll.append(chr(i))
+    return ll
+
+
+a = l()
+
+
+def dq():
+    dq = collections.deque()
+
+    for i in range(1000):
+        dq.append(chr(i))
+    return dq
+
+
+b = dq()
+
+
+def access_elem(z, n):
+    return z[n]
+
+
+print('List time spent for insertion: ')
+print(timeit.timeit("l()", globals=globals(), number=10000))
+print('Deque timespent for insertion: ')
+print(timeit.timeit("dq()", globals=globals(), number=10000))
+print('List time spent for access: ')
+print(timeit.timeit("access_elem(a,458)", globals=globals(), number=100000))
+print('Deque time spent for access: ')
+print(timeit.timeit("access_elem(b,458)", globals=globals(), number=100000))
+"""
+List time spent for insertion: 
+1.4377334
+Deque timespent for insertion: 
+1.3755154
+List time spent for access: 
+0.011105499999999768
+Deque time spent for access: 
+0.013333999999999957
+
+
+Как видно из статистики deque немного быстрее списка в случае наполнения, 
+но список немного быстрее deque в случае доступа к элементам.
 """
