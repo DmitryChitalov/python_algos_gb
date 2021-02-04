991918ed3197e44f6ed8841669f46f393b3bda83
pingvvin <71318818+pingvvin@users.noreply.github.com>
2021-02-04 10:39:49 +0100

    Add files via upload

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -14,4 +14,44 @@
 В базе данных хранится строка: 555a3581d37993843efd4eba1921f1dcaeeafeb855965535d77c55782349444b
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
+
+Допускаются любые усложения задания - валидация, подключение к БД, передача данных в файл
 """
+
+import hashlib as h
+
+
+def encode(inp, salt=''):
+    if salt != '':
+        return h.sha256((str(inp) + str(salt)).encode('utf-8')).hexdigest()
+    else:
+        return h.sha256(str(inp).encode('utf-8')).hexdigest()
+
+
+database = {'johny': '123',
+            'seigurd': '1234fsdf',
+            'valeriy': 'leontiev'}
+
+enc_data = {}
+
+for login, password in database.items():
+    enc_data[encode(login)] = encode(login, password)
+
+
+def login(log, pas):
+    if encode(log) in enc_data.keys():
+        if encode(log, pas) in enc_data.values():
+            return 'You have been succesfully logged in'
+        else:
+            return 'Password is not correct. Please check it again.'
+    else:
+        return 'Login does not exist'
+
+
+def login_attempt():
+    a = str(input('Enter your login: '))
+    b = str(input('Enter your password: '))
+    print(login(a, b))
+
+
+login_attempt()
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -5,6 +5,24 @@
 Функция должна принимать url-адрес и проверять
 есть ли в кэше соответствующая страница, если нет, то вносит ее в кэш
 
-Подсказка: задачу решите обязательно с применением 'соленого' хеширования
+Подсказка: задачу решите обязательно с применением 'соленого' хеширования и хеш-таблиц
 Можете условжнить задачу, реализовав ее через ООП
+Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
+
+import uuid, hashlib
+
+salt = uuid.uuid4().hex
+database = {}
+
+def webpage(adress):
+    if adress in database.keys():
+        print(f"Web page {adress} already cached.")
+    else:
+        val = hashlib.sha256(salt.encode() + adress.encode()).hexdigest()
+        database[adress] = val
+        print(database[adress])
+
+
+webpage('www.google.com')
+webpage('www.google.com')
