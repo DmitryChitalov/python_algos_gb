e3d6d169e0e017c1aaeabcfd2053d217dc1ddb1b
pingvvin <71318818+pingvvin@users.noreply.github.com>
2021-02-04 10:49:24 +0100

    Add files via upload

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,3 +1,4 @@
+
 """
 Задание 3.
 
@@ -7,18 +8,23 @@
 
 Сделайте профилировку каждого алгоритма через cProfile и через timeit
 
-Сделайте вывод, какая из трех реализаций эффективнее и почему
+Сделайте вывод, какая из трех реализаций эффективнее и почему!!!
+
+И можете предложить еще свой вариант решения!
+Без аналитики задание считается не принятым
 """
 
+import timeit as t
+from cProfile import run
 
-def revers(enter_num, revers_num=0):
+def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return revers_num
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        return revers_1(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -34,3 +40,21 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+a = 654684649896416546516352186645146849846494654164894613231688446516484351349465489
+
+print(t.timeit("revers_1(a)", globals=globals(), number=10000))
+run('revers_1(a)')
+
+print(t.timeit("revers_2(a)", globals=globals(), number=10000))
+run('revers_2(a)')
+
+print(t.timeit("revers_3(a)", globals=globals(), number=10000))
+run('revers_3(a)')
+
+
+"""
+Принимая во внимание числы выведенные выше, наиболее оптимальным вариантом решения
+будет третий вариант "переворота списка" с помощью которого будет затрачено наименее времени,
+а также отсутствует большое количество вызовов функций как в первом варианте и не требует манипуляций
+с дополнительными переменными как во втором варианте, что даст лучший результат в итоге.
+"""
--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -1,18 +1,23 @@
 """
-Задание 5.*
+Задание 5.**
 
 Приведен наивный алгоритм нахождения i-го по счёту
 простого числа (через перебор делителей).
 
 Попробуйте решить эту же задачу,
-применив алгоритм "Решето эратосфена" (https://younglinux.info/algorithm/sieve)
+применив алгоритм "Решето Эратосфена" (https://younglinux.info/algorithm/sieve)
 
 Подсказка:
 Сравните алгоритмы по времени на разных порядковых номерах чисел:
 10, 100, 1000
 Опишите результаты, сделайте выводы, где и какой алгоритм эффективнее
 Подумайте и по возможности определите сложность каждого алгоритма
+
+Укажите формулу сложности О-нотация каждого алгоритма
+и сделайте обоснвование рез-ам
 """
+
+import timeit as t
 
 
 def simple(i):
@@ -35,5 +40,46 @@ def simple(i):
     return n
 
 
-i = int(input('Введите порядковый номер искомого простого числа: '))
-print(simple(i))
+def erato(n):
+    i = 2
+    last_elem = n * 10
+    all_num = [z for z in range(last_elem)]
+    all_num[1] = 0
+    while i < last_elem:
+        if all_num[i] != 0:
+            check = i * 2
+            while check < last_elem:
+                all_num[check] = 0
+                check = check + i
+        i = i + 1
+    return [x for x in all_num if x != 0][n - 1]
+
+
+i1 = 10
+i2 = 100
+i3 = 1000
+
+print()
+print(f'{i1}')
+print("Вариант без решета Эратосфена:")
+print(t.timeit("simple(i1)", globals=globals(), number=50))
+print("Вариант с решетом Эратосфена:")
+print(t.timeit("erato(i1)", globals=globals(), number=50))
+print()
+print(f'{i2}')
+print("Вариант без решета Эратосфена:")
+print(t.timeit("simple(i2)", globals=globals(), number=50))
+print("Вариант с решетом Эратосфена:")
+print(t.timeit("erato(i2)", globals=globals(), number=50))
+print()
+print(f'{i3}')
+print("Вариант без решета Эратосфена:")
+print(t.timeit("simple(i3)", globals=globals(), number=50))
+print("Вариант с решетом Эратосфена:")
+print(t.timeit("erato(i3)", globals=globals(), number=50))
+
+"""
+Сложность первой функции O(n^2)
+Сложность второй функции O(n log n)
+Второй вариант более оптимален для поиска натурального числа
+"""
