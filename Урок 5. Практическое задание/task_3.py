3568099c9103d322b8c5d21a00a3aa4e016d11db
KIRILLNAX <03-033@mail.ru>
2021-02-07 10:33:48 +0300

    выполнил 3 задания

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,92 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+
+from collections import deque
+from timeit import timeit
+
+num = 100
+list = [el for el in range(10000000)]
+
+
+def list_append(num):
+    list1 = []
+    for i in range(num):
+        list1.append(i)
+
+def deque_append(num):
+    list1 = deque()
+    for i in range(num):
+        list1.append(i)
+
+def list_appendleft(num):
+    list1 = []
+    for i in range(num):
+        list1.insert(0, i)
+
+def deque_appendleft(num):
+    list1 = deque()
+    for i in range(num):
+        list1.appendleft(i)
+
+def list_pop(list1):
+    for i in range(len(list1)):
+        a = list1.pop()
+
+def deque_pop(list1):
+    for i in range(len(list1)):
+        l = list1.pop()
+
+
+def list_popleft(list1):
+    for i in range(len(list1)):
+        l = list1.pop(0)
+
+
+def deque_popleft(list1):
+    for i in range(len(list1)):
+        l = list1.popleft()
+
+
+
+print('list_append -',
+    timeit(
+        'list_append(num)', globals=globals(), number=100000))
+deque_append(num)
+print('deque_append -',
+    timeit(
+        'deque_append(num)', globals=globals(), number=100000))
+print('list_appendleft -',
+    timeit(
+        'list_appendleft(num)', globals=globals(), number=100000))
+print('deque_appendleft -',
+    timeit(
+        'deque_appendleft(num)', globals=globals(), number=100000))
+print('list_pop -',
+    timeit(
+        'list_pop(list)', globals=globals(), number=1000000))
+print('deque_pop -',
+    timeit(
+        'deque_pop(list)', globals=globals(), number=1000000))
+print('list_popleft -',
+    timeit(
+        'list_popleft(list)', globals=globals(), number=10000000))
+print('deque_popleft -',
+    timeit(
+        'deque_popleft(list)', globals=globals(), number=10000000))
+
+'''
+List отрабатывает на равне с deque в append, pop_left. Deque на много лучше показал себя в append_left, pop 
+
+list_append - 0.8490065
+deque_append - 0.7456488999999997
+
+list_appendleft - 1.3975592
+deque_appendleft - 0.7122562000000006
+
+list_pop - 1.8785327
+deque_pop - 0.4482677000000006
+
+list_popleft - 4.317518300000001
+deque_popleft - 4.3892772
+'''
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,103 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+
+from timeit import timeit
+from collections import OrderedDict as ord_d
+
+dict1 = 1000
+dict2 = {str(i): i for i in range(1000)}
+dict3 = {str(i): i for i in range(100000, 100000)}
+
+ord_dict1 = ord_d(dict2)
+ord_dict2 = ord_d(dict3)
+
+
+def fill_dict(num):
+    d = dict()
+    for i in range(num):
+        d[str(i)] = i
+
+def fill_ord_d(num):
+    d = ord_d()
+    for i in range(num):
+        d[str(i)] = i
+
+def sort_dict(dict):
+    d = sorted(dict.items(), key=lambda item: item[1])
+
+def sort_ord_d(dict):
+    d = sorted(dict.items(), key=lambda item: item[1])
+
+def get_dict(dict):
+    for i in range(len(dict)):
+        dict.get(str(i))
+
+def get_ord_d(dict):
+    for i in range(len(dict)):
+        dict.get(str(i))
+
+def update_dict(dict_1, dict_2):
+    dict_1.update(dict_2)
+
+def update_ord_d(dict_1, dict_2):
+    dict_1.update(dict_2)
+
+def pop_dict(dict):
+    for i in range(len(dict)):
+        d = dict.popitem()
+
+def pop_ord_d(dict):
+    for i in range(len(dict)):
+        d = dict.popitem()
+
+
+print('fill_dict -',
+    timeit(
+        'fill_dict(dict1)', globals=globals(), number=10000))
+print('fill_ord_d -',
+    timeit(
+        'fill_ord_d(dict1)', globals=globals(), number=10000))
+print('sort_dict -',
+    timeit(
+        'sort_dict(dict2)', globals=globals(), number=10000))
+print('sort_ord_d -',
+    timeit(
+        'sort_ord_d(dict2)', globals=globals(), number=10000))
+print('get_dict -',
+    timeit(
+        'get_dict(dict2)', globals=globals(), number=10000))
+print('get_ord_d -',
+    timeit(
+        'get_ord_d(dict2)', globals=globals(), number=10000))
+print('update_dict -',
+    timeit(
+        'update_dict(dict2, dict3)', globals=globals(), number=10000))
+print('update_ord_d -',
+    timeit(
+        'update_ord_d(dict2, dict3)', globals=globals(), number=10000))
+print('pop_dict -',
+    timeit(
+        'pop_dict(dict2)', globals=globals(), number=10000))
+print('pop_ord_d -',
+    timeit(
+        'pop_ord_d(dict2)', globals=globals(), number=10000))
+
+'''
+Замеры очень близки по показателям, немного выбивается заполнение словаря
+
+fill_dict - 4.8075568
+fill_ord_d - 5.5965471
+
+sort_dict - 1.4189903000000008
+sort_ord_d - 1.3862094000000003
+
+get_dict - 4.901506800000002
+get_ord_d - 4.9839342
+
+update_dict - 0.002828300000000894
+update_ord_d - 0.0027681000000008282
+
+pop_dict - 0.004577699999998686
+pop_ord_d - 0.004605200000000309
+'''
