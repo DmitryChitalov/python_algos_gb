6d6023c7df3fb257d9f79af6fb47f53f88cc12d2
Tatiana Grishechkina <tatiana.grishechkina@amdocs.com>
2021-02-08 00:05:07 +0300

    My homework3 (all tasks)

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,20 @@
 р
 а
 """
+import hashlib
+
+
+def substrings(user_d):
+    set_substrings = set()
+    count = len(user_d)
+    for el in range(len(user_d)):
+        for i in range(len(user_d)):
+            if user_d not in user_d[i:i+count]:
+                set_substrings.add(hashlib.sha256(user_d[i:i+count].encode('utf-8')).hexdigest())
+        count -= 1
+    return set_substrings
+
+
+user_data = input("Введите строку состоящую только из строчных латинских букв: ")
+print(f"Количество подстрок: {len(substrings(user_data))}")
+
