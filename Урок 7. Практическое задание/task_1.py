5abbc2abca2068d2ea842c6fe865d8fb7ffb7697
Lajilit <lajilit@gmail.com>
2021-02-07 22:29:45 +0300

    tasks 1-3

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -12,3 +12,56 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+from random import randint
+from timeit import timeit
+
+
+def bubble_sort(lst):
+    last_el = len(lst) - 1
+    for x in range(last_el):
+        for idx in range(last_el - x):
+            if lst[idx] < lst[idx + 1]:
+                lst[idx], lst[idx + 1] = lst[idx + 1], lst[idx]
+    return lst
+
+
+def bubble_sort_optimized(lst):
+    last_el = len(lst) - 1
+    for x in range(last_el):
+        is_changed = False
+        for idx in range(last_el - x):
+            if lst[idx] < lst[idx + 1]:
+                lst[idx], lst[idx + 1] = lst[idx + 1], lst[idx]
+                is_changed = True
+        if not is_changed:
+            return lst
+
+    return lst
+
+
+start_list = [randint(-100, 100) for i in range(1000)]
+new_list = bubble_sort(start_list[:])
+new_list_opt = bubble_sort_optimized(start_list[:])
+
+# print(f'Start list: \n{start_list}')
+# print(f'New list: \n{new_list}')
+# print(f'New list optimized: \n{new_list_opt}')
+
+print('Time for bubble_sort: ', timeit('bubble_sort(start_list[:])',
+                                       globals=globals(), number=100))
+
+print('Time for bubble_sort_optimized: ',
+      timeit('bubble_sort_optimized(start_list[:])',
+             globals=globals(), number=100))
+
+"""
+Замеры для списка из 1000 элементов на 100 измерений:
+
+Time for bubble_sort:  16.363335199999998
+Time for bubble_sort_optimized:  16.982484200000002
+
+Мой вариант оптимизации кода не дает экономии времени. В зависимости от 
+самого списка экономия возможна, то есть в некоторых случаях всё-таки 
+оптимизированная функция работает быстрее, но в большинстве случаев время 
+работы остается таким же или увеличивается.
+"""
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -1,5 +1,6 @@
 """
-3. Массив размером 2m + 1, где m – натуральное число, заполнен случайным образом.
+3. Массив размером 2m + 1, где m – натуральное число,
+заполнен случайным образом.
 Найдите в массиве медиану. Медианой называется элемент ряда, делящий его на
 две равные части: в одной находятся элементы, которые не меньше медианы,
 в другой – не больше медианы.
@@ -9,7 +10,42 @@
 
 Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках: Шелла, Гномья, ...
-
-arr[m]
-from statistics import median
 """
+from random import randint
+
+
+def shell_sort(lst):
+    d = len(lst) // 2
+    while d:
+        for i, el in enumerate(lst):
+            while i >= d and lst[i - d] > el:
+                lst[i] = lst[i - d]
+                i -= d
+            lst[i] = el
+        d = 1 if d == 2 else int(d * 5.0 / 11)
+    return lst
+
+
+def find_median(m: int) -> int:
+    lst = [randint(-100, 100) for _ in range(2 * m + 1)]
+    print('List:', lst)
+    sorted_lst = shell_sort(lst[:])
+    return sorted_lst[m]
+
+
+print('Median is:', find_median(3))
+
+
+# Without sort
+
+
+def find_median_without_sort(m: int) -> int:
+    lst = [randint(-100, 100) for _ in range(2 * m + 1)]
+    print('List:', lst)
+    tmp = lst[:]
+    for i in range(m):
+        tmp.remove(max(tmp))
+    return max(tmp)
+
+
+print('Median is:', find_median_without_sort(3))
