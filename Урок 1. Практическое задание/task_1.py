b679e9864ffe88c05be5fbc34c90e63c948df1eb
daniilro <daniil_ro@mail.ru>
2021-02-07 19:14:17 +0300

    Test

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 Задание 1.
 
 Для каждой из трех задач выполнить следующее:
@@ -28,7 +28,7 @@ def check_1(lst_obj):
 
     Сложность: !!!.
     """
-    lst_to_set = set(lst_obj)  # !!!
+    lst_to_set = set(lst_obj)  # !!!!
     return lst_to_set
 
 
