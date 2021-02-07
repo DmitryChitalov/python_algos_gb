3de3c13bec03193d5f567a20aa586fd89ac87baf
Nikita <kulkovnik93@gmail.com>
2021-02-07 15:24:29 +0300

    add 7_hw
    added 7_hw

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -12,3 +12,87 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+
+from timeit import timeit
+import random
+
+
+def buble_funk(arr_obj):
+    n = 1
+    while n < len(arr_obj):
+        for i in range(len(arr_obj)-n):
+            if arr_obj[i] < arr_obj[i+1]:
+                arr_obj[i], arr_obj[i+1] = arr_obj[i+1], arr_obj[i]
+        n += 1
+    return arr_obj
+
+
+def bubble_funck_mod(arr_obj):
+    n = 1
+    flag_breakpoint = 0
+    while n < len(arr_obj):
+        for i in range(len(arr_obj)-n):
+            if arr_obj[i] < arr_obj[i+1]:
+                arr_obj[i], arr_obj[i+1] = arr_obj[i+1], arr_obj[i]
+                flag_breakpoint = 1
+        if flag_breakpoint == 0:
+            break
+        n += 1
+    return arr_obj
+
+
+if __name__ == '__main__':
+    arr_obj_original_one = [random.randint(-100, 100) for _ in range(10)]
+    arr_obj_original_two = [random.randint(-100, 100) for _ in range(10)]
+    print(f'Оригинальный список: {arr_obj_original_one}')
+    print(f'Результат сортировки функции buble_funk: \
+{buble_funk(arr_obj_original_one)}\n')
+    print(f'Оригинальный список: {arr_obj_original_two}')
+    print(f'Результат сортировки функции buble_funk: \
+{bubble_funck_mod(arr_obj_original_two)}')
+
+    print('\nСкрипт профилировки buble_funk:')
+    print(
+        timeit(
+            "buble_funk(arr_obj_original_one)",
+            setup="from __main__ import buble_funk, arr_obj_original_one",
+            number=100))
+    print('\nСкрипт профилировки bubble_funck_mod:')
+    print(
+        timeit(
+            "bubble_funck_mod(arr_obj_original_two)",
+            setup="from __main__ import bubble_funck_mod, arr_obj_original_two",
+            number=100))
+
+"""
+В качестве вывода ->
+Оригинальный список: [100, -87, 35, -90, -5, 70, -91, 51, 64, 23]
+Результат сортировки функции buble_funk:[100, 70, 64, 51, 35, 23, -5, -87, -90, -91]
+
+Оригинальный список: [-69, -18, 51, 36, -27, 15, 83, -72, 55, 13]
+Результат сортировки функции buble_funk: [83, 55, 51, 36, 15, 13, -18, -27, -69, -72]
+
+Скрипт профилировки buble_funk:
+0.0015046409998831223
+
+Скрипт профилировки bubble_funck_mod:
+0.00025248899964935845
+________________________________________________________
+
+Наблюдается незначительное оптимизация,
+с учетом установки флага + маленькие вх. данные.
+Вопрос => нужна-ли она при реальной работе?
+Попробуем увеличить входящие данные до 1000:
+Опишу только проф-ку:
+
+Скрипт профилировки buble_funk:
+4.778609343000426
+
+Скрипт профилировки bubble_funck_mod:
+0.016413667000051646
+
+В данном случае очевидно, что флаг значительно сокращает время,
+делая вариант сортировки "пузырьком" ->
+конкурирующим с вариантами,
+которые также были обозначены на уроке
+"""
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -1,5 +1,6 @@
 """
-3. Массив размером 2m + 1, где m – натуральное число, заполнен случайным образом.
+3. Массив размером 2m + 1, где m – натуральное число, заполнен случайным
+образом.
 Найдите в массиве медиану. Медианой называется элемент ряда, делящий его на
 две равные части: в одной находятся элементы, которые не меньше медианы,
 в другой – не больше медианы.
@@ -13,3 +14,29 @@
 arr[m]
 from statistics import median
 """
+
+from random import randint
+from statistics import median
+
+
+def shell_funk(data):
+    inc = len(data) // 2
+    while inc:
+        for i, el in enumerate(data):
+            while i >= inc and data[i - inc] > el:
+                data[i] = data[i - inc]
+                i -= inc
+            data[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+    return data
+
+
+def shell_median_funk(rand_list):
+    return shell_funk(rand_list)[len(rand_list) // 2]
+
+
+m = 10
+rand_list = [randint(0, 100) for i in range(2 * m + 1)]
+print(f"Исходный список -> {rand_list}")
+print(f"Медиана (встроенной функции) -> {shell_funk(rand_list)}")
+print(f"Медиана (SHELL) -> {shell_median_funk(rand_list)}")
