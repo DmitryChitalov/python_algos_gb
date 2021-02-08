f375b0fdd7469d149dde80dcef90723b340606cc
rfxsx <rfxsx@yandex.ru>
2021-02-09 00:24:33 +0300

    lesson3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -15,3 +15,50 @@
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
+
+import sqlite3
+from hashlib import sha256
+
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS PASS (user,password)''')
+
+
+def insert_into_pass(conn, usr, hash_pass):
+    c = conn.cursor()
+    c.execute('''INSERT INTO PASS values(?, ?)''', (usr, hash_pass))
+    conn.commit()
+
+
+def select_from_pass_by_usr(conn, usr):
+    c = conn.cursor()
+    c.execute('SELECT password FROM PASS WHERE user=?', (usr,))
+    row = c.fetchone()
+    print(row)
+    if row is None:
+        return None
+    else:
+        return row[0]
+
+
+def hashing_password(pas, usr):
+    return sha256(pas.encode() + usr.encode()).hexdigest()
+
+
+while True:
+    input_user = input("Введите имя пользователя: ")
+    res = select_from_pass_by_usr(conn, input_user)
+    input_password = input('Введите пароль: ')
+    p_hash = hashing_password(input_password, input_user)
+    if res is None:
+        # new user create
+        insert_into_pass(conn, input_user, p_hash)
+        res = p_hash
+    print('В базе данных хранится строка: {}'.format(res))
+    same_pas=input('Введите пароль еще раз для проверки: ')
+    new_hash = hashing_password(same_pas, input_user)
+    if res == new_hash:
+        print('Вы ввели правильный пароль')
+    else:
+        print('Пароль введен не правильно')
+
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,20 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+from uuid import uuid4
+from hashlib import md5
+
+
+obj = {}
+
+def check_page(url: str):
+    if obj.get(url):
+        print('{} уже присутствует в кэше'.format(url))
+    else:
+        salt = url[::-1] + uuid4().hex
+        obj[url] = md5(salt.encode() + url.encode()).hexdigest()
+        print(obj)
+
+check_page('http://yandex.ru')
+check_page('http://beloved-wed.ru')
+check_page('http://yandex.ru')
