c60fa587df597c47d0e69526adc30e19882e6b77
Vitalii Gulin <gvitaly@mail.ru>
2021-02-10 00:57:53 +0300

    Lesson 5.

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,67 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+from collections import deque
+from random import choice
+from string import ascii_lowercase
+from timeit import timeit
+
+
+def ins_deque():
+    dq = deque('abc')
+    letters = ascii_lowercase
+    for i in range(5000):
+        dq.append(choice(letters))
+
+
+def ins_list():
+    ls = list('abc')
+    letters = ascii_lowercase
+    for i in range(5000):
+        ls.append(choice(letters))
+
+
+def pop_deque():
+    dq = deque('abc')
+    letters = ascii_lowercase
+    for i in range(5000):
+        dq.append(choice(letters))
+    for i in range(5000):
+        dq.pop()
+
+
+def pop_list():
+    ls = list('abc')
+    letters = ascii_lowercase
+    for i in range(5000):
+        ls.append(choice(letters))
+    for i in range(5000):
+        ls.pop()
+
+
+def get_deque():
+    dq = deque('abc')
+    letters = ascii_lowercase
+    for i in range(5000):
+        dq.append(choice(letters))
+    for i in range(5000):
+        bb = dq[i]
+
+
+def get_list():
+    ls = list('abc')
+    letters = ascii_lowercase
+    for i in range(5000):
+        ls.append(choice(letters))
+    for i in range(5000):
+        bb = ls[i]
+
+
+print('ins_deque()', timeit('ins_deque()', setup='from __main__ import ins_deque', number=1000))
+print('pop_deque()', timeit('pop_deque()', setup='from __main__ import pop_deque', number=1000))
+print('get_deque()', timeit('get_deque()', setup='from __main__ import get_deque', number=1000))
+print('ins_list()', timeit('ins_list()', setup='from __main__ import ins_list', number=1000))
+print('pop_list()', timeit('pop_list()', setup='from __main__ import pop_list', number=1000))
+print('get_list()', timeit('get_list()', setup='from __main__ import get_list', number=1000))
+# Документация не совсем корректна: дека быстрее листа получается.
+# Причем для всех операций.
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,53 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+from collections import deque
+from random import choice
+from string import ascii_lowercase
+from timeit import timeit
+from collections import OrderedDict
+
+
+def ins_dict():
+    cm_di = dict()
+    letters = ascii_lowercase
+    for i in range(5000):
+        cm_di[i] = choice(letters)
+
+
+def ins_ord():
+    or_di = OrderedDict()
+    letters = ascii_lowercase
+    for i in range(5000):
+        or_di[i] = choice(letters)
+
+
+def get_dict():
+    cm_di = dict()
+    letters = ascii_lowercase
+    for i in range(5000):
+        cm_di[i] = choice(letters)
+    for i in range(5000):
+        bb = cm_di[i]
+
+
+def get_ord():
+    or_di = OrderedDict()
+    letters = ascii_lowercase
+    for i in range(5000):
+        or_di[i] = choice(letters)
+    for i in range(5000):
+        bb = or_di[i]
+
+
+print('ins_dict()', timeit('ins_dict()',
+                           setup='from __main__ import ins_dict', number=1000))
+print('ins_ord()', timeit('ins_ord()',
+                          setup='from __main__ import ins_ord', number=1000))
+print('get_dict()', timeit('get_dict()',
+                           setup='from __main__ import get_dict', number=1000))
+print('get_ord()', timeit('get_ord()',
+                          setup='from __main__ import get_ord', number=1000))
+# Словарь dict быстрее словаря OrderedDict для вставки и извлечения.
+# Видимо, обеспечение упорядоченности влечет дополнительные
+# затраты по времени.
