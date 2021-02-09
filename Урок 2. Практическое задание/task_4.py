6b011cf7ddae3c98ce13bb00ac89584ab641fd3f
LaN3ro <78478735+LaN3ro@users.noreply.github.com>
2021-02-10 02:13:34 +0300

    Update task_4.py

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -9,3 +9,19 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+count = int(input("Введите количество элементов (n) ряда "))
+calc = 1
+res = 0
+
+
+def my_func(count, calc, res):
+    res = res + calc
+    if count == 1:
+        print("Сумма ряда равна " + str(res))
+        exit()
+    calc = calc / 2 * (-1)
+    count -= 1
+    my_func(count, calc, res)
+
+
+my_func(count, calc, res)
