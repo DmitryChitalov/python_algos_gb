3b7c851e86c91f44d139ff4669e1da08bb9ce0e2
timonen87 <71833451+timonen87@users.noreply.github.com>
2021-02-09 12:02:01 +0300

    Update task_3.py

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,33 @@
 arr[m]
 from statistics import median
 """
+from random import randint
+from statistics import median
+
+
+def shell(seq):
+    inc = len(seq) // 2
+    while inc:
+        for i, el in enumerate(seq):
+            while i >= inc and seq[i - inc] > el:
+                seq[i] = seq[i - inc]
+                i -= inc
+            seq[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+    return seq
+
+
+def shell_median(list_sort):
+    return shell(list_sort)[len(list_sort) // 2]
+
+
+m = 10
+list_sort = [randint(0, 100) for i in range(2 * m + 1)]
+print(f'Начальный список: {list_sort}')
+print(f'После сортировки:  {shell(list_sort)}')
+print(f'Медиана:  {shell_median(list_sort)}')
+
+"""
+Изспользвоал метод сортировки SHELL и расчет медианы. 
+
+"""
