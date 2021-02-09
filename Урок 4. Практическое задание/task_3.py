76c7f78deb47574a665b63b31a133a504b7988b9
balexand <alexandra.bereslavskaya@gmail.com>
2021-02-09 23:29:14 +0300

    My_homework

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -9,6 +9,24 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+"""
+Введите пожалуйста произвольное число: 80137519875365917659125
+Замер с помощью инструмента timeit 
+revers 0.16138669999999955 seconds
+revers_2 0.06287960000000048 seconds
+revers_3 0.005424200000000212 seconds 
+
+Видно, что в revers много лишних операций, а потому и время очень сильно просидает. 
+memorise в данном задании не используется, но как мы могли убедиться в task_2 он был бы оптимальный.
+
+Замер с помощью cProfile показался на данном примере не очень результативным.
+По нулям. 
+Как я поняла это очень удобный инструмент для анализа сложных алгоритмов - программ. 
+С большим количеством вложенных функция. Для данного случая бессмысленен.
+"""
+
+from timeit import timeit
+from cProfile import run
 
 
 def revers(enter_num, revers_num=0):
@@ -29,8 +47,27 @@ def revers_2(enter_num, revers_num=0):
     return revers_num
 
 
+#  Эх! Тут есть такое решение :((( Я не оригинальна... :(
 def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
 
+
+def timeit_check():
+    print("Замер с помощью инструмента timeit ")
+    print(f"revers {timeit('revers(i_number)', globals=globals(), number=10000)} seconds")
+    print(f"revers_2 {timeit('revers_2(i_number)', globals=globals(), number=10000)} seconds")
+    print(f"revers_3 {timeit('revers_3(i_number)', globals=globals(), number=10000)} seconds")
+
+
+def cprofile_check():
+    revers(i_number)
+    revers_2(i_number)
+    revers_3(i_number)
+
+
+i_number = int(input("Введите пожалуйста произвольное число: "))
+
+timeit_check()
+run('cprofile_check()')
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -9,6 +9,18 @@
 Попытайтесь написать третью версию, которая будет самой быстрой.
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
 """
+"""
+Замер с помощью инструмента timeit 
+func_1 0.1941711 seconds
+func_2 0.30075219999999997 seconds
+func_3 0.18761289999999997 seconds
+
+Как видно самый быстрый вариант решения - третий. В котором мы воспользовались возможностями встроенных функций.
+И самый долгий второй. В котором для много лишних повторений операции count, создание доп списка, поиск max 
+и сопостовление двух списков. 
+"""
+from timeit import timeit
+
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -37,5 +49,21 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+def func_3():
+    # пробегает по array и возвращает максимальное значение для переданной функции
+    # в данном случае для count()
+    mostly = max(array, key=array.count)
+    return f'Чаще всего встречается число {mostly}, ' \
+           f'оно появилось в массиве {array.count(mostly)} раз(а)'
+
+
+def timeit_check():
+    print("Замер с помощью инструмента timeit ")
+    print(f"func_1 {timeit('func_1()', globals=globals(), number=100000)} seconds")
+    print(f"func_2 {timeit('func_2()', globals=globals(), number=100000)} seconds")
+    print(f"func_3 {timeit('func_3()', globals=globals(), number=100000)} seconds")
+
+
+print(f"Рассмотрим список: {array}. {func_3()} ")
+
+timeit_check()
