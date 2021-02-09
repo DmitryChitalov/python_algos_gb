84a8a80ccc831e06441966427e0806a884064f85
Sergey Smirnov <sgr372013@yandex.ru>
2021-02-09 15:04:47 +0000

    Домашняя работа к Уроку 5

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,67 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+from collections import namedtuple
+
+def calc_rate(c_data):
+    """
+    Возвращает словарь в формате 'НаименованиеКомпании':'ГодоваяПрибыль'
+    """
+    cr = dict()
+    for c in c_data:
+        cr[c[0]] = sum(c[1:])
+    
+    print("Годовая прибыль компаний",cr)
+    return cr
+
+def sort_companies(c_raiting):
+    """ 
+    Сортирует названия компаний в два списка  1 - список прибыль выше среднего значения
+    2 - список прибыль ниже среднего
+
+    """
+    up_avg = list()
+    dw_avg = list()
+    n = len(c_raiting.keys())
+
+    avp = sum(c_raiting.values())/n # считаем среднюю прибыль всех компаний
+    print("Средняя годовая прибыль всех предприятий", round(avp,1))
+
+    for key  in c_raiting:
+        if c_raiting[key] > avp:
+            up_avg.append(key)
+        else:
+            dw_avg.append(key)
+
+
+    return up_avg, dw_avg
+
+def user_poll():
+    """
+    Ввод данных по компаниям
+    """
+    n_comp = int(input("Введите количество предприятий для расчета прибыли \n"))
+
+    c_data = list() # массив для хранения данных по компаниям
+
+    # цикл ввод информаци по компаниям
+    Company = namedtuple('Company','name Q1 Q2 Q3 Q4') 
+
+    for i in range(0, n_comp):
+        c_name = input(f"Введите название {i+1} предприятия \n")
+        c_return = input(f"Через пробел введите квартальные прибыли предприятия (4 квартала) \n")
+        qr = [float(el) for el in c_return.split(' ')]
+        c_data.append(Company(c_name, qr[0], qr[1], qr[2], qr[3]))
+
+    c_raiting = calc_rate(c_data)
+
+    up, dw = sort_companies(c_raiting)
+
+    print(f"Предприятия с прибылью выше среднего {up}\n")
+    print(f"Предприятия с приыбылью ниже среднего {dw}\n")
+
+    return
+
+
+if __name__ == '__main__':
+    user_poll()
