e8d5540dc6175724ec4b0ee34111805fcd8bcde4
Arkasha <ar246@yandex.ru>
2021-02-11 20:24:51 +0300

    HW8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -9,6 +9,7 @@
 
 Поработайте с доработанной структурой, позапускайте на реальных данных.
 """
+import traceback
 
 class BinaryTree:
     def __init__(self, root_obj):
@@ -21,59 +22,82 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
+        # только если значение меньше
+        if new_node < self.root:
+            # если у узла нет левого потомка
+            if self.left_child is None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.left_child = BinaryTree(new_node)
+            # если у узла есть левый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+            print('Невозможная операция')
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
+        # только если значение больше
+        if new_node < self.root:
+            # если у узла нет правого потомка
+            if self.right_child is None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.right_child = BinaryTree(new_node)
+            # если у узла есть правый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+            print('Невозможная операция')
 
     # метод доступа к правому потомку
     def get_right_child(self):
-        return self.right_child
+        if self.right_child is not None:
+            return self.right_child
+        else:
+            return 'Нет правого ребёнка'
 
     # метод доступа к левому потомку
     def get_left_child(self):
-        return self.left_child
+        if self.left_child is not None:
+            return self.left_child
+        else:
+            return 'Нет левого ребёнка'
 
     # метод установки корня
     def set_root_val(self, obj):
-        self.root = obj
+        try:
+            self.root = obj
+        except AttributeError:
+            print('Невозможная операция')
 
     # метод доступа к корню
     def get_root_val(self):
-        return self.root
+        try:
+            return self.root
+        except AttributeError:
+            return 'Корень отсутствует'
 
+try:
+    r = BinaryTree(8)
+    print(r.get_root_val())
+    print(r.get_left_child())
+    r.insert_left(10)
+    print(r.get_left_child())
+    print(r.get_left_child().get_root_val())
+    r.insert_right(12)
+    print(r.get_right_child())
+    print(r.get_right_child().get_root_val())
+    r.get_right_child().set_root_val(16)
+    print(r.get_right_child().get_root_val())
+except AttributeError as err:
+    print('Вы пытались выполнить невыполнимое по атрибутам действие: \n', traceback.format_exc())
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(4)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
