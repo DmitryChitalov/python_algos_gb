feae583241829ca75080fdf7c2c5924527009fd3
rfxsx <rfxsx@yandex.ru>
2021-02-12 00:03:42 +0300

    lesson5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,80 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+
+from timeit import timeit
+from collections import deque
+
+n = 1000
+my_list = [i for i in range(n)]
+my_deq = deque(my_list)
+
+
+def append_left_list(n):
+    my_list = []
+    for i in range(n):
+        my_list.insert(0, i)
+
+
+def append_right_list(n):
+    my_list = []
+    for i in range(n):
+        my_list.append(i)
+
+
+def pop_left_list(my_list):
+    for i in range(len(my_list)):
+        my_list.pop(0)
+
+
+def pop_right_list(my_list):
+    for i in range(len(my_list)):
+        my_list.pop()
+
+
+def pop_right_deque(my_deque):
+    for i in range(len(my_deque)):
+        my_deque.pop()
+
+
+def pop_left_deque(my_deque):
+    for i in range(len(my_deque)):
+        my_deque.popleft()
+
+
+def append_right_deque(n):
+    my_deque = deque()
+    for i in range(n):
+        my_deque.append(i)
+
+
+def append_left_deque(n):
+    my_deque = deque()
+    for i in range(n):
+        my_deque.appendleft(i)
+
+
+def print_timeint(func_name, n):
+    print(f'{func_name} {timeit(f"{func_name}({n})", globals=globals(), number=10000)}')
+
+
+print_timeint('append_left_list', n)
+print_timeint('append_left_deque', n)
+print_timeint('append_right_list', n)
+print_timeint('append_right_deque', n)
+print_timeint('pop_left_list', my_list)
+print_timeint('pop_left_deque', my_deq)
+print_timeint('pop_right_list', my_list)
+print_timeint('pop_right_deque', my_deq)
+"""
+append_left_list 4.6302728
+append_left_deque 0.5582171000000002
+append_right_list 0.6263875000000008
+append_right_deque 0.5385233999999999
+pop_left_list 1.4993705000000004
+pop_left_deque 0.6285075999999998
+pop_right_list 0.5421580000000006
+pop_right_deque 0.6070908999999993
+
+По выводу видно что существенно выигрывает deq. Самый лучший результат при добавлении данныс в начало массива 
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,66 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+
+from collections import OrderedDict
+from timeit import timeit
+
+
+def create_dict(num):
+    my_dict = dict()
+    for i in range(num):
+        my_dict[i] = i
+    return my_dict
+
+
+def create_ordered_dict(num):
+    my_dict = OrderedDict()
+    for i in range(num):
+        my_dict[i] = i
+    return my_dict
+
+
+def pop_dict(my_dict):
+    for i in range(len(my_dict)):
+        el = my_dict.popitem()
+
+
+def pop_ordered_dict(my_dict):
+    for i in range(len(my_dict)):
+        el = my_dict.popitem()
+
+
+def get_dict(my_dict):
+    for i in range(len(my_dict)):
+        el = my_dict.get(i)
+
+
+def get_ordered_dict(my_dict):
+    for i in range(len(my_dict)):
+        el = my_dict.get(i)
+
+
+def print_timeint(func_name, n):
+    print(f'{func_name} {timeit(f"{func_name}({n})", globals=globals(), number=10000)}')
+
+n = 1000
+simple_dict = create_dict(n)
+ordered_dict = create_ordered_dict(n)
+
+print_timeint('create_dict', n)
+print_timeint('create_ordered_dict', n)
+print_timeint('pop_dict', simple_dict)
+print_timeint('pop_ordered_dict', ordered_dict)
+print_timeint('get_dict', simple_dict)
+print_timeint('get_ordered_dict', ordered_dict)
+
+"""
+create_dict 0.6007888
+create_ordered_dict 1.2769747
+pop_dict 1.1560505
+pop_ordered_dict 2.5944715
+get_dict 0.9619989000000002
+get_ordered_dict 1.8534487000000004
+
+По всем показателям OrderedDict существенно хуже чем обычный словарь. 
+"""
