aa9d4163d1cf2eb4af19cc9bf6895b8ccc86cb62
nik1tam <48382667+nik1tam@users.noreply.github.com>
2021-02-13 14:19:01 +0300

    Lesson 5 HW
    Lesson 5 homework

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -4,3 +4,42 @@
 Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
 """
+"""
+OrderedDict требует больше ресурсов, чем обычный словарь, но реализация позволяет 
+использовать его как, например, стек, или хранение отсортированных данных
+"""
+from timeit import timeit
+from random import randint
+import collections
+
+v_ord_dict = collections.OrderedDict([(1, 'a'), (2, 'b'), (3, 'c')])
+v_def_dict = {1: 'a', 2: 'b', 3: 'c'}
+
+key = 2
+val = 'b'
+for i in range(1000):
+    v_ord_dict[i] = str(i)
+
+print(v_ord_dict)
+print(v_def_dict)
+
+print(
+    timeit(
+        "v_def_dict[key]=val",
+        setup='from __main__ import v_def_dict, key, val',
+        number=1000))
+print(
+    timeit(
+        "v_def_dict[key]",
+        setup='from __main__ import v_def_dict, key',
+        number=1000))
+print(
+    timeit(
+        "v_ord_dict[key]=val",
+        setup='from __main__ import v_ord_dict, key, val',
+        number=1000))
+print(
+    timeit(
+        "v_ord_dict.popitem()",
+        setup='from __main__ import v_ord_dict',
+        number=1000))
