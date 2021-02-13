6972b9599523547aa0bf890b2da48e993a84f2aa
Artem Sivkov <thomasxiii@gmail.com>
2021-02-13 14:12:34 +0500

    L_3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,19 @@
 р
 а
 """
+
+import hashlib
+
+inp_str = input('Введите строку из строчных латинских букв: ')
+N = len(inp_str)
+
+set_of_substr = set()
+
+for i in range(1, N):
+    for j in range(i + 1):
+        print(set_of_substr)
+        print(inp_str[j:j + N - i])
+        set_of_substr.add(hashlib.md5(inp_str[j:j + N - i].encode('utf-8')).hexdigest())
+        print(set_of_substr)
+
+print(f'Число подстрок в строке {inp_str} составляет {len(set_of_substr)}')
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,27 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+
+
+#from uuid import uuid4
+import hashlib
+
+class Url:
+
+
+
+cache_file=open("my_cache", w)
+
+
+def get_page(url):
+    if cache_obj.get(url):
+        print(f'Данный адрес: {url} присутствует в кэше')
+    else:
+        res = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+        cache_obj[url] = res
+        print(cache_obj)
+
+
+get_page('https://geekbrains.ru/')
+get_page('https://geekbrains.ru/')
+get_page('https://geekbrains.ru/')
