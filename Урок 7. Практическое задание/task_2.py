8b0d4b9271d4df632c46474d015f7e8bd834bdb2
fireflysmk <fireflysmk@gmail.com>
2021-02-14 14:55:15 +0300

    lesson07, task1,2

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,41 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+import operator
+import random
+
+
+def merge_sort(L, compare=operator.lt):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = int(len(L) / 2)
+        left = merge_sort(L[:middle], compare)
+        right = merge_sort(L[middle:], compare)
+        return merge(left, right, compare)
+
+
+def merge(left, right, compare):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if compare(left[i], right[j]):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
+
+a = random.sample(range(50), 50)
+
+print(a)
+print(merge_sort(a))
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,37 @@
 arr[m]
 from statistics import median
 """
+
+import random
+
+def median(l, half):
+    if len(l) == 0:
+        return 0
+    if len(l) == 1:
+        return l[0]
+
+    pivot = l[0]
+
+    less = []
+    more = []
+    pivots = []
+    for item in l:
+        if item < pivot:
+            less.append(item)
+        elif item > pivot:
+            more.append(item)
+        else:
+            pivots.append(item)
+
+    if len(less) > half:
+        return median(less, half)
+    elif len(less) + len(pivots) > half:
+        return pivots[0]
+    else:
+        return median(more, half - len(more) - len(pivots))
+
+
+n = 6
+array = [random.randint(-100, 100) for _ in range(2 * n + 1)]
+print(f'Исходный массив {array}')
+print(f'Медиана {median(array, n)}')
