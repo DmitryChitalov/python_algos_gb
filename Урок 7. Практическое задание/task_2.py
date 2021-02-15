786a7d355fe2812703b3812b858f93920ea1c277
Роман Тетерин <rateterin@yandex.ru>
2021-02-15 15:37:30 +0300

    Tasks 1 - 3 for lesson 7 Algs

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,50 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+from random import random
+
+
+def get_uint(msg='', err=''):
+    while True:
+        res = input(('Enter Unsigned Int number: ', msg)[bool(msg)])
+        if res != '':
+            try:
+                res = int(res)
+            except ValueError:
+                pass
+            else:
+                return res
+        print(('This is not UInt, try again, please!', err)[bool(err)])
+
+
+def merge_sort(unsorted_list):
+    if len(unsorted_list) > 1:
+        mid = len(unsorted_list) // 2
+        left = unsorted_list[:mid]
+        right = unsorted_list[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i, j, k = 0, 0, 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                unsorted_list[k] = left[i]
+                i += 1
+            else:
+                unsorted_list[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            unsorted_list[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            unsorted_list[k] = right[j]
+            j += 1
+            k += 1
+        return unsorted_list
+
+
+origin_list = [random() * 50 for el in range(get_uint())]
+print(f'Исходный - {origin_list}')
+print(f'Отсортированный - {merge_sort(origin_list)}')
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,38 @@
 arr[m]
 from statistics import median
 """
+
+
+from random import randint
+from statistics import median
+
+
+def get_uint(msg='', err=''):
+    while True:
+        res = input(('Enter Unsigned Int number: ', msg)[bool(msg)])
+        if res != '':
+            try:
+                res = int(res)
+            except ValueError:
+                pass
+            else:
+                return res
+        print(('This is not UInt, try again, please!', err)[bool(err)])
+
+
+_ = get_uint(
+    msg='Для массива длиной 2m + 1 введите m (целое положительное число): ',
+    err='Требуется ввести целое положительное число!')
+arr = [randint(0, 1000) for _ in range(2 * _ + 1)]
+
+
+def get_median(unsorted_arr):
+    m = len(unsorted_arr) // 2
+    while len(unsorted_arr) > m + 1:
+        unsorted_arr.remove(max(unsorted_arr))
+    return max(unsorted_arr)
+
+
+print(f'Исходный массив: {arr}')
+print(f'Решение: {get_median(arr[:])}')
+print(f'Проверка: {median(arr[:])}')
