0cebad914705ee0c467d49bf6a33670a612ab64d
LaN3ro <78478735+LaN3ro@users.noreply.github.com>
2021-02-16 02:06:04 +0300

    Update task_4.py

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -9,8 +9,8 @@
 Попытайтесь написать третью версию, которая будет самой быстрой.
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
 """
-
-array = [1, 3, 1, 3, 4, 5, 1]
+from timeit import timeit
+from random import randint
 
 
 def func_1():
@@ -37,5 +37,17 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+def my_func():
+    res = max((array.count(i), i) for i in array)
+    return f'Чаще всего встречается число {res[1]}, ' \
+           f'оно появилось в массиве {res[0]} раз(а)'
+
+
+# array = [1, 3, 1, 3, 4, 5, 1]
+array = [randint(0, 1000) for i in range(20)]
+
+print(f"{timeit('func_1()', number=10000, globals=globals())}")
+print(f"{timeit('func_2()', number=10000, globals=globals())}")
+print(f"{timeit('my_func()', number=10000, globals=globals())}")
+
+# Со списком в задании выигрывают функции 1,2. Но как только берем список больше, выигрывает мой вариант
