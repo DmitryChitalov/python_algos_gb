384bd4ad24c2d99b305360d9c090be3cb7025f48
antony.s.a <anton_smirnoff@yahoo.com>
2021-02-16 16:48:45 +0300

    Выполнил task_4

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -27,3 +27,31 @@
 Для реализации хранилища можно применить любой подход,
 который вы придумаете, например, реализовать словарь.
 """
+
+
+# def authorization_1(users, user_name, user_password):  # O(n)
+#     for key, value in users.items():
+#         if key == user_name:
+#             if value['password'] == user_password and value['activation']:
+#                 return "Добро пожаловать. Доступ предоставлен"
+#         elif value['password'] == user_password and not value['activation']:
+#             return "Учетная запись не активна. Пройдите активацию"
+#         elif value['password'] != user_password:
+#             return "Пароль не верный"
+#
+#     return "Данного пользователя не существует"
+
+
+def authorization_2(users, user_name, user_password):  # O(1)
+    if users.get(user_name):
+        if users[user_name]['password'] == user_password and users[user_name]['activation']:
+            return "Добро пожаловать. Доступ предоставлен"
+        elif users[user_name]['password'] == user_password and not users[user_name]['activation']:
+            return "Учетная запись не активна. Пройдите активацию"
+        elif users[user_name]['password'] != user_password:
+            return "Пароль не верный"
+
+    return "Данного пользователя не существует"
+
+
+# Второе решения является более эффективным, т.к. оно константно
