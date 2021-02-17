e825ec6184fdec2d48b136ca3b25b8198cad9c0b
antony.s.a <anton_smirnoff@yahoo.com>
2021-02-17 15:14:14 +0300

    выполнил task_1

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -28,3 +28,51 @@
 Вы вместо трехзначного числа ввели строку (((. Исправьтесь
 Введите операцию (+, -, *, / или 0 для выхода):
 """
+
+
+def calc():
+    operation_type = input("Введите операцию +, -, *, / или 0 для выхода: ")
+
+    if operation_type == '0':
+        return "Выход"
+
+    else:
+        if operation_type in "+-*/":
+            try:
+                num_1 = int(input("Введите 1-е число: "))
+                num_2 = int(input("Введите 2-е число: "))
+
+                if operation_type == '+':
+                    res = num_1 + num_2
+                    print(f'Результат {res}')
+                    return calc()
+
+                elif operation_type == '-':
+                    res = num_1 - num_2
+                    print(f'Результат {res}')
+                    return calc()
+
+                elif operation_type == '*':
+                    res = num_1 * num_2
+                    print(f'Результат {res}')
+                    return calc()
+
+                elif operation_type == '/':
+                    if operation_type != 0:
+                        res = num_1 / num_2
+                        print(f"Ваш результат {res}")
+                    else:
+                        print("Деление на 0 невозможно")
+                    return calc()
+
+            except ValueError:
+                print(
+                    "Вы вместо числа ввели строку.")
+                return calc()
+
+        else:
+            print('Введен не верный символ, попробуйте еще раз')
+            return calc()
+
+
+calc()
