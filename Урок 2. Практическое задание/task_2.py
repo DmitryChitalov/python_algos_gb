e7ff91b866e66eebd886479b6ee47d5bc178fe75
antony.s.a <anton_smirnoff@yahoo.com>
2021-02-17 16:17:29 +0300

    выполнил task_2

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -16,3 +16,24 @@
 Введите число: 123
 Количество четных и нечетных цифр в числе равно: (1, 2)
 """
+
+
+def recur_method(numb, even=0, odd=0):
+    if numb == 0:
+        return even, odd
+    else:
+        cur_n = numb % 10
+        numb = numb // 10
+        if cur_n % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+        return recur_method(numb, even, odd)
+
+
+try:
+    natural_num = int(input("Введите натуральное число: "))
+    print(f'Кол-во четных и нечетных цифр в числе = {recur_method(natural_num)}')
+except ValueError:
+    print('Вместо числа введена строка.')
+
