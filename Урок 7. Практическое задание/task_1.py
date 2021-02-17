2a838df89e6b6021c3a2861d956bd084a0a2e666
Andrey Fedin <afedin@amdocs.com>
2021-02-17 20:02:44 +0300

    Урок 7. Задания 1,2,3

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -1,8 +1,12 @@
 """
 1. Отсортируйте по убыванию методом "пузырька" одномерный целочисленный массив,
 заданный случайными числами на промежутке [-100; 100). Выведите на экран
-исходный и отсортированный массивы. Сортировка должна быть реализована в
-виде функции. Обязательно доработайте алгоритм (сделайте его умнее).
+исходный и отсортированный массивы.
+
+Сортировка должна быть реализована в
+виде функции.
+
+Обязательно доработайте алгоритм (сделайте его умнее)!
 
 Идея доработки: если за проход по списку не совершается ни одной сортировки,
 то завершение
@@ -10,5 +14,51 @@
 и обосновать дала ли оптимизация эффективность
 
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
-а по убыванию
+а по убыванию.
+
+Сделайте выводы!!!
+Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+from random import randint
+from timeit import timeit
+
+
+def bubble_sort_mod(array):
+    for j in range(len(array)):
+        swap = 0
+        for i in range(len(array) - j - 1):
+            if array[i] < array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swap = 1
+        if swap == 0:
+            break
+    return array
+
+
+def bubble_sort(array):
+    for j in range(len(array)):
+        for i in range(len(array) - j - 1):
+            if array[i] < array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+    return array
+
+
+rand_array = [randint(-100, 100) for i in range(100)]
+print(rand_array)
+print(bubble_sort(rand_array[:]))
+print(timeit("bubble_sort(rand_array[:])", globals=globals(), number=1000))
+print(timeit("bubble_sort_mod(rand_array[:])", globals=globals(), number=1000))
+
+
+rand_array = [randint(-100, 100) for i in range(1000)]
+
+print(timeit("bubble_sort(rand_array[:])", globals=globals(), number=1000))
+print(timeit("bubble_sort_mod(rand_array[:])", globals=globals(), number=1000))
+
+
+# Доработал алгоритм, добавил выход если за проход не было перестановок.
+# По замерам данная "доработка" преймущества не дает.
+# 0.7841395
+# 0.6920742000000001
+# 78.8818797
+# 82.0807847
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,29 @@
 arr[m]
 from statistics import median
 """
+from random import randint
+from statistics import median
+
+
+def median_1(array):
+    for i in range(len(array)):
+        count = 0
+        for el in array:
+            if array[i] >= el:
+                count += 1
+
+        if count == len(array) // 2 + 1:
+            return array[i]
+
+    return array[i]
+
+
+m = int(input("Ведите число :"))
+
+test_array = [randint(0, 100) for i in range(m * 2 + 1)]
+
+print(test_array)
+print(
+    f"Используем встроенную функцию для нахождения медианы : {median(test_array)}"
+)
+print(f"Используем метод подсчета : {median_1(test_array)}")
