89df36d51bbc4c8f7305b2e1bd61885a8249e519
antony.s.a <anton_smirnoff@yahoo.com>
2021-02-17 22:46:07 +0300

    выполнил task_3

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -15,3 +15,17 @@
 Введите число, которое требуется перевернуть: 123
 Перевернутое число: 321
 """
+
+
+def revers_number(numb):
+    rest_numb, numeral = divmod(numb, 10)
+    if rest_numb == 0:
+        return str(numeral)
+    else:
+        return str(numeral) + str(revers_number(rest_numb))
+
+
+number = int(input("Введите число, которое требуется перевернуть: "))
+print(f'перевернутое число: {revers_number(number)}')
+
+
