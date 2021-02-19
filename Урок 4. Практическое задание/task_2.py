f9f892d36ee287c43da30921957faa158e0e7c0d
Oleg Golubev <Oleg.Golubev@amdocs.com>
2021-02-20 00:40:25 +0300

    Lesson_4 Task_1-5

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,14 +7,17 @@
 Выполнена попытка оптимизировать решение через мемоизацию.
 Сделаны замеры обеих реализаций.
 
-Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?
-Если у вас есть идеи, предложите вариант оптимизации.
+Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?!!!
+Если у вас есть идеи, предложите вариант оптимизации,
+если мемоизация не имеет смысла.
+Без аналитики задание считается не принятым
 """
 
 from timeit import timeit
 from random import randint
 
 
+# Без мемоизации
 def recursive_reverse(number):
     if number == 0:
         return str(number % 10)
@@ -25,35 +28,39 @@ num_100 = randint(10000, 1000000)
 num_1000 = randint(1000000, 10000000)
 num_10000 = randint(100000000, 10000000000000)
 
-print('Не оптимизированная функция recursive_reverse')
+print('Не оптимизированная функция recursive_reverse:')
 print(
     timeit(
         "recursive_reverse(num_100)",
-        setup='from __main__ import recursive_reverse, num_100',
-        number=10000))
+        globals=globals(),
+        number=100000))
 print(
     timeit(
         "recursive_reverse(num_1000)",
-        setup='from __main__ import recursive_reverse, num_1000',
-        number=10000))
+        globals=globals(),
+        number=100000))
 print(
     timeit(
         "recursive_reverse(num_10000)",
-        setup='from __main__ import recursive_reverse, num_10000',
-        number=10000))
+        globals=globals(),
+        number=100000))
+
+print()
 
 
-def memoize(f):
+# С мемоизацией
+def memoize(f):  # декоратор memoize, f - функция-аргумент
     cache = {}
 
-    def decorate(*args):
+    def decorate(*args):  # функция-обёртка
 
         if args in cache:
             return cache[args]
         else:
-            cache[args] = f(*args)
+            cache[args] = f(*args)  # f - это будет recursive_reverse_mem
             return cache[args]
-    return decorate
+
+    return decorate  # возврат функции-обёртки
 
 
 @memoize
@@ -63,19 +70,34 @@ def recursive_reverse_mem(number):
     return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
 
 
-print('Оптимизированная функция recursive_reverse_mem')
+print('Оптимизированная функция recursive_reverse_mem:')
 print(
     timeit(
         'recursive_reverse_mem(num_100)',
-        setup='from __main__ import recursive_reverse_mem, num_100',
-        number=10000))
+        globals=globals(),
+        number=100000))
 print(
     timeit(
         'recursive_reverse_mem(num_1000)',
-        setup='from __main__ import recursive_reverse_mem, num_1000',
-        number=10000))
+        globals=globals(),
+        number=100000))
 print(
     timeit(
         'recursive_reverse_mem(num_10000)',
-        setup='from __main__ import recursive_reverse_mem, num_10000',
-        number=10000))
+        globals=globals(),
+        number=100000))
+
+"""
+Не оптимизированная функция recursive_reverse:
+0.281369348
+0.31858060399999993
+0.6150991250000001
+
+Оптимизированная функция recursive_reverse_mem:
+0.023437659999999916
+0.02317926800000003
+0.023351931999999964
+"""
+# Ответ на ? "Есть ли смысл применять мемоизацию?"
+# на замерах видно, что практически на порядок отличается время
+# выполнения функции с мемоизацией и без неё. То есть ответ: смысл есть.
