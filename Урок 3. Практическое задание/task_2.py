e67113920f93327c22bf300e44a764e26374648c
antony.s.a <anton_smirnoff@yahoo.com>
2021-02-20 13:07:03 +0300

    выполнил task_2

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -15,3 +15,38 @@
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
+
+from binascii import hexlify
+from hashlib import pbkdf2_hmac
+
+
+class UserData:
+    def __init__(self, new_login, new_pass: str):
+        self.login = new_login.lower()
+        password_hash = pbkdf2_hmac(hash_name='sha256',
+                                    password=new_pass.encode(),
+                                    salt=self.login.encode(),
+                                    iterations=100000)
+        self._password_hash = hexlify(password_hash)
+
+    def authorization(self, password):
+        check_password = pbkdf2_hmac(hash_name='sha256',
+                                     password=password.encode(),
+                                     salt=self.login.encode(),
+                                     iterations=100000)
+        if self._password_hash == hexlify(check_password):
+            print('Authorization completed')
+        else:
+            print('Authorization failed')
+
+
+login = input('Enter login: ')
+password = input('Enter password: ')
+user_1 = UserData(login, password)
+
+print('Password hash: ', user_1._password_hash)
+auth_pass = password = input('Enter password: ')
+
+user_1.authorization(auth_pass)
+
+
