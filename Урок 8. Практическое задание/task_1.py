63a8eb1807961244cb90099a4a7899058739245b
Ekaterina Ukhovskaya <ekateriu@amdocs.com>
2021-02-25 12:49:02 +0300

    Задание к 8 уроку

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -2,13 +2,58 @@
 Задание 1.
 Реализуйте кодирование строки "по Хаффману".
 У вас два пути:
-1) тема идет тяжело? тогда вы можете, опираясь на пример с урока, сделать свою версию алгоритма
+1) тема идет тяжело? тогда вы можете, опираясь на пример с урока, сделать свою!!! версию алгоритма
 Разрешается и приветствуется изменение имен переменных, выбор других коллекций, различные изменения
 и оптимизации.
-КОПИПАСТ ПРИМЕРА ПРИНИМАТЬСЯ НЕ БУДЕТ!
+
+
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
-
-ВНИМАНИЕ: примеры заданий будут размещены в последний день сдачи.
-Но постарайтесь обойтись без них.
 """
+
+from collections import Counter, deque
+
+
+def haffman_tree(s):
+    count = Counter(s)
+    # Counter({'a': 1, 'b': 1, 'o': 1, 'r': 3, 't': 2, 'e': 1, 'y': 1, ' ': 1})
+    sorted_elems = deque(sorted(count.items(), key=lambda item: item[1]))
+    # deque([('a', 1), ('b', 1), ('o', 1), ('e', 1), ('y', 1), (' ', 1), ('t', 2), ('r', 3)])
+    if len(sorted_elems) != 1:
+       while len(sorted_elems) > 1:
+           #веса - 2, 2, 2, 4, 7, 11
+            weight = sorted_elems[0][1] + sorted_elems[1][1]
+            comb = {0: sorted_elems.popleft()[0],
+                    1: sorted_elems.popleft()[0]}
+            for i, _count in enumerate(sorted_elems):
+                if weight > _count[1]:
+                    continue
+                else:
+                    sorted_elems.insert(i, (comb, weight))
+                    break
+            else:
+                sorted_elems.append((comb, weight))
+    else:
+        weight = sorted_elems[0][1]
+        comb = {0: sorted_elems.popleft()[0], 1: None}
+        sorted_elems.append((comb, weight))
+    return sorted_elems[0][0]
+
+
+code_line = dict()
+
+def haffman_code(tree, path=''):
+    if not isinstance(tree, dict):
+        code_line[tree] = path
+    else:
+        haffman_code(tree[0], path=f'{path}0')
+        haffman_code(tree[1], path=f'{path}1')
+
+
+s = "abort retry"
+
+haffman_code(haffman_tree(s))
+
+for i in s:
+    print(code_line[i], end=' ')
+print()
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -1,5 +1,5 @@
 """
-Задание 2.**
+Задание 2.
 
 Доработайте пример структуры "дерево",
 рассмотренный на уроке.
@@ -7,10 +7,10 @@
 Предложите варианты доработки и оптимизации
 (например, валидация значений узлов в соответствии с требованиями для бинарного дерева)
 
-Поработайте с доработанной структурой, позапускайте на реальных данных.
+Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
-class BinaryTree:
+class BTree:
     def __init__(self, root_obj):
         # корень
         self.root = root_obj
@@ -19,43 +19,64 @@ class BinaryTree:
         # правый потомок
         self.right_child = None
 
+    def __str__(self):
+        return str(self.root)
+
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+        try:
+            # если у узла нет левого потомка
+            if self.root >= new_node:
+                if self.left_child is None:
+                    # тогда узел просто вставляется в дерево
+                    # формируется новое поддерево
+                    self.left_child = BTree(new_node)
+                # если у узла есть левый потомок
+                else:
+                    # тогда вставляем новый узел
+                    tree_obj = BTree(new_node)
+                    # и спускаем имеющегося потомка на один уровень ниже
+                    tree_obj.left_child = self.left_child
+                    self.left_child = tree_obj
+            else:
+                raise Exception('Ошибка значения')
+        except Exception as e:
+            print(e)
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        try:
+            if self.root <= new_node:
+                # если у узла нет правого потомка
+                if self.right_child is None:
+                    # тогда узел просто вставляется в дерево
+                    # формируется новое поддерево
+                    self.right_child = BTree(new_node)
+                # если у узла есть правый потомок
+                else:
+                    # тогда вставляем новый узел
+                    tree_obj = BTree(new_node)
+                    # и спускаем имеющегося потомка на один уровень ниже
+                    tree_obj.right_child = self.right_child
+                    self.right_child = tree_obj
+            else:
+                raise Exception('Ошибка значения')
+        except Exception as e:
+            print(e)
 
     # метод доступа к правому потомку
     def get_right_child(self):
-        return self.right_child
+        try:
+            return self.right_child
+        except AttributeError:
+            print('Ошибка доступа к атрибуту класса.')
 
     # метод доступа к левому потомку
     def get_left_child(self):
-        return self.left_child
+        try:
+            return self.left_child
+        except AttributeError:
+            print('Ошибка доступа к атрибуту класса.')
 
     # метод установки корня
     def set_root_val(self, obj):
@@ -63,13 +84,18 @@ class BinaryTree:
 
     # метод доступа к корню
     def get_root_val(self):
-        return self.root
+        try:
+            return self.root
+        except AttributeError:
+            print('Ошибка доступа к атрибуту класса.')
 
 
-r = BinaryTree(8)
+
+r = BTree(8)
 print(r.get_root_val())
 print(r.get_left_child())
-r.insert_left(4)
+r.insert_left(65)
+r.insert_left(7)
 print(r.get_left_child())
 print(r.get_left_child().get_root_val())
 r.insert_right(12)
