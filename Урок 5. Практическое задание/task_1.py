b418e88d769bc686b905d420c0f49eaa9e38d16c
antony.s.a <anton_smirnoff@yahoo.com>
2021-03-01 09:30:47 +0300

    выполнил task_1

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,46 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+
+import collections
+
+while True:
+    try:
+        num = int(input('Введите количество компаний: '))
+    except ValueError:
+        print('Вы ввели недопустимое значение')
+    break
+
+companies = collections.defaultdict()
+prof_c = collections.deque()
+unprof_c = collections.deque()
+all_profit = 0
+QUARTER = 4
+
+for i in range(num):
+    name = input(f'\nВведите название {i + 1}й компании: ')
+    profit = 0
+    q = 1
+    while q <= QUARTER:
+        try:
+            profit += float(input(f'Введите прибыль за {q}й квартал: '))
+        except ValueError:
+            print('Вы ввели недопустимое значение')
+            continue
+        q += 1
+    companies[name] = profit
+    all_profit += profit
+
+mid_profit = all_profit / num
+for i, item in companies.items():
+    if item >= mid_profit:
+        prof_c.append(i)
+    else:
+        unprof_c.append(i)
+print(f'Средняя прибыль для всех компаний составила: {mid_profit}')
+print(f'Прибыль выше среднего у {len(prof_c)} компании:')
+for name in prof_c:
+    print(name)
+print(f'Прибыль ниже среднего у {len(unprof_c)} компании')
+for name in unprof_c:
+    print(name)
