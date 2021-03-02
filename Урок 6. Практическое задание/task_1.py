64c829a839aaf55df0e14553012958c1cf7a71df
LaN3ro <78478735+LaN3ro@users.noreply.github.com>
2021-03-02 13:15:09 +0300

    Update task_1.py

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -13,3 +13,61 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+from timeit import default_timer
+from random import randint
+import memory_profiler as mem_profile
+
+
+def memorize(func):
+    def my_func(*args, **kwargs):
+        m1 = mem_profile.memory_usage()
+        start = default_timer()
+        res = func(*args, **kwargs)
+        stop = default_timer()
+        m2 = mem_profile.memory_usage()
+        mem_res = m2[0] - m1[0]
+        time_res = stop - start
+        print(f"{func.__name__}: {time_res} s, {mem_res} MB")
+        return res
+
+    return my_func
+
+
+@memorize
+def sum_list(l):
+    g = 0
+    for i in l:
+        g += int(i)
+    return g
+
+
+@memorize
+def sum_c(l):
+    return sum(l)
+
+
+my_list = [randint(10, 100) for i in range(99999999)]
+
+print(sum_list(my_list))
+print(sum_c(my_list))
+print("Потребление МЕМ крайне низкое в обоих вариантах, но быстрее 2 вариант, т.к. нет перебора через цикл")
+
+
+@memorize
+def append_in_list():
+    my_list = []
+    for i in range(99999999):
+        my_list.append(i)
+    return my_list
+
+
+@memorize
+def append_in_list2():
+    return [i for i in range(99999999)]
+
+
+append_in_list()
+append_in_list2()
+print(
+    "Потребление МЕМ высокое в обоих вариантах из-за перебора огромного количества данных. Время работы быстрее во втором, т.к нет перебора через цикл")
