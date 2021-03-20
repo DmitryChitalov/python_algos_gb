5b4505afdbf5ce8641850b82ae562cad309ac937
daniilro <daniil_ro@mail.ru>
2021-03-20 16:29:05 +0300

    Task - 5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -10,3 +10,27 @@ deque – это обобщение стеков и очередей.
 Сделайте замеры и оцените, насколько информация в документации
 соответствует дейстивтельности.
 """
+
+from timeit import timeit
+from collections import deque
+
+def append_left_list(num):
+    my_list = []
+    for i in range(num):
+        my_list.insert(0, i)
+
+
+def append_left_deque(num):
+    my_deque = deque()
+    for i in range(num):
+        my_deque.appendleft(i)
+
+num = 1000
+
+my_list1 = [el for el in range(num)]
+my_deque1 = deque(my_list1)
+
+print(f"append_left_list({num}): ",
+      timeit(f"append_left_list({num})", globals=globals(), number=1000))
+print(f"append_left_deque({num}): ",
+      timeit(f"append_left_deque({num})", globals=globals(), number=1000))
