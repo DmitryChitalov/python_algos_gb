0e3b9ceadf6d26116d28cc90ed28b8ba1a37379b
daniilro <daniil_ro@mail.ru>
2021-03-20 20:46:51 +0300

    Lesson - 6

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -23,9 +23,11 @@
 """
 
 from collections import OrderedDict
+from collections import deque
 from timeit import default_timer
 
 from memory_profiler import memory_usage
+from memory_profiler import profile
 
 
 ###################################################################
@@ -58,6 +60,7 @@ def populate_dict(dict, num):
 
 
 @decor_mem_time
+@profile
 def populate_o_dict(o_dict, num):
     for i in range(num):
         o_dict[-i] = i
@@ -67,3 +70,59 @@ def populate_o_dict(o_dict, num):
 
 populate_dict(def_dict, num)
 populate_o_dict(def_o_dict, num)
+
+'''
+populate_dict: 0.16587884400000008 секунд, 101.93359375 MiB
+populate_o_dict: 0.2347099559999999 секунд, 149.04296875 MiB
+
+На данном примере однозначно dict лучше чем OrderedDict
+
+'''
+###################################################################
+
+my_list = []
+my_deque = deque()
+
+
+@decor_mem_time
+def append_left_list(my_list, num):
+    for i in range(num):
+        my_list.insert(0, i)
+
+
+@decor_mem_time
+def append_left_deque(my_deque, num):
+    for i in range(num):
+        my_deque.appendleft(i)
+
+
+@decor_mem_time
+def append_right_list(my_list, num):
+    for i in range(num):
+        my_list.append(i)
+
+
+@decor_mem_time
+def append_right_deque(my_deque, num):
+    for i in range(num):
+        my_deque.append(i)
+
+
+###################################################################
+
+num1 = 100000
+
+append_left_list(my_list, num1)
+append_left_deque(my_deque, num1)
+append_right_list(my_list, num1)
+append_right_deque(my_deque, num1)
+
+'''
+append_left_list: 3.629230653 секунд, 4.375 MiB
+append_left_deque: 0.013127733000000141 секунд, 3.48828125 MiB
+append_right_list: 0.01770816700000033 секунд, 4.6171875 MiB
+append_right_deque: 0.007293352999999669 секунд, 3.33984375 MiB
+
+На вставке deque показал лучшие результаты чем list
+
+'''
