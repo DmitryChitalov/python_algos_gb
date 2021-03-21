521cf027ef309eac842f6a6e381047ecf7ab4877
daniilro <daniil_ro@mail.ru>
2021-03-21 15:24:56 +0300

    Lesson - 7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -20,16 +20,24 @@
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
 
+from timeit import timeit
+import random
 import numpy as np
 
 ###########################################################
-min = -100
-max = 100
-nel = 5
+min = -1000
+max = 1000
+nel = 5000
 
-def_randnums = list(np.random.randint(min, max, nel))
+#def_randnums = list(np.random.randint(min, max, nel))
+def_randnums = [random.randint(min, max) for _ in range(nel)]
 
-print(def_randnums)
+
+# def_randnums = []
+# def_randnums.extend(range(nel, 1, -1))
+# def_randnums.extend(range(1, nel, 1))
+
+# print(def_randnums)
 
 
 ###########################################################
@@ -46,8 +54,8 @@ def ext_bubble(nums):
     stop_flag = False
 
     for i in range(len(nums)):
-        if stop_flag:
-            break
+        if stop_flag == True:
+            return nums
         stop_flag = True
         for j in range(len(nums) - 1 - i):
             if nums[j] < nums[j + 1]:
@@ -57,13 +65,54 @@ def ext_bubble(nums):
 
 
 ###########################################################
+def list_sort_s(nums):
+    return sorted(nums, key=int, reverse=True)
+
+
+###########################################################
+n_iter = 50
+###########################################################
 test_randnums = def_randnums.copy()
 res = classic_bubble(test_randnums)
-print(f"classic_bubble: {res}")
+test_randnums = def_randnums.copy()
+print(
+    timeit(
+        f"classic_bubble: test_randnums",
+        setup=f'from __main__ import classic_bubble, test_randnums',
+        number=n_iter))
 
-test_randnums = test_randnums.copy()
+###########################################################
+
+test_randnums = def_randnums.copy()
 res = ext_bubble(test_randnums)
-print(f"ext_bubble: {res}")
+test_randnums = def_randnums.copy()
+print(
+    timeit(
+        f"ext_bubble: test_randnums",
+        setup=f'from __main__ import ext_bubble, test_randnums',
+        number=n_iter))
 
-print(res)
-print(def_randnums)
+###########################################################
+
+test_randnums = def_randnums.copy()
+res = list_sort_s(test_randnums)
+test_randnums = def_randnums.copy()
+print(
+    timeit(
+        f"list_sort_s: test_randnums",
+        setup=f'from __main__ import list_sort_s, test_randnums',
+        number=n_iter))
+
+###########################################################
+
+'''
+
+1.132000000403366e-06
+1.1329999995979279e-06
+7.560000003792311e-07
+
+На больших размерах у меня комп зависает.
+Видно что улучшеный пузырёк лучше классического.
+Но всторенный sort работает быстрее.
+
+'''
