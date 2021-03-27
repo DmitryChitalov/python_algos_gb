001fd91bc96fa537d14154d3114a8b20147dd8b9
Your Name <you@example.com>
2021-03-27 16:18:41 +0300

    homework_03

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,3 +14,20 @@
 р
 а
 """
+
+
+def count_substrings(s):
+    bag = set()
+    for i in range(len(s)):
+        for j in range(i+1, len(s)+1):
+            bag.add(hash(s[i:j]))
+    return len(bag)-1
+
+
+for s in ("bbbb", "baba", "bcde"):
+    print(f"{s} -> {count_substrings(s)}")
+
+# ---output---
+# bbbb -> 3
+# baba -> 6
+# bcde -> 9
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,89 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+import hashlib
+from random import randint, seed
+
+
+# Поскольку в этой задаче абстрактные объекты типа "хеш" или "словарь"
+# наделяются конкретным смыслом (страница, url), логично ожидать,
+# что класс будет моделировать процесс загрузки страницы.
+# Это значит, что должны быть по крайней мере два метода.
+# 1. Узнать, есть ли страница в кэше, если да, то получить
+# из кэша содержимое.
+# 2. Положить в кэш содержимое после успешной загрузки.
+#
+# Примечание: класс не защищен от коллизий. Грубо говоря,
+# есть вероятность, что по запросу http://kremlin.ru
+# будет показан http://whitehouse.gov или наоборот.
+# Соль никак не защищает от этого, вернее, какие-то коллизии
+# она убирает, но какие-то добавляет, и заранее это не определить,
+# (иначе это уже не криптография).
+class Cache:
+    def __init__(self, salt):
+        self.thecache = {}
+        self.salt = salt.encode()
+
+    def get_hash(self, url):
+        return hashlib.sha256(url.encode()+self.salt).hexdigest()
+
+    def __getitem__(self, url):
+        hash_ = self.get_hash(url)
+        return self.thecache.get(hash_)
+
+    def __setitem__(self, url, text):
+        hash_ = self.get_hash(url)
+        self.thecache[hash_] = text
+
+
+# Для симуляции процесса загрузки
+def get_text(url):
+    return f"<Это текст страницы {url}>"
+
+
+cache = Cache(
+    "Подсказка: задачу решите обязательно с применением 'соленого' хеширования")
+
+urls = [
+    "http://yandex.ru",
+    "http://google.com",
+    "http://geekbrains.ru"
+]
+
+# симулируем пользовательскую активность
+seed(12)
+order = [randint(0, 2) for _ in range(10)]
+# [1, 1, 2, 2, 2, 1, 0, 1, 0, 1]
+
+for i in order:
+    url = urls[i]
+    text = cache[url]
+    if not text:
+        # загружаем страницу
+        text = get_text(url)
+        print("Страница ", url, "не найдена в кэше, загружаем ее:\n", text)
+        cache[url] = text
+    else:
+        print("Страница ", url, "есть в кэше, ее содержимое:\n", text)
+
+# ---output---
+# Страница  http://google.com не найдена в кэше, загружаем ее:
+#  <Это текст страницы http://google.com>
+# Страница  http://google.com есть в кэше, ее содержимое:
+#  <Это текст страницы http://google.com>
+# Страница  http://geekbrains.ru не найдена в кэше, загружаем ее:
+#  <Это текст страницы http://geekbrains.ru>
+# Страница  http://geekbrains.ru есть в кэше, ее содержимое:
+#  <Это текст страницы http://geekbrains.ru>
+# Страница  http://geekbrains.ru есть в кэше, ее содержимое:
+#  <Это текст страницы http://geekbrains.ru>
+# Страница  http://google.com есть в кэше, ее содержимое:
+#  <Это текст страницы http://google.com>
+# Страница  http://yandex.ru не найдена в кэше, загружаем ее:
+#  <Это текст страницы http://yandex.ru>
+# Страница  http://google.com есть в кэше, ее содержимое:
+#  <Это текст страницы http://google.com>
+# Страница  http://yandex.ru есть в кэше, ее содержимое:
+#  <Это текст страницы http://yandex.ru>
+# Страница  http://google.com есть в кэше, ее содержимое:
+#  <Это текст страницы http://google.com>
