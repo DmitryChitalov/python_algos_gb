1a4a348f4d2adbb5177ed2ed534254deba39b3a7
Elena <alnorda@gmail.com>
2021-04-06 01:59:44 +0300

    E_Maksimova_lesson_08

---
+++ lesson_08/task_2.py
@@ -0,0 +1,94 @@
+"""
+Задание 2.**
+
+Доработайте пример структуры "дерево",
+рассмотренный на уроке.
+
+Предложите варианты доработки и оптимизации
+(например, валидация значений узлов в соответствии с требованиями для бинарного дерева)
+
+Поработайте с доработанной структурой, позапускайте на реальных данных.
+"""
+
+
+class BinaryTree:
+    def __init__(self, root_obj):
+        self.root = root_obj
+        self.left_child = None
+        self.right_child = None
+
+    def __str__(self):
+        # добавлено строкове представление
+        left = self.left_child.root if self.left_child is not None else None
+        right = self.right_child.root if self.right_child is not None else None
+        return f'<Узел {self.root}, дети: {left}, {right}>'
+
+    def insert_left(self, new_node):
+        # добавлена валидация нового узла по отношению к родительскому
+        if not self.validate(new_node):
+            if self.left_child is None:
+                self.left_child = BinaryTree(new_node)
+            else:
+                # добавлено автоматическое смещение прежнего узла в правого или левого потомка нового узла
+                tree_obj = BinaryTree(new_node)
+                if tree_obj.validate(self.left_child.root):
+                    tree_obj.right_child = self.left_child
+                    self.left_child = tree_obj
+                else:
+                    tree_obj.left_child = self.left_child
+                    self.left_child = tree_obj
+        else:
+            raise ValueError('Значение узла больше или равно корню!')
+
+    def insert_right(self, new_node):
+        # добавлена валидация нового узла по отношению к родительскому
+        if self.validate(new_node):
+            if self.right_child is None:
+                self.right_child = BinaryTree(new_node)
+            else:
+                # добавлено автоматическое смещение прежнего узла в правого или левого потомка нового узла
+                tree_obj = BinaryTree(new_node)
+                if tree_obj.validate(self.right_child.root):
+                    tree_obj.right_child = self.right_child
+                    self.right_child = tree_obj
+                else:
+                    tree_obj.left_child = self.right_child
+                    self.right_child = tree_obj
+        else:
+            raise ValueError('Значение нового узла меньше корня!')
+
+    def validate(self, value):
+        if value >= self.root:
+            return True
+        else:
+            return False
+
+    # метод просто возвращает значение атрибута, дублирование функционала, поэтому убрала
+    # def get_right_child(self):
+    #     return self.right_child
+
+    # метод просто возвращает значение атрибута, дублирование функционала, поэтому убрала
+    # def get_left_child(self):
+    #     return self.left_child
+
+    def set_root_val(self, obj):
+        self.root = obj
+
+    # метод просто возвращает значение атрибута, дублирование функционала, поэтому убрала
+    # def get_root_val(self):
+    #     return self.root
+
+
+if __name__ == '__main__':
+
+    tree = BinaryTree(20)
+    tree.insert_left(19)
+    tree.insert_right(30)
+    print(tree.left_child)
+    print(tree.right_child)
+    tree.insert_left(15)
+    tree.insert_right(30)
+    print(tree.left_child)
+    print(tree.right_child)
+    print(tree.left_child.right_child)
+    print(tree.right_child.right_child)
