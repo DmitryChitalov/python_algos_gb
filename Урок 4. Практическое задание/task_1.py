e5a52f694504fb048e3eb4bd528827bae96228b6
european <artik-54@mail.ru>
2021-05-01 20:08:42 +0700

    new

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -1,18 +1,26 @@
 """
 Задание 1.
-
 Приведен код, который позволяет сохранить в
 массиве индексы четных элементов другого массива
-
 Сделайте замеры времени выполнения кода с помощью модуля timeit
-
 Попробуйте оптимизировать код, чтобы снизить время выполнения
 Проведите повторные замеры
-
 Добавьте аналитику: что вы сделали и почему
 """
 
 from timeit import timeit
+from timeit import Timer
+import random
+
+
+# Добавили новую функцию, которая бегает по циклу через enumerate, enumerate уже сам строит индексы массива
+# И нам не нужно выбирать этот индекс самостоятельно из листа nums.
+def func_2(nums):
+    new_arr = []
+    for i, num in enumerate(nums, start=0):
+        if num % 2 == 0:
+            new_arr.append(i)
+    return new_arr
 
 
 def func_1(nums):
@@ -21,3 +29,15 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+# l = [1,2,4,7,8,10,22]
+
+l = random.randint(0, 10000000000)
+
+t = Timer("func_1", "from __main__ import func_1")
+t1 = Timer("func_2", "from __main__ import func_2")
+
+# При больших объемах, через функцию enumerate, работает быстрее. Что показывает статистика func1= 0.0165083 func2= 0.0164659
+
+print("func1=", t.timeit(number=1000000), "func2=", t1.timeit(number=1000000))
