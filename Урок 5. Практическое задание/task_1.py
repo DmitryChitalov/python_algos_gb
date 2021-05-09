faccf4f969a4c1a132ca96503299bc5b7369a434
european <artik-54@mail.ru>
2021-05-09 16:50:15 +0700

    new

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,6 +25,48 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+from collections import Counter
 
+l_cnt_cmp = int(input("Введите количество предприятий для подсчета: "))
+l_obj = Counter()
+l_obj_f = Counter()
 
+for i in range(l_cnt_cmp):
+    l_cmp_name = str(input("Введите название Компании: "))
+    l_cmp_fund = str()
+    for ifunds in range(4):
+        l_cmp_fund = l_cmp_fund + ' ' + str(input("Введите прибыль за " + str(ifunds+1) + "квартал для " + l_cmp_name + " = "))
+    l_obj[l_cmp_name] += 1
+    l_obj_f[l_cmp_name] += 1
+    l_cmp_fund = l_cmp_fund[1:]
+    l_obj[l_cmp_name] = l_cmp_fund
 
+for i_cmp in l_obj:
+    l_sum_cmp = l_obj[i_cmp].split(" ")
+    l_sum_cmp_f = int(0)
+    for i_sum_cmp in range(len(l_sum_cmp)):
+        l_v = int(l_sum_cmp[i_sum_cmp])
+        l_sum_cmp_f = l_v + l_sum_cmp_f
+    l_obj_f[i_cmp] = l_sum_cmp_f
+
+l_cmp_avg_f = int(0)
+for i_cmp_avg_f in l_obj_f:
+    l_cmp_avg_f = l_obj_f[i_cmp_avg_f] + l_cmp_avg_f
+
+l_cmp_avg_f = l_cmp_avg_f / len(l_obj)
+
+print("Средняя годовая прибыль компаний = " + str(l_cmp_avg_f))
+
+l_cmp_name_max = str("Компании с годовой прибылью больше средней - ")
+for i_max_f in l_obj_f:
+    if l_obj_f[i_max_f] > l_cmp_avg_f:
+        l_cmp_name_max = l_cmp_name_max + i_max_f
+
+print(l_cmp_name_max)
+
+l_cmp_name_min = str("Компании с годовой прибылью меньше средней - ")
+for i_min_f in l_obj_f:
+    if l_obj_f[i_min_f] < l_cmp_avg_f:
+        l_cmp_name_min = l_cmp_name_min + " " + i_min_f
+
+print(l_cmp_name_min)
