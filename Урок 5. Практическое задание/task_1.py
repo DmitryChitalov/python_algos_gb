e373b4d45785ea68ae0e4df0eace7b3c30b4cad6
european <artik-54@mail.ru>
2021-05-13 10:45:35 +0700

    new

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -70,3 +70,44 @@ for i_min_f in l_obj_f:
         l_cmp_name_min = l_cmp_name_min + " " + i_min_f
 
 print(l_cmp_name_min)
+"""
+from collections import namedtuple
+
+
+def calc():
+    my_var - "company"
+    n = int(input("Введите количество предприятий: "))
+    companies = namedtuple(
+        my_var,
+        " name_period_1 period_2 period_3 period_4")
+    profit_aver = {}
+    
+    for i in range(n):
+        company = companies(
+            name=input("Введите название предприятия: "), period_1=int(
+                input("Введите прибыль за первый квартал: ")), period_2=int(
+                input("Введите прибыль за второй квартал: ")), period_3=int(
+                input("Введите прибыль за третий квартал: ")), period_4=int(
+                    input("Введите прибыль за четвёртый квартал: ")))
+                
+        profit_aver[company.name] = (
+            company.period_1 + company.period_2 +
+            company.period_3 + company.period_4) / 4
+        
+    total_aver = 0
+    for value in profit_aver.values():
+        total_aver += value
+    total_aver = total_aver / n
+    
+    for key, value in profit_aver.items():
+        if value > total_aver:
+            print(f"{key} - прибыль выше среднего")
+        elif value < total_aver:
+            print(f"{key} - прибыль ниже среднего")
+        elif value == total_aver:
+            print(f"{key} - прибыль средняя")
+            
+calc()
+#  Выше пример кода от преподавателя.
+
+"""
