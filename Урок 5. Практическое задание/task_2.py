5b37b86ce6d11220eb59d1471d0979cddd6e819a
european <artik-54@mail.ru>
2021-05-13 13:08:55 +0700

    new

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -49,3 +49,27 @@ if k == 1:
   third.append('1')
 
 print(third[::-1])
+
+"""
+def calc():
+    nums = collections.defaultdict(list)
+    
+    #  defaultdict(<class 'list'>,
+    #  {'1-A2': ['A', '2'], '2-C4F': ['C', '4', 'F']})
+    for d in range(2):
+        n = input(f"Введите {d+1}-e натуральное шестнадцатеричное число: ")
+        nums[f"{d+1}-{n}"] = list(n)
+    print(nums)
+    
+    #  16-указываем с числами какой системы делаем операции
+    sum_res = sum([int(''.join(i), 16) for i in nums.values()])
+    print(sum_res)
+    #  '%X' Число в шестнадцатиричной системе счисления
+    
+    print("Сумма: ", list('%X' % sum_res))
+    #  f'{15:x}' -> f
+    mul_res = functools.reduce(lambda a, b: a * b,
+                               [int(''.join(i), 16) for i in nums.values()])
+    print("Произведение: ", list('%X' % mul_res))
+    
+"""
