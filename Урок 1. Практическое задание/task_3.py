8e6d7cd8fe5bef19f424ca96e516672199728afc
desexcile <34170928+desexcile@users.noreply.github.com>
2021-05-21 16:21:40 +0300

    add 3-7

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -1,11 +1,9 @@
 """
 Задание 3.
-
 Для этой задачи:
 1) придумайте 1-3 решения (желательно хотя бы два)
 2) оцените сложность каждого решения в нотации О-большое
 3) сделайте вывод, какое решение эффективнее и почему
-
 Примечание:
 Без выполнения пунктов 2 и 3 задание считается нерешенным. Пункты 2 и 3 можно выполнить
 через строки документации в самом коде.
@@ -13,8 +11,6 @@
 а не писать "мы это не проходили)".
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
-
-
 Сама задача:
 Имеется хранилище с информацией о компаниях: название и годовая прибыль.
 Для реализации хранилища можно применить любой подход,
@@ -22,3 +18,24 @@
 Реализуйте поиск трех компаний с наибольшей годовой прибылью.
 Выведите результат.
 """
+
+COMPANY_DICT = {"Марвел": 23587, "ДиСиКомикс": 54754, "Амбрелла": 2545549, "ДолгостройЛТД": 3153223, "МММ": 3465987}
+
+company_dict_values = list(COMPANY_DICT.values())
+company_dict_keys = list(COMPANY_DICT.keys())
+company_dict_values_2 = company_dict_values.copy()
+
+# 1 вариант - сложность O(n log n):
+sorted_values = sorted(COMPANY_DICT.values(), reverse=True)[:3]  # O(n log n)
+for item in sorted_values:                                       # O(1)
+    find_index = company_dict_values.index(item)                 # O(n)
+    max_key = company_dict_keys[find_index]
+    print(max_key)
+
+# 2 вариант - сложность O(n):
+for i in range(3):                                               # O(1)
+    max_value = max(company_dict_values_2)                       # O(n)
+    find_index = company_dict_values.index(max_value)            # O(n)
+    max_key = company_dict_keys[find_index]                      # O(1)
+    print(max_key)
+    company_dict_values_2.remove(max_value)                      # O(n)
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -1,17 +1,89 @@
 """
 Задание 7.
 Задание на закрепление навыков работы с очередью
-
 Примечание: в этом задании вспомните ваши знания по работе с ООП
 и опирайтесь на пример урока
-
 Реализуйте структуру "доска задач".
-
-
 Структура должна предусматривать наличие несольких очередей задач, например
 1) базовой, откуда задачи берутся, решаются и отправляются в список решенных
 2) очередь на доработку, когда нерешенные задачи из первой очереди отправляются
 на корректировку решения
-
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+
+class QueueClass:
+    def __init__(self):
+        self.elements_list = []
+
+    def is_empty(self):
+        return self.elements_list == []
+
+    def to_queue(self, item):
+        self.elements_list.insert(0, item)
+
+    def from_queue(self):
+        return self.elements_list.pop()
+
+    def get_item(self):
+        return self.elements_list[-1]
+
+    def size(self):
+        return len(self.elements_list)
+
+
+class Kanban:
+    def __init__(self):
+        self.base_tasks = QueueClass()
+        self.other_tasks = QueueClass()
+        self.closed_tasks = []
+
+    def add_task(self, task):
+        self.base_tasks.to_queue(task)
+
+    def close_task(self):
+        task = self.base_tasks.from_queue()
+        self.closed_tasks.append(task)
+        return task
+
+    def rework_task(self):
+        task = self.base_tasks.from_queue()
+        print(f'На доработку отправлена: {task}')
+        self.other_tasks.to_queue(task)
+
+    def get_task(self):
+        task = self.base_tasks.get_item()
+        return task
+
+    def count_task(self):
+        return self.base_tasks.size(), self.other_tasks.size(), len(self.closed_tasks)
+
+
+ExObject = Kanban()
+
+while 1 == 1:
+    to_base_q, to_rework_q, done_q = ExObject.count_task()
+    print(f'В базовой очереди: {to_base_q} задач\nНа доработку: {to_rework_q}\nЗадач уже выполнено: {done_q}')
+    try:
+        print(f'Что будем делать с задачей: {ExObject.get_task()}?')
+    except IndexError:
+        print('Задач пока нет')
+    option = input(f'Добавить задачу - нажмите 1, отметить как решенную - 2, на доработку - 3, выход - 4: ')
+    if len(option) == 0:
+        break
+    if option == "1":
+        ExObject.add_task(input('Введите задачу:   '))
+    elif option == "2":
+        try:
+            ExObject.close_task()
+        except IndexError:
+            print('Сначала добавь задачу')
+    elif option == "3":
+        try:
+            ExObject.rework_task()
+        except IndexError:
+            print('Сначала добавь задачу')
+    elif option == "4":
+        break
+    else:
+        print('Неверная команда!')
