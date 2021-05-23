5f4fd394921392b9fa65ba905ba7116ce7b3bce7
european <artik-54@mail.ru>
2021-05-23 18:36:45 +0700

    new

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -18,92 +18,72 @@
 from collections import Counter, deque
 
 
-def haffman_tree(s):
-    # Считаем уникальные символы.
-    # Counter({'e': 4, 'b': 3, 'p': 2, ' ': 2, 'o': 2, 'r': 1, '!': 1})
-    count = Counter(s)
-    # Сортируем по возрастанию количества повторений.
-    # deque([('r', 1), ('!', 1), ('p', 2), (' ', 2), ('o', 2), ('b', 3), ('e', 4)])
-    sorted_elements = deque(sorted(count.items(), key=lambda item: item[1]))
-    # Проверка, если строка состоит из одного повторяющего символа.
-    if len(sorted_elements) != 1:
-        # Цикл для построения дерева
-        while len(sorted_elements) > 1:
-            # далее цикл объединяет два крайних левых элемента
-            # Вес объединенного элемента (накопленная частота)
-            # веса - 2, 4, 4, 7, 8, 15
-            weight = sorted_elements[0][1] + sorted_elements[1][1]
-            # Словарь из 2 крайних левых элементов, попутно вырезаем их
-            # из "sorted_elements" (из очереди).
-            # comb - объединенный элемент
-            '''
-            {0: 'r', 1: '!'}
-            {0: {0: 'r', 1: '!'}, 1: 'p'}
-            {0: ' ', 1: 'o'}
-            {0: 'b', 1: {0: ' ', 1: 'o'}}
-            {0: {0: {0: 'r', 1: '!'}, 1: 'p'}, 1: 'e'}
-            {0: {0: 'b', 1: {0: ' ', 1: 'o'}}, 1: {0: {0: {0: 'r', 1: '!'}, 1: 'p'}, 1: 'e'}}
-            '''
-            comb = {0: sorted_elements.popleft()[0],
-                    1: sorted_elements.popleft()[0]}
+class HuffmanClass:
 
-            # Ищем место для ставки объединенного элемента
-            for i, _count in enumerate(sorted_elements):
-                if weight > _count[1]:
-                    continue
+    def __init__(self, user_string):
+        self.user_string = user_string
+        self.code_table = dict()
+        self.huffman_code(self.get_tree())
+
+    def get_counter_symbol(self):
+        return Counter(self.user_string)
+
+    def sort_counter_value(self):
+        return deque(sorted(self.get_counter_symbol().items(),
+                            key=lambda item: item[1]))
+
+    def get_tree(self):
+        sort_value = self.sort_counter_value().copy()
+        if len(sort_value) != 1:
+            while len(sort_value) > 1:
+                weight = sort_value[0][1] + sort_value[1][1]
+                new_elem = {0: sort_value.popleft()[0],
+                            1: sort_value.popleft()[0]}
+                for i, _count in enumerate(sort_value):
+                    if weight > _count[1]:
+                        continue
+                    else:
+                        sort_value.insert(i, (new_elem, weight))
+                        break
                 else:
-                    # Вставляем объединенный элемент
-                    sorted_elements.insert(i, (comb, weight))
-                    break
-            else:
-                # Добавляем объединенный корневой элемент после
-                # завершения работы цикла
+                    sort_value.append((new_elem, weight))
+        else:
+            weight = sort_value[0][1]
+            new_elem = {0: sort_value.popleft()[0], 1: None}
+            sort_value.append((new_elem, weight))
+        return sort_value[0][0]
 
-                sorted_elements.append((comb, weight))
-            '''
-            deque([({0: 'r', 1: '!'}, 2), ('p', 2), (' ', 2), ('o', 2), ('b', 3), ('e', 4)])
-            deque([(' ', 2), ('o', 2), ('b', 3), ({0: {0: 'r', 1: '!'}, 1: 'p'}, 4), ('e', 4)])
-            deque([('b', 3), ({0: ' ', 1: 'o'}, 4), ({0: {0: 'r', 1: '!'}, 1: 'p'}, 4), ('e', 4)])
-            deque([({0: {0: 'r', 1: '!'}, 1: 'p'}, 4), ('e', 4), ({0: 'b', 1: {0: ' ', 1: 'o'}}, 7)])
-            deque([({0: 'b', 1: {0: ' ', 1: 'o'}}, 7), ({0: {0: {0: 'r', 1: '!'}, 1: 'p'}, 1: 'e'}, 8)])
-            deque([({0: {0: 'b', 1: {0: ' ', 1: 'o'}}, 1: {0: {0: {0: 'r', 1: '!'}, 1: 'p'}, 1: 'e'}}, 15)])
-            '''
-    else:
-        # приравниваемыем значение 0 к одному повторяющемуся символу
-        weight = sorted_elements[0][1]
-        comb = {0: sorted_elements.popleft()[0], 1: None}
-        sorted_elements.append((comb, weight))
-    # sorted_elements - deque([({0: {0: 'b', 1: {0: ' ', 1: 'o'}}, 1: {0: {0: {0: 'r', 1: '!'}, 1: 'p'}, 1: 'e'}}, 15)])
-    # {0: {0: 'b', 1: {0: ' ', 1: 'o'}}, 1: {0: {0: {0: 'r', 1: '!'}, 1: 'p'}, 1: 'e'}}
-    # словарь - дерево
-    return sorted_elements[0][0]
+    def huffman_code(self, tree, path=''):
+        if not isinstance(tree, dict):
+            self.code_table[tree] = path
+        else:
+            self.huffman_code(tree[0], path=f'{path}0')
+            self.huffman_code(tree[1], path=f'{path}1')
 
+    def get_user_string_code(self):
+        res = ''
+        for symbol in self.user_string:
+            res += self.code_table[symbol]
+        return res
 
-code_table = dict()
-
-# tree - {0: {0: 'b', 1: {0: ' ', 1: 'o'}}, 1: {0: {0: {0: 'r', 1: '!'}, 1: 'p'}, 1: 'e'}}
-def haffman_code(tree, path=''):
-    # Если элемент не словарь, значит мы достигли самого символа
-    # и заносим его, а так же его код в словарь (кодовую таблицу).
-    if not isinstance(tree, dict):
-        code_table[tree] = path
-    # Если элемент словарь, рекурсивно спускаемся вниз
-    # по первому и второму значению (левая и правая ветви).
-    else:
-        haffman_code(tree[0], path=f'{path}0')
-        haffman_code(tree[1], path=f'{path}1')
+    def decoding(self, code_string):
+        res = ''
+        i = 0
+        codes_dict = self.code_table
+        while i < len(code_string):
+            for code in codes_dict:
+                if code_string[i:].find(codes_dict[code]) == 0:
+                    res += code
+                    i += len(codes_dict[code])
+        return res
 
 
-# строка для кодирования
-s = "beep boop beer!"
+user_string = input("Введите строку: ")
+a_1 = HuffmanClass(user_string)
+print(f"Исходная строка:\n'{user_string}'")
 
-# функция заполняет кодовую таблицу (символ-его код)
-# {'b': '00', ' ': '010', 'o': '011', 'r': '1000', '!': '1001', 'p': '101', 'e': '11'}
-haffman_code(haffman_tree(s))
+tree_1 = a_1.get_tree()
 
-# code_table - {'b': '00', ' ': '010', 'o': '011', 'r': '1000', '!': '1001', 'p': '101', 'e': '11'}
-
-# выводим коды для каждого символа
-for i in s:
-    print(code_table[i], end=' ')
-print()
+code_user_string = a_1.get_user_string_code()
+print(f"Строка кода после кодирования:\n{code_user_string}")
+print(f"Декодированная строка:\n'{a_1.decoding(code_user_string)}'")
